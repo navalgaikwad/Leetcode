365009285692d3cfc5396c9391e90a801c11d608
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-09-09 16:06:09 -0400

    Time: 14 ms (69.87%), Space: 43.9 MB (35.08%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,38 +1,20 @@
 class Solution {
-    public int maxConsecutiveAnswers(String num, int k) {
-       int noTrue=0;
-        int left=0;
-        int max=0;
-       char[] nums = num.toCharArray();
-        for(int r=0; r<nums.length; r++){
-            if(nums[r]=='T'){
-                noTrue++;
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+       int ans = 0;
+        int max_count = 0;
+        int[] count = new int[2];
+        for(int l = 0,r=0;r<answerKey.length();r++)
+        {
+            max_count = Math.max(max_count,++count[answerKey.charAt(r) == 'T' ? 1: 0]);
+            while(r-l+1 - max_count > k)
+            {
+                --count[answerKey.charAt(l++) == 'T'? 1 : 0];
             }
-            while(noTrue>k){
-                if(nums[left]=='T'){
-                  noTrue--;  
-                }
-                left++;
-            }
-            max=Math.max(max, r-left+1);
-            
+            ans = Math.max(ans,r-l+1);
         }
-         int noFalse=0;
-         left=0;
-         for(int r=0; r<nums.length; r++){
-            if(nums[r]=='F'){
-                noFalse++;
-            }
-            while(noFalse>k){
-                if(nums[left]=='F'){
-                  noFalse--;  
-                }
-                left++;
-            }
-            max=Math.max(max, r-left+1);
-            
-        }
-        return max;
+        return ans;
+   // end-s+1-maxFreq>k
     }
 }
 
+    
