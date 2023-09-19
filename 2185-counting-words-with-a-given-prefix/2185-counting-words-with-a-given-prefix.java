c4dff5b8b03bd9c7e78a81d8b632f59dd686d541
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-09-19 17:06:13 -0400

    Time: 0 ms (100.00%), Space: 42 MB (53.35%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count=0;
+        for(String word : words){
+            if(word.startsWith(pref)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+//indexof()
