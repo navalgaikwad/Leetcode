0017fa822d4c45caa9256cc723fabad0ed70d17f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-16 19:13:00 -0400

    Time: 1 ms (99.90%), Space: 48.4 MB (43.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int size = nums.length/2; 
-        int count =0;
-        int pre = nums[0];
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]!=pre){
-                count =0;
-                pre = nums[i];
+        int maj = nums[0];
+        int count =1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == maj){
                 count++;
             }else{
-                count++;
-                if(count>size){
-                    return nums[i];
-                }
+                count --;
+            }
+            if(count == 0){
+                maj = nums[i];
+                count = 1;
             }
         }
-        return 0;
+        return maj;
     }
 }
