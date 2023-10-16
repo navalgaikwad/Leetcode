9064302d45bab67f620a3a10ccc17a3f9d0a44e0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-16 12:37:45 -0400

    Time: 1 ms (96.70%), Space: 43.7 MB (73.04%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while(right < nums.length){
+            if(nums[right] > nums[left]){
+                left++;
+                nums[left] = nums[right];
+            }
+            right++;
+        }
+        return left+1;
+    }
+}
