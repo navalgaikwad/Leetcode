dcefcdd557c399d37131944cb39a747461223451
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-17 19:42:45 -0400

    Time: 1 ms (8.92%), Space: 39.1 MB (73.08%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+       int[][] dp = new int[m][n];
+        dp[0][0] =1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if( i-1 >=0){
+                    dp[i][j] = dp[i-1][j] + dp[i][j];
+                }
+                if(j-1>=0){
+                      dp[i][j] = dp[i][j-1] + dp[i][j];
+                }
+            } 
+        }
+        return dp[m-1][n-1];
+    }
+    
+}
