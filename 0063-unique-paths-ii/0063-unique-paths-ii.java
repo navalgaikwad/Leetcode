d55df4e2eff62e06e0f500d022676223380ca5bd
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-17 19:36:52 -0400

    Time: 1 ms (24.09%), Space: 41.3 MB (6.47%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+       
+        dp[0][0] = 1;
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(grid[i][j]==1){
+                 dp[i][j] =0; 
+                }else{
+                    if( i-1>=0 ){
+                    dp[i][j] =  dp[i][j] + dp[i-1][j];
+                } if(j -1>=0){
+                     dp[i][j] =  dp[i][j] + dp[i][j-1];
+                }
+              }
+            }
+        }
+        return dp[row-1][col-1];
+    }
+}
