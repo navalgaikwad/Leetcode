b8ad727541c70faa1ab7c090e2f88c717badd5d2
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-22 15:27:03 -0400

    Time: 8 ms (9.00%), Space: 40.6 MB (5.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       int[][] dp = new int[m][n];
-        dp[0][0] =1;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if( i-1 >=0){
-                    dp[i][j] = dp[i-1][j] + dp[i][j];
-                }
-                if(j-1>=0){
-                      dp[i][j] = dp[i][j-1] + dp[i][j];
-                }
-            } 
-        }
-        return dp[m-1][n-1];
+        return recurssion(m, n, new HashMap<>());
     }
     
+    int recurssion(int m, int n, HashMap<String, Integer> map){
+        String key = m+"-"+n;
+        
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+        if(m == 1 && n == 1){
+            return 1;
+        }
+        if(m == 0 || n == 0){
+            return 0;
+        }
+      int val1 =  recurssion(m - 1, n, map);
+      int val2 =  recurssion(m, n - 1, map);
+      map.put( key, val1 + val2);
+      return map.get(key);
+    }
 }
