fbf64dc7b0e7928951b6c15bdda2bf2182d71022
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-22 15:49:56 -0400

    Time: 0 ms (100.00%), Space: 39 MB (72.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,25 +4,8 @@ class Solution {
         return recurssion(m, n, dp);
     }
     
-//     int recurssion(int m, int n, HashMap<String, Integer> map){
-//         String key = m+"-"+n;
-        
-//         if(map.containsKey(key)){
-//             return map.get(key);
-//         }
-//         if(m == 1 && n == 1){
-//             return 1;
-//         }
-//         if(m == 0 || n == 0){
-//             return 0;
-//         }
-//       int val1 =  recurssion(m - 1, n, map);
-//       int val2 =  recurssion(m, n - 1, map);
-//       map.put( key, val1 + val2);
-//       return map.get(key);
-//     }
-    
     int recurssion(int m, int n, int[][] dp){
+        
          if(dp[m][n] != 0){
              return dp[m][n];
          }
@@ -35,7 +18,9 @@ class Solution {
         
         int val1 =  recurssion(m - 1, n, dp);
         int val2 =  recurssion(m, n - 1, dp);
+        
         dp[m][n] = val1 + val2; 
+        
         return dp[ m ][ n ];
     }
 }
