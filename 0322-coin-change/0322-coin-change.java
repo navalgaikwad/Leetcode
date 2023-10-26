91e35124d2be07aab1a251a40fce2c7344d96c21
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-25 21:54:29 -0400

    Time: 246 ms (5.01%), Space: 45.7 MB (5.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,16 +1,40 @@
-public class Solution {
-  public int coinChange(int[] coins, int amount) {
-    int max = amount + 1;
-    int[] dp = new int[amount + 1];
-    Arrays.fill(dp, max);
-    dp[0] = 0;
-    for (int i = 1; i <= amount; i++) {
-      for (int j = 0; j < coins.length; j++) {
-        if (coins[j] <= i) {
-          dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-        }
-      }
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        Integer result = helper2(amount, coins, memo);
+        System.out.print(memo);
+
+       // if(sum != amount) return -1;
+        return result != null ? result : -1;
     }
-    return dp[amount] > amount ? -1 : dp[amount];
-  }
+
+
+ static Integer helper2(int target, int[] coins, HashMap<Integer, Integer> memo){
+        if( memo.containsKey(target) ){
+            return memo.get(target);
+        }
+        if( target == 0 ){
+           
+            return 0;
+        }
+        if(target < 0) return -1;
+
+        Integer result = -1;
+
+        for(Integer coin :coins){
+           Integer remainderList =  helper2(target - coin , coins, memo);
+              if(remainderList != -1){
+
+                  Integer newCombination = 0;
+                   newCombination = remainderList + 1;
+
+                  if(result ==-1 || newCombination < result){
+                      result = newCombination;
+                 }
+         }      
+      }
+     memo.put(target, result);
+     return result;
+    }
 }
