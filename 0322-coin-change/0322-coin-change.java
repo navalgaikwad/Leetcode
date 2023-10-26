33a0312f7dd8bc5359e85b4b59fbf920a37b9306
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-25 21:57:06 -0400

    Time: 150 ms (5.52%), Space: 45 MB (6.60%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,11 +1,7 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
 
-        HashMap<Integer, Integer> memo = new HashMap<>();
-        Integer result = helper2(amount, coins, memo);
-        System.out.print(memo);
-
-       // if(sum != amount) return -1;
+        Integer result = helper2(amount, coins, new HashMap<Integer, Integer>() );
         return result != null ? result : -1;
     }
 
@@ -14,10 +10,8 @@ class Solution {
         if( memo.containsKey(target) ){
             return memo.get(target);
         }
-        if( target == 0 ){
-           
-            return 0;
-        }
+        if( target == 0 ) return 0;
+        
         if(target < 0) return -1;
 
         Integer result = -1;
@@ -25,9 +19,7 @@ class Solution {
         for(Integer coin :coins){
            Integer remainderList =  helper2(target - coin , coins, memo);
               if(remainderList != -1){
-
-                  Integer newCombination = 0;
-                   newCombination = remainderList + 1;
+                  Integer newCombination = remainderList + 1;
 
                   if(result ==-1 || newCombination < result){
                       result = newCombination;
