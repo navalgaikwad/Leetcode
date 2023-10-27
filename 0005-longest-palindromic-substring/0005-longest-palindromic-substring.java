d0bba316bf37315797ea0239724956de7c863e32
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-27 00:23:38 -0400

    Time: 19 ms (58.15%), Space: 44.4 MB (25.20%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,37 +1,39 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() == 0) return "";
-        
-        int start = 0, end = 0;
-        Boolean[][] palindromeMemo = new Boolean[s.length()][s.length()];
-        
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j <= i; j++) {
-                if (palindrome(j, i, s, palindromeMemo)) {
-                    if (i - j > end - start) {
-                        start = j;
-                        end = i;
-                    }
-                }
-            }
-        }
-        
-        return s.substring(start, end + 1);
-    }
+        int n = s.length();
+        int max_Length = 0;
+        String result ="";
+        for(int i = 0; i < n; i++){
+              int left = i;
+              int right = i;
+              while(left >= 0 && right<n && s.charAt(left) == s.charAt(right)){
+                  if(right - left +1 >max_Length){
+                      result = s.substring( left, right + 1 );
+                      max_Length = ( right - left + 1);
+                  }
+                left--;
+                right++;
+              }
+            
+          }
     
-    boolean palindrome(int start, int end, String s, Boolean[][] palindromeMemo) {
-        if (start >= end) return true;
-        if (palindromeMemo[start][end] != null) {
-            return palindromeMemo[start][end];
-        }
-        
-        if (s.charAt(start) == s.charAt(end)) {
-            boolean result = palindrome(start + 1, end - 1, s, palindromeMemo);
-            palindromeMemo[start][end] = result;
-            return result;
-        }
-        
-        palindromeMemo[start][end] = false;
-        return false;
+       for(int i=0; i < n; i++){
+              int left = i;
+              int right = i + 1;
+              while(left >= 0 && right < n && s.charAt(left) == s.charAt(right)){
+                  if(right - left + 1 > max_Length){
+                      result = s.substring(left, right + 1);
+                      max_Length = (right - left + 1);
+                  }
+                left--;
+                right++;
+              }
+            
+          }
+     return result;
     }
 }
+    
+    
+    
+ 
