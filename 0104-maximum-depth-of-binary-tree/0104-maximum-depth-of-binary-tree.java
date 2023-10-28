cca83db74af9c71e41769f4aa21591cfc4e8f2de
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-28 19:34:34 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (95.16%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,14 +14,28 @@
  * }
  */
 class Solution {
+    int max = Integer.MIN_VALUE;
     public int maxDepth(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
+//         if(root == null){
+//             return 0;
+//         }
         
-       int left = maxDepth(root.left);
-       int right = maxDepth(root.right);
-       int dept = 1 + Math.max(left, right);
-        return dept;
+//        int left = maxDepth(root.left);
+//        int right = maxDepth(root.right);
+//        int dept = 1 + Math.max(left, right);
+//         return dept;
+        helper(root, 0);
+        return max;
+            
+    }
+    
+    void helper( TreeNode root, int sum){
+        if(root == null ){
+            max = Math.max(max, sum);
+            return;
+        }
+        //sum = sum + 1;
+        helper(root.left, sum + 1 );
+        helper(root.right, sum + 1);
     }
 }
