3f6170487c2a0bffc2de0c905f5169831196aea0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-30 22:54:14 -0400

    Time: 0 ms (100.00%), Space: 43.8 MB (21.89%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,32 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       int left = 0;
-        int right = 0;
-        int count = 0;
-        int len = nums.length;
-        //1. left and right is not same
-        //2. if same
-        // 2.1 check for counter == 2
-          //2.2 increament left ++
-         //2.2 swap left and right
-        while(right < len){
-            //same logic if you find left != right if you find duplicate
-            if(nums[right] != nums[left]){
-                left++;
-                nums[left] = nums[right];
-                count =1;      
-            }else{
+        int left =1;
+        int right = 1;
+        int count =1;
+        while(right < nums.length){
+            if(nums[ right ]==nums[right - 1 ]){
                 count ++;
-                  if(count == 2){
-                       left++;
-                      //swap everytime when you find counter ==2
-                       nums[left] = nums[right];
+            }else{
+                count = 1;
             }
             
+            if(count <= 2){
+               nums[left++] = nums[right]; 
+            }
+            right++;
         }
-        right++;
-        }
-        return left + 1;  
+        return left;
     }
 }
-
