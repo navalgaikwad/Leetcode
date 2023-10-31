0ac8a3ff82e6b1bfe0457c38796ccaf63a84c482
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-10-30 21:45:11 -0400

    Time: 0 ms (100.00%), Space: 43.8 MB (34.24%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -4,6 +4,11 @@ class Solution {
         int right = 0;
         int count = 0;
         int len = nums.length;
+        //1. left and right is not same
+        //2. if same
+        // 2.1 check for counter == 2
+          //2.2 increament left ++
+         //2.2 swap left and right
         while(right < len){
             //same logic if you find left != right if you find duplicate
             if(nums[right] != nums[left]){
@@ -16,7 +21,8 @@ class Solution {
                        left++;
                       //swap everytime when you find counter ==2
                        nums[left] = nums[right];
-            }   
+            }
+            
         }
         right++;
         }
