86be34c766578bd60b4483ccff885d07dc4e8f27
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-01 14:02:54 -0400

    Time: 2 ms (80.73%), Space: 44.4 MB (39.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,32 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class Solution {
     public boolean canJump(int[] nums) {
-        //nums = [3,2,1,0,4]
-        //[2,3,1,1,4]
-        // 2,3,1,1, 4
-        //
-//         int len = nums.length-1; 
-//         if(len<=0){
-//             return true;
-//         }
-//         int prev =nums[0];
-        
-//         int j=0;
-//         for(int i=1; i< nums.length ; i++){
-//             if(nums[i]> prev && prev>=i){
-//               prev = nums[i];//3
-//               if(prev == len - i){
-//                   return true;
-//               }
-//             }
-//         }
-//        return false;
-         int reachable = 0; //3
+      int reachable = 0; //3
        for(int i = 0; i < nums.length; i ++) {
            if(i > reachable) return false;
            reachable = Math.max(reachable, i + nums[i]);
        } 
        return true;
         
-        
     }
 }
+
