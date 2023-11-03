7f2a0be73b424700c43209f678370099da6c492a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-03 00:46:03 -0400

    Time: 44 ms (5.08%), Space: 54.3 MB (5.00%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,28 +1,47 @@
 class Solution {
+    
+    private static boolean isValid(char[][] grid, int row, int col) {
+    return (row > -1) && (row < grid.length) && (col > -1) && (col < grid[0].length) && (grid[row][col] == '1');
+    }
+
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j]=='1'){
-                   helper(grid, i , j);
-                   count++; 
-                }
-            }
-        }
+       Set<String> visited = new HashSet<>();
+       int count = 0;
+       for(int i=0; i<grid.length; i++){
+           for(int j=0; j<grid[0].length; j++){
+               if(grid[i][j]=='1' && !visited.contains(i+"-"+j)){
+                   visited.add(i+"-"+j);
+                   helper(grid, i, j, visited);
+                   count++;
+               }
+           }
+       }
         return count;
     }
     
-    void helper(char[][] grid, int i, int j){
-        if(i < 0 || i >=grid.length || j < 0 || j>=grid[0].length || grid[i][j]=='0'){
-            return;
+    void helper(char[][] grid, int row, int col, Set<String> visited ){
+         int[][] directions = new int[][]{{1,0}, {0,1}, {-1,0}, {0, -1}};
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{row, col});
+        
+        while(!queue.isEmpty()){
+            int n = queue.size();
+          
+            for(int i = 0; i < n; i++){
+                int[] current = queue.remove();  
+               
+                for(int[] dir: directions){
+                    int nextRow = current[0] + dir[0];
+                    int nextCol = current[1] + dir[1];
+                    if(isValid(grid, nextRow, nextCol) && !visited.contains(nextRow + "-" + nextCol)){
+                         queue.offer(new int[]{nextRow, nextCol});
+                         visited.add(nextRow + "-" + nextCol);
+                    }
+                }
+            }
+            
         }
-        grid[i][j] = '0';
-        helper(grid, i+1, j);
-        helper(grid, i, j+1);
-        helper(grid, i, j-1);
-        helper(grid, i-1, j);
         
     }
 }
