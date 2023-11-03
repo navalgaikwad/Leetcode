0975bf383be40026cd91aa0a0111968466a46c64
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-03 00:54:38 -0400

    Time: 41 ms (5.08%), Space: 53.7 MB (5.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -10,7 +10,7 @@ class Solution {
        for(int i=0; i<grid.length; i++){
            for(int j=0; j<grid[0].length; j++){
                if(grid[i][j]=='1' && !visited.contains(i+"-"+j)){
-                   visited.add(i+"-"+j);
+                    visited.add(i + "-" + j);
                    helper(grid, i, j, visited);
                    count++;
                }
@@ -23,20 +23,20 @@ class Solution {
          int[][] directions = new int[][]{{1,0}, {0,1}, {-1,0}, {0, -1}};
         
         Queue<int[]> queue = new LinkedList<>();
+       
         queue.add(new int[]{row, col});
-        
         while(!queue.isEmpty()){
             int n = queue.size();
           
             for(int i = 0; i < n; i++){
                 int[] current = queue.remove();  
-               
+               // visited.add(nextRow + "-" + nextCol); adjacency list
                 for(int[] dir: directions){
                     int nextRow = current[0] + dir[0];
                     int nextCol = current[1] + dir[1];
                     if(isValid(grid, nextRow, nextCol) && !visited.contains(nextRow + "-" + nextCol)){
                          queue.offer(new int[]{nextRow, nextCol});
-                         visited.add(nextRow + "-" + nextCol);
+                         visited.add(nextRow + "-" + nextCol);//only for grid
                     }
                 }
             }
