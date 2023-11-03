91ba9908c2cee6ca29d01c46f105ae031a6a0a65
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-03 19:42:25 -0400

    Time: 106 ms (5.60%), Space: 45.7 MB (5.65%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -19,7 +19,7 @@ class Solution {
         
         while(!queue.isEmpty()){
             int size = queue.size();
-         // for(int i = 0; i < size; i++){
+          for(int i = 0; i < size; i++){
               
             int[] current = queue.remove();
             int row = current[0];
@@ -36,7 +36,7 @@ class Solution {
                      visited.add(rx + "-" + ry);//only for grid
                 }
             }
-          //}       
+          }       
         }
         return -1;
     }
