24d974c0e12f8ae6ea58807fd84839beed4368f7
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-04 13:42:49 -0400

    Time: 4 ms (91.82%), Space: 45.1 MB (8.90%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -8,20 +8,19 @@ class Solution {
         }
         
         for(int[] pre: prerequisites){
-            adj[pre[0]].add(pre[1]);
+            adj[pre[1]].add(pre[0]);
         }
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         for(int i=0; i<n; i++){
-            if(!dfs(adj, i, visited, st)){
+            if(!dfs(adj, i, visited, stack)){
                 return new int[]{};
             }
         }
-        int[] ans = new int[n];
-        int i = 0;
-        for(Integer s: st){
-            ans[i++] = s;
+       int[] order = new int[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            order[i] = stack.pop();
         }
-        return ans;
+        return order;
       
     }
     boolean dfs(ArrayList<Integer>adj[], int i, int[] visited, Stack<Integer> stack ){
@@ -32,13 +31,65 @@ class Solution {
            return true;
         }
         visited[i] = 1;
+       
         for(Integer neighbour: adj[i]){
             if(!dfs(adj, neighbour, visited, stack)){
                 return false;
             }
         }
+        
         visited[i] = 2;
         stack.push(i);
         return true;
     }
 }
+
+// class Solution {
+//     public int[] findOrder(int numCourses, int[][] prerequisites) {
+//         List<Integer>[] adj = new ArrayList[numCourses];
+//         for(int i = 0; i < numCourses; i++){
+//             adj[i] = new ArrayList<>();
+//         }
+        
+//         for(int[] pre: prerequisites){
+//             adj[pre[1]].add(pre[0]);
+//         }
+        
+//         Stack<Integer> stack = new Stack<>();
+//         int[] visited = new int[numCourses];
+//         for(int i = 0; i < numCourses; i++){
+//             if(visited[i] == 0){
+//                 if(!dfs(adj, i, visited, stack)){
+//                     return new int[]{};
+//                 }
+//             }
+//         }
+        
+//         int[] order = new int[numCourses];
+//         for(int i = 0; i < numCourses; i++){
+//             order[i] = stack.pop();
+//         }
+//         return order;
+//     }
+    
+//     boolean dfs(List<Integer>[] adj, int course, int[] visited, Stack<Integer> stack) {
+//         if(visited[course] == 1){
+//             return false; // Cycle detected.
+//         }
+//         if(visited[course] == 2){
+//             return true; // Already visited and processed.
+//         }
+        
+//         visited[course] = 1; // Mark the current course as being visited.
+        
+//         for(int neighbour: adj[course]){
+//             if(!dfs(adj, neighbour, visited, stack)){
+//                 return false; // Cycle detected in the DFS path.
+//             }
+//         }
+        
+//         visited[course] = 2; // Mark the current course as fully visited.
+//         stack.push(course); // Add the course to the stack which represents the order.
+//         return true;
+//     }
+// }
