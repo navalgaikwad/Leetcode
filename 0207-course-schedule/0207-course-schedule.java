10574d801ecf9d740c582b58a3199f7e85f043e3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-04 21:18:28 -0400

    Time: 2 ms (99.92%), Space: 43.6 MB (79.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,37 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<Integer> adj[] =new ArrayList[numCourses];
-        int[] indegree = new int[numCourses];
-        for(int i=0; i<numCourses; i++){
-            adj[i] = new ArrayList<>();
-        }
-        for(int[] prerequisite:prerequisites){
-             adj[prerequisite[1]].add(prerequisite[0]);
-             indegree[prerequisite[0]]++;
-            // indegree[prerequisite[1]]++;
-        }
-        Queue<Integer> q = new LinkedList<>();
-        for( int i=0; i<numCourses; i++){
-            if(indegree[i] == 0){
-               q.add(i); 
+    public boolean canFinish(int n, int[][] prerequisites) {
+       ArrayList<Integer>adj[] = new ArrayList[n];
+       for(int i=0; i<n; i++){
+           adj[i] = new ArrayList<>();
+       }
+       for(int[] prerequisite:prerequisites){
+           adj[prerequisite[1]].add(prerequisite[0]);
+       }
+        Boolean[] visited = new Boolean[n];
+        for(int i=0; i<n; i++){
+            if(!dfs(adj, i, visited)){
+                return false;
             }
         }
-        int count =0;
-        while(!q.isEmpty()){
-            int current = q.remove();
-            System.out.print(current);
-            count++;
-            for(Integer neighbour:adj[current]){
-                indegree[neighbour]--;
-                if(indegree[neighbour] == 0){
-                  q.add(neighbour);  
-                }
-            }
+        return true;
+    }
+    
+    boolean dfs(ArrayList<Integer>adj[] , int i,  Boolean[] visited){
+        if(visited[i] != null && visited[i] == false){
+            return false;
         }
-        if(count == numCourses){
+        if(visited[i] != null && visited[i] == true){
             return true;
         }
-        return false;
+        visited[i] = false;
+        
+        for(Integer neighbour: adj[i]){
+            if(!dfs(adj, neighbour, visited)){
+                return false;
+            }
+        }
+        visited[i] = true;
+        return true;
     }
+   
 }
