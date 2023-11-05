54c13fde19e138e397945163a6c0dadfab616ca3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-05 15:47:07 -0500

    Time: 13 ms (24.60%), Space: 43.5 MB (60.15%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -11,8 +11,10 @@ class Solution {
       Pair<Integer, Integer> cell[] = new Pair[ n * n + 1 ];
       for(int row = n - 1; row >=0; row--){
           for(int col : columns){
-             //System.out.print(label+" label, "+row +" row, "+col+" col ");
-            cell[ label++ ] = new Pair<>( row, col );  
+              
+            //System.out.print(label+" label, "+row +" row, "+col+" col ");
+            cell[ label++ ] = new Pair<>( row, col ); 
+              
           }
            Collections.reverse(Arrays.asList(columns));
       }
@@ -37,13 +39,13 @@ class Solution {
             for( int i = 1; i < 7; i++ ){ //roll the dice 6 time i.e 1 to 6
                 
               int nextLabel = currentStep + i; //next label i.e position 1 to 6 no from dice
-                //1 + 1
+                //1 + 1 = 2
                   if( nextLabel <= target ){ //2 <37
 
                       int row = cell[nextLabel].getKey(); // get the value form cell row 5  
                       int col = cell[nextLabel].getValue();// col 0
 
-                      int destination = ( board[row][col] != -1 ) ? board[row][col]: nextLabel;
+                      int destination = ( board[row][col] != -1 ) ? board[row][col] : nextLabel;
 
                      if(!visited.contains(destination)){
                          
