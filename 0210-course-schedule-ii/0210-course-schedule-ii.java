2fbb64a2e140b241bcf84d42591c91cccece7705
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-05 00:34:02 -0400

    Time: 5 ms (77.76%), Space: 45.1 MB (13.64%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int n =numCourses;
         int[] visited = new int[n];
+        int[] indegree = new int[n];
         ArrayList<Integer>adj[] = new ArrayList[n];
         for(int i=0; i<n; i++){
             adj[i] = new ArrayList<>();
@@ -9,87 +10,39 @@ class Solution {
         
         for(int[] pre: prerequisites){
             adj[pre[1]].add(pre[0]);
+            indegree[pre[0]]++;
         }
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0; i<n; i++){
-            if(!dfs(adj, i, visited, stack)){
-                return new int[]{};
-            }
-        }
-       int[] order = new int[numCourses];
-        for(int i = 0; i < numCourses; i++){
-            order[i] = stack.pop();
-        }
-        return order;
-      
-    }
-    boolean dfs(ArrayList<Integer>adj[], int i, int[] visited, Stack<Integer> stack ){
-        if(visited[i] == 1){
-           return false;
-        }
-        if(visited[i] == 2){
-           return true;
-        }
-        visited[i] = 1;
-       
-        for(Integer neighbour: adj[i]){
-            if(!dfs(adj, neighbour, visited, stack)){
-                return false;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<n ;i++){
+            if(indegree[i]==0){
+               queue.add(i); 
             }
         }
         
-        visited[i] = 2;
-        stack.push(i);
-        return true;
+        int[] result = new int[n];
+        List<Integer> list = new ArrayList<>();
+        int i =0;
+        while(!queue.isEmpty()){
+            int current = queue.remove();
+            list.add(current);
+            for(Integer neighbour: adj[current]){
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0){
+                    queue.add(neighbour);
+                }
+            }
+        }
+       // System.out.print(result.length);
+        if(list.size() != n ){
+           return new int[0];
+        }else{
+            for(Integer li: list){
+               result[i++]= li; 
+            }
+        }
+       
+      return result;
     }
 }
-
-// class Solution {
-//     public int[] findOrder(int numCourses, int[][] prerequisites) {
-//         List<Integer>[] adj = new ArrayList[numCourses];
-//         for(int i = 0; i < numCourses; i++){
-//             adj[i] = new ArrayList<>();
-//         }
-        
-//         for(int[] pre: prerequisites){
-//             adj[pre[1]].add(pre[0]);
-//         }
-        
-//         Stack<Integer> stack = new Stack<>();
-//         int[] visited = new int[numCourses];
-//         for(int i = 0; i < numCourses; i++){
-//             if(visited[i] == 0){
-//                 if(!dfs(adj, i, visited, stack)){
-//                     return new int[]{};
-//                 }
-//             }
-//         }
-        
-//         int[] order = new int[numCourses];
-//         for(int i = 0; i < numCourses; i++){
-//             order[i] = stack.pop();
-//         }
-//         return order;
-//     }
-    
-//     boolean dfs(List<Integer>[] adj, int course, int[] visited, Stack<Integer> stack) {
-//         if(visited[course] == 1){
-//             return false; // Cycle detected.
-//         }
-//         if(visited[course] == 2){
-//             return true; // Already visited and processed.
-//         }
-        
-//         visited[course] = 1; // Mark the current course as being visited.
-        
-//         for(int neighbour: adj[course]){
-//             if(!dfs(adj, neighbour, visited, stack)){
-//                 return false; // Cycle detected in the DFS path.
-//             }
-//         }
-        
-//         visited[course] = 2; // Mark the current course as fully visited.
-//         stack.push(course); // Add the course to the stack which represents the order.
-//         return true;
-//     }
-// }
+   
