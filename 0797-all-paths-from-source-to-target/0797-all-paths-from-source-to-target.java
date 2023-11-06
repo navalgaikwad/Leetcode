358beb8dc191b461743452d35fc104c172d071de
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-05 20:53:33 -0500

    Time: 2 ms (54.67%), Space: 45.6 MB (34.05%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -8,16 +8,19 @@ class Solution {
     }
     
     void helper(int[][] graph, int target, int src, List<Integer> ans){
+        
         ans.add(src);
+        
         if(src == target){
           result.add(new ArrayList<>(ans));  
           return;
         }
         
         for(Integer neighbour: graph[src]){
-         
-           helper(graph, target, neighbour , ans);
-           ans.remove(ans.size() - 1);
+            
+           helper(graph, target, neighbour , new ArrayList<>(ans));
+           //ans.remove(ans.size() - 1); By backtracking
+           // new ArrayList<>(ans) to copy at stack
         }
     }
 }
