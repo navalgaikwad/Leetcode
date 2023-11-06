750b354753ebecbf5d8f67b40282b2ba1b10ce29
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-05 19:29:14 -0500

    Time: 412 ms (5.18%), Space: 97 MB (7.45%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> ans = new ArrayList<>();
+        helper(n, k, 1, ans);
+        return result;
+        
+    }
+    
+    void helper(int n, int k, int index, List<Integer> ans){
+        if(ans.size() == k){
+           System.out.println(index + " "+ ans);
+           result.add(new ArrayList<>(ans)); 
+           return;
+        }
+        
+        for(int i = index; i <= n; i++){
+           // System.out.println(i);
+            ans.add(i);
+            helper(n, k, i + 1, ans);
+            ans.remove(ans.size() - 1);
+        }
+    }
+    
+}
