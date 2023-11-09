31ed6c2bc60a23719e696660f866f526487cbb49
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-09 02:30:46 -0500

    Time: 81 ms (54.48%), Space: 59.8 MB (52.71%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -3,17 +3,17 @@ class Solution {
         int n = profits.length;
         List<int[]> projects = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            projects.add(new int[]{profits[i], capital[i]});
+            projects.add(new int[]{profits[i], capital[i]}); //addd profit and capital
         }
-        projects.sort((a, b) -> Integer.compare(a[1], b[1]));
+        projects.sort((a, b) -> Integer.compare(a[1], b[1]));// sorted based on capital
         
-        PriorityQueue<Integer> maxProfitQueue = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        PriorityQueue<Integer> maxProfitQueue = new PriorityQueue<>((a, b) -> Integer.compare(b, a));// sorted in maxium
        // System.out.print(maxProfitQueue);
         int total = w;
         int index = 0;
         while (k > 0) {
-            while (index < n && projects.get(index)[1] <= total) {
-                 System.out.println(projects.get(index)[0]);
+            while (index < n && projects.get(index)[1] <= total) { //if they have same add to queue
+                 //System.out.println(projects.get(index)[0]);
                 maxProfitQueue.add(projects.get(index)[0]);
                 index++;
             }
