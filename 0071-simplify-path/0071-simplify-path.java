78f56dff5a73123194acaf2ecd2a5916b858f25e
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-09 19:57:37 -0500

    Time: 5 ms (54.75%), Space: 43.5 MB (34.11%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] parts = path.split("/");
+  
+        Stack<String> st = new Stack<>();
+     
+        for(String part : parts){
+            
+            if((part.equals("..") )){
+                if(!st.isEmpty()){
+                   st.pop();  
+                }
+            }else if(!part.isEmpty()&& !part.equals(".")){
+             st.push(part);   
+           }
+        }
+       
+        if(st.isEmpty())  return "/";
+     StringBuilder sb = new StringBuilder();
+   
+         for(String s: st){
+              sb.append("/"+s);
+         }
+             
+       return sb.toString();
+    }
+}
