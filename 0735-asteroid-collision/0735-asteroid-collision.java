ed7b8f89a6cf753b843a64a76840fbf5b0373412
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-09 21:29:18 -0500

    Time: 7 ms (54.54%), Space: 44.2 MB (23.85%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -3,20 +3,20 @@ class Solution {
         Stack < Integer > stack = new Stack < > ();
         for (int ast: asteroids) {
             boolean isDestroyed = false;
-            while (!stack.isEmpty() && ast < 0 && stack.peek() > 0) {
-                if (-ast > stack.peek()) {
+            while (!stack.isEmpty() && ast < 0 && stack.peek() > 0) { //pop(>) //dont do(<) // pop (==)
+                if (-ast > stack.peek()) { //greater than ahe pop kart raha //pop kar
                     stack.pop();
-                } else if (-ast < stack.peek()) {
-                    isDestroyed = true;
+                } else if (-ast < stack.peek()) {//dont add in the stack or pop
+                    isDestroyed = true; // ast less than ahe tar kahi karu noko dont add in stack
                     break;
-                } else if (-ast == stack.peek()) {
-                    stack.pop();
+                } else if (-ast == stack.peek()) { // pop
+                    stack.pop(); //equal ahet stack ani ast tar pop kar stack madhun ani destroy pan kar
                     isDestroyed = true;
                     break;
                 }
             }
 
-            if (!isDestroyed) {
+            if (!isDestroyed) { // not destroyed asel tar add kart raha
                 stack.push(ast);
             }
         }
