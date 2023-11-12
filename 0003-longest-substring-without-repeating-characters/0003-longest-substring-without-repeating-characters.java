4e7627c8628fb5fee7b3a8b5d17c2b77497752e9
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-12 12:04:55 -0500

    Time: 9 ms (23.10%), Space: 43.3 MB (83.17%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,52 +1,22 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-       Map<Character, Integer> map = new HashMap<>();
-        int j=0;
-        int len =0;
-        for(int i=0; i<s.length(); i++){
-          
-           char r = s.charAt(i);
-            map.put(r, map.getOrDefault(r,0) + 1);
-            while(map.get(r) > 1){
-              
-                char l = s.charAt(j);
-                map.put(l, map.get(l) - 1);
-              
-                if( map.get(s.charAt(j))==0){
-                    map.remove(s.charAt(j));
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int left = 0;
+        int len = s.length();
+        for(int right = 0; right< len; right++){
+            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+            while(map.get(s.charAt(right)) > 1)  {
+                int counter = map.get(s.charAt(left));
+                map.put(s.charAt(left), counter - 1 );
+                counter = map.get(s.charAt(left));
+                if(counter == 0){
+                    map.remove(left);
                 }
-                j++;
+                left++;
             }
-            
-            len =  Math.max(len, i-j+1);
+            max = Math.max(max, right - left +1);
         }
-        return len;
+        return max;
     }
 }
-
-// public class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         Map<Character, Integer> chars = new HashMap();
-
-//         int left = 0;
-//         int right = 0;
-
-//         int res = 0;
-//         while (right < s.length()) {
-//             char r = s.charAt(right);
-//             chars.put(r, chars.getOrDefault(r,0) + 1);
-            
-//             while (chars.get(r) > 1) {
-//                 char l = s.charAt(left);
-//                 chars.put(l, chars.get(l) - 1);
-//                 left++;
-//             }
-
-//             res = Math.max(res, right - left + 1);
-
-//             right++;
-//         }
-//         return res;
-//     }
-// }
-
