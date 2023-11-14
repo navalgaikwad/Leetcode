5b774d4939970cd14ec4f75b4ffaf1ddc90e58fa
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-13 22:37:43 -0500

    Time: 0 ms (100.00%), Space: 44.1 MB (46.62%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length + 1;
+        int[] no = new int[len];
+        int i=0;
+        for(int num: nums){
+            no[num]++;
+          //  i++;
+        }
+       for(int n =0; n<len; n++){
+        if(no[n]==0){
+            return n;
+        }
+       }
+        
+        return 0;
+    }
+}
