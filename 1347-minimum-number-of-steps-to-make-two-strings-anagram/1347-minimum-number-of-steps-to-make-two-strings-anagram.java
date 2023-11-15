3ee3a3851c94749683b6855382705d9af17d1eb6
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-14 21:56:20 -0500

    Time: 54 ms (29.50%), Space: 44.8 MB (5.71%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minSteps(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int replace = 0;
+        for(char c : t.toCharArray()) {
+            if(map.getOrDefault(c, 0) == 0){
+                replace++;
+            }else{
+                map.put(c, map.getOrDefault(c, 0) - 1);
+            }
+        }
+        return replace;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//b : 2
+//a: 1
+
+// int[] ch = new int[26];
+//         for(char c : s.toCharArray()){
+//             ch[c-'a']++;
+//         }
+//         int replace = 0;
+//         for(char c : t.toCharArray()){
+//             if(ch[c-'a']==0)
+//                 replace++;
+//             else
+//                 ch[c-'a']--;
+//         }
+//         return replace;
+    
