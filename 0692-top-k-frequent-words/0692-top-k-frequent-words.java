0b68db229e1062ff28d78842e38e6d45c46be33a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-15 00:58:54 -0500

    Time: 14 ms (5.76%), Space: 44.1 MB (19.88%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,14 +1,18 @@
 class Solution {
+    //counting
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> map=new HashMap<>();
         for(int i=0; i<words.length; i++){
              map.put(words[i],map.getOrDefault(words[i], 0)+1);
             
         }
+        System.out.print(map);
         PriorityQueue<String> pq =new PriorityQueue<>((a,b)->map.get(b)!=map.get(a) ? map.get(b)-map.get(a) : a.compareTo(b));      
         pq.addAll(map.keySet());
+        System.out.print(pq);
         List<String> ans=new ArrayList<>();
         for(int i=0; i<k; i++){
+            System.out.print(pq.peek());
             ans.add(pq.poll());
         }
         return ans;
