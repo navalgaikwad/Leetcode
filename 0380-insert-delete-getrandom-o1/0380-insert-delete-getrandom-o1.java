bddababf1947e15a5b56a249d69e4e8658d5d600
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-18 11:23:26 -0500

    Time: 25 ms (57.79%), Space: 89.8 MB (27.00%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,33 +1,40 @@
-import java.util.Collections;
 class RandomizedSet {
-    Set<Integer> set;
-    public RandomizedSet() {
-       set = new HashSet<>(); 
-    }
-    
-    public boolean insert(int val) {
-        return set.add(val);
-    }
-    
-    public boolean remove(int val) {
-        return set.remove(val);
-    }
-    
-    public int getRandom() {
-       // if(set.size()>1) {
-           List<Integer> list = new ArrayList<>(set);
-            Collections.shuffle(list);
-            int randomNumber = list.get(0); 
-            return randomNumber;
-       // }
-       // return 0;
-    }
-}
+  Map<Integer, Integer> dict;
+  List<Integer> list;
+  Random rand = new Random();
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
+  /** Initialize your data structure here. */
+  public RandomizedSet() {
+    dict = new HashMap();
+    list = new ArrayList();
+  }
+
+  /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+  public boolean insert(int val) {
+    if (dict.containsKey(val)) return false;
+
+    dict.put(val, list.size());
+    list.add(list.size(), val);
+    return true;
+  }
+
+  /** Removes a value from the set. Returns true if the set contained the specified element. */
+  public boolean remove(int val) {
+    if (! dict.containsKey(val)) return false;
+
+    // move the last element to the place idx of the element to delete
+    int lastElement = list.get(list.size() - 1);
+    int idx = dict.get(val);
+    list.set(idx, lastElement);
+    dict.put(lastElement, idx);
+    // delete the last element
+    list.remove(list.size() - 1);
+    dict.remove(val);
+    return true;
+  }
+
+  /** Get a random element from the set. */
+  public int getRandom() {
+    return list.get(rand.nextInt(list.size()));
+  }
+}
