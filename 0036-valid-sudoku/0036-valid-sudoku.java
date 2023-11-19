333cd3cf3e9d54fde74136a382947d54f9b869eb
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-18 22:59:14 -0500

    Time: 12 ms (33.42%), Space: 44.6 MB (5.31%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -3,13 +3,13 @@ class Solution {
         Set<String> col = new HashSet<>();
         Set<String> row = new HashSet<>();
         Set<String> boxes = new HashSet<>();
-        for(int i = 0; i < board.length; i++){
-            for(int j=0; j<board[0].length; j++){
+        //we are dealing with value not row and coloumns
+        for(int i = 0; i < board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
                 int box = (i/3) + (j/3)*3;
                 if(board[i][j] != '.'){
                     char val =  board[i][j];
-                    if(col.contains(i+"-"+val) || row.contains(j+"-"+val) ||
-                      boxes.contains(box+"-"+val)){
+                    if(col.contains(i +"-"+ val) || row.contains(j +"-"+ val) || boxes.contains(box+"-"+val)) {
                         return false;
                     }
                     col.add(i+"-"+val);
