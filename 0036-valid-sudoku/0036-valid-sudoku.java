49e09aad2e6eb843c654483165047187303b298f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-18 22:48:56 -0500

    Time: 12 ms (33.42%), Space: 44.4 MB (7.19%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> col = new HashSet<>();
+        Set<String> row = new HashSet<>();
+        Set<String> boxes = new HashSet<>();
+        for(int i = 0; i < board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                int box = (i/3) + (j/3)*3;
+                if(board[i][j] != '.'){
+                    char val =  board[i][j];
+                    if(col.contains(i+"-"+val) || row.contains(j+"-"+val) ||
+                      boxes.contains(box+"-"+val)){
+                        return false;
+                    }
+                    col.add(i+"-"+val);
+                    row.add(j+"-"+val);
+                    boxes.add(box+"-"+val);
+                }
+            }
+        }
+        return true;
+    }
+}
