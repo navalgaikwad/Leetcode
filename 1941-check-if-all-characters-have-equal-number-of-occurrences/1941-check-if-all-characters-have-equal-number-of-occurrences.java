3f1eead4e3bfde6c977c7f975f4dc32a748558b0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-20 16:40:02 -0500

    Time: 9 ms (31.89%), Space: 41.5 MB (9.65%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean areOccurrencesEqual(String s) {
-        int[] results = new int[26];
-        for(char c: s.toCharArray()){
-            results[c - 'a' ]++;
+        Map<Character, Integer> map =new HashMap<>();
+        for(char ch: s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
         
-         int val = results[s.charAt(0) - 'a'];
-        for(int result : results){
-            if(result != 0 && val != result){
+        int count =  map.get(s.charAt(0));
+        for(char ch: s.toCharArray()){
+            if(count !=  map.get(ch)){
                 return false;
             }
         }
-        return true;
+        return true;    
     }
 }
