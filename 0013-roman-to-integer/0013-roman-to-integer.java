e5d13250b5ad988d7834ff47c35bddef90f25170
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-19 19:16:03 -0500

    Time: 4 ms (66.54%), Space: 43.9 MB (19.60%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,28 +1,38 @@
 class Solution {
     public int romanToInt(String s) {
-       
-    
-    int answer = 0, number = 0, prev = 0;
-
-    for (int j = s.length() - 1; j >= 0; j--) {
-        switch (s.charAt(j)) {
-            case 'M' -> number = 1000;
-            case 'D' -> number = 500;
-            case 'C' -> number = 100;
-            case 'L' -> number = 50;
-            case 'X' -> number = 10;
-            case 'V' -> number = 5;
-            case 'I' -> number = 1;
+        int prev = Integer.MAX_VALUE;
+        int total = 0;
+        int len = s.length() - 1;
+        //MCMXCIV
+                //+5
+        Map<Character, Integer> map = new HashMap<>();
+        
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        for(int i = len; i>=0; i--){
+            char ch = s.charAt(i);
+            int value = map.get(ch);
+            if(prev!=Integer.MAX_VALUE && value < prev){
+                total = total - value;
+              
+            }else{
+                total = total + value;//5
+            }
+            prev = value;//5
         }
-        if (number < prev) {
-            answer -= number;
-        }
-        else {
-            answer += number;
-        }
-        prev = number;
+        return total;
     }
-    return answer;
 }
-    
-}
+
+//"MCMXCIV"
+//
+//take prev
+//set it to max
+// check current value is greater than or previous
+//if greater increament
+// else minus it from total
