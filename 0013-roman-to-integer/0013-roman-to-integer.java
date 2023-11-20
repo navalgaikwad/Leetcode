9b78a09af89725ee2d45fb0c57fa7e003de31a37
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-20 18:06:11 -0500

    Time: 4 ms (66.79%), Space: 44 MB (14.21%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,12 +1,9 @@
 class Solution {
     public int romanToInt(String s) {
-        int prev = Integer.MAX_VALUE;
+        int prev =0;
         int total = 0;
         int len = s.length() - 1;
-        //MCMXCIV
-                //+5
         Map<Character, Integer> map = new HashMap<>();
-        
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
@@ -15,24 +12,14 @@ class Solution {
         map.put('D', 500);
         map.put('M', 1000);
         for(int i = len; i>=0; i--){
-            char ch = s.charAt(i);
-            int value = map.get(ch);
-            if(prev!=Integer.MAX_VALUE && value < prev){
+           int value =  map.get(s.charAt(i));
+            if(value < prev){
                 total = total - value;
-              
             }else{
-                total = total + value;//5
-            }
-            prev = value;//5
+               total = total + value;
+           }
+            prev = value;
         }
         return total;
     }
 }
-
-//"MCMXCIV"
-//
-//take prev
-//set it to max
-// check current value is greater than or previous
-//if greater increament
-// else minus it from total
