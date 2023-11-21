2d92360d52b372350052e1716493419056aa6fc7
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-20 21:25:20 -0500

    Time: 57 ms (25.20%), Space: 44.8 MB (6.85%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,48 +1,19 @@
 class Solution {
     public int minSteps(String s, String t) {
+        //take map find count
+        //if count ==0 from map than increase the result
         Map<Character, Integer> map = new HashMap<>();
-        for(char c: s.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        for(char ch: s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        int replace = 0;
-        for(char c : t.toCharArray()) { //when count ==0 it is the magic a ==0
-            if(map.getOrDefault(c, 0) == 0){
-                replace++;
-            }else{
-                map.put(c, map.getOrDefault(c, 0) - 1);
+        int count = 0;
+        for(char ch: t.toCharArray()){
+            
+            if(map.getOrDefault(ch, 0) <= 0){
+                count++;
             }
+            map.put(ch, map.getOrDefault(ch, 0) - 1);
         }
-        return replace;
+        return count;
     }
 }
-
-//b = 1
-//a = 0 count = 1
-//
-
-
-
-
-
-
-
-
-
-
-
-//b : 2
-//a: 1
-
-// int[] ch = new int[26];
-//         for(char c : s.toCharArray()){
-//             ch[c-'a']++;
-//         }
-//         int replace = 0;
-//         for(char c : t.toCharArray()){
-//             if(ch[c-'a']==0)
-//                 replace++;
-//             else
-//                 ch[c-'a']--;
-//         }
-//         return replace;
-    
