b9923cf80d64f052803ef3b3f47aa45d43bd6b6a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-21 13:55:17 -0500

    Time: 60 ms (27.67%), Space: 51.3 MB (5.32%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,38 +1,24 @@
 class BrowserHistory {
-    TreeMap<Integer, String> map;
-    int ptr =0;
-    int homePtr =0;
+    TreeMap<Integer, String> map = new TreeMap<>();
+    int ptr = 0;
     public BrowserHistory(String homepage) {
-        map = new TreeMap<>();
         map.put(ptr, homepage);
-        //ptr++;
     }
     
     public void visit(String url) {
         ptr++;
-        map.put(ptr, url); //leet,go,fb, yt
+        map.put(ptr, url);
         map.tailMap(ptr + 1).clear();
     }
     
     public String back(int steps) {
-       
-            ptr = Math.max(ptr - (steps), 0);
-        
-//             if(ptr <0){
-//             ptr = 0;
-//             }
-           return map.get(ptr);
+        ptr = Math.max(0, ptr - steps);
+        return map.get(ptr);
     }
     
-    public String forward(int steps) { // adding can beyond the size so add the condition ptr > map.size()
-        
-             ptr = Math.min(ptr + steps, map.lastKey());
-            
-            // if(ptr > map.size()){
-            //   ptr = map.lastKey();
-            // }
-           return map.get(ptr);
-            
+    public String forward(int steps) {
+         ptr = Math.min(ptr + steps, map.lastKey());
+        return map.get(ptr);
     }
 }
 
