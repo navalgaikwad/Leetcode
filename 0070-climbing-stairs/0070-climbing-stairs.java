de51e95d3e86b8ee29c74bd4a3ce02dde6cf39e2
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-25 15:31:29 -0500

    Time: 0 ms (100.00%), Space: 40 MB (7.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,7 +5,7 @@ class Solution {
     int helper(int n, HashMap<Integer, Integer>memo){
         if(memo.containsKey(n)) return memo.get(n);
         if (n == 0 || n == 1) return 1;
-        if(n<0) return 0;
+        if(n < 0) return 0;
        
         int total = helper(n-1, memo) + helper(n-2, memo); 
         memo.put(n, total);
