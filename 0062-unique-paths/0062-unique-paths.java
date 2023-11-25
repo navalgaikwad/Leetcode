84158977256b7ae3b46c8ca779da1aabc81952a8
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-25 15:45:15 -0500

    Time: 14 ms (9.51%), Space: 40.2 MB (5.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return helper(m, n, new HashMap<>());
-    }
-    
-    int helper(int m, int n, Map<String, Integer> memo){
-        String key = m+"-"+n;
-        if(memo.containsKey(key)) return memo.get(key);
-        if(m == 1 && n == 1) return 1;
-        if(m == 0 || n == 0) return 0;
         
-        int first = helper(m - 1, n, memo);
-        int second = helper(m, n-1, memo);
+        int[][] dp = new int[m+1][n+1];
+        dp[1][1] = 1;
         
-        memo.put(key, first+second);
-        return memo.get(key);
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                
+              if(i - 1 >= 0) dp[i][j]+=dp[i-1][j];
+              if(j - 1 >= 0) dp[i][j]+=dp[i][j-1];  
+                
+             System.out.println(dp[i][j]);
+            }
+        }
+       
+        return dp[m][n];
     }
 }
