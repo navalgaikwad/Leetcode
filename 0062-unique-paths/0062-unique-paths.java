5ac25fe15f6c3e7b130b724ecf9f4c24b8caa646
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-25 15:45:59 -0500

    Time: 13 ms (9.51%), Space: 40.4 MB (5.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,19 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        int[][] dp = new int[m+1][n+1];
-        dp[1][1] = 1;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
         
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 
-              if(i - 1 >= 0) dp[i][j]+=dp[i-1][j];
-              if(j - 1 >= 0) dp[i][j]+=dp[i][j-1];  
+              if(i - 1 >= 0) dp[i][j] += dp[i-1][j];
+              if(j - 1 >= 0) dp[i][j] += dp[i][j-1];  
                 
              System.out.println(dp[i][j]);
             }
         }
        
-        return dp[m][n];
+        return dp[m - 1][n - 1];
     }
 }
