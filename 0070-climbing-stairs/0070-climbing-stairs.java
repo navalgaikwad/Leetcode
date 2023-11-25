8a5fea7ecbc838828326d6d260fdc976332eb7f2
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-25 15:21:34 -0500

    Time: 0 ms (100.00%), Space: 39.8 MB (7.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 2){
-            return n;
-        }
-        
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        
-        for(int i=3; i<=n; i++){
-             dp[i] = dp[i - 1] + dp[i - 2];  
-        }
-        
-        return dp[n];
+        return helper(n, new HashMap<>());
+    }
+    int helper(int n, HashMap<Integer, Integer>memo){
+        if(memo.containsKey(n)) return memo.get(n);
+        if (n == 0 || n == 1) return 1;
+        if(n<0) return 0;
+       
+        int total = helper(n-1, memo) + helper(n-2, memo); 
+        memo.put(n, total);
+        return total;
     }
 }
