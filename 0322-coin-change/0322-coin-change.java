9f8d9321d2bde5cd959a8ddf51087da98b525abe
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-25 20:08:50 -0500

    Time: 14 ms (86.31%), Space: 44 MB (17.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Integer result = helper2(amount, coins, new HashMap<Integer, Integer>() );
-        return result != null ? result : -1;
-    }
+        long[] dp = new long[amount + 1];
+        Arrays.fill(dp, Long.MAX_VALUE); // Filling the array with max value instead of -1
+        dp[0] = 0;
 
-
- static Integer helper2(int target, int[] coins, HashMap<Integer, Integer> memo){
-        if( memo.containsKey(target) ){
-            return memo.get(target);
-        }
-        if( target == 0 ) return 0;
-        
-        if(target < 0) return -1;
-
-        Integer result = -1;
-
-        for(Integer coin :coins){
-           Integer remainderList =  helper2(target - coin , coins, memo);
-              if(remainderList != -1){
-                  
-                  Integer newCombination = remainderList + 1;
-
-                  if(result ==-1 || newCombination < result){ //check minimum 
-                      result = newCombination;
+        for (int i = 0; i <= amount; i++) {
+            if (dp[i] != Long.MAX_VALUE) {
+                for (int coin : coins) {
+                    if (i <= amount - coin) { // Check for potential overflow
+                        dp[i + coin] = Math.min(dp[i + coin], dp[i] + 1);
+                    }
+                }
             }
-         }      
-      }
-     memo.put(target, result);
-     return result;
+        }
+
+        return dp[amount] > amount ? -1 : (int) dp[amount];
     }
 }
