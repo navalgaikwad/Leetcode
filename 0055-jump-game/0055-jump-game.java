e6075f27736d60ac47cca80166d5adb4c793e6ed
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-27 14:09:20 -0500

    Time: 2396 ms (5.00%), Space: 47.4 MB (7.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums[0]==0 && nums.length<2) return true;
+      //  if(nums[0] == 0 && nums.length < 2) return true;
         return dp(nums, 0, new HashMap<>());
     }
     boolean dp(int[] nums, int currentIndex, HashMap<Integer, Boolean> memo){
-        if(currentIndex==nums.length -1){
+        if(currentIndex == nums.length -1){
             return true;
         }
         if(memo.containsKey(currentIndex)) return memo.get(currentIndex);
         
         for(int i = 1; i< nums[currentIndex]+1; i++){
-            int nextStep = i + currentIndex;
-            if(nextStep < nums.length){
-               if(dp(nums, nextStep, memo)){
-                memo.put(currentIndex, true);
-                return true;
-            } 
-          }
-            
+            int nextStep = i + currentIndex; // it is same as coin change
+            //move to next step
+            if(nextStep < nums.length){//check for nextstep is less than array length
+                if(dp(nums, nextStep, memo)){
+                    memo.put(currentIndex, true);
+                    return true;
+                } 
+            }
         }
         memo.put(currentIndex, false);
         return false;
