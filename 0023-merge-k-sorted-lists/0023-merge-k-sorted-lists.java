4f100ef1d1223f14c1e048ad165dfe7915673cd3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-29 01:31:15 -0500

    Time: 5 ms (42.82%), Space: 42.9 MB (86.15%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,67 +9,25 @@
  * }
  */
 class Solution {
-    ListNode head = new ListNode(0);
-    ListNode dummy = head;
     public ListNode mergeKLists(ListNode[] lists) {
-        for(ListNode  list: lists){
-            helper(list);
-        }
-       return sort(head.next);
-    }
-    
-   void helper(ListNode root){
-        while(root!=null){
-            dummy.next =root;
-            root = root.next;
-           dummy = dummy.next;
-        }
-    }
-    
-     private ListNode sort(ListNode head){
-        if(head == null || head.next == null){
-             return  head;
-        }
-         
-     ListNode mid = middle(head);
-     ListNode next = mid.next;
-     mid.next = null;
-     ListNode  left = sort(head);
-     ListNode  right = sort(next);
-         
-     return mergeLinkList(left, right);
-    }
-    
-    private ListNode mergeLinkList(ListNode left, ListNode right){
-        ListNode temp = new ListNode(0);
-        ListNode dm = temp;
-        
-        while(left!= null && right!=null){
-           if(left.val < right.val){
-                dm.next = left;
-                left = left.next;
-            }else{
-                dm.next = right;
-                right = right.next; 
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for(ListNode list :lists) {
+            if(list != null){
+                pq.add(list);
             }
-            dm = dm.next;
         }
-    
-        dm.next = (left == null) ? right : left;  
-        return temp.next;
-    }
-    
-    private ListNode middle(ListNode head){
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = null;
+        ListNode root = new ListNode(-1);
+        ListNode current = root;
         
-        while(fast !=null && fast.next != null){
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
+        while(!pq.isEmpty()){
+            ListNode node = pq.remove();
+            current.next = node;
+            current = current.next;
+            if(node.next != null){
+               pq.add(node.next); 
+            }
         }
-        
-        return prev;
+        return root.next;
     }
+    //find the middle of the list
 }
