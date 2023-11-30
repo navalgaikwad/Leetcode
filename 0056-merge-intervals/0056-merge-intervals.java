535bb11247e9a7e83f823724d670ea777017823b
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-29 22:02:38 -0500

    Time: 8 ms (91.11%), Space: 45.7 MB (27.50%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,26 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        LinkedList<int[]> merge = new LinkedList<>();
+        LinkedList<int[]> mergedList = new LinkedList<>();
         
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]); 
         
         for(int[]  interval: intervals){
-            if(merge.isEmpty() || merge.getLast()[1] < interval[0]){
-                merge.add(interval);
-            }else if(interval[0] <= merge.getLast()[1]){
-                merge.getLast()[1] =  Math.max(interval[1], merge.getLast()[1]);
+            if(mergedList.isEmpty() || mergedList.getLast()[1] < interval[0]){
+                mergedList.add(interval);
+            }else if(interval[0] <= mergedList.getLast()[1]){
+                mergedList.getLast()[1] =  Math.max(interval[1], mergedList.getLast()[1]);
                 //take max from intervals
             }
         }
         
-        int[][] arr = new int[merge.size()][2];
+        int[][] result = new int[mergedList.size()][2];
         int i = 0;
-        for (int[] interval : merge){
-            arr[i] = interval;
+        for (int[] interval : mergedList){
+            result[i] = interval;
             i += 1;
         }
         
-        return arr;
+        return result;
     }
 }
 
