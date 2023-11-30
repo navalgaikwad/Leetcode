ccb4f901da91191030da1123621b87100b93ed32
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-30 17:38:00 -0500

    Time: 47 ms (57.53%), Space: 97.2 MB (63.63%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,20 +1,25 @@
 class Solution {
+    private boolean overlap(int[] a, int b){
+        return a[0] < b; // play with endTime
+    }
+    //[[0,2],[1,3],[2,4],[3,5],[4,6]]
     public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int endTime = intervals[0][1];
         int count =0;
-        int min = Integer.MIN_VALUE;
-       boolean flag =false;
-        
-       Arrays.sort(intervals, (a, b) ->Integer.compare(a[1], b[1]));
-        int[] prev = intervals[0];
-        for(int i=1 ;i<intervals.length; i++){
-           int[] current = intervals[i];
-           if(current[0] >= prev[1] ){
-               prev =  current; 
-           }else{
-             count ++;
-           }
-            
-        } 
-          return count;
+        for(int i = 1 ;i<intervals.length; i++){
+            if(overlap(intervals[i], endTime)){ 
+                count++; //if there is count++, 
+                endTime = Math.min(intervals[i][1], endTime);
+            }else {// if there is no overlap take next overlapp
+                endTime = intervals[i][1];
+            }
         }
+        return count;
+    }
 }
+
+//[[1,2],[2,3],[3,4],[1,3]]
+//[3, 4]
+// [1, 3]
+//
