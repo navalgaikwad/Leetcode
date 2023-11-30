f4d68e5cd373ed5e416418bdef562254ba9b27b2
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-11-30 16:49:19 -0500

    Time: 2 ms (48.78%), Space: 44.1 MB (32.83%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,19 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<int[]> mergedList = new ArrayList<>();
-        for(int i =0; i<intervals.length; i++) {
-            if(newInterval[1] < intervals[i][0]) {//if it at start
+        
+        for (int i = 0; i < intervals.length; i++) {
+            if (newInterval[1] < intervals[i][0]) { // if it's at the start
                 mergedList.add(newInterval);
-                // Collections.addAll(mergedList, intervals.subList(i, intervals.length));
-                 Collections.addAll(mergedList, Arrays.copyOfRange(intervals, i, intervals.length));
-                 return mergedList.toArray(new int[mergedList.size()][]); 
-            }else if(intervals[i][1] < newInterval[0]) { //in middle
+                Collections.addAll(mergedList, Arrays.copyOfRange(intervals, i, intervals.length));
+                return mergedList.toArray(new int[mergedList.size()][]); 
+            } else if (intervals[i][1] < newInterval[0]) { // in the middle
                 mergedList.add(intervals[i]);
-            } else { //overlapping 
+            } else { // overlapping 
                 newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
                 newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             }
         }
+        
         mergedList.add(newInterval);
         return mergedList.toArray(new int[mergedList.size()][]);  
     }
