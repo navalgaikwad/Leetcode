2f1dbad5e98be69dc8c781873df6adc6ad15c038
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-01 00:35:20 -0500

    Time: 2 ms (99.23%), Space: 43.6 MB (29.78%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,22 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<nums2.length; i++){
-           for(int j=i+1;j<nums2.length; j++){
-               if(nums2[j]>nums2[i]){
-                   map.put(nums2[i], nums2[j]);
-                   break;
-               }
-           }
-            
+        Map<Integer, Integer> map =new HashMap<>();
+       // int i =0;
+        int len = nums2.length;
+        
+        for(int i =0; i<nums2.length; i++){
+            map.put(nums2[i], i);
         }
+        
         int[] result = new int[nums1.length];
         Arrays.fill(result, -1);
-        for(int i=0; i< nums1.length; i++){
-            if(map.containsKey(nums1[i])){
-                result[i]=map.get(nums1[i]);
+        int j =0;
+        
+        for(int num : nums1){
+            int index = map.get(num);
+            for(int i =index; i<len; i++){
+                if(nums2[i] > num){
+                    result[j] = nums2[i];
+                    break;
+                }
             }
+            j++;
         }
         return result;
     }
 }
+// nums1 = [4,1,2], nums2 = [1,3,4,2]
+//[4, 1,  ]
+//[]
