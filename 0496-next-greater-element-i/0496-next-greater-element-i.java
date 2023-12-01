50341b4f26267dfdb24b65f7ee84ecba737170f1
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-01 00:50:07 -0500

    Time: 3 ms (89.63%), Space: 43.7 MB (20.94%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,30 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map =new HashMap<>();
-       // int i =0;
+        Stack<Integer> st = new Stack<>();
         int len = nums2.length;
         
-        for(int i =0; i<nums2.length; i++){
-            map.put(nums2[i], i);
-        }
-        
-        int[] result = new int[nums1.length];
-        Arrays.fill(result, -1);
-        int j =0;
-        
-        for(int num : nums1){
-            int index = map.get(num);
-            for(int i =index; i<len; i++){
-                if(nums2[i] > num){
-                    result[j] = nums2[i];
-                    break;
-                }
+        Map<Integer, Integer> map =new HashMap<>();
+        for(int i =0; i<len; i++) {
+            while(st.size()>0 && nums2[st.peek()] < nums2[i]){
+                int index = st.pop();
+                map.put(nums2[index], nums2[i]);
             }
-            j++;
+            st.push(i);
         }
+        int[] result = new int[nums1.length];
+        int i =0 ;
+        for(int nums : nums1){
+            result[i++] = map.getOrDefault(nums, -1);
+        }
+        
         return result;
     }
 }
-// nums1 = [4,1,2], nums2 = [1,3,4,2]
-//[4, 1,  ]
-//[]
