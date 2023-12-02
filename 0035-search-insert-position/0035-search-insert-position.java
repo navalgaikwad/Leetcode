f223216debccffe5cd7c796e01e6ab2cc87d97e6
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-02 17:50:32 -0500

    Time: 0 ms (100.00%), Space: 43.2 MB (47.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,18 +1,18 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int l = 0;
-        int r =nums.length - 1;
-        while(l<=r) {
-            int mid = l + (r - l)/2;
+        int left = 0;
+        int right =nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left)/2;
             if(nums[mid] == target) {
                 return mid;
             }
             if(nums[mid] > target) {
-               r = mid - 1; 
+               right = mid - 1; 
             }else {
-                l = mid + 1;
+                left = mid + 1;
             }
         }
-        return r + 1;
+        return right + 1;
     }
 }
