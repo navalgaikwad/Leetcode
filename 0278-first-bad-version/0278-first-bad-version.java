ddadb5da179078eced99e083e7aa969390bf8051
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-02 18:27:43 -0500

    Time: 13 ms (83.33%), Space: 39.4 MB (23.82%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -5,14 +5,16 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
+        int firstBadVersion = 0;
         while(left <= right){
             int mid = left + (right - left)/2;
             if(isBadVersion(mid)){
+                firstBadVersion = mid;
                 right = mid - 1;
             }else {
                 left = mid + 1;
             }
         }
-        return (left);
+        return (firstBadVersion);
     }
 }
