2b68c841fcd32633fdf37d34eb9fd95f2fd95b36
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-02 17:55:18 -0500

    Time: 0 ms (100.00%), Space: 43.3 MB (38.65%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,17 +2,22 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int left = 0;
         int right =nums.length - 1;
+        //normal binary search
+        
         while(left <= right) {
             int mid = left + (right - left)/2;
             if(nums[mid] == target) {
                 return mid;
             }
+            //for finding left position first index where it contains duplicate use  nums[mid] >= target
+            //last index   nums[mid] > target
             if(nums[mid] > target) {
-               right = mid - 1; 
+               right = mid - 1; // use  right = mid; 
             }else {
                 left = mid + 1;
             }
         }
         return right + 1;
     }
+
 }
