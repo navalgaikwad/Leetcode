caf4cca3b9c2daefbd3d26a5f2594e9ff5f4d0e9
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-03 16:29:23 -0500

    Time: 0 ms (100.00%), Space: 41 MB (36.74%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int search(int[] nums, int target) {
+         int result = binarySearch(nums, target);
+        return result;
+    }
+    //11,9,8,1,2,3,4,5
+    int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[left] <= nums[mid]) {// normal sorted
+               if(nums[left] <= target && nums[mid] > target ) {
+                    right = mid - 1;
+                }else {
+                    left = mid + 1;
+                } 
+            } else {
+            if(nums[right] >= nums[mid]) {// right side normal sorted
+               if(nums[right] >= target && nums[mid] < target ) {
+                    left = mid + 1;
+                }else {
+                    right = mid - 1;
+                } 
+              }
+            }
+            
+        }
+        return -1;
+    }
+}
