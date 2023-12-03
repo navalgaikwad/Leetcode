ddf233dd401a4d706dbc133ac606ce6f8211ff37
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-03 11:43:30 -0500

    Time: 0 ms (100.00%), Space: 40.9 MB (51.68%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int left = 0;
+        int right = rows * cols - 1; // Calculate the right boundary based on the total number of elements
+
+       return binarySearch(matrix, target, 0, right);
+    }
+    
+    boolean binarySearch(int[][] matrix, int target, int left, int right) {
+        
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid/matrix[0].length;
+            int col = mid % matrix[0].length;
+            
+            if( matrix[row][col] == target) {
+                return true;
+            }
+            if(matrix[row][col] > target) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}
+
+//[
+//0 ->[1,3,5,7],
+//1 ->[10,11,16,20],
+//2 ->[23,30,34,60]
+//]
