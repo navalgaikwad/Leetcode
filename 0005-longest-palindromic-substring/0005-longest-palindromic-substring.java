044e1b375e75bbffa295967b9f5870355c49b81e
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-07 18:28:46 -0500

    Time: 19 ms (60.26%), Space: 44 MB (40.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -7,7 +7,7 @@ class Solution {
             int left = i;
             int right = i;
             //odd size
-            while(left >= 0 && right < len && s.charAt(left)==s.charAt(right)) {
+            while(left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
                 if(right - left + 1 > resultLength) {//need to find longest
                     result = s.substring(left, right+1);
                     resultLength = right - left + 1;
@@ -17,10 +17,10 @@ class Solution {
             }
             //even side
             left = i; 
-            right = i + 1;
-            while(left >= 0 && right < len && s.charAt(left)==s.charAt(right)) {
+            right = i + 1; //character might even for odd so need to check for both
+            while(left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
                 if(right - left + 1 > resultLength) {//need to find longest
-                    result = s.substring(left, right+1);
+                    result = s.substring(left, right+1);// update the longest 
                     resultLength = right - left + 1;
                 }
                 left--;
@@ -32,3 +32,7 @@ class Solution {
     }
 }
 //no of subtring n*n -1 //2 so brute force it takes O(n3)
+//1. Take one charatcer and check in i and j side, do we maximum value lenght
+//2. check for even side
+//3. check for odd side
+//4. return the result who ever gives best answer give it
