cc879cc3cc3d528faa1a77dce8935c5c49c32842
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-08 00:51:15 -0500

    Time: 445 ms (5.02%), Space: 217.8 MB (5.09%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int result= dp(text1, text2, text1.length() - 1, text2.length() - 1, new HashMap<>());
+        return result;
+    }
+    int dp(String text1, String text2, int i, int j, HashMap<String, Integer> memo) {
+        String key = i+"-"+j;
+        if(memo.containsKey(key)) {
+           return memo.get(key); 
+        }
+        if(i<0 || j<0) {
+            return 0;
+        }
+        if(text1.charAt(i) == text2.charAt(j)) {
+            return 1 + dp(text1, text2, i - 1, j - 1, memo);
+        }
+        
+        int result = Math.max(dp(text1, text2, i - 1, j, memo), dp(text1, text2, i, j-1, memo));
+        memo.put(key, result);
+        return result;
+    }
+}
