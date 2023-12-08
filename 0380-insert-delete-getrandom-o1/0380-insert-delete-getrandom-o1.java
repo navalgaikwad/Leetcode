5bfbfc79b31a80cf8f9ee3c93aec946850863485
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-08 16:54:01 -0500

    Time: 113 ms (15.84%), Space: 93.6 MB (5.30%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,27 +1,44 @@
 class RandomizedSet {
-    Set<Integer> hs = new HashSet<>();
+   
+    Map<Integer, Integer> indexMap = new HashMap<>();
     public RandomizedSet() {
         
     }
-    
+    int index =0;
     public boolean insert(int val) {
-        return hs.add(val);
+         if(!indexMap.containsKey(val)) {
+            
+             indexMap.put(val, val);
+        
+             return true;
+         }
+        return false;
+        
     }
     
     public boolean remove(int val) {
-        return hs.remove(val);
+           Integer ind = indexMap.remove(val);
+           if(ind!=null) {
+            
+               return true;
+           }
+        return false;
     }
     
     public int getRandom() {
-        Integer[] arrayNumbers = hs.toArray(new Integer[hs.size()]); 
+      //  Integer[] arrayNumbers = hs.toArray(new Integer[hs.size()]); 
   
         // generate a random number 
         Random rndm = new Random(); 
   
         // this will generate a random number between 0 and 
         // HashSet.size - 1 
-        int rndmNumber = rndm.nextInt(hs.size()); 
-        return arrayNumbers[rndmNumber];
+        int rndmNumber = rndm.nextInt(indexMap.size());
+        // Get the key at the random index
+        Integer randomKey = new ArrayList<>(indexMap.keySet()).get(rndmNumber);
+        
+        return randomKey;
+        
     }
 }
 
