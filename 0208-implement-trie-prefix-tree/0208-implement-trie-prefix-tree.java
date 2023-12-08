4eabdd440f35027ea54d548050843cb60499e406
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-08 18:18:28 -0500

    Time: 43 ms (28.61%), Space: 55.2 MB (26.33%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,70 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Trie {
+
+    // Trie Node
+    static class Node {
+        char c;
+        Map<Character, Node> children;
+        boolean isWord;
+
+        public Node(char c) {
+            this.c = c;
+            this.children = new HashMap<>();
+            this.isWord = false;
+        }
+
+        @Override
+        public String toString() {
+            return "c: " + c + " " +
+                   "children: " + children.keySet().toString();
+        }
+    }
+
+    private Node root;
+
+    public Trie() {
+        this.root = new Node('\0');
+    }
+
+    public void insert(String word) {
+        Node curr = root;
+        for (char ch : word.toCharArray()) {
+            curr.children.putIfAbsent(ch, new Node(ch));
+            curr = curr.children.get(ch);
+        }
+        curr.isWord = true;
+    }
+
+    public boolean search(String word) {
+        Node lastNode = findLastNode(word);
+        return lastNode != null && lastNode.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        return findLastNode(prefix) != null;
+    }
+
+    private Node findLastNode(String str) {
+        Node curr = root;
+        for (char ch : str.toCharArray()) {
+            if (!curr.children.containsKey(ch)) {
+                return null;
+            }
+            curr = curr.children.get(ch);
+        }
+        return curr;
+    }
+
+    // Usage
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));   // returns true
+        System.out.println(trie.search("app"));     // returns false
+        System.out.println(trie.startsWith("app")); // returns true
+        trie.insert("app");
+        System.out.println(trie.search("app"));     // returns true
+    }
+}
