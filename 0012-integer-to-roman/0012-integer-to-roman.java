53765df34e6b97c717c11966dadb41e02da4a2ba
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-07 23:10:16 -0500

    Time: 3 ms (97.31%), Space: 43.4 MB (57.11%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,32 +1,17 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-public class Solution {
+class Solution {
     public String intToRoman(int num) {
-        LinkedHashMap<Integer, String> valuesToSymbols = new LinkedHashMap<>();
-        valuesToSymbols.put(1000, "M");
-        valuesToSymbols.put(900, "CM");
-        valuesToSymbols.put(500, "D");
-        valuesToSymbols.put(400, "CD");
-        valuesToSymbols.put(100, "C");
-        valuesToSymbols.put(90, "XC");
-        valuesToSymbols.put(50, "L");
-        valuesToSymbols.put(40, "XL");
-        valuesToSymbols.put(10, "X");
-        valuesToSymbols.put(9, "IX");
-        valuesToSymbols.put(5, "V");
-        valuesToSymbols.put(4, "IV");
-        valuesToSymbols.put(1, "I");
-        
-        StringBuilder res = new StringBuilder();
-        
-        for (Map.Entry<Integer, String> entry : valuesToSymbols.entrySet()) {
-            while (num >= entry.getKey()) {
-                num -= entry.getKey();
-                res.append(entry.getValue());
+        int []values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String []notations={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0;i<values.length;i++){
+            while(num>=values[i])
+            {
+                num-=values[i];
+                sb.append(notations[i]);
             }
         }
-        
-        return res.toString();
+
+        return sb.toString();
     }
 }
