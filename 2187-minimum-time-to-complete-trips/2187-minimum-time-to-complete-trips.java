bcef9f1c17bc09c8a4ac25a278ad2e1fe266e989
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-09 16:51:56 -0500

    Time: 95 ms (19.15%), Space: 56.3 MB (32.98%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long minimumTime(int[] time, int totalTrips) {
+        long left = 1;
+        long right = 100000000000000L;
+        while(left < right) {
+            long mid = left + (right-left)/2;
+            if(compelteTheTrip(time, mid, totalTrips)>=totalTrips) {
+                right = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    
+    long compelteTheTrip(int[] times, long mid, int totalTrips) {
+        long count = 0;
+        for(int time : times) {
+            long t = (long)time;
+            count +=mid/t;
+            // if(count >= totalTrips ){
+            //     break;
+            // }
+        }
+        return count;
+    }
+}
