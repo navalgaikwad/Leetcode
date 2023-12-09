b16c1cd7d5f9196a7b7713986c14f590b90513ef
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-08 21:00:09 -0500

    Time: 42 ms (32.36%), Space: 55.4 MB (17.72%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,76 +1,58 @@
-class TrieNode{
-    private TrieNode[] children;
-    private boolean isEndOfWord; // which will have children and isEnd
-
-    public TrieNode() {
-        children = new TrieNode[26];  // 26 letters of the alphabet
-        isEndOfWord = false;
-    }
-
-    public boolean containsKey(char ch) {
-        return children[ch - 'a'] != null;//convert ch to integer and return the true o
-    }
-
-    public TrieNode get(char ch) {
-        return children[ch - 'a'];//
-    }
-
-    public void put(char ch, TrieNode node) {
-        children[ch - 'a'] = node;
-    }
-
-    public void setEnd() {
-        isEndOfWord = true;
-    }
-
-    public boolean isEnd() {
-        return isEndOfWord;
-    }
-}
-
 class Trie {
-    // new class 
-    //TrieNode
-    // which will have children and isEnd
-    private TrieNode root;
 
+    class Node {
+        char c;
+        Map<Character, Node> children;
+        boolean isEnd;
+        Node(char c) {
+            this.c = c;
+            this.children = new HashMap<>();
+            this.isEnd = false;
+        }
+    }
+    Node root;
     public Trie() {
-        root = new TrieNode();
+        this.root = new Node('\0');
     }
-
-    public void insert(String word) {//same as clone node
-        TrieNode node = root;
-        for (int i = 0; i < word.length(); i++) {
-            char currentChar = word.charAt(i);
-            if (!node.containsKey(currentChar)) { // check if present if not present add currentChar to with next trieNode
-                node.put(currentChar, new TrieNode());
+    
+    public void insert(String word) {
+        Node curr = root;
+        for(char c : word.toCharArray()) {
+            if(!curr.children.containsKey(c)) {
+                curr.children.put(c, new Node(c));
             }
-            node = node.get(currentChar);
+            curr = curr.children.get(c);
         }
-        node.setEnd();// it will be at the last
+        curr.isEnd = true;
     }
-
+    
     public boolean search(String word) {
-        TrieNode node = searchPrefix(word);
-        return node != null && node.isEnd(); //check node is not null and not end
-    }
-
-    public boolean startsWith(String prefix) {
-        TrieNode node = searchPrefix(prefix);
-        return node != null;
-    }
-
-    private TrieNode searchPrefix(String word) {
-        TrieNode node = root; //searching start from root
-        for (int i = 0; i < word.length(); i++) {
-            char curLetter = word.charAt(i);// take one char
-            if (node.containsKey(curLetter)) {// check if key is present
-                node = node.get(curLetter); // assign next node value to node
-            } else {
-                return null; // if not matching return null
+        Node curr = root;
+        for(char c : word.toCharArray()) {
+            if(!curr.children.containsKey(c)) {
+                return false;
             }
+            curr = curr.children.get(c);
         }
-        return node;
+        return curr.isEnd;
+    }
+    
+    public boolean startsWith(String prefix) {
+        Node curr = root;
+        for(char c : prefix.toCharArray()) {
+            if(!curr.children.containsKey(c)) {
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return true;
     }
 }
 
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
