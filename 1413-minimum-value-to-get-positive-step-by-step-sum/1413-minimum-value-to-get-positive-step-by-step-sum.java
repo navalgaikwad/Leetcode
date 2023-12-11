4b8a32c98de54621c0a86d3f465dcb42607f4c32
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-11 17:08:34 -0500

    Time: 0 ms (100.00%), Space: 40.5 MB (13.21%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
@@ -1,14 +1,19 @@
 class Solution {
     public int minStartValue(int[] nums) {
-        int currentPrefixSum = 0;
-        int minPrefixSum = 0; 
+        int minCumulativeSum = 0;
+        int cumulativeSum = 0;
+
         for (int num : nums) {
-            currentPrefixSum += num;
-            minPrefixSum = Math.min(currentPrefixSum, minPrefixSum);
-          
+            cumulativeSum += num;
+            minCumulativeSum = Math.min(minCumulativeSum, cumulativeSum);
         }
-        System.out.print(minPrefixSum);
-        return -(minPrefixSum) +1;
-    
+
+        return 1 - minCumulativeSum;//-4
     }
 }
+
+//-3,2,-3,4,2
+//-3,-1,-4,0,2 pref
+// 2,5,3,6,2 su
+
+//1,-1,-4
