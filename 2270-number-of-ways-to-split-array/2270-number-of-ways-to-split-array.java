78de89110a99efc041281d87ab539583c825a6b4
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-10 23:26:49 -0500

    Time: 2 ms (100.00%), Space: 63.9 MB (5.33%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,36 +1,21 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        int n = nums.length;
-        long[] prefixSum = new long[n];
-        long[] suffixSum = new long[n];
-
-        // Calculate prefix sum
-        prefixSum[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
-        }
-        //10  14  6  13
-        for(long suffix: prefixSum) {
-            System.out.print(suffix +"  ");
+        int ans = 0;
+        long leftSection = 0;
+        long total = 0;
+        
+        for (int num: nums) {
+            total += num;
         }
         
-        // Calculate suffix sum 
-        //13  3  -1  7  
-        suffixSum[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffixSum[i] = suffixSum[i + 1] + nums[i];
-        }
-        for(long suffix: suffixSum) {
-            System.out.print(suffix +"  ");
-        }
-        // Count valid splits
-        int count = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (prefixSum[i] >= suffixSum[i + 1]) {
-                count++;
+        for (int i = 0; i < nums.length - 1; i++) {
+            leftSection += nums[i];
+            long rightSection = total - leftSection;
+            if (leftSection >= rightSection) {
+                ans++;
             }
         }
-
-        return count;
+        
+        return ans;
     }
 }
