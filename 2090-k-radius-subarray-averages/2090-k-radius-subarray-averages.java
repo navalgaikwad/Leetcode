8f1c1d973c776b94142c47b870da57af31961d68
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-11 21:20:56 -0500

    Time: 8 ms (11.60%), Space: 61.4 MB (45.55%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,29 +1,35 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        int len = nums.length;
-        long[] prefix = new long[len];
-        long[] suffix = new long[len];
+        long[] prefix = new long[nums.length];
+        long[] suffix = new long[nums.length];
+        prefix[0] = nums[0];
+        int n = nums.length;
        
-        long sumFromStart = 0, sumFromEnd = 0;
-        for (int i = 0, j = len - 1; i < len; i++, j--) {
-            sumFromStart += nums[i];
-            sumFromEnd += nums[j];
-            prefix[i] = sumFromStart;
-            suffix[j] = sumFromEnd;
+        for(int i=1; i<n; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
         }
-        int[] result = new int[len];
-        //int i =0;
-        for(int i=0; i<len; i++){
-            if(i - k < 0 || i + k >=len){
-               result[i]=-1; 
-            }else{ 
-                // suffice to find element from left 
-                //prefix to find element from right side
-                long sum = (suffix[i - k] - suffix[i]) + (long) nums[i] + (prefix[i + k] -prefix[i]);
-                sum = sum/(2* k + 1);
-                result[i] =(int) sum; // k(left)+ 1 + k (right) 2k + 1
+        
+        suffix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + nums[i];
+        }
+        
+        int[] result = new int[n];
+        for(int i = 0; i<n; i++) {
+            if(i + k >= n || i-k < 0 ) {
+                result[i] = -1;
+            }else {
+                long suff = suffix[i - k] - suffix[i];// from that element to left 
+                long pref = prefix[i + k] - prefix[i];// from that elemnt to right
+                long sum = suff + (long)nums[i] + pref;// do sum from left and right
+                long noOfElement = (2 * k + 1);//no of elemnt
+                long answer = sum/noOfElement;
+                result[i] = (int)answer;
             }
         }
         return result;
     }
 }
+
+//7,4,3,9,1,8,5,2,6 k=3
+//
