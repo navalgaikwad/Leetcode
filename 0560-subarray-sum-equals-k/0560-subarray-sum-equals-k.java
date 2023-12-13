8f8522962d68bd3bd931126c7cad98e00726c748
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-12 20:35:24 -0500

    Time: 22 ms (88.73%), Space: 46.3 MB (49.94%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,27 +1,20 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int len = nums.length;
-
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-
-        int curr = 0;
-        int ans = 0;
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> sumFrequency = new HashMap<>();
+        sumFrequency.put(0, 1); // To handle the case when the sum equals k at the beginning
 
         for (int num : nums) {
-            curr += num; //prefix sum kar
-
-            // Check if (curr - k) exists in the map(1,2,3,4) k =2 (3 -1) = 2 : (4 -2) = 2
-            if (map.containsKey(curr - k)) { //value asel tarr add kar ans
-                ans += map.get(curr - k);
+            sum += num;
+            // Check if there is a subarray (ending at the current index) whose sum equals k
+            if (sumFrequency.containsKey(sum - k)) {
+                count += sumFrequency.get(sum - k);
             }
-
-            map.put(curr, map.getOrDefault(curr, 0) + 1);
+            // Update the frequency of the current sum
+            sumFrequency.put(sum, sumFrequency.getOrDefault(sum, 0) + 1);
         }
 
-        return ans;
+        return count;
     }
 }
