15f03094cf4185bb518a72697e0b77662c4f7517
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-13 10:10:19 -0500

    Time: 29 ms (77.56%), Space: 56.6 MB (79.45%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,21 +1,23 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-     int left=0;int right=0; int ans=0;
-     Arrays.sort(nums);
-     long sum=nums[0];
-     while(right<nums.length){
-        // int max=;
-         if((right-left+1)*nums[right]<=k+sum){ //important crux
-            ans=Math.max(ans, right-left+1);
-              right++;
-            if(right<nums.length){
-            sum=sum+nums[right];
+        int maxFrequency = 0; // Initialize the maximum frequency
+        long currentSum = 0; // Initialize the current sum of the subarray elements
+
+        Arrays.sort(nums); // Sort the array in ascending order
+
+        for (int left = 0, right = 0; right < nums.length; ++right) {
+            currentSum += nums[right]; // Include the current element in the subarray sum
+
+            // Check if the current subarray violates the condition (sum + k < nums[right] * length)
+            while (currentSum + k < (long) nums[right] * (right - left + 1)) {
+                currentSum -= nums[left]; // Adjust the subarray sum by removing the leftmost element
+                left++; // Move the left pointer to the right
             }
-         }else{
-         sum=sum-nums[left];
-         left++;
-         }
-     }
-     return ans;  
+
+            // Update the maximum frequency based on the current subarray length
+            maxFrequency = Math.max(maxFrequency, right - left + 1);
+        }
+
+        return maxFrequency;
     }
 }
