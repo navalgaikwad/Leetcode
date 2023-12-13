9aeee1c983e7f2c07311fb8d59ec992c1eef6dd0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-13 13:13:22 -0500

    Time: 67 ms (49.74%), Space: 75.2 MB (13.28%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
@@ -2,28 +2,25 @@ class Solution {
     public int[] fullBloomFlowers(int[][] flowers, int[] people) {
         int[] sortedPeople = Arrays.copyOf(people, people.length);
         Arrays.sort(sortedPeople);
-        
         Arrays.sort(flowers, (a, b) -> Arrays.compare(a, b));
-        Map<Integer, Integer> dic = new HashMap();
-        PriorityQueue<Integer> heap = new PriorityQueue();
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> endTimeheap = new PriorityQueue();
         int i = 0;
-        for (int person : sortedPeople) {
-            while (i < flowers.length && flowers[i][0] <= person) {
-                heap.add(flowers[i][1]);
+        
+        for(int person : sortedPeople) {
+            while(i < flowers.length && flowers[i][0] <= person) {//start date is less than person date
+                endTimeheap.add(flowers[i][1]);//add in the queue
                 i++;
             }
-            
-            while (!heap.isEmpty() && heap.peek() < person) {
-                heap.remove();
+            while(!endTimeheap.isEmpty() && endTimeheap.peek() < person) {//
+                endTimeheap.remove();
             }
-            
-            dic.put(person, heap.size());
+            map.put(person, endTimeheap.size());
         }
         
         int[] ans = new int[people.length];
         for (int j = 0; j < people.length; j++) {
-            ans[j] = dic.get(people[j]);
+            ans[j] = map.get(people[j]);
         }
         
         return ans;
