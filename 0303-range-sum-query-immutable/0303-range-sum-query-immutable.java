606c765f5bb81aee17d8b201966cfddaf0b7e8e7
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-12 19:35:59 -0500

    Time: 8 ms (76.91%), Space: 49 MB (19.07%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,20 +1,17 @@
 class NumArray {
-    int len = 0;
-    int[] preSum;
+    int[] prefix;
+    int[] nums;
     public NumArray(int[] nums) {
-        len = nums.length;
-       this.preSum = nums;
-        int sumFromStart = 0, sumFromEnd = 0;
-        for (int i = 1; i < len ; i++) {
-            preSum[i] =preSum[i] +nums[i - 1];
-           // System.out.print( preSum[i]+" "+ i +" ");
+        prefix = new int[nums.length];
+        prefix[0] = nums[0];
+        for(int i= 1; i<nums.length; i++) {
+           prefix[i] = prefix[i - 1] + nums[i]; 
         }
+        this.nums = nums;
     }
     
     public int sumRange(int left, int right) {
-         if (left == 0) return preSum[right];
-        
-         return preSum[right] - preSum[left-1];
+       return  prefix[right] - prefix[left] + nums[left];
     }
 }
 
@@ -23,19 +20,3 @@ class NumArray {
  * NumArray obj = new NumArray(nums);
  * int param_1 = obj.sumRange(left,right);
  */
-//-2, 0, 3, -5, 2, -1
-//-2,-2, 1, -4, -2, -3
-
-// class NumArray { // 6 ms, faster than 100.00%
-//     int[] preSum;
-//     public NumArray(int[] nums) {
-//         preSum = nums; // pass by pointer!
-//         for (int i = 1; i < preSum.length; ++i)
-//             preSum[i] += preSum[i-1]; 
-//     }
-    
-//     public int sumRange(int left, int right) {
-//         if (left == 0) return preSum[right];
-//         return preSum[right] - preSum[left-1];
-//     }
-// }
