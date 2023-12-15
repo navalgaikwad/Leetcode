4ab7922863acc87e6b8fb7f2a7d491e6b9407f6c
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-15 14:25:18 -0500

    Time: 14 ms (11.13%), Space: 45.6 MB (21.43%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,14 +1,14 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
         //it is same as divide chocolate problrm
-        int left = Arrays.stream(weights).max().getAsInt();
+        int left = Arrays.stream(weights).max().getAsInt();//We may not load more weight than the maximum weight capacity of the ship.
         int right = Arrays.stream(weights).sum();
         int ans = 0;
         while(left<=right) {
             int mid = left + (right - left)/2;
             if(checkCapacity(weights, days, mid)) {
                 ans = mid;
-                right = mid - 1;
+                right = mid - 1;//to check minimum
             }else {
                 left = mid + 1;
             }
@@ -16,8 +16,9 @@ class Solution {
         return ans;
     }
     
+    // it will check capcity and it will create partition it will do untill me minimum answer
     boolean checkCapacity(int[] weights, int days, int mid) {
-        int capacity = 1;
+        int capacity = 1; //kep capacity 1
         int sum =0;
         for(int weight : weights) {
             sum = sum + weight;
