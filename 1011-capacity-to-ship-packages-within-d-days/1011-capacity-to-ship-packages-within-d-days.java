bdf28dc413a72cc2cba6671920ae5fff4a70bb12
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-15 14:28:18 -0500

    Time: 14 ms (11.13%), Space: 45.1 MB (65.30%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,32 +1,43 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        //it is same as divide chocolate problrm
-        int left = Arrays.stream(weights).max().getAsInt();//We may not load more weight than the maximum weight capacity of the ship.
+        // Find the largest single weight as the lower bound (left), because the ship needs to be
+        // able to carry at least the heaviest package.
+        int left = Arrays.stream(weights).max().getAsInt();
+        // Find the sum of all weights as the upper bound (right), because the ship should also
+        // be able to carry all packages at once.
         int right = Arrays.stream(weights).sum();
-        int ans = 0;
-        while(left<=right) {
-            int mid = left + (right - left)/2;
+        int ans = 0; // This will store the final answer.
+
+        // Binary search to find the least weight capacity.
+        while(left <= right) {
+            // Check the middle value between the current bounds.
+            int mid = left + (right - left) / 2;
+            
+            // Check if the current capacity (mid) allows us to ship within days.
             if(checkCapacity(weights, days, mid)) {
-                ans = mid;
-                right = mid - 1;//to check minimum
-            }else {
-                left = mid + 1;
+                ans = mid; // If it's possible, this could be our answer.
+                right = mid - 1; // Try to find a smaller capacity that still works.
+            } else {
+                left = mid + 1; // If not possible, we need a larger capacity.
             }
         }
-        return ans;
+        return ans; // Return the least weight capacity found.
     }
     
-    // it will check capcity and it will create partition it will do untill me minimum answer
+    // Helper method to check if the current capacity allows shipping within the given days.
     boolean checkCapacity(int[] weights, int days, int mid) {
-        int capacity = 1; //kep capacity 1
-        int sum =0;
+        int capacity = 1; // Start with the first day.
+        int sum = 0; // This will accumulate the weight of packages for the current day.
+
         for(int weight : weights) {
-            sum = sum + weight;
+            sum += weight; // Add package weight to the current day's load.
+            // If the current day's load exceeds the capacity, we need to use an additional day.
             if(sum > mid) {
-                capacity++;
-                sum = weight;
+                capacity++; // Increment the day count.
+                sum = weight; // Start the next day with the current package.
             }
         }
-        return capacity<= days;
+        // Return true if the number of days needed is less than or equal to the allowed days.
+        return capacity <= days;
     }
 }
