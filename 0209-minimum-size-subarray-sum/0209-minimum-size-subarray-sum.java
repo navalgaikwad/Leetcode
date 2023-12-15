b31df949ae65c1e5ba025aa4584880adb8db4b80
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-15 15:33:05 -0500

    Time: 3 ms (8.28%), Space: 54.4 MB (51.83%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,21 +1,43 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left =0;
-        int len = nums.length;
-        int sum = 0;
-        int result = Integer.MAX_VALUE;
-        for(int right =0; right < len; right++) {
-            sum = sum + nums[right];
-            while(sum >= target) {
-                result = Math.min(result, right - left + 1);
-                sum = sum - nums[left];
-                left++;
+        int n = nums.length;
+        int result = n + 1; // Initialize result to max possible value + 1
+        int left = 1; // Minimum possible length
+        int right = n; // Maximum possible length
+
+        // Binary search for the minimum length
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isSubArrayExists(nums, mid, target)) {
+                result = mid; // Update result with the new minimum length
+                right = mid - 1; // Try to find a smaller length
+            } else {
+                left = mid + 1; // Try to find a valid larger length
             }
-//             if(sum == target) {
-                 
-//             }
-           
         }
-        return result == Integer.MAX_VALUE ? 0 : result;
+
+        // If result is still n + 1, it means we didn't find any subarray
+        return result == n + 1 ? 0 : result;
+    }
+
+    private boolean isSubArrayExists(int[] nums, int length, int target) {
+        int sum = 0;
+        // Calculate the sum of the first 'length' elements
+        for (int i = 0; i < length; i++) {
+            sum += nums[i];
+        }
+        // If the sum is already greater than or equal to the target, we found a subarray
+        if (sum >= target) {
+            return true;
+        }
+        // Slide the window of 'length' elements and check the sums
+        for (int i = length; i < nums.length; i++) {
+            sum = sum - nums[i - length] + nums[i]; // Update the sum to reflect the window slide
+            if (sum >= target) {
+                return true; // We found a subarray
+            }
+        }
+        // No subarray of this length satisfies the condition
+        return false;
     }
 }
