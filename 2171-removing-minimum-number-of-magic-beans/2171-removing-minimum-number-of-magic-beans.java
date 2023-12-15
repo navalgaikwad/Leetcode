acdf5fbebbe0b402763b915fb8363ffee461a224
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-15 17:14:51 -0500

    Time: 36 ms (47.24%), Space: 57.7 MB (40.94%) - LeetHub

--- 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.java
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.java
@@ -1,15 +1,27 @@
-
 class Solution {
     public long minimumRemoval(int[] beans) {
-        long len = beans.length;
-        long ans = Long.MAX_VALUE, sum=0;
-
+        long len = beans.length, sum=0;
         for(int bean:beans) sum+=bean;
         Arrays.sort(beans);
+        long result = Long.MAX_VALUE;
         
-        for(int i=0; i<len; i++){
-            ans = Math.min(ans, sum-(len-i)*beans[i]);
+        for(int i =0;i<len; i++) {
+           result = Math.min(result, sum - (len - i)* beans[i]) ;
         }
-        return ans;
+        return result;
     }
 }
+// class Solution {
+//     public long minimumRemoval(int[] beans) {
+//         long len = beans.length;
+//         long ans = Long.MAX_VALUE, sum=0;
+
+//         for(int bean:beans) sum+=bean;
+//         Arrays.sort(beans);
+        
+//         for(int i=0; i<len; i++){
+//             ans = Math.min(ans, sum-(len-i)*beans[i]);
+//         }
+//         return ans;
+//     }
+// }
