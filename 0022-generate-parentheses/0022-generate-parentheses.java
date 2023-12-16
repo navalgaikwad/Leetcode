4bfe71388c825e7e1301f6aeaf935992f9706884
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-16 18:08:18 -0500

    Time: 1 ms (88.73%), Space: 43.8 MB (23.79%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,23 +1,23 @@
 class Solution {
     List<String> result = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        StringBuilder sb = new StringBuilder();
-        helper(n, 0, 0, sb);
+       
+        helper(n, 0, 0, "");
         return result;
     }
     
-    void helper(int n, int open, int close, StringBuilder sb) {
+    void helper(int n, int open, int close, String sb) {
         if(n*2 == open+close) {
             result.add(sb.toString());
             return;
         }
         if(open < n) {
-           helper(n , open + 1,close, sb.append("("));
-            sb.deleteCharAt(sb.length() - 1); // backtrack
+           helper(n , open + 1,close, sb + "(");
+            //sb.deleteCharAt(sb.length() - 1); // backtrack
         } 
         if(close < open){
-           helper(n, open, close+1, sb.append(")"));
-           sb.deleteCharAt(sb.length() - 1); // backtrack
+           helper(n, open, close+1, sb + ")");
+           //sb.deleteCharAt(sb.length() - 1); // backtrack
         }
         
     }
