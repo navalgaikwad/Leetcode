df69e50be2b7122e8ba9b20405ded6bf2399f76f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-19 16:16:17 -0500

    Time: 9 ms (18.01%), Space: 42.7 MB (26.59%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -9,24 +9,31 @@ class Solution {
         int len = s.length();
         if(part == 4 || i == len) {
             if(part == 4 && i == len) {
-                result.add(ans.substring(0, ans.length()-1)); 
+                result.add(ans.substring(0, ans.length() - 1)); 
             }
             return;
         }
-        backTracking(s, i+1,ans+s.charAt(i)+".", part+1, result);  
-        if(i+2 <= len && helper(s.substring(i, i + 2))) { //divided in 2 part size of 2 that means
-            backTracking(s, i + 2, ans+s.substring(i, i+2)+".", part+1, result);
+        //for single value
+        backTracking(s, i + 1, ans + s.charAt(i)+".", part + 1, result);  
+        if(i + 2 <= len && helper(s.substring(i, i + 2))) { //divided in 2 part size of 2 that means
+            backTracking(s, i + 2, ans + s.substring(i, i + 2)+".", part + 1, result);
         }
-        if(i+3 <= len && helper(s.substring(i, i + 3))) {//divided in 3 part size of 3 that means
-            backTracking(s, i + 3, ans+s.substring(i, i+3)+".", part+1, result);
+        if(i + 3 <= len && helper(s.substring(i, i + 3))) {//divided in 3 part size of 3 that means
+            backTracking(s, i + 3, ans + s.substring(i, i + 3)+".", part + 1, result);
         }  
     }
     
     
     boolean helper(String s) {
-        if(s.charAt(0)=='0') return false;
+        if(s.charAt(0) == '0') return false;
         return Integer.parseInt(s) <= 255;
     }
 }
 //25525511135
 
+//for single
+//i + 1 : 1
+//i + 2 : for 2 value
+//i + 3 : for 3 value
+//use string rather than stringbuilder
+//ans append to s.substring
