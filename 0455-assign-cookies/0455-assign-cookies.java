1af6fcc5e419c359c1de678fb5160e3fc68382eb
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-18 22:56:45 -0500

    Time: 9 ms (37.37%), Space: 44.8 MB (19.51%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(s);
+        Arrays.sort(g);
+        //g[10,9,8,7]
+        //s[5,6,7,8]
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        while(left <g.length && right<s.length) {
+            if(s[right]>=g[left]) {
+                count++;
+                left++;
+            }
+            
+            right++;
+        }
+        return count;
+    }
+}
+
+// g = [1,2,3], s = [1,1] 
+//minimum size 
+//1 child 1
