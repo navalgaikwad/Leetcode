90e23edac5c3e57020ddd22ddc4df3c1638d7008
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-19 15:07:21 -0500

    Time: 5 ms (6.25%), Space: 43.7 MB (38.80%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+       
+        int j =0;
+        int[] result =  Arrays.copyOf(prices, prices.length);
+        for(int i =0; i<prices.length; i++) {
+            while(!stack.isEmpty() && prices[stack.peek()]>=prices[i]) {
+                result[stack.peek()] = Math.abs(prices[stack.peek()] - prices[i]);
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
+
+//i//1 - price 8 j 1
+//i//2 - price 4 j 2
+//i//3 - price 6 j 3
+//i//4 - price 2 j 4
+//i//5 - price 3 j 5
+    
+//i//1 - price 8 j 1
+//i//2 - price 4 j 2  j>i
+
+//8,4,6 
+//4,4,6
+//4, 
+//4, 4, 6
+//1. motha ala ki kahi karu noko, ani same ala ki
+//2.bakicha vwli minus kar
+//[8,4,6,2,3]
+//2
+//4, 2, 4,2, 3
+//[2]
+//4,2, 4, 2 
