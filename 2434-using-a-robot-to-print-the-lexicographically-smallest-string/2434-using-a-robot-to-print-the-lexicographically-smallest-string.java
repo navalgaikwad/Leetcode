6cb934b562f7627bc866ba8887a7a12b4762fa3e
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-20 13:08:17 -0500

    Time: 194 ms (10.46%), Space: 55.2 MB (6.98%) - LeetHub

--- 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.java
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -1,37 +1,30 @@
-import java.util.Stack;
-
-public class Solution {
+class Solution {
     public String robotWithString(String s) {
         int[] frequency = new int[26];
-        for (int i = 0; i < s.length(); i++) {
+        for(int i =0; i<s.length(); i++) {
             frequency[s.charAt(i) - 'a']++;
         }
-
-        StringBuilder result = new StringBuilder();
+        
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            stack.push(ch);
-            frequency[ch - 'a']--;
-
-            while (!stack.isEmpty() && isSmallestCharacter(stack.peek(), frequency)) {
+        StringBuilder result= new StringBuilder();
+        for(int i =0;i<s.length(); i++) {
+            stack.push(s.charAt(i));
+            frequency[s.charAt(i) - 'a']--;
+            
+            while(!stack.isEmpty() && helper(stack.peek(), frequency)) {
                 result.append(stack.pop());
             }
         }
-
-        while (!stack.isEmpty()) {
-            result.append(stack.pop());
-        }
-
         return result.toString();
     }
-
-    private boolean isSmallestCharacter(char ch, int[] frequency) {
-        for (char c = 'a'; c < ch; c++) {
-            if (frequency[c - 'a'] > 0) {
+    
+    boolean helper(char s, int[] frequency) {
+        for( int i = 'a'; i<s; i++) {
+            if(frequency[ i - 'a']>0) {
                 return false;
             }
         }
         return true;
     }
+    
 }
