18503532ef9532450d42f9cf419630bb9f79db2a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-20 23:24:59 -0500

    Time: 42 ms (10.71%), Space: 45.8 MB (5.36%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,21 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        int[] degree = new int[n];
-        boolean connected[][] = new boolean[n][n];
-        for(int road[]:roads){
-            degree[road[0]]++;//
-            degree[road[1]]++;
-            connected[road[0]][road[1]] =true;
-            connected[road[1]][road[0]]=true;
+        Set<String> roadSet = new HashSet<>();
+        int[] degrees = new int[n];
+        
+        for (int[] road : roads) {
+            int a = road[0];
+            int b = road[1];
+            roadSet.add(a + "-" + b);
+            roadSet.add(b + "-" + a);
+            degrees[a]++;
+            degrees[b]++;
         }
-        int result = 0;
-        for(int i =0; i< n; i++) {
-            for(int j = i + 1; j<n; j++) { // to avoid duplicate 
-                int sum = degree[i] + degree[j] - (connected[i][j]?1:0);
-                result = Math.max(result, sum);
+
+        int rank = 0;
+        for (int a = 0; a < n; a++) {
+            for (int b = a + 1; b < n; b++) {
+                int currRank = degrees[a] + degrees[b];
+                if (roadSet.contains(a + "-" + b)) {
+                    currRank--;
+                }
+                rank = Math.max(rank, currRank);
             }
         }
-        return result;
-        
+
+        return rank;
     }
 }
