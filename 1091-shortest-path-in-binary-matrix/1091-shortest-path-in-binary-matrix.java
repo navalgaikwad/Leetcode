464a1ebad4c451c4dc37fa290c3dba61964be17e
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-22 16:09:21 -0500

    Time: 17 ms (56.29%), Space: 45.7 MB (10.05%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -6,23 +6,33 @@ class Solution {
     
     int bfs(int[][] grid, int i, int j) {
        int[][] direction = new int[][]{{0,-1}, {-1,0}, {1,1}, {-1, -1}, {1, 0}, {0, 1}, {-1, 1}, {1, -1}};
+        
         int len = grid.length;
+        
         boolean[][] visited = new  boolean[len][len];
         Queue<int[]> q = new LinkedList<>();
+        
         q.add(new int[]{i, j, 1});
         visited[i][j] = true; 
+        
         while(!q.isEmpty()) {
+            
             int[] current = q.remove();
+            
             int row = current[0];
             int col = current[1];
             int level =current[2];
-            if(current[0] == len - 1 && current[1] == len -1) {
+            
+            if(row == len - 1 && col == len -1) {
                 return level;
             }
+            
             for(int[] dir : direction) {
+                
                 int newX = row + dir[0];
                 int newY = col + dir[1];
-                if(newX>=0 && newX<len && newY>=0 && newY<grid[0].length && !visited[newX][newY] && grid[newX][newY]!=1) {
+                
+                if(newX >= 0 && newX < len && newY >= 0 && newY < grid[0].length && !visited[newX][newY] && grid[newX][newY] == 0) {
                     visited[newX][newY] = true;
                     q.add(new int[]{newX, newY, level + 1});
                 }
