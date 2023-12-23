20de8ae4c97f984de5f80204c0c1f92b1b53c168
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-22 19:31:36 -0500

    Time: 12 ms (27.30%), Space: 45.3 MB (5.14%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,61 +1,60 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
-      int n = board.length;
-      int target = n * n;
-      Integer[] columns = new Integer[n];
-      for(int i = 0; i < n; i++){
-          columns[i] = i;
-      }
+        int n = board.length;
+        int target = n * n;
+        Integer[] columns = new Integer[n];
         
-      int label = 1;
-      Pair<Integer, Integer> cell[] = new Pair[ n * n + 1 ];
-      for(int row = n - 1; row >=0; row--){
-          for(int col : columns){
-              
-            //System.out.print(label+" label, "+row +" row, "+col+" col ");
-            cell[ label++ ] = new Pair<>( row, col ); 
-              
-          }
-           Collections.reverse(Arrays.asList(columns));
-      }
+        //most important 
+        //first define columns array
+        for(int i =0 ;i< n; i++) {
+            columns[i] = i; // add the value in the columns
+        }
         
-        Set<Integer> visited = new HashSet<>();
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.add(new int[]{ 1, 0 }); //start from one
-        
-        //cell is processed and board have value
-        while( !q.isEmpty() ){
-            
-            int[] current = q.remove();
-            int currentStep = current[0]; //1
-            int level = current[1];
-            
-            if(currentStep == target){
-                return level;
+        int label = 1;
+        //n * n + 1 total element in the array
+        Pair<Integer, Integer> cell[] = new Pair[n * n + 1];// use pair defined as Key value in array
+        for(int row = n -1; row >=0; row--) {// start from the end so n -1
+            for(int col : columns) { // start from first
+                cell[label++] = new Pair<>(row, col); // at each level we will be giving marking
             }
+            Collections.reverse(Arrays.asList(columns));// reverse it
+        }
     
-            
-            for( int i = 1; i < 7; i++ ){ //roll the dice 6 time i.e 1 to 6
+    Set<Integer> visited = new HashSet<>();
+    Queue<int[]> q = new LinkedList<>();
+    q.add(new int[]{1, 0}); //start from one rather 0 it is given in the problem
+    
+    while(!q.isEmpty()) {
+        int[] current = q.remove();
+        int currentStep = current[0];
+        int level = current[1];
+        if(currentStep == target) {
+            return level;
+        }
+        
+        for (int i =1; i<7; i++) { //it will move 7 times 0 to 6
+            int nextPosition = i + currentStep;
+            if(nextPosition <= target) {
+                int nextRow = cell[nextPosition].getKey(); // get the value form cell row 5
+                int nextCol = cell[nextPosition].getValue();// col 0
+                int destination = ( board[nextRow][nextCol] != -1 ) ? board[nextRow][nextCol] : nextPosition;
                 
-              int nextLabel = currentStep + i; //next label i.e position 1 to 6 no from dice
-                //1 + 1 = 2
-                  if( nextLabel <= target ){ //2 <37
-
-                      int row = cell[nextLabel].getKey(); // get the value form cell row 5  
-                      int col = cell[nextLabel].getValue();// col 0
-
-                      int destination = ( board[row][col] != -1 ) ? board[row][col] : nextLabel;
-
-                     if(!visited.contains(destination)){
+                  if(!visited.contains(destination)){
                          
                          visited.add( destination );
                          q.add(new int[]{ destination , level + 1 });
                          
-                     } 
-                }     
+                } 
             }
-       }
-     return -1;
+        }
+    }
+    return -1;
     }
 }
+
+// [[-1,-1,-1,-1,-1,-1],
+//  [-1,-1,-1,-1,-1,-1],
+//  [-1,-1,-1,-1,-1,-1],
+//  [-1,35,-1,-1,13,-1],
+//  [-1,-1,-1,-1,-1,-1],
+//  [-1,15,-1,-1,-1,-1]]
