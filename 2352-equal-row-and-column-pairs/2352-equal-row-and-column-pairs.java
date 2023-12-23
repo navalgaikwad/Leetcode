0d0cf67177d3300dd6fdffc2bc43e26563fa948c
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-23 01:55:22 -0500

    Time: 33 ms (64.18%), Space: 49.2 MB (57.77%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,31 +1,4 @@
-// import java.util.*;
 
-// class Solution {
-//     public int equalPairs(int[][] grid) {
-//         Map<String, Integer> map = new HashMap<>();
-//         for (int row = 0; row < grid.length; row++) {
-//             StringBuilder value = new StringBuilder();
-//             for (int col = 0; col < grid[0].length; col++) {
-//                 value.append(grid[row][col]);
-//             }
-//             map.put(value.toString(), map.getOrDefault(value.toString(), 0) + 1);
-//         }
-        
-//         int n = grid[0].length;
-//         int count = 0;
-//         for (int col = 0; col < n; col++) {
-//             StringBuilder colValue = new StringBuilder();
-//             for (int row = 0; row < n; row++) {
-//                 colValue.append(grid[row][col]);
-//             }
-//             count += map.getOrDefault(colValue.toString(), 0);
-//         }
-        
-//         return count;
-//     }
-// }
-import java.util.HashMap;
-import java.util.Map;
 class Solution {
     public int equalPairs(int[][] grid) {
         int count = 0;
@@ -38,7 +11,7 @@ class Solution {
             String rowString = Arrays.toString(row);
             rowCounter.put(rowString,  rowCounter.getOrDefault(rowString, 0) + 1);
         }
-        System.out.print(rowCounter);
+       // System.out.print(rowCounter);
         // Add up the frequency of each column in the map.
         for (int c = 0; c < n; c++) 
         {
