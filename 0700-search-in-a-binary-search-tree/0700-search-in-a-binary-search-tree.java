a60e89e1475b9a6f9d7cd1696955c7fdd0670bd0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-24 14:56:31 -0500

    Time: 0 ms (100.00%), Space: 45.2 MB (6.91%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        //TreeNode result = null;
+        if(root == null ) {
+            return null;
+        }
+        if(root.val == val) {
+             //root;
+            return root;
+        }
+      
+         TreeNode left = searchBST(root.left, val);
+         TreeNode right = searchBST(root.right, val);
+        if(left == null) {
+            return right;
+        }else if(right == null) {
+            return left;
+        }
+        return root;
+    }
+}
