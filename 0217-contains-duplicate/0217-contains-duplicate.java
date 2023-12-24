538aae9c001334ec7f8791d335b2645f8833c33f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-24 12:36:46 -0500

    Time: 20 ms (23.40%), Space: 55 MB (76.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums) {
-            if(set.contains(num)) {
-                return true;
-            }
-            set.add(num);
+        Arrays.sort(nums); 
+        for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == nums[i+1]) {
+                    return true;
+                }
         }
         return false;
     }
