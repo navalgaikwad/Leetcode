a467bd4c6b0e799264d4bbae37fb13b7ce717a7a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-23 20:18:20 -0500

    Time: 6 ms (97.81%), Space: 56.5 MB (27.59%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,22 +1,29 @@
-public class Solution {
+class Solution {
     public int numEnclaves(int[][] grid) {
         int result = 0;
         int m = grid.length;
         int n = grid[0].length;
-
-        for (int i = 0; i < m; i++) {
-            dfs(grid, i, 0);
-            dfs(grid, i, n - 1);
+        
+        if(grid.length == 1 || grid[0].length == 1) {
+            return 0;
         }
         
-        for (int j = 0; j < n; j++) {
-            dfs(grid, 0, j);
-            dfs(grid, m - 1, j);
+        
+        for(int i = 0; i<m; i++) {
+            dfs(grid, i, 0); 
+            dfs(grid, i, n - 1); 
         }
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
+        for(int i = 0; i<n; i++) {
+            dfs(grid, 0, i); 
+            dfs(grid, m - 1, i); 
+            
+        }
+         
+        
+        for(int i = 0 ;i < m; i++) {
+            for(int j =0; j < n; j++) {
+                if(grid[i][j] == 1) {
                     result++;
                 }
             }
@@ -25,15 +32,50 @@ public class Solution {
         return result;
     }
    
-    private void dfs(int[][] grid, int i, int j) {
-        
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+    void dfs(int[][] grid, int i, int j) {
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]==0) {
             return;
         }
         grid[i][j] = 0;
+        //visited[i][j] = true;
         dfs(grid, i + 1, j);
         dfs(grid, i, j + 1);
         dfs(grid, i - 1, j);
         dfs(grid, i, j - 1);
     }
 }
+
+// 0 0 0 0 0 0 0 0 0 0 1
+// 0 0 0 0 0 1 0 0 0 0 0 
+// 0 0 0 0 0 0 0 0 0 0 0 
+// 0 0 0 0 0 0 0 0 0 0 1 
+// 0 0 0 0 0 0 0 0 1 0 0 
+// 0 0 0 0 0 0 0 0 0 0 0 
+// 0 0 0 0 0 0 0 0 1 0 0 
+// 0 0 0 0 0 0 1 1 1 0 0 
+// 0 0 0 0 0 0 0 0 0 0 0 
+// 0 0 0 0 0 0 0 0 0 0 1 
+
+// 0 0 0 0 0 0 0 0 0 0 0 
+// 0 0 0 0 0 1 0 0 0 0 0 
+// 0 0 0 0 0 0 0 0 0 1 0 
+// 0 0 0 0 0 0 0 0 0 0 0 
+// 0 0 0 0 0 0 0 0 1 0 0 
+// 0 0 0 0 0 0 0 0 0 0 0 
+// 0 0 0 0 0 0 0 0 1 0 0 
+// 0 0 0 0 0 0 1 1 1 0 0 
+// 0 0 0 0 0 0 0 0 0 0 0 
+// 0 0 0 0 0 0 0 1 0 0 0 
+
+
+//[
+//[0,0,1,1,1,0,1,1,1,0,1],
+//[1,1,1,1,0,1,0,1,1,0,0],
+//[0,1,0,1,1,0,0,0,0,1,0],
+//[1,0,1,1,1,1,1,0,0,0,1],
+//[0,0,1,0,1,1,0,0,1,0,0],
+//[1,0,0,1,1,1,0,0,0,1,1],
+//[0,1,0,1,1,0,0,0,1,0,0],
+//[0,1,1,0,1,0,1,1,1,0,0],
+//[1,1,0,1,1,1,0,0,0,0,0],
+//[1,0,1,1,0,0,0,1,0,0,1]]
