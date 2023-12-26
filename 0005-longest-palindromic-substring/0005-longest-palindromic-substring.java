72b68ef7c35a0113ec2b5b68e47eb17541bcedfa
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-26 13:38:07 -0500

    Time: 19 ms (62.49%), Space: 45 MB (20.81%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,25 @@
 class Solution {
     public String longestPalindrome(String s) {
         String result = "";
-        int resultLength = 0;
+        int maxLength = 0;
         int len = s.length();
-        for(int i =0; i<s.length(); i++) {
+        for(int i =0;i < s.length(); i++) {
             int left = i;
             int right = i;
-          //from each i check how much we can extend
-            while(left>=0 && right< len && s.charAt(left)== s.charAt(right)) {
-                 if(right - left + 1 > resultLength) {//need to find longest
-                    result = s.substring(left, right+1);// update the longest 
-                    resultLength = right - left + 1;
+            while( left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
+                if(right - left+1  > maxLength) {
+                   result = s.substring(left, right + 1);
+                   maxLength = right - left + 1; 
                 }
                 left--;
                 right++;
             }
-            
             left = i;
-            right = i+1;
-            while(left>=0 && right< len && s.charAt(left)== s.charAt(right)) {
-                 if(right - left + 1 > resultLength) {//need to find longest
-                    result = s.substring(left, right+1);// update the longest 
-                    resultLength = right - left + 1;
+            right = i + 1;
+            while(left >= 0  && right < len && s.charAt(left) == s.charAt(right)) {
+                if(right - left +1  > maxLength) {
+                   result =  s.substring(left, right + 1);
+                   maxLength = right - left + 1; 
                 }
                 left--;
                 right++;
