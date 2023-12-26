f5776330afadd1a817541757eb2eb2310ea23791
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-26 12:17:12 -0500

    Time: 9 ms (22.77%), Space: 44.8 MB (6.78%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,16 +1,15 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        int result  =0;
+        int left =0 ;
         Map<Character, Integer> map = new HashMap<>();
-        int left = 0;
-
-        int result =0;
-        for(int right =0; right < s.length(); right++) {
+        for(int right = 0; right<s.length(); right++) {
             map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
-            while(map.get(s.charAt(right))>1) {
-                 map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) - 1);
-                if(map.get(s.charAt(left)) == 0) {
-                    map.remove(s.charAt(left));
-                }
+            while(right<s.length()&& map.get(s.charAt(right))>1) {
+               map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) - 1);
+               if( map.get(s.charAt(left))==0) {
+                  map.remove(s.charAt(left));
+               }
                 left++;
             }
             result = Math.max(result, right - left + 1);
@@ -18,3 +17,6 @@ class Solution {
         return result;
     }
 }
+
+//define a map
+//when ever i get character value greater than 2 i will decrement window size
