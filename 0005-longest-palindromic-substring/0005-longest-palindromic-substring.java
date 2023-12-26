9143f497bc4093d296c4fafe67ff2ac28e96e45b
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-26 13:36:40 -0500

    Time: 20 ms (57.92%), Space: 45.4 MB (15.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,25 +1,25 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String result ="";
+        String result = "";
         int resultLength = 0;
         int len = s.length();
-        for(int i =0; i < len; i++) {
+        for(int i =0; i<s.length(); i++) {
             int left = i;
             int right = i;
-            //odd size
-            while(left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
-                if(right - left + 1 > resultLength) {//need to find longest
-                    result = s.substring(left, right+1);
+          //from each i check how much we can extend
+            while(left>=0 && right< len && s.charAt(left)== s.charAt(right)) {
+                 if(right - left + 1 > resultLength) {//need to find longest
+                    result = s.substring(left, right+1);// update the longest 
                     resultLength = right - left + 1;
                 }
                 left--;
                 right++;
             }
-            //even side
-            left = i; 
-            right = i + 1; //character might even for odd so need to check for both
-            while(left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
-                if(right - left + 1 > resultLength) {//need to find longest
+            
+            left = i;
+            right = i+1;
+            while(left>=0 && right< len && s.charAt(left)== s.charAt(right)) {
+                 if(right - left + 1 > resultLength) {//need to find longest
                     result = s.substring(left, right+1);// update the longest 
                     resultLength = right - left + 1;
                 }
@@ -28,11 +28,5 @@ class Solution {
             }
         }
         return result;
-        
     }
 }
-//no of subtring n*n -1 //2 so brute force it takes O(n3)
-//1. Take one charatcer and check in i and j side, do we maximum value lenght
-//2. check for even side
-//3. check for odd side
-//4. return the result who ever gives best answer give it
