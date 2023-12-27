0200f931290ab6ff3b5b022070e27df97c47d9ab
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-27 18:05:01 -0500

    Time: 7 ms (37.86%), Space: 41.2 MB (30.41%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,18 +1,18 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        if(n == 1 && k == 1) {
+        if(n == 1 && k == 1) { //handling the edge condition
             return 1;
         }
         List<Integer> q = new LinkedList<>();
         for(int i = 1; i<= n; i++) {
             q.add(i);
-        }
-        int i = 0; //make it global 
+        }// added all the numbers
+        int i = 0; //make it global so we play with i to find next index
         k = k - 1;// reduce it by 1
         while(!q.isEmpty()) {
             i = (i + k) % q.size(); //based on size and find the index
             q.remove(i);
-            if(q.size() == 1) {
+            if(q.size() == 1) { // we need size is equal to one than break it
                 break;
             }
         }
