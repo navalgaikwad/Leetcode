f503809355a879285811cca290bcb5cae9a29718
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-27 14:02:28 -0500

    Time: 468 ms (5.02%), Space: 223.9 MB (5.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -8,15 +8,16 @@ class Solution {
         if(memo.containsKey(key)) {
            return memo.get(key); 
         }
-        if(i<0 || j<0) {
+        if(i < 0 || j < 0) {
             return 0;
         }
         if(text1.charAt(i) == text2.charAt(j)) {
             return 1 + dp(text1, text2, i - 1, j - 1, memo);
         }
-        
+        // if not matching reduce index of text1 to -1 and reduce index of text2 by -1
         int result = Math.max(dp(text1, text2, i - 1, j, memo), dp(text1, text2, i, j-1, memo));
         memo.put(key, result);
         return result;
     }
 }
+//https://leetcode.com/problems/longest-common-subsequence/discuss/3541424/recursion-memoization-tabulation/
