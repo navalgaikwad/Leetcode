1fbe462c682f5c632a0aed4ae4d0bf106c7977f1
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-27 19:17:15 -0500

    Time: 1606 ms (5.03%), Space: 45.3 MB (14.17%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int k = p.length();
+        int left = 0;
+        List<Integer> result = new ArrayList<>();
+        for(int right = 0; right <= s.length() - k; right++) {
+            String sub = s.substring(left, right + k);
+            if(checkAnagram(sub, p)) {
+                result.add(left);
+            }
+            left++;
+        }
+        return result;
+    }
+    
+    boolean checkAnagram(String sub, String p) {
+        // Frequency arrays for comparison
+        int[] subCount = new int[26];
+        int[] pCount = new int[26];
+
+        // Populate the frequency arrays
+        for (int i = 0; i < sub.length(); i++) {
+            subCount[sub.charAt(i) - 'a']++;
+            pCount[p.charAt(i) - 'a']++;
+        }
+
+        // Compare the frequency arrays
+        for (int i = 0; i < 26; i++) {
+            if (subCount[i] != pCount[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
