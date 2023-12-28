6ed9366159d7b96ca434e1be6c75d09692be7534
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-27 21:27:45 -0500

    Time: 96 ms (5.20%), Space: 42.4 MB (29.48%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -32,6 +32,7 @@ class Solution {
  
       // Update the Value of ans and Increment i by 1
         ans = (ans + k) % i;
+        System.out.println(ans +"-"+ k +"-" + i);
         i++;
        }
  
@@ -39,3 +40,34 @@ class Solution {
     return ans + 1;
     }
 }
+//https://www.geeksforgeeks.org/josephus-problem/
+//n = 5
+// k = 2
+
+// ans = 0  # Initial value before the loop starts
+
+// # Loop starts
+// for i in range(1, n + 1):
+//     ans = (ans + k) % i
+
+// # Iteration 1:
+// #   i = 1, ans = (0 + 2) % 1 = 0
+
+// # Iteration 2:
+// #   i = 2, ans = (0 + 2) % 2 = 0
+
+// # Iteration 3:
+// #   i = 3, ans = (0 + 2) % 3 = 2
+
+// # Iteration 4:
+// #   i = 4, ans = (2 + 2) % 4 = 0
+
+// # Iteration 5:
+// #   i = 5, ans = (0 + 2) % 5 = 2
+
+// # Loop ends
+
+// # Final result
+// result = ans + 1  # ans = 2, so result = 3
+
+// print(result)
