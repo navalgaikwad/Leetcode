cfe91076dc5e48073f5c5c33949c0d22329d999a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-28 17:39:48 -0500

    Time: 10 ms (6.42%), Space: 44.7 MB (9.89%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,8 +1,5 @@
 class Solution {
     public int romanToInt(String s) {
-        int prev =0;
-        int total = 0;
-        int len = s.length() - 1;
         Map<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
@@ -11,16 +8,18 @@ class Solution {
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
-        for( int i =len; i>=0; i--) {
-            char c = s.charAt(i);
-            int value = map.get(c);
-            if( value < prev) {//5
-                total = total - value;
+        int len = s.length() -1;
+        int prev = Integer.MAX_VALUE;
+        int result =0;
+        for(int i = len; i>=0; i--) {
+            int value  = map.get(s.charAt(i));
+            if(prev!=Integer.MAX_VALUE && value < prev) {
+                result = result - value;
             }else {
-               total = total + value; 
+                result = result + value;
             }
             prev = value;
         }
-        return total;
+        return result;
     }
 }
