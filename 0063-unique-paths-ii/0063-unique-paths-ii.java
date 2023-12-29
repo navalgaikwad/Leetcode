b8bbf85be5b936864b85727ec31a9babdcf87acf
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-29 17:28:17 -0500

    Time: 1 ms (24.07%), Space: 41.8 MB (7.42%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,23 +1,49 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-       int result = dfs(obstacleGrid, obstacleGrid.length - 1, obstacleGrid[0].length - 1, new HashMap<>());
-       return result; 
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = 1;
+        for(int i = 0; i < row; i++){
+            for(int j =0; j < col; j++){
+                if(grid[i][j]==1){
+                 dp[i][j] =0; 
+                }else {
+                if(grid[i][j] != 1){
+                   if(i +1 < row){
+                       dp[i+1][j] += dp[i][j];
+                   }
+                    if(j + 1 < col){
+                        dp[i][j+1] +=dp[i][j];
+                    }
+                }
+              }
+            } 
+            
+        }
+        return dp[row-1][col-1];
     }
-    
-    int dfs(int[][] grid, int i, int j, Map<String, Integer> memo) {
-        String key = i+"-"+j;
-        if(memo.containsKey(key)) {
-            return memo.get(key);
-        }
-        if(i == 0 && j == 0 && grid[i][j] == 0) {//last no obstacle when  i == 0 && j == 0 && grid[i][j] == 0 then return 1;
-            return 1;
-        }
-        if( i< 0 || j < 0 || grid[i][j] == 1) { // if anywhere you find the i<0 or j <0 or grid[i][j] == 1 return 0
-            return 0;
-        }
-        int result = dfs(grid, i - 1, j, memo) + dfs(grid, i, j - 1, memo);//this is main we need to go down i -1 and j -1 which will evetaully give the answer
-        memo.put(key, result);
-        return result;
-    }
-    
 }
+// class Solution {
+//     public int uniquePathsWithObstacles(int[][] grid) {
+//         int row = grid.length;
+//         int col = grid[0].length;
+//         int[][] dp = new int[row][col];
+       
+//         dp[0][0] = 1;
+//         for(int i=0; i<row; i++){
+//             for(int j=0; j<col; j++){
+//                 if(grid[i][j]==1){
+//                  dp[i][j] =0; 
+//                 }else{
+//                     if( i+1<row ){
+//                     dp[i+1][j] +=  dp[i][j];
+//                 } if(j +1<col){
+//                      dp[i][j+1] +=  dp[i][j];
+//                 }
+//               }
+//             }
+//         }
+//         return dp[row-1][col-1];
+//     }
+// }
