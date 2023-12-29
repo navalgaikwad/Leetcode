03cf94f07cf2ca34e4c01c5af30561419e602e82
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-29 17:14:13 -0500

    Time: 14 ms (6.29%), Space: 42.6 MB (5.75%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-       int result = dfs(obstacleGrid, obstacleGrid.length -1, obstacleGrid[0].length-1, new HashMap<>());
+       int result = dfs(obstacleGrid, obstacleGrid.length - 1, obstacleGrid[0].length - 1, new HashMap<>());
        return result; 
     }
     
@@ -9,13 +9,13 @@ class Solution {
         if(memo.containsKey(key)) {
             return memo.get(key);
         }
-        if(i == 0 && j == 0 && grid[i][j]==0) {
+        if(i == 0 && j == 0 && grid[i][j] == 0) {//last no obstacle when  i == 0 && j == 0 && grid[i][j] == 0 then return 1;
             return 1;
         }
-        if( i< 0 || j < 0 || grid[i][j]==1) {
+        if( i< 0 || j < 0 || grid[i][j] == 1) { // if anywhere you find the i<0 or j <0 or grid[i][j] == 1 return 0
             return 0;
         }
-        int result = dfs(grid, i - 1, j, memo) + dfs(grid, i, j - 1, memo);
+        int result = dfs(grid, i - 1, j, memo) + dfs(grid, i, j - 1, memo);//this is main we need to go down i -1 and j -1 which will evetaully give the answer
         memo.put(key, result);
         return result;
     }
