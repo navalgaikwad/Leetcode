b7b262e0b2158b76c5f447e802b665832355abc9
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-28 21:01:30 -0500

    Time: 1199 ms (5.14%), Space: 82.5 MB (6.43%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,33 +1,33 @@
 class Solution {
     public int minInsertions(String s) {
-        String reverse ="";
-        for(int i =0; i<s.length(); i++) {
-            reverse = s.charAt(i)+reverse+"";
+        String sb = "";
+        for(int i=0;i<s.length(); i++) {
+            sb =s.charAt(i)+sb+"";
         }
-       int len = dp(s, reverse, s.length()-1, reverse.length()-1, new HashMap<>());
-       return s.length() - len; 
+        
+        int len = lcs(s, sb, s.length() - 1, s.length()-1, new HashMap<>());
+        return s.length() - len;
     }
     
-    int dp(String s, String t, int i, int j, Map<String, Integer> memo) {
+    int lcs(String s, String t, int i, int j, Map<String, Integer> memo) {
         String key = i+"-"+j;
-        if(i < 0 || j < 0) {
-            return 0;
-        }
         if(memo.containsKey(key)) {
             return memo.get(key);
         }
-        if(s.charAt(i)==t.charAt(j)) {
-            return 1 + dp(s, t, i - 1, j - 1, memo);
+        if( i< 0 || j < 0) { //if it less than zero, i made == 0
+            return 0;
         }
-        
-        int result = Math.max(dp(s, t, i-1, j, memo), dp(s, t, i, j - 1, memo));
+        if(s.charAt(i)==t.charAt(j)) {
+            return 1 + lcs(s, t, i - 1, j-1, memo);
+        }
+        int result = Math.max(lcs(s, t, i - 1, j, memo), lcs(s, t, i, j-1, memo));
         memo.put(key, result);
         return result;
     }
 }
 
-//https://leetcode.com/problems/longest-common-subsequence/ this is an extension to lcs
-//and longest palindrome subsequnces
-//here find reverse of the word
+//make identify the string is palindrome or not
+//make reverse of string
 //pass it to the lcs
-//total len of current string and sequnces length
+//when it return value
+// minus it from total len and value
