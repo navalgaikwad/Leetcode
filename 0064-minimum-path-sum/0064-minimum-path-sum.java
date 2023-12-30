e4262ed9a4effb8f7c968398430c53b72ca5cfa3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-30 02:40:04 -0500

    Time: 39 ms (5.34%), Space: 48.5 MB (6.13%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,26 +1,23 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int row = grid.length - 1;
-        int col = grid[0].length -1;
-        return helper(grid, row, col, new HashMap<>() );
+        return dp(grid, grid.length - 1, grid[0].length - 1, new HashMap<>());
     }
     
-    int helper(int[][] grid, int i, int j, HashMap<String, Integer> memo){
+    int dp(int[][] grid, int i, int j, HashMap<String, Integer> memo) {
         String key = i+"-"+j;
-        if(memo.containsKey(key)){
-           return memo.get(key); 
+        if(memo.containsKey(key)) {
+            return memo.get(key);
         }
-         if( i < 0 || j < 0 ){
-            return Integer.MAX_VALUE;
-        }
-        if( i == 0 && j == 0){
+        if( i==0 && j == 0) {
             return grid[i][j];
         }
-       int val1 =  helper(grid, i - 1, j, memo);
-       int val2 =  helper(grid, i, j - 1, memo);
+        if(i < 0 || j <0) {
+            return Integer.MAX_VALUE;
+        }
+        int down = dp(grid, i-1, j, memo);
+        int up = dp(grid, i, j-1, memo);
         
-    
-        int result = Math.min(val1, val2)+ grid[i][j];
+        int result = grid[i][j] + Math.min(down, up);
         memo.put(key, result);
         return result;
     }
