43172eed01db772f9206b8ae1d83c9b416f231d5
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-29 22:13:48 -0500

    Time: 162 ms (5.03%), Space: 46.8 MB (5.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,38 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        //HashMap<Integer, ArrayList<Integer>> memo = new HashMap<>();
-        HashMap<Integer, Integer> memo = new HashMap<>();
-        Integer result = dp(coins, amount, memo);
-        //System.out.print(result);
-        //return result != null ? result.size() : -1;
-        return result != null ? result : -1;
+        Integer value = dp(coins, amount, new HashMap<>());
+        if(value!=null) return value;
+        return -1;
     }
-    
     Integer dp(int[] coins, int target, HashMap<Integer, Integer> memo) {
         if(memo.containsKey(target)) {
             return memo.get(target);
         }
         if(target == 0) {
-            // ArrayList<Integer> baseList = new ArrayList<>();
-            // return baseList;
+           // ArrayList<Integer> baseList = new ArrayList<>();
             return 0;
         }
         if(target < 0) {
             return null;
         }
-        //ArrayList<Integer> result = null;
         Integer result = null;
         for(int coin : coins) {
             int remainder = target - coin;
-            Integer remainderList = dp(coins, remainder, memo);
-            if(remainderList != null) {
-                // ArrayList<Integer> combination = new ArrayList<>();
-                // combination.addAll(remainderList);
-                // combination.add(coin);
-                int combination = remainderList + 1;
-                
-                if(result==null || combination < result) {
-                    result = combination;
+            Integer combinations = dp(coins, remainder, memo);
+            if(combinations != null) {
+                Integer newCombination = combinations + 1;
+                if(result == null || newCombination < result) {
+                    result = newCombination;
                 }
             }
         }
