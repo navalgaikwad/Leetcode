d883ba43f9827e3f089884e8ff11179d9c9a61b0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-29 19:41:00 -0500

    Time: 6 ms (65.21%), Space: 45.4 MB (7.33%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,38 +1,36 @@
-class Solution {
+import java.util.Arrays;
+
+public class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int min = Integer.MAX_VALUE;
-        Integer[][] memo = new Integer[m][n];
 
-        //A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right.
-        for (int j = 0; j < n; j++) {
-            min = Math.min(min, dfs(matrix, 0, j, memo));
+        // Create a DP array with the same dimensions as the matrix
+        int[][] dp = new int[m][n];
+
+        // Initialize the first row of the DP array with the first row of the matrix
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = matrix[0][i];
         }
 
-        return min;
+        // Calculate the minimum falling path sum from the second row downwards
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Start with a high value to find the minimum
+                dp[i][j] = Integer.MAX_VALUE;
+
+                // Check all possible previous positions (up, left-diagonal, right-diagonal)
+                for (int k = -1; k <= 1; k++) { //direction
+                    int prevCol = j + k;
+                    if (prevCol >= 0 && prevCol < n) {
+                        dp[i][j] = Math.min(dp[i][j], matrix[i][j] + dp[i - 1][prevCol]);
+                    }
+                }
+            }
+        }
+
+        // Find the minimum value in the last row of the DP array
+        return Arrays.stream(dp[m - 1]).min().getAsInt();
     }
-   //same as minimum path sum same condition
-    //building from bottom to top 
-    private int dfs(int[][] matrix, int i, int j, Integer[][] memo) {
-        if (j < 0 || j >= matrix[0].length) { // correct condition for j 
-            return Integer.MAX_VALUE;
-        }
 
-        if (i == matrix.length - 1) {// we going to the end
-            return matrix[i][j];
-        }
-
-        if (memo[i][j] != null) {
-            return memo[i][j];
-        }
-
-        int down = dfs(matrix, i + 1, j, memo);
-        int left = dfs(matrix, i + 1, j - 1, memo);
-        int right = dfs(matrix, i + 1, j + 1, memo);
-
-        memo[i][j] = matrix[i][j] + Math.min(down, Math.min(left, right));
-
-        return memo[i][j];
-    }
 }
