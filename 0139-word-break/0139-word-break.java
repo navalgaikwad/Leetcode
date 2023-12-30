bf9ac656cf9331d6732999d218cebfed39595e37
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-30 14:37:56 -0500

    Time: 3 ms (83.13%), Space: 42 MB (67.96%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet<>(wordDict);
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        int len = s.length();
-        boolean[] seen = new boolean[len + 1];
-        
-        while(!q.isEmpty()) {
-            int start = q.remove();
-            if(start == len){return true;}
-          
-            for(int end = start + 1; end<=len; end++) {
-                
-                if(seen[end]) {continue;}
-                if(set.contains(s.substring(start, end))) {
-                   seen[end] = true;
-                   q.add(end);
-               } 
-            } 
+        return dp(s, wordDict, new HashMap<>());
+    }
+    
+    boolean dp(String target, List<String> wordDict, HashMap<String, Boolean> memo) {
+        if(memo.containsKey(target)) {
+            return memo.get(target);
         }
+        if(target=="") {
+            return true;
+        }
+        for(String word : wordDict) {
+            if(target.startsWith(word)) {
+                String slice = target.substring(word.length());
+                if(dp(slice, wordDict, memo)) {
+                    memo.put(target, true);
+                    return true;
+                }
+            }
+           
+        }
+        memo.put(target, false);
         return false;
     }
 }
