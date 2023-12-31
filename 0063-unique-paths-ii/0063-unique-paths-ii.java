fcde4b3512ad24566ce1433787e867bd11f29e75
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2023-12-30 19:19:16 -0500

    Time: 8 ms (6.35%), Space: 42.7 MB (6.20%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,46 +1,21 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int[][] dp = new int[row][col];
-        dp[0][0] = 1;
-        for(int i = 0; i < row; i++){
-            for(int j =0; j < col; j++){
-                if(grid[i][j]==1){//when you find obstacle 
-                 dp[i][j] =0; 
-                }else if(grid[i][j] == 0){
-                   if(i +1 < row){
-                       dp[i+1][j] += dp[i][j];
-                   }
-                    if(j + 1 < col){
-                        dp[i][j+1] +=dp[i][j];
-                  }
-                }
-              }
-           }
-        return dp[row-1][col-1];
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        return dp(obstacleGrid, obstacleGrid.length - 1, obstacleGrid[0].length - 1, new HashMap<>());
+    }
+    
+    int dp(int[][] grid, int i, int j, HashMap<String, Integer>memo) {
+        String key = i+"-"+j;
+        if(memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        if(i < 0 || j < 0 || grid[i][j] == 1) {
+            return 0;
+        }
+        if(i == 0 && j == 0) {
+            return 1;
+        }
+        int result = dp(grid, i - 1, j, memo) + dp(grid, i, j - 1, memo);
+        memo.put(key, result);
+        return result;
     }
 }
-// class Solution {
-//     public int uniquePathsWithObstacles(int[][] grid) {
-//         int row = grid.length;
-//         int col = grid[0].length;
-//         int[][] dp = new int[row][col];
-       
-//         dp[0][0] = 1;
-//         for(int i=0; i<row; i++){
-//             for(int j=0; j<col; j++){
-//                 if(grid[i][j]==1){
-//                  dp[i][j] =0; 
-//                 }else{
-//                     if( i+1<row ){
-//                     dp[i+1][j] +=  dp[i][j];
-//                 } if(j +1<col){
-//                      dp[i][j+1] +=  dp[i][j];
-//                 }
-//               }
-//             }
-//         }
-//         return dp[row-1][col-1];
-//     }
-// }
