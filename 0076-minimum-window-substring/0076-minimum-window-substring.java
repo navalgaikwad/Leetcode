282bb5d102ffab6ca7affa410afd34e4c0aea842
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-02 01:12:39 -0500

    Time: 3 ms (84.84%), Space: 43.3 MB (98.06%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -8,7 +8,9 @@ public class Solution {
         int counter = t.length(), begin = 0, end = 0;
         int minLen = Integer.MAX_VALUE, head = 0;
         while (end < s.length()) {
-            if (map[s.charAt(end++)]-- > 0) {
+            int count = map[s.charAt(end)]--;
+            end++;
+            if (count > 0) {
                 counter--;
             }
             while (counter == 0) {
@@ -16,10 +18,13 @@ public class Solution {
                     minLen = end - begin;
                     head = begin;
                 }
-                if (map[s.charAt(begin++)]++ == 0) {
+                int beginCount = map[s.charAt(begin)]++;
+                begin++;
+                if (beginCount == 0) {
                     counter++;
                 }
             }
+            
         }
         return minLen == Integer.MAX_VALUE ? "" : s.substring(head, head + minLen);
     }
