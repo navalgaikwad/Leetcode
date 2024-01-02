28e8cba9ed4febe320086530063897152f9c65ca
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-02 15:33:28 -0500

    Time: 0 ms (100.00%), Space: 41.2 MB (11.01%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String longestDiverseString(int a, int b, int c) {
+        int a_count=0;
+        int b_count=0;
+        int c_count=0;
+        
+        StringBuilder sb=new StringBuilder();
+        
+        int total=a +b +c;
+        //logic is no more than 2 character should appear   
+        for(int i=0;i<total;i++){
+            if((a>=b && a>=c && a_count<2) || (b_count==2 && a>0) || (c_count==2 && a>0)){
+                 sb.append("a");//a is greater than everyone
+                 a_count++;//increase a_counter
+                 a--;
+                 b_count=0;
+                 c_count=0;
+                
+            } else  if((b>=a && b>=c && b_count<2) || (a_count==2 && b>0) || (c_count==2 && b>0)){
+                 sb.append("b");
+                 b_count++;
+                 b--;
+                 c_count=0;
+                 a_count=0;
+                
+            } else  if((c>=b && c>=a && c_count<2) || (b_count==2 && c>0) || (a_count==2 && c>0)){
+                 
+                 sb.append("c");
+                 c_count++;
+                 c--;
+                 b_count=0;
+                 a_count=0;
+                
+            }
+        }
+        return sb.toString();
+        
+    }
+}
