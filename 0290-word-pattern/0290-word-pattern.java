e3fd8d233ba3bb1e2d05d12bbd184d1cc6334174
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-01 19:15:45 -0500

    Time: 2 ms (15.70%), Space: 41.4 MB (11.15%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -4,23 +4,28 @@ class Solution {
         if(pattern.length() != arr.length) {
             return false;
         }
-        Map<Character, String> map = new HashMap<>();
-        for(int i = 0; i<arr.length; i++) {
-            char c = pattern.charAt(i);
-            String word = arr[i];
-            
+        
+        HashMap<Character, String> map = new HashMap<>();
+        for(int i =0 ;i<pattern.length(); i++) {
+            char c = pattern.charAt(i);//a
+            String str = arr[i];//dog
             if(map.containsKey(c)) {
-                if(!map.get(c).equals(word)) {
+                String value = map.get(c);
+                if(!value.equals(str)) {
                     return false;
                 }
-                
             }else {
-                if(map.containsValue(word)) {
+                if(map.containsValue(str)) {
                     return false;
                 }
-                map.put(c, word);
+                map.put(c, str);
             }
         }
         return true;
     }
 }
+
+
+//pattern = "abba", s = "dog cat cat dog"
+//"abba"   "dog dog dog dog"
+
