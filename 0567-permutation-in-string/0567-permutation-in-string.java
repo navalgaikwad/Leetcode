351000bed0407d179c28b82078c205a8560935e1
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-01 22:32:26 -0500

    Time: 32 ms (29.92%), Space: 43.8 MB (29.26%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,21 +1,33 @@
 class Solution {
+    // abc   eidbabc
     public boolean checkInclusion(String s1, String s2) {
-        int left = 0;
-        int k = s1.length();
-        char[] chars = s1.toCharArray();
-        Arrays.sort(chars);
-        s1 = String.valueOf(chars);
-        for(int right =0; right<=s2.length() - k; right++) {
-            String substring = s2.substring(left, right + k);
-            char[] c = substring.toCharArray();
-            Arrays.sort(c);
-            String str = String.valueOf(c);
-            if(s1.equals(str)) {
-                return true;
-            }
-            left++;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c: s1.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
+        int i = 0, j = 0;
+        while(j < s2.length()) {
+            char c = s2.charAt(j);
+            if(map.containsKey(c)) {
+                j++;
+                int count = map.get(c);
+                if(count == 1)
+                    map.remove(c);
+                else
+                    map.put(c, count - 1);
+            } else {
+                if(i == j) {
+                    i++; 
+                    j++;
+                } else {
+                    map.put(s2.charAt(i), map.getOrDefault(s2.charAt(i), 0) + 1);
+                    i++;
+                }
+            }    
+            if(map.isEmpty())
+                return true;
+        }
         return false;
     }
 }
