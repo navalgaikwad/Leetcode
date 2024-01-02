d4b3810e6d4484db6a45ef3d4a83d4e6119a8e96
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-02 08:52:58 -0500

    Time: 2 ms (98.12%), Space: 43.1 MB (98.88%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,8 +1,8 @@
 public class Solution {
     public String minWindow(String s, String t) {
-        int[] map = new int[128];
+        int[] map = new int[128];//size 128
         for (char c : t.toCharArray()) {
-            map[c]++;
+            map[c]++;//add all value in map 
         }
 
         int counter = t.length(), begin = 0, end = 0;
@@ -29,3 +29,7 @@ public class Solution {
         return minLen == Integer.MAX_VALUE ? "" : s.substring(head, head + minLen);
     }
 }
+//maintain the counter 
+//counter is minus untill 0
+//when zero find the min length
+//if we find any character we need to increase the counter and character counter
