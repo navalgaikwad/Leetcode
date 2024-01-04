217b57e7a3b33246cd46f59e97aa953c58a29178
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-03 21:55:58 -0500

    Time: 11 ms (34.74%), Space: 44.7 MB (11.25%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> col = new HashSet<>();
         Set<String> row = new HashSet<>();
-        Set<String> boxes = new HashSet<>();
-        //we are dealing with value not row and coloumns
-        for(int i = 0; i < board.length; i++) {
+        Set<String> col = new HashSet<>();
+        Set<String> box = new HashSet<>();
+        
+        for(int i=0; i<board.length; i++) {
             for(int j=0; j<board[0].length; j++) {
-                int box = (i/3) + (j/3)*3;
-                if(board[i][j] != '.'){
-                    char val =  board[i][j];
-                    if(col.contains(i +"-"+ val) || row.contains(j +"-"+ val) || boxes.contains(box+"-"+val)) {
+                int boxe = (i/3) + (j/3)*3;
+                char value = board[i][j];
+                if(value!='.') {
+                    String r = i+"-"+value;
+                    String c = j+"-"+value;
+                    String b = boxe+"-"+value;
+                    if(row.contains(r) || col.contains(c) || box.contains(b)) {
                         return false;
                     }
-                    col.add(i+"-"+val);
-                    row.add(j+"-"+val);
-                    boxes.add(box+"-"+val);
-                }
+                    row.add(r);
+                    col.add(c);
+                    box.add(b);
+               }
             }
         }
         return true;
     }
 }
+
