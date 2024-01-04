bcd684ce8b5264465a2dfd3c569b204699c2251c
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-04 18:43:29 -0500

    Time: 0 ms (100.00%), Space: 44.5 MB (40.63%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -13,21 +13,22 @@
  *     }
  * }
  */
+
+
 class Solution {
     int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        helper(root);
+        dfs(root);
         return max;
     }
-    
-    int helper(TreeNode root){
-       if( root == null ){
+    int dfs(TreeNode root) {
+        if(root == null) {
             return 0;
         }
-      int left  = Math.max(helper(root.left), 0);
-      int right = Math.max(helper(root.right), 0);
-      int result = root.val + left + right;
-      max = Math.max(max,  result);
-      return root.val + Math.max(left, right);
+        int left = Math.max(dfs(root.left), 0);
+        int right = Math.max(dfs(root.right), 0);
+        int result = root.val + Math.max(left, right);
+        max = Math.max(max, root.val + left+ right);
+        return result;
     }
 }
