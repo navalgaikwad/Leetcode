bf7f97e430680447a82b9ec74d0f0356192a8c74
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-04 16:33:13 -0500

    Time: 0 ms (100.00%), Space: 41.5 MB (23.18%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,21 +1,24 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        return dp(obstacleGrid, obstacleGrid.length - 1, obstacleGrid[0].length - 1, new HashMap<>());
-    }
-    
-    int dp(int[][] grid, int i, int j, HashMap<String, Integer>memo) {
-        String key = i+"-"+j;
-        if(memo.containsKey(key)) {
-            return memo.get(key);
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int col = grid.length;
+        int row = grid[0].length;
+        int[][] dp = new int[col+1][row+1];
+        dp[0][0] = 1; 
+        for(int i = 0; i<col; i++) {
+            for(int j = 0; j<row; j++) {
+                if(grid[i][j]==1) {
+                    dp[i][j] = 0;
+                }else {
+                    if(i-1 >= 0) {
+                     dp[i][j]+= dp[i-1][j];   
+                    }
+                    if(j-1 >= 0) {
+                     dp[i][j]+=dp[i][j-1];    
+                    }
+                }
+                
+            }
         }
-        if(i < 0 || j < 0 || grid[i][j] == 1) {
-            return 0;
-        }
-        if(i == 0 && j == 0) {
-            return 1;
-        }
-        int result = dp(grid, i - 1, j, memo) + dp(grid, i, j - 1, memo);
-        memo.put(key, result);
-        return result;
+        return dp[col-1][row-1];
     }
 }
