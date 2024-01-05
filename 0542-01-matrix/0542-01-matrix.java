e88438445ebbfa7a466ce9fecfaa0130d0bef139
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-05 16:46:23 -0500

    Time: 122 ms (6.52%), Space: 50.8 MB (5.29%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,43 +1,44 @@
 class Solution {
-    public boolean isValid(int[][] mat, int i, int j){
-        return i >= 0 && i <mat.length && j >=0 &&  j < mat[0].length;
+    boolean isValid(int[][] mat, int i, int j) {
+        return i>=0&& i<mat.length&& j>=0&&j<mat[0].length;
     }
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         Set<String> visited = new HashSet<>();
-        Queue<int[]> queue = new LinkedList<>();
-        for( int i = 0; i< m; i++){
-            for(int j = 0; j<n; j++){
-                if(mat[i][j] == 0){
-                  queue.add(new int[]{i, j, 0});
-                  visited.add(i+"-"+j);
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0; i<m; i++) {
+            for(int j =0; j<n; j++) {
+                if(mat[i][j]==0) {
+                    q.add(new int[]{i, j, 0});
+                    visited.add(i+"-"+j);
                 }
             }
         }
-       helper(queue, visited, mat);
-       return mat; 
+        return helper(visited, q, mat);
+        
     }
     
-    void helper(Queue<int[]> queue,  Set<String> visited, int[][] mat ){
-        int[][] directions = {{0,1},{1,0}, {-1, 0}, {0, -1}};
-        while(!queue.isEmpty()){
-            int[] current = queue.remove();
-            int row = current[0];
-            int col = current[1];
-            int length = current[2];
-            if(mat[row][col] != 0){
-              mat[row][col] = length;  
+    int[][] helper(Set<String> visited, Queue<int[]> q, int[][] mat) {
+        int[][] dir = {{1,0}, {0, 1}, {-1, 0}, {0, -1}};
+        while(!q.isEmpty()) {
+            int[] current = q.remove();
+            int i = current[0];
+            int j = current[1];
+            int len = current[2];
+            if(mat[i][j]!=0) {
+              mat[i][j] = len;  
             }
-            for(int[] direction: directions){
-                int rx = direction[0] + row;
-                int cy = direction[1] + col;
-                
-                if(isValid(mat, rx, cy) && !visited.contains(rx+"-"+cy)){
-                   queue.add(new int[]{rx, cy, length + 1 }); 
-                     visited.add(rx+"-"+cy);
+            for(int[] di : dir) {
+                int xDir = di[0] + i;
+                int yDir = di[1] + j;
+                if(isValid(mat, xDir, yDir) && !visited.contains(xDir+"-"+yDir)) {
+                    q.add(new int[]{xDir, yDir, len+1});
+                    visited.add(xDir+"-"+yDir);
                 }
             }
         }
+        return mat;
     }
+    
 }
