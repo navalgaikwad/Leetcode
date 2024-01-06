d660fd087aeec2311d1a8b0444c86cc400452d84
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-05 21:03:13 -0500

    Time: 49 ms (51.00%), Space: 73.2 MB (95.53%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,25 +1,22 @@
 class Solution {
-    private boolean overlap(int[] a, int b){
-        return a[0] < b; // play with endTime
-    }
-    //[[0,2],[1,3],[2,4],[3,5],[4,6]]
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        int endTime = intervals[0][1];
+        Arrays.sort(intervals, (a, b) ->a[0] - b[0]);
         int count =0;
-        for(int i = 1 ;i<intervals.length; i++){
-            if(overlap(intervals[i], endTime)){ 
-                count++; //if there is count++, 
-                endTime = Math.min(intervals[i][1], endTime);
-            }else {// if there is no overlap take next overlapp
+        int endTime = intervals[0][1];
+        for(int i=1; i<intervals.length; i++) {
+            if(intervals[i][0] < endTime) {
+                count++;
+                endTime = Math.min(endTime, intervals[i][1]);
+            }else {
                 endTime = intervals[i][1];
             }
+            
         }
         return count;
     }
 }
-
-//[[1,2],[2,3],[3,4],[1,3]]
-//[3, 4]
-// [1, 3]
-//
+//100, 11
+//[[1,100],[1,11],[2,12],[11,22],]
+//1 < 100 - 1
+//2<11 -2
+//11<11-2
