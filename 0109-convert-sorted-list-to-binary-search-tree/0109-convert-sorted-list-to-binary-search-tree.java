d70cd375cf2d6ea8efaa174ecc93aa635efe99ba
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-05 22:15:55 -0500

    Time: 1 ms (45.32%), Space: 44.8 MB (37.24%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -1,56 +1,50 @@
 /**
- * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode(int
- * x) { val = x; } }
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
 /**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
- * right; TreeNode(int x) { val = x; } }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-
-  private ListNode findMiddleElement(ListNode head) {
-
-    // The pointer used to disconnect the left half from the mid node.
-    ListNode prevPtr = null;
-    ListNode slowPtr = head;
-    ListNode fastPtr = head;
-
-    // Iterate until fastPr doesn't reach the end of the linked list.
-    while (fastPtr != null && fastPtr.next != null) {
-      prevPtr = slowPtr;
-      slowPtr = slowPtr.next;
-      fastPtr = fastPtr.next.next;
+    public TreeNode sortedListToBST(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while(head!=null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        return buildTree(list, 0, list.size()-1);
     }
-
-    // Handling the case when slowPtr was equal to head.
-    if (prevPtr != null) {
-      prevPtr.next = null;
+    
+    TreeNode buildTree(List<Integer> list, int low, int high) {
+        if(low > high) {
+            return null;
+        }
+        
+        int mid = low + (high - low)/2;
+        TreeNode root = new TreeNode(list.get(mid));
+        root.left = buildTree(list, low, mid - 1);
+        root.right = buildTree(list, mid + 1, high);
+        
+        return root;
     }
-
-    return slowPtr;
-  }
-
-  public TreeNode sortedListToBST(ListNode head) {
-
-    // If the head doesn't exist, then the linked list is empty
-    if (head == null) {
-      return null;
-    }
-
-    // Find the middle element for the list.
-    ListNode mid = this.findMiddleElement(head);
-
-    // The mid becomes the root of the BST.
-    TreeNode node = new TreeNode(mid.val);
-
-    // Base case when there is just one element in the linked list
-    if (head == mid) {
-      return node;
-    }
-
-    // Recursively form balanced BSTs using the left and right halves of the original list.
-    node.left = this.sortedListToBST(head);
-    node.right = this.sortedListToBST(mid.next);
-    return node;
-  }
+    
+    
 }
