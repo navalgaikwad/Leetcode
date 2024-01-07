d741f473518c3f61eab834f0d9d40b77ef12d693
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-06 19:06:15 -0500

    Time: 2 ms (67.66%), Space: 42 MB (6.64%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,18 +1,13 @@
 class Solution {
-    public int hIndex(int[] cit) {
-        //6, 5,3, 1, 0
-        int n = cit.length;
-        
-        Arrays.sort(cit);
-        
-        int count = 0;
-     
-        //dont need to check greater because it is already sorted
-        //idea is similar to jump game start from back side
-        for(int i = n-1; i >= 0 && cit[i] > count ; i--){
-                count ++;
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        Arrays.sort(citations);
+        int count =0;
+        for(int i = n-1; i>=0 && citations[i]>count; i-- ) {
+            count++;
         }
-        
         return count;
     }
 }
+// [3,0,6,1,5]
+//[      5,5,5]
