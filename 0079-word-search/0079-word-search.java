8088f816a8980f7f8c77ff9da78309f96ba8f304
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-06 20:35:49 -0500

    Time: 133 ms (83.03%), Space: 41.3 MB (40.14%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,33 +1,37 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[i].length; j++){
-                if(word.charAt(0)==board[i][j] &&dfs(board, word, i, j, 0) ){
-                    return true;
+         boolean[][] visited = new boolean[board.length][board[0].length];
+        for(int i =0; i<board.length; i++) {
+            for(int j =0; j<board[0].length; j++) {
+               
+                if(board[i][j]==word.charAt(0)) {
+                    
+                    if(dfs(board, word, i, j, 0) ){
+                        return true;
+                    }
                 }
             }
-            
         }
         return false;
     }
-    // index of word
-    // word in char not match return false;
-    //if i > word.length return true;
     
-    boolean dfs(char[][] board, String word, int i, int j, int index){
-        if(index == word.length()){
+    boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if(index >= word.length()) {
             return true;
         }
-        if(i >=board.length || i<0 || j<0 || j>=board[0].length ||
-           board[i][j] != word.charAt(index) || board[i][j] == '1'){
+        if(i >=board.length || i< 0 || j >= board[0].length || j<0 
+           || word.charAt(index)!= board[i][j] || board[i][j]=='$') {
             return false;
         }
         
-        char temp =  board[i][j];
-        board[i][j] = '1';
-        boolean flag = dfs(board, word, i + 1, j, index + 1) || dfs(board, word, i - 1, j, index + 1)|| dfs(board, word, i, j + 1, index + 1) 
-                      || dfs(board, word, i , j - 1, index + 1);
-        board[i][j] = temp;
+        char c = board[i][j];
+        //visited[i][j] = true;
+        board[i][j] ='$';
+        boolean flag = dfs(board, word, i+1, j, index+1) || 
+                       dfs(board, word, i-1, j, index+1) || 
+                       dfs(board, word, i, j-1, index+1) || 
+                       dfs(board, word, i, j+1, index+1);
+        board[i][j] = c;
         return flag;
     }
 }
