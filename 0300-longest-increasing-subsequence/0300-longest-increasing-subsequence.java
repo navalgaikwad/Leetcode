c68f811e79adbaa67416f55badcc6694a3220d40
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-07 12:23:41 -0500

    Time: 45 ms (36.72%), Space: 43.9 MB (53.26%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,22 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int len = nums.length;
-        int[] dp = new int[len+1];
+        int[] dp = new int[ len + 1 ];
         Arrays.fill(dp, 1);
-        for(int i =0; i < len; i++) {
-            for(int j=0; j < i; j++) {
+        int count = 0;
+       
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < i; j++) {
                 int next_count = dp[j] + 1;
                 if(nums[i] > nums[j] && dp[i] < next_count) {
-                    dp[i]  =next_count;
+                    dp[i] = next_count;
                 }
             }
         }
-        int max = 0;
-        for (int i = 0; i < len; ++i) {
-            max = Math.max(max, dp[i]);
+        count =0;
+        for(int v : dp) {
+           count = Math.max(v, count); 
         }
-        
-        return max;
+        return count;
     }
 }
-//fill arr with 1
-//run for loop on arr length
-//inside loop on index till j <i
-//check nums[i] > nums[j] and count < nextcount
-//then update the dp[i]
