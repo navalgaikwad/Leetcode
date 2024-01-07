d3731decf414c0293019f631afedbe0e1767103b
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-07 18:09:27 -0500

    Time: 23 ms (33.18%), Space: 54.7 MB (20.62%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -5,11 +5,11 @@ class RecentCounter {
     }
     
     public int ping(int t) {
-        q.offer(t);
-        
-        while(t - q.peek() > 3000){
+        q.add(t);
+        while (!q.isEmpty() && t - q.peek() > 3000) {
             q.poll();
         }
+        //q.add(t);
         return q.size();
     }
 }
