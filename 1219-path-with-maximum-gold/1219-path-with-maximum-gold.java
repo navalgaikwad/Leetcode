31fd328ffab6a8018c44a257b300417207e94f45
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-07 18:38:53 -0500

    Time: 59 ms (70.97%), Space: 41.4 MB (18.89%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,33 +1,34 @@
 class Solution {
-    int r = 0;
-    int c = 0;
-    int max = 0;
+    int max =0;
     public int getMaximumGold(int[][] grid) {
-        r = grid.length;
-        c = grid[0].length;
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-                if(grid[i][j] != 0) {
-                    dfs(grid, i, j, 0);
+        int row = grid.length;
+        int col = grid[0].length;
+        
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(grid[i][j]!=0) {
+                   dfs(grid, i, j, 0);
                 }
             }
         }
+        
         return max;
     }
     
-    private void dfs(int[][] grid, int i, int j, int cur) {
-       // int sum=0;
-      if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]==0){
-         max= Math.max(max, cur);
-          return;
-      }
-        int temp =grid[i][j];
-         cur =cur+temp;
-        grid[i][j]=0;
-        dfs(grid, i+1, j, cur);
-        dfs(grid, i-1, j, cur);
-        dfs(grid, i, j+1, cur);
-        dfs(grid, i, j-1, cur);
-        grid[i][j]=temp;
+    void dfs(int[][] grid , int i, int j, int sum) {
+        int row = grid.length;
+        int col = grid[0].length;
+        if(i >=row || i<0 || j>=col || j < 0 || grid[i][j] == 0) {
+            max = Math.max(max, sum);
+            return;
+        }
+        sum = sum + grid[i][j];
+        int temp = grid[i][j];
+        grid[i][j] = 0;
+        dfs(grid, i + 1, j, sum);
+        dfs(grid, i , j+ 1, sum);
+        dfs(grid, i - 1, j, sum);
+        dfs(grid, i , j-1, sum);
+        grid[i][j] = temp;
     }
 }
