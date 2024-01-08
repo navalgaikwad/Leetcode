0c083a1388c1aee4aee1d2b19c7e836a79cdcf6a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-08 18:02:59 -0500

    Time: 167 ms (83.84%), Space: 58.4 MB (46.34%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.java
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.java
@@ -1,33 +1,20 @@
 class Solution {
     public int halveArray(int[] nums) {
-        PriorityQueue<Double> pq = new PriorityQueue<>((a, b) -> Double.compare(b, a));
-        if(nums.length<2){
-            return 1;
-        }
+        PriorityQueue<Double> pq = new PriorityQueue<>((a,b)->Double.compare(b,a));
         double sum = 0;
-        for(int num : nums){
-            pq.add((double)num);
+        for(int num : nums) {
             sum = sum + num;
+            pq.add((double)num);
         }
-        double half = (sum/2);
-        int count =0 ;
-        while(sum>half){
+        double half = sum/2;
+        double count =0;
+        while(sum > half) {
             double value = pq.remove();
             value = value/2;
             sum = sum - value;
             pq.add(value);
             count++;
         }
-        return count;
+        return (int)count;
     }
 }
-
-//5,19,8,1
-//25+8 = 33 /2 = 16.5
-
-//19.5 9.5
-//16.5
-//19,8,5,1 = 33 - 9.5 = 23.5 > 16.5
-//9.5, 8, 5, 1
-//8,5, 4.75,1 23.5 - 4.74 = 18.75 > 16.5
-//4
