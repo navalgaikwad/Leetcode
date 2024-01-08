aa88f3f1dc19f58d762926a44ffeaa44d5b5c694
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-07 21:50:18 -0500

    Time: 11 ms (34.84%), Space: 45.1 MB (6.81%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,60 +1,47 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
-        int n = board.length;
-        int target = n * n;
-        Integer[] columns = new Integer[n];
-        
-        //most important 
-        //first define columns array
-        for(int i =0 ;i< n; i++) {
-            columns[i] = i; // add the value in the columns
+        int m = board.length;
+        int n = board[0].length;
+        Integer[] column = new Integer[m];
+        int target = m*m;
+        for(int i=0;i<m;i++) {
+            column[i] = i;
         }
         
+        int[][] cell = new int[m*m+1][2];
         int label = 1;
-        //n * n + 1 total element in the array
-        Pair<Integer, Integer> cell[] = new Pair[n * n + 1];// use pair defined as Key value in array
-        for(int row = n -1; row >=0; row--) {// start from the end so n -1
-            for(int col : columns) { // start from first
-                cell[label++] = new Pair<>(row, col); // at each level we will be giving marking
+        for(int row = m-1; row>=0; row--) {
+            for(int col: column) {
+                cell[label][0] = row;
+                cell[label][1] = col;
+                label++;
             }
-            Collections.reverse(Arrays.asList(columns));// reverse it
-        }
-    
-    Set<Integer> visited = new HashSet<>();
-    Queue<int[]> q = new LinkedList<>();
-    q.add(new int[]{1, 0}); //start from one rather 0 it is given in the problem
-    
-    while(!q.isEmpty()) {
-        int[] current = q.remove();
-        int currentStep = current[0];
-        int level = current[1];
-        if(currentStep == target) {
-            return level;
+            Collections.reverse(Arrays.asList(column));
         }
         
-        for (int i =1; i<7; i++) { //it will move 7 times 0 to 6
-            int nextPosition = i + currentStep;
-            if(nextPosition <= target) {
-                int nextRow = cell[nextPosition].getKey(); // get the value form cell row 5
-                int nextCol = cell[nextPosition].getValue();// col 0
-                int destination = ( board[nextRow][nextCol] != -1 ) ? board[nextRow][nextCol] : nextPosition;
-                
-                  if(!visited.contains(destination)){
-                         
-                         visited.add( destination );
-                         q.add(new int[]{ destination , level + 1 });
-                         
-                } 
+        Set<Integer> visited = new HashSet<>();
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{1,0});
+        while(!q.isEmpty()) {
+            int[] current = q.remove();
+            int currentStep = current[0];
+            int level = current[1];
+            if(currentStep == target) {
+                return level;
+            }
+            for(int i =1; i<7; i++) {
+                int nextPosition = i+currentStep;
+                if(nextPosition <= target) {
+                    int nextRow = cell[nextPosition][0];
+                    int nextCol = cell[nextPosition][1];
+                    int destination = board[nextRow][nextCol] == -1 ? nextPosition : board[nextRow][nextCol];
+                    if(!visited.contains(destination)) {
+                        visited.add(destination);
+                        q.add(new int[]{destination, level+1});
+                    }
+                }
             }
         }
-    }
-    return -1;
+        return -1;
     }
 }
-
-// [[-1,-1,-1,-1,-1,-1],
-//  [-1,-1,-1,-1,-1,-1],
-//  [-1,-1,-1,-1,-1,-1],
-//  [-1,35,-1,-1,13,-1],
-//  [-1,-1,-1,-1,-1,-1],
-//  [-1,15,-1,-1,-1,-1]]
