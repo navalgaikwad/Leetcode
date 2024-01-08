1b74d7854cba9c75e21388dd1ca14d35ab104ea4
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-08 09:09:01 -0500

    Time: 99 ms (61.77%), Space: 157.8 MB (35.35%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,56 +1,32 @@
 class Solution {
-    // 
-    public boolean validPath(int n, int[][] edges, int s, int d) {
-        if(edges.length == 0)
-            return true;
-        ArrayList<Integer>[] map = new ArrayList[n];
-    
-        boolean[] visited=new boolean[n];
-        for(int i = 0; i < n; i++) {
-            map[i] = new ArrayList<>();
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        boolean[] visited = new boolean[n]; 
+        ArrayList<Integer> adj[] = new ArrayList[n];
+        for(int i=0; i<n; i++) {
+            adj[i] = new ArrayList<>();
         }
-        
-        for(int[] edge: edges) {
-            map[edge[0]].add(edge[1]);
-            map[edge[1]].add(edge[0]);
+        for(int[] edge : edges) {
+            adj[edge[0]].add(edge[1]);
+            adj[edge[1]].add(edge[0]);
         }
-        
-        Queue<Integer> q=new LinkedList<>();
-        q.offer(s);
-        visited[s]=true;
-        while(!q.isEmpty()){
-           int curr = q.poll();
-            
-           if(curr == d){
-               return true;
-           }else{
-               for(int nei: map[curr]){
-                   if(!visited[nei]) {
-                       q.add(nei);
-                       visited[nei] = true; 
-                   }
-               }
-
-           }
-       }
-       return false;      
-}
+        //for(int i=0; i<n;i++ ) {
+            if(dfs(adj, source, destination, visited)) {
+                return true;
+            }
+       // }
+        return false;
+    }
     
-//     boolean dfs(Map<Integer,ArrayList<Integer>>map, boolean[] v,int s, int d){
-//         if(s==d) return true;
-//         if(v[s]){
-//             return false;
-//         }
-//        // v.add(s, 1);
-//         v[s]=true;
-        
-//         for(int i=0;i<map.get(s).size();i++){
-//             if(dfs(map, v, map.get(s).get(i), d)){
-//                 return true;
-//             }
-//         }
-        
-//         return false;
-//     }
-    
+    boolean dfs(ArrayList<Integer> adj[], int src, int dest, boolean[] visited) {
+        if(src == dest) return true;
+        visited[src] = true;
+        for(int neighbour:adj[src]) {
+            if(!visited[neighbour]) {
+                if(dfs(adj, neighbour, dest, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
