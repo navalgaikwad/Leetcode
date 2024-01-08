33e1f35f235630af6c610550015caa18e57b8ea3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-08 16:56:16 -0500

    Time: 8 ms (17.93%), Space: 62.4 MB (44.55%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,35 +1,30 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        long[] prefix = new long[nums.length];
-        long[] suffix = new long[nums.length];
+        int len = nums.length;
+        long[] prefix = new long[len];
+        long[] suffix = new long[len];
         prefix[0] = nums[0];
-        int n = nums.length;
-       
-        for(int i=1; i<n; i++) {
+        for(int i = 1; i<len; i++) {
             prefix[i] = prefix[i-1] + nums[i];
         }
-        
-        suffix[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] + nums[i];
+        suffix[len-1] = nums[len-1];
+        for(int i =len-2; i>=0; i--) {
+            suffix[i] = suffix[i+1]+nums[i];
         }
         
-        int[] result = new int[n];
-        for(int i = 0; i<n; i++) {
-            if(i + k >= n || i-k < 0 ) {
+        int[] result = new int[len];
+        for(int i=0; i<len; i++) {
+            if(i-k <0 || i+k >=len) {
                 result[i] = -1;
             }else {
-                long suff = suffix[i - k] - suffix[i];// from that element to left 
-                long pref = prefix[i + k] - prefix[i];// from that elemnt to right
-                long sum = suff + (long)nums[i] + pref;// do sum from left and right
-                long noOfElement = (2 * k + 1);//no of elemnt
-                long answer = sum/noOfElement;
-                result[i] = (int)answer;
+                long suffixValue = suffix[i-k] - suffix[i];
+                long prefixValue = prefix[i+k] - prefix[i];
+                long sum = suffixValue + prefixValue + (long)nums[i];
+                long noEle = (2*k + 1);
+                long avg = sum/noEle;
+                result[i] = (int)avg;
             }
         }
         return result;
     }
 }
-
-//7,4,3,9,1,8,5,2,6 k=3
-//
