54fb6c1d697a9b02c39ce5585db1381a1db5d3c5
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-08 21:09:50 -0500

    Time: 59 ms (26.54%), Space: 60 MB (40.86%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
@@ -1,18 +1,18 @@
 class Solution {
     public int minimumCardPickup(int[] cards) {
         int left = 0;
-       // int right = 0;
         int len = cards.length;
         Map<Integer, Integer> map = new HashMap<>();
         int min = Integer.MAX_VALUE;
         
         for(int right = 0; right < len; right++){
-            // if there is duplicate window size will increase and map size is will be same
+            // if there is duplicate, window size will increase and map size is will be same
             map.put(cards[right], map.getOrDefault(cards[right], 0)+ 1);  
             
-            while(map.size() < right -left + 1) {
-         //1,2,3,1,4 L=0 R =3 for this it will come inside while loop R-L+1 = 4
-        // {1:2, 2:1, 3:1} size = 3
+            //jeva jeva map size choti hoil teva teva value remove kar
+            while(map.size() < right - left + 1) {
+                //1,2,3,1,4 L=0 R =3 for this it will come inside while loop R-L+1 = 4
+                // {1:2, 2:1, 3:1} size = 3
                 min = Math.min(min, right - left + 1);
                 
                 int value = map.get(cards[left]);
@@ -22,12 +22,8 @@ class Solution {
                     map.remove(cards[left]);
                 }
                 left++;
-            }
-           
+            } 
         }
-        
-    
-        
         return min!=Integer.MAX_VALUE? min : -1;
     }
 }
