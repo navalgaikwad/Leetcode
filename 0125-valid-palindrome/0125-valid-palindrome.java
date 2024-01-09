1c2cd88cdc94cd38954729407fa8a30d2876a1ca
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-09 18:15:35 -0500

    Time: 16 ms (36.19%), Space: 45.2 MB (11.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int[] count = new int[26];
-       // int left =0;
-        int right = s.length()-1;
-        for(int left=0; left<s.length(); left++) {
-          if(s.charAt(left)!=s.charAt(right)) {
-              return false;
-          }
-            right--;
-        }
-        
-          
-        return true;
+        if(null == s) {
+			System.out.println("Input string is false");
+			return false;
+		}
+		
+		if(1 > s.length() || s.length() > 200000) {
+			System.out.println("Input string is very small OR big" + s);
+			return false;
+		}
+		
+		s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+		if(s.isEmpty()) {
+			return true;
+		}
+		
+		char[] chArray = s.toCharArray();
+		for(int i=0, j=chArray.length-1; i<=j; i++, j--) {
+			if(chArray[i] != chArray[j]){
+				return false;
+			}
+		}
+		return  true;
     }
 }
