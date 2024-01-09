9e9672315dcd1bfbbdbc56e4ea1584edc51a45ad
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-09 18:50:29 -0500

    Time: 1 ms (89.83%), Space: 45.6 MB (7.90%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,21 +1,22 @@
 class Solution {
     public int countBattleships(char[][] board) {
-        int res = 0;
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[i].length; j++) {
-                if(board[i][j] == '.')
+        int result =0;
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
+                if(board[i][j]=='.') {
                     continue;
-                if(i > 0 && board[i-1][j] == 'X')
-                    continue;
-                if(j > 0 && board[i][j-1] == 'X')
-                    continue;
-                res++;
+                } if(i>0) {
+                    if(board[i-1][j]=='X'){
+                        continue;
+                    }
+                } if(j>0) {
+                    if(board[i][j-1]=='X'){
+                        continue;
+                    }
+                }
+                result++;
             }
         }
-        return res;
+        return result;
     }
 }
-//check not equal to X
-//1st row i =0 j-1
-//1st col j =0 i-1
-//then i-1 and j-1
