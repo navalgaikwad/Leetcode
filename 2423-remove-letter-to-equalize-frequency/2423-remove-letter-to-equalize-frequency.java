a93b6b435ed603463aa6595b7444f1beaf2c420f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-10 10:48:34 -0500

    Time: 1 ms (58.37%), Space: 41.1 MB (32.56%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.java
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.java
@@ -1,27 +1,24 @@
 class Solution {
     public boolean equalFrequency(String word) {
         Map<Character, Integer> wordCounter = new HashMap<>();
-        for (char c : word.toCharArray()) {
-            wordCounter.put(c, wordCounter.getOrDefault(c, 0) + 1);
+        for(char c : word.toCharArray()) {
+            wordCounter.put(c, wordCounter.getOrDefault(c, 0)+1);
         }
-        
-        if (wordCounter.size() == 1 || wordCounter.size() == word.length()) {
+        if( wordCounter.size() == 1 || wordCounter.size() == word.length()) {
             return true;
         }
-        
         Map<Integer, Integer> frequencyCounter = new HashMap<>();
         for (int count : wordCounter.values()) {
             frequencyCounter.put(count, frequencyCounter.getOrDefault(count, 0) + 1);
         }
-        
-        if (frequencyCounter.size() != 2) {
+         if (frequencyCounter.size() != 2) {
             return false;
         }
-        
         int maxCount = Collections.max(frequencyCounter.keySet());
         int minCount = Collections.min(frequencyCounter.keySet());
         
-        return (minCount == 1 && frequencyCounter.get(minCount) == 1) ||
-               (maxCount - minCount == 1 && frequencyCounter.get(maxCount) == 1);
+        return (minCount==1 && frequencyCounter.get(minCount) == 1) ||
+            (maxCount - minCount == 1) && frequencyCounter.get(maxCount)==1;
     }
 }
+
