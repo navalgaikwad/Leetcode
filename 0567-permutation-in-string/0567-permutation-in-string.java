eb5d591786635b3e2feab60bd74c91ec40683583
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-12 14:32:59 -0500

    Time: 25 ms (39.30%), Space: 43 MB (39.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,34 +1,37 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-       Map<Character, Integer> map = new HashMap<>();
-        for(char c: s1.toCharArray()) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : s1.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int left = 0;
-        int right = 0;
-         while(right < s2.length()) {
-            char c = s2.charAt(right);
-            if(map.containsKey(c)) {
-                right++;
-                int count = map.get(c);
-                if(count == 1)
-                    map.remove(c);
-                else {
-                    map.put(c, count - 1);
+        int counter =map.size();
+        int left =0;
+       // int right =0;
+        for(int right = 0; right < s2.length(); right++) {
+            
+         if(map.containsKey(s2.charAt(right))) {
+             map.put(s2.charAt(right), map.get(s2.charAt(right))-1);
+             if(map.get(s2.charAt(right)) == 0) {
+                 counter--;
+             }
+         }
+         
+            while(counter == 0) {
+                if(right - left +1 == s1.length()) {
+                    return true;
                 }
-            }else {
-                if( left == right) {
-                    left++;
-                    right++;
-                }else {// to make j equal to i 
-                    map.put(s2.charAt(left), map.getOrDefault(s2.charAt(left), 0) + 1);
-                    left++;
-                }
-            }
-            if(map.isEmpty()) {
-                return true;
-            }
+              if(map.containsKey(s2.charAt(left))) {
+             map.put(s2.charAt(left), map.get(s2.charAt(left))+1);
+             if(map.get(s2.charAt(left))> 0) {
+                 counter++;
+             }
+         }
+            left++;
         }
-        return false;
+         
+            
+        //return false;    
     }
+        return false;  
+}
 }
