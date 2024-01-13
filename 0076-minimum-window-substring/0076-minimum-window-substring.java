3848436b2e80851825811df20c1eaeb4a33493b5
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-13 13:23:33 -0500

    Time: 62 ms (24.56%), Space: 45.4 MB (16.30%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,31 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
+        int left =0;
+       // int right =0;
         Map<Character, Integer> map = new HashMap<>();
         for(char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int left =0;
         int counter = map.size();
         int len = Integer.MAX_VALUE;
-
         String ans = "";
-        for(int right =0; right<s.length(); right++) {
-           
-            if(map.containsKey(s.charAt(right))) {
-                map.put(s.charAt(right), map.get(s.charAt(right)) - 1);
-                if(map.get(s.charAt(right)) == 0) {
+        for(int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            if(map.containsKey(rightChar)) {
+                map.put(rightChar, map.getOrDefault(rightChar, 0) - 1);
+                if(map.get(rightChar) == 0) {
                     counter--;
                 }
             }
             
             while(counter == 0) {
-                 if (right - left < len) {
-                    len = right - left ;
+                if(right - left + 1 < len) {
+                    len = right - left + 1;
                     ans = s.substring(left, right + 1);
                 }
-                if(map.containsKey(s.charAt(left))) {
-                    map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
-                    if(map.get(s.charAt(left)) > 0) {
+                
+                char leftChar = s.charAt(left);
+                if(map.containsKey(leftChar)) {
+                    map.put(leftChar, map.getOrDefault(leftChar, 0) + 1);
+                    if(map.get(leftChar) >0) {
                         counter++;
                     }
                 }
@@ -35,3 +37,6 @@ class Solution {
         return ans;
     }
 }
+//1. first i will take count
+//2. i will go chacryer by character to find the each charcter untill that value in the map count =0
+//3. onces i found i will take length and string
