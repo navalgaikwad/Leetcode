6959fe9c0c8e7bac0666dced14e39b79cdaa8f25
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-13 18:13:23 -0500

    Time: 4 ms (99.38%), Space: 48 MB (6.13%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,20 +1,19 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int j=0;
-        int max=0;
-        int product=1;
-        if(k<=1){
-            return 0;
-        }
-        for(int i=0; i<nums.length; i++){
-            product=product*nums[i];
-            while(product>=k){
-               product= product/nums[j];
-               j++;
+        int left =0;
+        int product =1;
+        int len = 0;
+        if(k<=1) return 0;
+       // Map<Integer, Inetegr> map = new HashMap<>();
+        for(int right =0; right < nums.length; right++) {
+            product *= nums[right];
+            while(product>=k && left < nums.length) {
+                product/=nums[left];
+                left++;
             }
-            max=max+ i-j+1;
+            len +=right - left + 1;
         }
         
-        return max;
+        return len < 0? 0 : len;
     }
 }
