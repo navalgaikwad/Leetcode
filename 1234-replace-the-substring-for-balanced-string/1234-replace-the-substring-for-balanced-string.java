1dd9f35e5b48ea29d97b6444ed44039d2e353307
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-14 18:36:00 -0500

    Time: 8 ms (86.73%), Space: 44.2 MB (49.29%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.java
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.java
@@ -1,17 +1,35 @@
 class Solution {
-public int balancedString(String s) {
+    public int balancedString(String s) {
         int[] count = new int[128];
-        int n = s.length(), res = n, i = 0, k = n / 4;
-        for (int j = 0; j < n; ++j) {
-            ++count[s.charAt(j)];
+        for (char c : s.toCharArray()) {
+            ++count[c];
         }
-        for (int j = 0; j < n; ++j) {
-            --count[s.charAt(j)];
-            while (i < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k && count['R'] <= k) {
-                res = Math.min(res, j - i + 1);
-                ++count[s.charAt(i++)];
+        int left = 0;
+        int k = s.length() / 4;
+        int len = s.length();
+        for (int right = 0; right < s.length(); right++) {
+            --count[s.charAt(right)];
+            while (left < s.length() && count['Q'] <= k && count['W'] <= k && count['E'] <= k&& count['R'] <= k) {
+                len = Math.min(len, right - left + 1);
+                ++count[s.charAt(left)];
+                left++;
             }
         }
-        return res;
+        return len;
     }
 }
+// public int balancedString(String s) {
+//         int[] count = new int[128];
+//         int n = s.length(), res = n, i = 0, k = n / 4;
+//         for (int j = 0; j < n; ++j) {
+//             ++count[s.charAt(j)];
+//         }
+//         for (int j = 0; j < n; ++j) {
+//             --count[s.charAt(j)];
+//             while (i < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k && count['R'] <= k) {
+//                 res = Math.min(res, j - i + 1);
+//                 ++count[s.charAt(i++)];
+//             }
+//         }
+//         return res;
+//     }
