0ee832109400660da1fe42cea574de65a4095150
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-14 18:36:30 -0500

    Time: 8 ms (86.73%), Space: 44.5 MB (38.86%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.java
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.java
@@ -2,16 +2,16 @@ class Solution {
     public int balancedString(String s) {
         int[] count = new int[128];
         for (char c : s.toCharArray()) {
-            ++count[c];
+            count[c]++;
         }
         int left = 0;
         int k = s.length() / 4;
         int len = s.length();
         for (int right = 0; right < s.length(); right++) {
-            --count[s.charAt(right)];
+            count[s.charAt(right)]--;
             while (left < s.length() && count['Q'] <= k && count['W'] <= k && count['E'] <= k&& count['R'] <= k) {
                 len = Math.min(len, right - left + 1);
-                ++count[s.charAt(left)];
+                count[s.charAt(left)]++;
                 left++;
             }
         }
