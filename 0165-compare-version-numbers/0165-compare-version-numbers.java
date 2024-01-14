3fbb036f96db9ebbafc6f066d5c3f91a62b34529
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-14 01:48:51 -0500

    Time: 1 ms (82.77%), Space: 41.3 MB (30.41%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -2,7 +2,12 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] version1Array = version1.split("\\.");
         String[] version2Array = version2.split("\\.");
-
+        //seperate by .
+        //two array 1 and 2
+        //i and j<array1 and array2 i++ and j++
+        //array1 length > array2 len i<array1 num1>0 return 1
+        //array2 length > array1 len j<array2 num2>0 return 1
+        //other wise zero
         int i = 0, j = 0;
 
         while (i < version1Array.length && j < version2Array.length) {
