9119f9bdded09db4c8d0bd3465e2b74145d8c769
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-15 11:23:57 -0500

    Time: 569 ms (11.02%), Space: 46.1 MB (5.25%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,21 +1,19 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        return longestSub(s, k, 0, s.length() - 1);
+        return longestSubString(s, k, 0, s.length() - 1);
     }
-    
-    int longestSub(String s, int k, int start, int end) {
-        if(end - start + 1 < k) {
+    int longestSubString(String s, int k, int start, int end) {
+        if(end-start + 1<k) {
             return 0;
         }
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = start; i <= end; i++) {
+        Map<Character, Integer> map =new HashMap<>();
+        for(int i = start; i<=end; i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
-        for(int  i = start; i <= end; i++) {
-            char c = s.charAt(i);
-            if(map.get(c) < k) {
-                int left = longestSub(s, k, start, i - 1);
-                int right = longestSub(s, k, i + 1, end);
+        for(int i=start; i<=end;i++) {
+            if(map.get(s.charAt(i)) < k) {
+                int left = longestSubString(s, k, start, i - 1);
+                int right = longestSubString(s, k, i+1, end);
                 return Math.max(left, right);
             }
         }
