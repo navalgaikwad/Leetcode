7fbb7659d02a00915cac987e9a644c4331aac393
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-15 13:45:44 -0500

    Time: 1126 ms (5.00%), Space: 45.6 MB (29.92%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,24 +1,34 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length + 1];
-        Arrays.fill(dp, -1);
-        dp[0] = nums[0];
-        int result =0;
-        for(int i =0; i<=nums.length; i++) {
-            if(dp[i] != -1) {
-                for(int coin =1; coin<=nums[i]; coin++) {
-                    int nextPosition = i + coin;
-                    if(nextPosition < nums.length) {
-                        dp[nextPosition] = nextPosition;
-                    }
-                }
+        double[] dp = new double[nums.length + 1];
+        Arrays.fill(dp, Double.MAX_VALUE);
+        dp[0] = 0;
+        for(int i =0; i<nums.length; i++) {
+            if(dp[i]!=Double.MAX_VALUE) {
+               for(int j =0; j<=nums[i]; j++) {
+                   int nextPosition = i + j;
+                   if(nextPosition < nums.length) {
+                       dp[nextPosition] = 1 + dp[i];
+                   }
+               } 
             }
         }
-         return dp[nums.length - 1] == -1? false : true;
+        System.out.print(dp[nums.length-1]);
+        return dp[nums.length-1] == Double.MAX_VALUE? false : true;
     }
 }
-
-// //nums = [2,3,1,1,4]
-//          [2,-1,-1,-1,4]
-// max = 2, 3
-//     1+3 = 4
+// int[] dp = new int[nums.length + 1];
+//         Arrays.fill(dp, -1);
+//         dp[0] = nums[0];
+//         int result =0;
+//         for(int i =0; i<=nums.length; i++) {
+//             if(dp[i] != -1) {
+//                 for(int coin =1; coin<=nums[i]; coin++) {
+//                     int nextPosition = i + coin;
+//                     if(nextPosition < nums.length) {
+//                         dp[nextPosition] = nextPosition;
+//                     }
+//                 }
+//             }
+//         }
+//          return dp[nums.length - 1] == -1? false : true;
