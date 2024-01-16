4dca533a648d66f29b040559ad4ec35b75c7e800
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-16 15:02:28 -0500

    Time: 15 ms (19.61%), Space: 45.1 MB (7.84%) - LeetHub

---
+++ 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.java
@@ -0,0 +1,19 @@
+class Solution {
+    //start from first and end
+    //
+    public int longestDecomposition(String S) {
+        int res = 0, n = S.length();
+        String l ="", r ="";
+        for(int i = 0; i<n; i++) {
+            l = l + S.charAt(i);
+            r = S.charAt(n - i - 1) + r; 
+            if(l.equals(r)) {
+                res++;
+                l="";
+                r="";
+            }
+        }
+        return res;
+    }
+}
+
