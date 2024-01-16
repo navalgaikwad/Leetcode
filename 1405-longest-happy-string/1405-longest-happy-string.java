c607a3315a3d65f39f8f23b333f2a82062070569
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-15 23:38:28 -0500

    Time: 0 ms (100.00%), Space: 41.3 MB (12.01%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.java
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -5,32 +5,25 @@ class Solution {
         int c_count=0;
         StringBuilder sb = new StringBuilder();
         int total = a+b+c;
-        for(int i=0;i<total;i++){
-            if((a>=b && a>=c && a_count<2) || (b_count==2 && a>0) 
-              || (c_count==2 && a>0)){
+        for(int i=0; i<total; i++) {
+            if((a>=b && a>=c &&a_count<2 )|| (b_count == 2&& a>0) || (c_count == 2&& a>0)) {
                 sb.append("a");
                 a_count++;
                 a--;
-                 b_count=0;
-                 c_count=0;
-                
-            } else  if((b>=a && b>=c && b_count<2) || (a_count==2 && b>0) 
-              || (c_count==2 && b>0)){
-                 sb.append("b");
+                b_count=0;
+                c_count =0;
+            }else if( (b>=a && b>=c &&b_count<2) || (a_count == 2&& b>0) || (c_count == 2&& b>0)) {
+                sb.append("b");
                 b_count++;
                 b--;
-                 c_count=0;
-                 a_count=0;
-                
-            } else  if((c>=b && c>=a && c_count<2) || (b_count==2 && c>0) 
-              || (a_count==2 && c>0)){
-                 
-                     sb.append("c");
+                a_count=0;
+                c_count =0;
+            } else if( (c>=b && c>=a &&c_count<2) || (b_count == 2&& c>0) || (a_count == 2&& c>0)) {
+                sb.append("c");
                 c_count++;
                 c--;
-                 b_count=0;
-                 a_count=0;
-                
+                b_count=0;
+                a_count =0;
             }
         }
         return sb.toString();
