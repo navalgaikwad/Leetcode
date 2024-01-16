90c9abed00107bfbbbff6c63794ac0d3f2834dfb
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-16 15:32:45 -0500

    Time: 40 ms (77.38%), Space: 47.8 MB (20.83%) - LeetHub

---
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+        Map<String, Integer> map = new HashMap();
+        
+        int res = 0;
+        
+        for (int i = 0; i + minSize - 1 < s.length(); i++) {
+            String subs = s.substring(i, i + minSize);
+            map.put(subs, map.getOrDefault(subs, 0) + 1);
+        }
+        
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            Set<Character> chrs = new HashSet();
+            for (char c : e.getKey().toCharArray()) {
+                chrs.add(c);
+            }
+            
+            if (chrs.size() <= maxLetters) {
+                res = Math.max(res, e.getValue());
+            }
+        }
+        
+        return res;
+    }
+}
