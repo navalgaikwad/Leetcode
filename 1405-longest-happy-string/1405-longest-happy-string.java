c01989b8f4bb83e6b8004738d1013e7f66ea6966
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-15 23:37:51 -0500

    Time: 0 ms (100.00%), Space: 40.2 MB (70.87%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.java
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -1,29 +1,36 @@
 class Solution {
     public String longestDiverseString(int a, int b, int c) {
-        int a_count =0;
-        int b_count =0;
-        int c_count =0;
-        int total = a+b+c;
+        int a_count=0;
+        int b_count=0;
+        int c_count=0;
         StringBuilder sb = new StringBuilder();
-        for(int i =0; i<total; i++) {
-            if( (a>=b && a>=c && a_count < 2) || (b_count==2 & a > 0) || (c_count==2 && a > 0)) {
+        int total = a+b+c;
+        for(int i=0;i<total;i++){
+            if((a>=b && a>=c && a_count<2) || (b_count==2 && a>0) 
+              || (c_count==2 && a>0)){
                 sb.append("a");
                 a_count++;
                 a--;
-                b_count=0;
-                c_count=0;
-            }else if( (b>=a && b>=c && b_count < 2) || (a_count==2 & b > 0) || (c_count==2 && b > 0)) {
-                sb.append("b");
+                 b_count=0;
+                 c_count=0;
+                
+            } else  if((b>=a && b>=c && b_count<2) || (a_count==2 && b>0) 
+              || (c_count==2 && b>0)){
+                 sb.append("b");
                 b_count++;
                 b--;
-                a_count=0;
-                c_count=0;
-            }else if( (c>=a && c>=b && c_count < 2) || (a_count==2 & c > 0) || (b_count==2 && c > 0)) {
-                sb.append("c");
+                 c_count=0;
+                 a_count=0;
+                
+            } else  if((c>=b && c>=a && c_count<2) || (b_count==2 && c>0) 
+              || (a_count==2 && c>0)){
+                 
+                     sb.append("c");
                 c_count++;
                 c--;
-                b_count=0;
-                a_count=0;
+                 b_count=0;
+                 a_count=0;
+                
             }
         }
         return sb.toString();
