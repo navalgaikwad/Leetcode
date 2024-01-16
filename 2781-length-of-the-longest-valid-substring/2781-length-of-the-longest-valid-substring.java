57a14d572aa75b1bacef2bd543eda4a97d379632
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-16 16:31:03 -0500

    Time: 137 ms (71.58%), Space: 61.5 MB (57.71%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
@@ -1,29 +1,22 @@
-import java.util.HashSet;
-import java.util.List;
-
-public class Solution {
+class Solution {
+    //slidding window
+    //insider loop for(int k=right; k>=Math.max(left, right - 9); k--) {
     public int longestValidSubstring(String word, List<String> forbidden) {
-        HashSet<String> set = new HashSet<>();
-        for (String s : forbidden) {
-            set.add(s);
-        }
-
+        HashSet<String> set = new HashSet<>(forbidden);
+        int left=0;
+        int right =0;
         int n = word.length();
-        int ans = 0;
-        int left = 0;
-        int right = 0;
-
-        while (right < n) {
-            for (int k = right; k >= Math.max(left, right - 9); k--) {
-                if (set.contains(word.substring(k, right + 1))) {
-                    left = k + 1;
+        int ans =0;
+        for(;right<n; right++) {
+            for(int k=right; k>=Math.max(left, right - 9); k--) {
+                if(set.contains(word.substring(k, right+1))) {
+                    left = k +1;
                     break;
                 }
             }
             ans = Math.max(right - left + 1, ans);
-            right++;
+            //right++;
         }
-
         return ans;
     }
 }
