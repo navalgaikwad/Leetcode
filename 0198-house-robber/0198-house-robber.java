2442ed2bd74f1e6025b24fbe26da95aa7cdcd89d
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-21 19:22:29 -0500

    Time: 0 ms (100.00%), Space: 41.3 MB (7.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-       int[] dp = new int[nums.length+1];
         int len = nums.length;
-        if(len<2){
+        int[] dp = new int[len + 1];
+         if(len<2){
             return nums[0];
         }
+        //Arrays.fill(dp , 0);
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
-        
-        for(int i=2; i<nums.length; i++) {
-            int currentHouse = nums[i] + dp[i - 2];
-            int previousHouse = dp[i-1];
-            dp[i] = Math.max(currentHouse, previousHouse);
+        for(int i = 2; i < len; i++) {
+            int currentRobAndPrevious = nums[i] + dp[i - 2];
+            int previous = 0 + dp[i-1];
+            dp[i] = Math.max(currentRobAndPrevious, previous);
         }
-        return dp[nums.length-1];
+        
+        return dp[len - 1];
     }
 }
