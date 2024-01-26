64bcb397bb9a558ba0e5c4dd33a7f87caf09d443
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-26 13:38:32 -0500

    Time: 0 ms (100.00%), Space: 42.1 MB (32.74%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        helper(root, 0);
+        return result;
+    }
+    
+    void helper(TreeNode root, int level) {
+        if(root == null) {
+           return;
+        }
+        if( result.size()==level){
+            result.add(root.val);
+        }
+        helper(root.right, level+1);
+        helper(root.left, level+1);
+    }
+}
