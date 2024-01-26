ef93820f211235ee45938336bcdd9d6b2b5607ef
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-26 00:57:48 -0500

    Time: 5 ms (5.28%), Space: 43 MB (5.81%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,36 +1,50 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int left = 0;
-        int right = rows * cols - 1; // Calculate the right boundary based on the total number of elements
-
-       return binarySearch(matrix, target, 0, right);
+        int len = matrix[0].length;
+        int[] column = new int[len];
+        for(int i=0; i<len; i++) {
+            column[i] = i;
+        }
+        List<Integer> list = new ArrayList<>();
+        //int[] nums = new int[matrix.length*matrix.length+2];
+        int j =0;
+        for(int i=0; i<matrix.length; i++) {
+            for(int col : column) {
+                list.add(matrix[i][col]);
+            }
+            Collections.reverse(Arrays.asList(column)); 
+        }
+      
+          System.out.print(list);  
+      
+        return binarySearch(list, target);
     }
     
-    boolean binarySearch(int[][] matrix, int target, int left, int right) {
-        
+    boolean binarySearch(List<Integer> nums, int target) {
+        int left = 0;
+        int right = nums.size() -1;
         while(left <= right) {
-            int mid = left + (right - left) / 2;
-            int row = mid / matrix[0].length;
-            int col = mid % matrix[0].length;
-            
-            if( matrix[row][col] == target) {
+            int mid = left + (right - left)/2;
+            if(nums.get(mid) == target){
                 return true;
             }
-            if(matrix[row][col] > target) {
-                right = mid - 1;
-            }else {
-                left = mid + 1;
+            if(nums.get(left) <= nums.get(mid)) {// normal sorted//starting and middle
+               if(nums.get(left)  <= target && nums.get(mid) > target ) { // normal condition
+                    right = mid - 1;
+                }else {
+                    left = mid + 1;
+                } 
+            } else {
+            if(nums.get(right) >= nums.get(mid)) {// right side normal sorted last and miidle
+               if(nums.get(right) >= target && nums.get(mid) < target ) {
+                    left = mid + 1;
+                }else {
+                    right = mid - 1;
+                } 
+              }
             }
+            
         }
         return false;
     }
 }
-
-//[
-//0 ->[1,3,5,7],
-//1 ->[10,11,16,20],
-//2 ->[23,30,34,60]
-//]
