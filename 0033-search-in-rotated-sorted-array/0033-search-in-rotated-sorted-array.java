5fb305740e37848496a6fe2771234e42f2d82508
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-26 05:11:31 -0500

    Time: 0 ms (100.00%), Space: 41.6 MB (64.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,33 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
-         int result = binarySearch(nums, target);
-        return result;
+        return binarySearch(nums, target);
     }
-    //11,9,8,1,2,3,4,5
+    
     int binarySearch(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length -1;
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target){
+        int start =0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target) {
                 return mid;
             }
-            if(nums[left] <= nums[mid]) {// normal sorted//starting and middle
-               if(nums[left] <= target && nums[mid] > target ) { // normal condition
-                    right = mid - 1;
+            //4,5,6,7,0,1,2
+            if(nums[start]<=nums[mid]) {
+                if(nums[start] <= target && nums[mid] > target) {
+                    end = mid - 1;
                 }else {
-                    left = mid + 1;
-                } 
-            } else {
-            if(nums[right] >= nums[mid]) {// right side normal sorted last and miidle
-               if(nums[right] >= target && nums[mid] < target ) {
-                    left = mid + 1;
-                }else {
-                    right = mid - 1;
-                } 
-              }
+                    start = mid+1;
+                }
+            }else {
+                if(nums[end]>=nums[mid]) {
+                    if(nums[end] >= target && nums[mid] < target) {
+                        start = mid + 1;
+                    }else {
+                        end = mid-1;
+                    }
+                }
             }
-            
         }
         return -1;
     }
