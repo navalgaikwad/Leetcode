dd459ce4399e6edcfd15c70360ed73eb98f0af94
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-27 23:52:38 -0500

    Time: 0 ms (100.00%), Space: 45.7 MB (15.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,26 +1,24 @@
 class Solution {
-      public void setZeroes(int[][] matrix) {
+    public void setZeroes(int[][] matrix) {
         int col0 = 1;
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        // Iterate through the matrix and mark the first element of each row and column
-        // where a zero is encountered.
-        for (int i = 0; i < rows; i++) {
-            if (matrix[i][0] == 0) col0 = 0;
-            for (int j = 1; j < cols; j++) {
-                if (matrix[i][j] == 0) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        //1. first add 0 to top and left
+        //2. when ever i find top zero it will add in bottom 
+        //3. when ever i find left zero add it in right side
+        for(int i=0; i<row; i++) {
+            if(matrix[i][0] == 0) col0 = 0;
+            for(int j =1; j<col; j++) {
+                if(matrix[i][j] == 0) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
-
-        // Iterate through the matrix in reverse and update the elements based on the
-        // marked first elements of rows and columns.
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = cols - 1; j >= 1; j--) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        
+        for(int i=row-1; i>=0; i--) {
+            for(int j=col-1; j>=1; j--) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
