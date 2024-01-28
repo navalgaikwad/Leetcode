093ceae9ba863e6ed6ed842d4827aabb255c277b
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-28 16:54:33 -0500

    Time: 2 ms (52.22%), Space: 45.3 MB (5.53%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -5,21 +5,41 @@ import java.util.Arrays;
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<int[]> mergedList = new ArrayList<>();
-        
-        for (int i = 0; i < intervals.length; i++) {
-            if (newInterval[1] < intervals[i][0]) { // if it's at the start
+        //1. if it is at first
+        //2. no overlapping
+        //3. update the newIntegervals
+        for(int i=0; i<intervals.length; i++) {
+            if(newInterval[1] < intervals[i][0]) {
                 mergedList.add(newInterval);
                 Collections.addAll(mergedList, Arrays.copyOfRange(intervals, i, intervals.length));
                 return mergedList.toArray(new int[mergedList.size()][]); 
-            } else if (intervals[i][1] < newInterval[0]) { // in the middle
+            }else if(intervals[i][1] < newInterval[0]) {
                 mergedList.add(intervals[i]);
-            } else { // overlapping 
+            }else {
                 newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
                 newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             }
         }
-        
         mergedList.add(newInterval);
         return mergedList.toArray(new int[mergedList.size()][]);  
     }
 }
+// class Solution {
+//     public int[][] insert(int[][] intervals, int[] newInterval) {
+//         for(int[] interval : intervals) {
+//             if(newInterval[0] < interval[1]) {
+//                 interval[1] = Math.max(interval[1], newInterval[0]);
+//                 interval[0] = Math.min(interval[0], newInterval[0]);
+//             }
+//         }
+//         return intervals;
+//     }
+// }
+
+// intervals = [[1,3],[6,9]], newInterval = [4,5]
+//[1, ]
+//2<3 merge start2 < end1
+//4<3 not merge 
+
+//intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+//[1,2]
