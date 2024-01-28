b1dddaebd8a47e8eb18ed38683c97c6ff786e3f6
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-28 00:30:09 -0500

    Time: 32 ms (72.85%), Space: 42.1 MB (85.94%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,15 +1,13 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
+      public boolean canPartition(int[] nums) {
         int sum = Arrays.stream(nums).sum();
         
         if(sum % 2 != 0) {
             return false;
         }
-        
-       
+    
         sum /= 2;
         boolean[] dp = new boolean[sum + 1];
-        Arrays.fill(dp, false);
         dp[0] = true;
         for(int i=0; i<nums.length; i++) {
             for(int j = sum; j>=nums[i]; j--) {
@@ -20,4 +18,19 @@ class Solution {
         }
         return dp[sum];
     }
+
+    // static boolean tabulation(int target, int[] num) {
+    //     boolean[] table = new boolean[target + 1];
+    //     table[0] = true;
+    //     for (int i = 0; i < target; i++) {  // Fix: Change i <= target to i < target
+    //         if (table[i]) {
+    //             for (int nu : num) {
+    //                 if (i + nu <= target) {
+    //                     table[i + nu] = true;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return table[target];
+    // }
 }
