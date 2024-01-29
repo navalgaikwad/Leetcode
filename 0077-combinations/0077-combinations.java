c96277801b6553178a173a56e3a32a560ed59840
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-29 15:41:04 -0500

    Time: 18 ms (68.48%), Space: 94.6 MB (39.79%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,20 +1,40 @@
 class Solution {
+    List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        backTracking(result, n, k, new ArrayList<>(), 1);
+        helper(n, k, new ArrayList<>(), 1);
         return result;
-        
     }
-    
-    void backTracking(List<List<Integer>> result, int n, int k, List<Integer> ans, int index) {
-        if(ans.size() == k) {
-            result.add(new ArrayList<>(ans));
-            return;
+    void helper(int n, int k, ArrayList<Integer> runningList, int index) {
+        if(runningList.size() == k) {   
+            result.add(new ArrayList<>(runningList));
         }
         for(int i = index; i<=n; i++) {
-            ans.add(i);
-            backTracking(result, n, k , ans, i + 1);
-            ans.remove(ans.size() -1);
+            //if(!runningList.contains(i)) {
+                runningList.add(i);
+                helper(n, k,runningList, i+1);
+                runningList.remove(runningList.size()-1);
+            //}
         }
     }
 }
+
+// class Solution {
+//     public List<List<Integer>> combine(int n, int k) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         backTracking(result, n, k, new ArrayList<>(), 1);
+//         return result;
+        
+//     }
+    
+//     void backTracking(List<List<Integer>> result, int n, int k, List<Integer> ans, int index) {
+//         if(ans.size() == k) {
+//             result.add(new ArrayList<>(ans));
+//             return;
+//         }
+//         for(int i = index; i<=n; i++) {
+//             ans.add(i);
+//             backTracking(result, n, k , ans, i + 1);
+//             ans.remove(ans.size() -1);
+//         }
+//     }
+// }
