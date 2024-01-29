afb03caa6f6f2bf814048616ceecaa3aed9001de
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-29 12:47:42 -0500

    Time: 9 ms (61.27%), Space: 46.5 MB (34.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -26,3 +26,24 @@ class Solution {
 
 //[[1,4],[3,5]]
 //[1, 5] end < satrt
+
+
+// class Solution {
+//     public int[][] insert(int[][] intervals, int[] newInterval) {
+//         for(int[] interval : intervals) {
+//             if(newInterval[0] < interval[1]) {
+//                 interval[1] = Math.max(interval[1], newInterval[0]);
+//                 interval[0] = Math.min(interval[0], newInterval[0]);
+//             }
+//         }
+//         return intervals;
+//     }
+// }
+
+// // intervals = [[1,3],[6,9]], newInterval = [4,5]
+// //[1, ]
+// //2<3 merge start2 < end1
+// //4<3 not merge 
+
+// //intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// //[1,2]
