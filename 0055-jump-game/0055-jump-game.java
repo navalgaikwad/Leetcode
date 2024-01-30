823956ac6d8dd2656597a2092ff914a90db3dda7
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-30 16:22:02 -0500

    Time: 1133 ms (5.02%), Space: 45.9 MB (14.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,34 +1,23 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        double[] dp = new double[nums.length + 1];
-        Arrays.fill(dp, Double.MAX_VALUE);
+        int[] dp = new int[nums.length + 1];
+        int len = nums.length;
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i =0; i<nums.length; i++) {
-            if(dp[i]!=Double.MAX_VALUE) {
-               for(int j =0; j<=nums[i]; j++) {
-                   int nextPosition = i + j;
-                   if(nextPosition < nums.length) {
-                       dp[nextPosition] = 1 + dp[i];
-                   }
-               } 
+        for(int i=0; i<len; i++) {
+            if(dp[i]!=Integer.MAX_VALUE) {
+                for(int coin=1; coin<=nums[i]; coin++) {
+                    int nextPosition = i + coin;
+                    if(nextPosition < len) {
+                        dp[nextPosition] = 1+ dp[i];
+                    }
+                }
             }
+            
         }
-        System.out.print(dp[nums.length-1]);
-        return dp[nums.length-1] == Double.MAX_VALUE? false : true;
+        return dp[len - 1] == Integer.MAX_VALUE ? false : true;
+        
     }
 }
-// int[] dp = new int[nums.length + 1];
-//         Arrays.fill(dp, -1);
-//         dp[0] = nums[0];
-//         int result =0;
-//         for(int i =0; i<=nums.length; i++) {
-//             if(dp[i] != -1) {
-//                 for(int coin =1; coin<=nums[i]; coin++) {
-//                     int nextPosition = i + coin;
-//                     if(nextPosition < nums.length) {
-//                         dp[nextPosition] = nextPosition;
-//                     }
-//                 }
-//             }
-//         }
-//          return dp[nums.length - 1] == -1? false : true;
+
+// [2,3,1,1,4]
