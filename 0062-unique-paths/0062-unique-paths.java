262117e237d74af80b27adfc9d8c3d12c4da40af
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-29 19:00:35 -0500

    Time: 7 ms (15.33%), Space: 42 MB (5.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       // Integer[][] memo = new Integer[m][n];
-        HashMap<String, Integer> memo = new HashMap<>();
-        return memoDP(m - 1, 0, m, n, memo);
+        return dp(m-1, n-1, new HashMap<>());
     }
-
-    private int memoDP(int i, int j, int m, int n, HashMap<String, Integer> memo) {
-        String key = i+"-"+j;
-        if (i == 0 && j == n - 1) {
+    int dp(int m, int n, HashMap<String, Integer> memo) {
+        String key = m+"-"+n;
+        if(m == 0 && n ==0) {
             return 1;
-        }
-        if (i < 0 || j >= n) {
+        } 
+        if(m < 0 || n < 0) {
             return 0;
-        }
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-        int down = memoDP(i - 1, j, m, n, memo);
-        int right = memoDP(i, j + 1, m, n, memo);
-        memo.put(key, down + right) ;
-        return down + right;
+        } 
+        if(memo.containsKey(key)) return memo.get(key);
+        int left = dp(m -1, n, memo);
+        int right = dp(m, n-1, memo);
+        
+        int result = left + right;
+        memo.put(key, result);
+        return result;
     }
 }
