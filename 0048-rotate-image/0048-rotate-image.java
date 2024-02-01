7612bd5a981a16a46fb360890d952046fc512cd6
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-01-31 19:04:06 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (23.99%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    /*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+    public void rotate(int[][] matrix) {
+        // Reverse the matrix
+       for(int i=0; i<matrix.length/2; i++) {
+           int[] temp = matrix[i];
+           matrix[i] = matrix[matrix.length - i - 1];
+           matrix[matrix.length - i - 1] = temp;
+       }
+        
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=i+1; j<matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
