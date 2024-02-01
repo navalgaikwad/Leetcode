cf83be26f9185553ed31348fdf3e7356fbb708e0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-01 13:32:37 -0500

    Time: 147 ms (59.05%), Space: 41.5 MB (56.05%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-         boolean[][] visited = new boolean[board.length][board[0].length];
-        for(int i =0; i<board.length; i++) {
-            for(int j =0; j<board[0].length; j++) {
-               
-                if(board[i][j]==word.charAt(0)) {
-                    
-                    if(dfs(board, word, i, j, 0) ){
+        int m = board.length;
+        int n = board[0].length;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(board[i][j] == word.charAt(0)) {
+                    if(helper(board, word, 0, i, j)) {
                         return true;
                     }
                 }
@@ -15,23 +14,21 @@ class Solution {
         return false;
     }
     
-    boolean dfs(char[][] board, String word, int i, int j, int index) {
-        if(index >= word.length()) {
+    boolean helper(char[][] board, String word, int index, int i, int j) {
+        if(word.length() == index) {
             return true;
         }
-        if(i >=board.length || i< 0 || j >= board[0].length || j<0 
-           || word.charAt(index)!= board[i][j] || board[i][j]=='$') {
+        if(i <0 || i>= board.length || j<0 || j>=board[0].length || board[i][j]!=word.charAt(index) || board[i][j]=='$') {
             return false;
         }
-        
         char c = board[i][j];
-        //visited[i][j] = true;
-        board[i][j] ='$';
-        boolean flag = dfs(board, word, i+1, j, index+1) || 
-                       dfs(board, word, i-1, j, index+1) || 
-                       dfs(board, word, i, j-1, index+1) || 
-                       dfs(board, word, i, j+1, index+1);
+        board[i][j] = '$';
+        boolean up = helper(board, word, index+1, i+1, j);
+        boolean left = helper(board, word, index+1, i-1, j);
+        boolean right = helper(board, word, index+1, i, j+1);
+        boolean down = helper(board, word, index+1, i, j-1);
         board[i][j] = c;
-        return flag;
+        
+        return up||left||right||down;
     }
 }
