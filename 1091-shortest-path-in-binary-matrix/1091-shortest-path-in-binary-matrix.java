7beec10c04c56305f4fd486f621f2844f008b1f2
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-01 14:18:18 -0500

    Time: 118 ms (5.90%), Space: 47 MB (5.47%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,44 +1,31 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if (grid[0][0] != 0) return -1;
-        return bfs(grid, 0, 0);
-    }
-    
-    boolean isValid(int[][] grid, int i, int j, boolean[][] visited) {
-        int len = grid.length;
-        return i >= 0 && i < len && j >= 0 && j < grid[0].length && !visited[i][j] && grid[i][j] == 0;
-    }
-    int bfs(int[][] grid, int i, int j) {
-        int[][] direction = new int[][]{{0,-1}, {-1,0}, {1,1}, {-1, -1}, {1, 0}, {0, 1}, {-1, 1}, {1, -1}};
-        
-        int len = grid.length;
-        
-        boolean[][] visited = new  boolean[len][len];
+        int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}, {-1,-1}, {1, 1}, {-1, 1}, {1, -1}};
+         if (grid[0][0] == 1) {
+            return -1; 
+        }
         Queue<int[]> q = new LinkedList<>();
-        
-        q.add(new int[]{i, j, 1});
-        visited[i][j] = true; //make sure to add visited true whenever you add value in the queue
-        
+        q.add(new int[]{0, 0, 1});
+        HashSet<String> visited = new HashSet<>();
+        //visited.add("0-0");
+        int m = grid.length;
+        int n = grid[0].length;
         while(!q.isEmpty()) {
+            int size = q.size();
             
             int[] current = q.remove();
-            
-            int row = current[0];
-            int col = current[1];
-            int level =current[2];
-            
-            if(row == len - 1 && col == len - 1) {
+            int i = current[0];
+            int j = current[1];
+            int level = current[2];
+            if(i == m - 1 && j == n - 1) {
                 return level;
             }
-            
-            for(int[] dir : direction) {
-                
-                int newX = row + dir[0];
-                int newY = col + dir[1];
-                
-                if(isValid(grid, newX, newY, visited)) {
-                    visited[newX][newY] = true;
-                    q.add(new int[]{newX, newY, level + 1});
+            for(int[] dir :dirs ) {
+                int x = i + dir[0];
+                int y = j + dir[1];
+                if(x >=0 && x<m && y>=0 && y<n && grid[x][y] !=1 && !visited.contains(x+"-"+y)) {
+                    visited.add(x+"-"+y);
+                    q.add(new int[]{x, y, level+1});
                 }
             }
         }
