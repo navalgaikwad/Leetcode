023ccac54d3d3bfba7910f38c92fae13c48279e6
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-02 13:49:46 -0500

    Time: 47 ms (77.72%), Space: 73.7 MB (55.64%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,22 +1,17 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, (a, b) ->a[0] - b[0]);
-        int count =0;
         int endTime = intervals[0][1];
-        for(int i=1; i<intervals.length; i++) {
+        int len = intervals.length;
+        int count =0;
+        for(int i=1; i<len; i++) {
             if(intervals[i][0] < endTime) {
                 count++;
-                endTime = Math.min(endTime, intervals[i][1]);
+                endTime = Math.min(intervals[i][1], endTime);
             }else {
-                endTime = intervals[i][1];
+                endTime = intervals[i][1];//update
             }
-            
         }
         return count;
     }
 }
-//100, 11
-//[[1,100],[1,11],[2,12],[11,22],]
-//1 < 100 - 1
-//2<11 -2
-//11<11-2
