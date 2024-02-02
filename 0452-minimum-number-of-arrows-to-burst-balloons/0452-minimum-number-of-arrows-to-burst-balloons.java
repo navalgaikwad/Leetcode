ca252b17af376bded3bb1959f1547cbbf25daa0f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-02 12:45:33 -0500

    Time: 52 ms (89.21%), Space: 70.1 MB (34.86%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,20 +1,22 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-       Arrays.sort(points, (a, b)->Integer.compare(a[1], b[1]));
-        int[] prev = points[0];
+        Arrays.sort(points, (a,b)->Integer.compare(a[1],b[1]));
+        int[] prev = new int[points.length];
+        prev = points[0];
         int count =points.length;
-        for(int i =1; i<points.length; i++) {
-            int[] intervals = points[i];
-            if(intervals[0]<=prev[1]) {
+        for(int i=1; i<points.length; i++) {
+            int[] interval = points[i];
+            if(interval[0] <= prev[1]) {
                 count--;
             }else {
-                prev = intervals;
+                prev = interval;
             }
         }
         return count;
     }
 }
-//sort start time
-//check if it is falling inside codition
-//if not change interval
-//10,16
+
+//[[10,16],[2,8],[1,6],[7,12]]
+//[ [2,8] [7,12] [10,16]]    [1,6]
+//2<6 1 2
+//      7
