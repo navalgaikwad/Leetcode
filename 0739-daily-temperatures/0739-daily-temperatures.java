ad02651fa058dd53d2832dd49a802924b45b71b4
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-02 17:07:33 -0500

    Time: 64 ms (53.62%), Space: 62.8 MB (21.32%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -3,16 +3,13 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         int len = temperatures.length;
         int[] result = new int[len];
-        for(int i =0; i<len; i++) {
-            while(st.size()>0 && temperatures[st.peek()] < temperatures[i]){
+        for(int i=0; i<len; i++) {
+            while(!st.isEmpty()&& temperatures[st.peek()] < temperatures[i]) {
                 int index = st.pop();
                 result[index] = i - index;
             }
             st.push(i);
         }
-        
         return result;
     }
 }
-// 0,  1, 2 , 
-//73, 74, 75, 71
