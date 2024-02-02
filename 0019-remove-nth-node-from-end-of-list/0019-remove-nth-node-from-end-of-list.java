9f57db4b6743d47da41e3b276dd168e440fa538f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-01 23:00:51 -0500

    Time: 0 ms (100.00%), Space: 41.3 MB (72.77%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,30 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null || n <= 0) {
-            return head; // Invalid input
-        }
-
-        ListNode dummy = new ListNode(0);
+        int len =0;
+        
+        ListNode dummy = new ListNode(-1);
         dummy.next = head;
-
-        int len = 0;
-        ListNode current = head;
-        while (current != null) {
-            len = len + 1;
-            current = current.next;
+        ListNode firstNode = head;
+        while(firstNode != null) {
+            firstNode = firstNode.next;
+            len++;
         }
-
-        if (n > len) {
-            return head; // Invalid input, n is greater than the length of the list
-        }
-
-        current = dummy;
-        for (int i = 0; i < len - n; i++) {
-            current = current.next;
-        }
-
-        current.next = current.next.next;
-
+        len = len - n; //5-5 =0 
+        firstNode = dummy;
+        while( len > 0) {
+            firstNode = firstNode.next;
+            len--;
+        } 
+        
+        firstNode.next = firstNode.next.next;
         return dummy.next;
+        
     }
 }
+//find length of the linkedList
+//n%len = len
+//go on 3  next = node.next.next;
+// 3 to 5
+// O(n)
