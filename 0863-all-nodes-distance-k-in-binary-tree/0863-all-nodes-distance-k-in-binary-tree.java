a4191c0c99cc4d6b3b100e3fc9cf56a8ef82eb7a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-02 18:08:53 -0500

    Time: 13 ms (67.75%), Space: 43.3 MB (12.39%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -8,46 +8,45 @@
  * }
  */
 class Solution {
-    private Map<Integer, ArrayList<Integer>> adjListMap = new HashMap<>();
+     private Map<Integer, ArrayList<Integer>> adjListMap = new HashMap<>();
     void buildList(TreeNode child, TreeNode parent) {
         if(child == null) {
             return;
         }
         if(!adjListMap.containsKey(child.val)) {
             adjListMap.put(child.val, new ArrayList<>());
-        }
-        if (parent != null) {
+        } 
+        if(parent!=null) {
             adjListMap.get(child.val).add(parent.val);
             adjListMap.get(parent.val).add(child.val);
         }
+        
         buildList(child.left, child);
         buildList(child.right, child);
-        
     }
+    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         buildList(root, null);
-       // System.out.print(adjListMap);
-        List<Integer> result = new ArrayList<>();
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{target.val, 0});
         HashSet<Integer> visited = new HashSet<>();
         visited.add(target.val);
-        
+        List<Integer> ans = new ArrayList<>();
         while(!q.isEmpty()) {
             int[] current = q.remove();
-            int src = current[0];
             int level = current[1];
+            int dest = current[0];
             if(level == k) {
-               result.add(src);
-               continue;
+                ans.add(dest);
+                continue;
             }
-            for(Integer neighbour: adjListMap.get(src)) {
+            for(Integer neighbour: adjListMap.get(dest)) {
                 if(!visited.contains(neighbour)) {
+                    q.add(new int[]{neighbour, level+1});
                     visited.add(neighbour);
-                    q.add(new int[]{neighbour, level +1});
                 }
             }
         }
-      return result;
+        return ans;
     }
 }
