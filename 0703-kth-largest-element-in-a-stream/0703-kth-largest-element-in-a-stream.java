762fe885659140018e0883e75f20f267fa511763
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-02 17:21:42 -0500

    Time: 13 ms (91.44%), Space: 47.1 MB (40.62%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -17,9 +17,3 @@ class KthLargest {
         return pq.peek();
     }
 }
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest obj = new KthLargest(k, nums);
- * int param_1 = obj.add(val);
- */
