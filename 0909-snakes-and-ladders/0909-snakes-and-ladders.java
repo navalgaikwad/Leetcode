3f9891262a7bc09c2c012af2e715bd6f0afbc23d
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-02 19:01:09 -0500

    Time: 11 ms (36.20%), Space: 45 MB (23.53%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,47 +1,49 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
-        int m=board.length;
-        
+        int m = board.length;
         int target = m * m;
+        
         Integer[] column = new Integer[m];
-        for(int i =0; i<m; i++) {
+        
+        for(int i=0; i<m ;i++) {
             column[i] = i;
         }
-         int[][] cell = new int[m*m+1][2];
+        List<Integer> list = new ArrayList<>();
+        int[][] cell = new int[m*m+1][2];
         int label = 1;
-        for(int row = m-1; row>=0; row--) {
-            for(int col: column) {
-                cell[label][0] = row;
+         for(int row = m-1; row>=0; row--) {
+            for(Integer col : column) {
+                cell[label][0] =row;
                 cell[label][1] = col;
                 label++;
             }
             Collections.reverse(Arrays.asList(column));
         }
-        Set<Integer> visited = new HashSet<>();
-        Queue<int[]> q= new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{1, 0});
-       // visited.add(1);
+        
         while(!q.isEmpty()) {
             int[] current = q.remove();
             int nextPosition = current[0];
             int level = current[1];
+            
             if(nextPosition == target) {
                 return level;
             }
-            for(int i =1; i<7; i++) {
-                int next = i+nextPosition;
+            for(int i=1; i<7; i++) {
+                int next = i + nextPosition;
                 if(next <= target) {
                     int nextRow = cell[next][0];
                     int nextCol = cell[next][1];
-                    int destination = board[nextRow][nextCol] == -1?next: board[nextRow][nextCol];
+                    int destination = board[nextRow][nextCol] == -1 ? next : board[nextRow][nextCol] ;
                     if(!visited.contains(destination)) {
-                        q.add(new int[]{destination, level+1});
                         visited.add(destination);
+                        q.add(new int[]{destination, level+1});
                     }
                 }
             }
         }
-        return -1;
+         return -1;
     }
 }
-
