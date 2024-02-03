2a43f9618acf10d26cee947aa6c9769867260a99
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-02 19:03:10 -0500

    Time: 2 ms (61.61%), Space: 47 MB (16.66%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
@@ -1,50 +1,44 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<TreeNode> allPossibleFBT(int n) {
-     
-        HashMap<Integer, List<TreeNode>> memo = new HashMap<>();
+    public List<TreeNode> allPossibleFBT(int N) {
+        // Recursive: build all possible FBT of leftSubTree and rightSubTree with number n
+        if(N <= 0 || N % 2 == 0) return new ArrayList<>();
         
+        //1. if N = 3 , the number of nodes combination are as follows
+        //      left    root    right
+        //       1       1        1 
+        //--------------N = 3, res = 1----------
         
-       System.out.print(memo);
-        return helper(n , memo);
+        //2. if N = 5 , the number of nodes combination are as follows
+        //      left    root    right
+        //       1       1        3 (recursion)
+        //       3       1        1 
+        //  --------------N = 5, res = 1 + 1 = 2----------
         
-    }
-    List<TreeNode> helper(int n,  HashMap<Integer, List<TreeNode>> memo){
-       if(memo.containsKey(n)){
-           return memo.get(n);
-       }
-         if(n %2 == 0){
-            return new ArrayList<TreeNode>();
+        //3. if N = 7 , the number of nodes combination are as follows
+        //      left    root    right
+        //       1       1        5 (recursion)
+        //       3       1        3 
+        //       5       1        1
+        //  --------------N = 7, res = 2 + 1 + 2 = 5----------
+        
+        //4. in order to make full binary tree, the node number must increase by 2
+        List<TreeNode> res = new ArrayList<>();
+        if(N == 1) {
+            res.add(new TreeNode(0));
+            return res;
         }
-        if(n == 1){
-            return Arrays.asList(new TreeNode());
-        }
-         List<TreeNode> res = new ArrayList<TreeNode>();
-        for(int i = 1; i < n; i+=2){
-          List<TreeNode> left =  helper(i, memo); //left
-          List<TreeNode> right =  helper(n - i -1, memo); //right
-          for(TreeNode l: left){
-              for(TreeNode r: right){
-               TreeNode root = new TreeNode(0, l, r);   
-               res.add(root);
+        for(int i = 1; i < N; i += 2) {
+            List<TreeNode> leftSubTrees = allPossibleFBT(i);
+            List<TreeNode> rightSubTrees = allPossibleFBT(N - i - 1);
+            for(TreeNode l : leftSubTrees) {
+                for(TreeNode r : rightSubTrees) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
             }
-          }
         }
-        memo.put(n, res);
         return res;
-    } 
+    }
 }
