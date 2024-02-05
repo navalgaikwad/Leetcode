1f0677d01728aab13584999f14440f7f3b18862b
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-04 23:08:12 -0500

    Time: 68 ms (65.40%), Space: 62.4 MB (58.96%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,50 +1,25 @@
 class Solution {
+    int maxTime =0;
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
         ArrayList<Integer> adj[] = new ArrayList[n];
-        for(int i=0 ;i<n; i++) {
+        for(int i=0; i<n; i++) {
             adj[i] = new ArrayList<>();
         }
         for(int i=0; i<n; i++) {
-            //manager = [2,2,-1,2,2,2,4]
-            if(manager[i] != -1) {
+            if(manager[i]!=-1) {
                adj[manager[i]].add(i); 
-            }  
-        }
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{headID, 0});
-        HashSet<Integer> visited = new HashSet<>();
-        int maxTime = 0;
-        while(!q.isEmpty()) {
-            int[] current = q.remove();
-            int x = current[0];
-            int level = current[1];
-            if(level >maxTime) {
-                maxTime = level;
-            }
-            
-            for(Integer neighbour :adj[x]) {
-                if(!visited.contains(neighbour)) {
-                    visited.add(neighbour);
-                    q.add(new int[]{neighbour, level+informTime[x]});
-                }
             }
         }
+        
+        dfs(adj, headID, informTime, 0);
         return maxTime;
     }
+    
+    void dfs(ArrayList<Integer> adj[], int parent, int[] informTime, int time) {
+        
+        for(int neighbour :adj[parent]) {
+            dfs(adj, neighbour, informTime, time + informTime[parent]);
+        }
+        maxTime = Math.max(maxTime, time);
+    }
 }
-
-//7
-//6
-//[1,2,3,4,5,6,-1]
-//[0,6,5,4,3,2,1]
-
-//if i created adjelist 
-// 0 - 2
-// 1 - 2
-// 2 - 2
-// 3 - 2
-// 4 - 2
-// 5 - 2
-//manager = [2,2,-1,2,2,2, 4]
-// 2 - 0 , 1, 3, 4, 5, 6
-// 4 - 7
