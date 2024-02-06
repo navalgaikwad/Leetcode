ed5bf568deed77b88ba9c90dbdee53f2048f01ae
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-06 16:17:15 -0500

    Time: 58 ms (62.51%), Space: 73 MB (64.06%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,57 +1,29 @@
 class Solution {
-    public int minReorder(int n, int[][] connections) {
+    public int minReorder(int n, int[][] con) {
         ArrayList<int[]> adj[] = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++) {
             adj[i] = new ArrayList<>();
         }
-
-        for (int[] con : connections) {
-            adj[con[0]].add(new int[]{con[1], 1});
-            adj[con[1]].add(new int[]{con[0], 0});
+        for(int[] c : con) {
+            adj[c[0]].add(new int[]{c[1], 1});
+            adj[c[1]].add(new int[]{c[0], 0});
         }
-
-        return dfs(adj, 0, new HashSet<>());
+        HashSet<Integer> visited = new HashSet<>();
+        return dfs(adj, 0, visited);
     }
-
+    
     int dfs(ArrayList<int[]> adj[], int parent, HashSet<Integer> visited) {
-        int solution = 0;
+        int total = 0;
         visited.add(parent);
-        for (int[] neighbour : adj[parent]) {
-            int neighbourNode = neighbour[0];
-            int count = neighbour[1];
-            if (!visited.contains(neighbourNode)) {
-                solution += count + dfs(adj, neighbourNode, visited);
+        for(int[] neighbours: adj[parent]) {
+            int neighbour = neighbours[0];
+            if(!visited.contains(neighbour)) {
+            int cost = neighbours[1];
+            int sum = dfs(adj, neighbour, visited);
+            total = total + cost+sum;  
             }
+            
         }
-        return solution;
+        return total;
     }
 }
-// class Solution {
-//     public int minReorder(int n, int[][] connections) {
-//         ArrayList<int[]>[] adj = new ArrayList[n];
-//         for (int i = 0; i < n; i++) {
-//             adj[i] = new ArrayList<>();
-//         }
-
-//         for (int[] con : connections) {
-//             adj[con[0]].add(new int[]{con[1], 1}); // Reorder: 1 means reorder needed
-//             adj[con[1]].add(new int[]{con[0], 0}); // No reorder: 0 means no reorder needed
-//         }
-
-//         return dfs(adj, 0, new HashSet<>());
-//     }
-
-//     int dfs(ArrayList<int[]>[] adj, int parent, HashSet<Integer> visited) {
-//         int solution = 0;
-//         visited.add(parent);
-//         for (int[] neighbour : adj[parent]) {
-//             int neighbourNode = neighbour[0];
-//             int isReorder = neighbour[1];
-//             if (!visited.contains(neighbourNode)) {
-//                 solution += isReorder + dfs(adj, neighbourNode, visited);
-//             }
-//         }
-//         return solution;
-//     }
-// }
-
