6afd8fab6340ae6ea34f82b6819f9e1ebd473f51
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-06 23:21:01 -0500

    Time: 100 ms (58.94%), Space: 97.7 MB (49.67%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
@@ -1,34 +1,42 @@
+import java.util.ArrayList;
+
 class Solution {
-    int max=1;
-    public int longestPath(int[] parent, String s) {
-        int n=parent.length;
-        if(n==1){
-            return 1;
-        }
-        ArrayList<Integer>adj[] =new ArrayList[n];
-        for(int i=0; i<n; i++){
-            adj[i]=new ArrayList<>();
-        }
-        
-        for(int i=1;i<n;i++){
-            adj[parent[i]].add(i);
-        }
-        int[] dist=new int[n];
-        dfs(adj, 0, -1, dist, s);
-        return max;
-        
-    }
+    int diameter = 0;
     
-    void dfs(ArrayList<Integer>adj[], int current, int parent, int[] dist, String s){
-        dist[current]=1;
-        for(Integer neighour :adj[current]){
-            if(neighour == parent) continue;
-            dfs(adj, neighour, current, dist, s);
-            if(s.charAt(current)!=s.charAt(neighour)){
-                max=Math.max(max, dist[current]+dist[neighour]);
-                dist[current]=Math.max(dist[current], dist[neighour]+1);
-                 
+    public int longestPath(int[] parent, String s) {
+        int len = parent.length;
+        ArrayList<Integer>[] adj = new ArrayList[len];
+        for (int i = 0; i < len; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < len; i++) {
+            if (parent[i] != -1) {
+                adj[parent[i]].add(i);
             }
         }
+        boolean[] visited = new boolean[len];
+        dfs(adj, 0, visited, s);
+        return diameter;
+    }
+
+    int dfs(ArrayList<Integer>[] adj, int parent, boolean[] visited, String s) {
+        int longestPath1 = 0; int longestPath2 = 0; // Initialize total to 0
+        visited[parent] = true;
+        for (int neighbour : adj[parent]) {
+            if (!visited[neighbour]) {
+                int count = dfs(adj, neighbour, visited, s);
+                
+                if (s.charAt(parent) != s.charAt(neighbour)) {
+                   if(longestPath1 < count) {
+                       longestPath2 = longestPath1;
+                       longestPath1 = count;
+                   }else if(longestPath2 < count) {
+                       longestPath2 = count;
+                   }
+                }
+            }
+        }
+        diameter = Math.max(diameter, longestPath1 + longestPath2 + 1); // Update diameter
+        return longestPath1 + 1;
     }
 }
