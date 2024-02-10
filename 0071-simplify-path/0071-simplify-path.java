18f6e44b5e36f3b7d9c6b18bf0217124cf1b251f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-10 15:29:07 -0500

    Time: 4 ms (85.73%), Space: 43.4 MB (43.95%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,23 +1,25 @@
 class Solution {
     public String simplifyPath(String path) {
-        String[] parts = path.split("/");
-        Stack<String> st = new Stack<>();
-        for(String part: parts) {
-            if(part.equals("..")) {//double dots
-                if(!st.isEmpty()) {
-                    st.pop();
+        String[] arr = path.split("/");
+        Stack<String> s = new Stack<>();
+        for(String a : arr) {
+            if(a.equals("..")) {
+                if(!s.isEmpty() || s.equals("")) {
+                    s.pop();
                 }
-            }else if(!part.isEmpty()&&!part.equals(".")) { //single dots
-                st.push(part);
+            }else {
+                if( !a.equals(".") && !a.isEmpty()) s.push(a);
             }
         }
-        if(st.isEmpty())  return "/";
-        StringBuilder sb = new StringBuilder();
-   
-         for(String s: st){
-              sb.append("/"+s);
-         }
-             
-       return sb.toString();
+        ///System.out.print(s);
+        if(s.isEmpty())  return "/";
+        StringBuilder sb= new StringBuilder();
+        for(String st:s) {
+                sb.append("/"+st);
+            }
+        return sb.toString();
     }
 }
+
+// /home/../ -> home .. 
+//
