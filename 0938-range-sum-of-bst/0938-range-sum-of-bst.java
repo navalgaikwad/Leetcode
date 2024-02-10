cfed61751c294ce8504ff6f6bc44493171498309
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-10 18:28:50 -0500

    Time: 1 ms (54.20%), Space: 51.4 MB (21.34%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -15,20 +15,15 @@
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
-        int[] sum = new int[1];
-        dfs(root, low, high, sum);
-        return sum[0];
-    }
-    
-    void dfs(TreeNode root, int low, int high, int[] sum) {
         if(root == null) {
-            return;
+            return 0;
         }
-        
-        if(root.val >= low && root.val <=high) {
-            sum[0] = sum[0] + root.val;
+        int left = rangeSumBST(root.left, low, high);
+        int right = rangeSumBST(root.right, low, high);
+        int sum =0;
+        if(root.val >=low && root.val <=high) {
+            sum =root.val;
         }
-        dfs(root.left, low, high, sum);
-        dfs(root.right, low, high, sum);
+        return sum + left + right;
     }
 }
