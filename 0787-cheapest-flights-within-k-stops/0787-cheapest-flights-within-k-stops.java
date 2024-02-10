98078c1e5094b004c58092daf8dd9494b1ac0646
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-10 12:59:38 -0500

    Time: 6 ms (86.22%), Space: 42.5 MB (99.97%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,41 +1,29 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<int[]> adj[] = new ArrayList[n];
-        int dist[] =new int[n];
-        for(int i=0; i<n; i++) {
-            adj[i] = new ArrayList<>();
-             dist[i]=Integer.MAX_VALUE;
-        }
-        for(int[] flight: flights) {
-            adj[flight[0]].add(new int[]{flight[1], flight[2]});
-        }
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{src, 0 , 0});
-        dist[src] = 0;
-        boolean[] visited = new boolean[n];
-        //visited[0] = true;
-        //int minCost =Integer.MAX_VALUE;
-        while(!q.isEmpty()) {
+        int[] prices = new int[n];
+        Arrays.fill(prices, Integer.MAX_VALUE);
+
+        prices[src] = 0;
+
+        for(int i = 0; i <= k; i++){
             
-                int[] current = q.remove();
-                int sc = current[0];
-                int cost = current[1];
-                int stops = current[2];
-                if(stops>k){
-                    continue;
-                }
-                for(int[] neighbour : adj[sc]) {
-                    int dest = neighbour[0];
-                    int cst = neighbour[1];
-                    if(cost+cst < dist[dest] && stops<=k) {
-                        dist[dest]=cst+cost;
-                        q.add(new int[]{dest, dist[dest], stops+1});
-                    }
-                }
+            int[] temp = new int[n];
+            temp = Arrays.copyOf(prices, prices.length);
+
+            for(int[] fl : flights){
+                int s = fl[0], d = fl[1], p = fl[2];
+
+                if(prices[s] == Integer.MAX_VALUE) continue;
+
+                int pricesFromHere = prices[s] + p;
+
+                if(pricesFromHere < temp[d]) temp[d] = pricesFromHere;
+            }
+
+            prices = temp;
         }
-         if(dist[dst]==Integer.MAX_VALUE){
-            return -1;
-        }
-        return dist[dst];
+
+        System.gc();
+        return (prices[dst] != Integer.MAX_VALUE) ? prices[dst] :  -1;
     }
 }
