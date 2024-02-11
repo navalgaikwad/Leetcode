b8e158b28edcc8e3a81cae8543cf82e73e35e1d9
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-11 12:08:27 -0500

    Time: 0 ms (100.00%), Space: 42.9 MB (91.89%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,15 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-
-    private boolean isValidBST(TreeNode root, long minValue, long maxValue) {
-       if(root == null) return true;
-        
-        if(root.val <= minValue || root.val >= maxValue) {
+    
+    boolean dfs(TreeNode root, long min, long max) {
+        if(root == null) {
+            return true;
+        }
+        if(min >=root.val   || max<=root.val) {
             return false;
         }
-        
-        return isValidBST(root.left, minValue, root.val) && isValidBST(root.right, root.val, maxValue);
+        boolean left = dfs(root.left, min , root.val);
+        boolean right = dfs(root.right, root.val, max);
+        return left && right;
     }
 }
