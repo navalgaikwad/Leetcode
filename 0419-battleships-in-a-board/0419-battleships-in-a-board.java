438f939afa739a6e5c7b515e9f010142b20b3207
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-11 12:29:21 -0500

    Time: 1 ms (94.68%), Space: 45.5 MB (19.78%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,28 +1,25 @@
 class Solution {
     public int countBattleships(char[][] board) {
-      int result=0;
-        for(int i=0;i<board.length; i++){
-          for(int j=0; j<board[0].length; j++){
-              if(board[i][j] =='X'){
-                  if(i ==0 && j ==0){
-                      result++;
-                  }else if(i==0){
-                      if(board[i][j-1] !='X'){ //check for previous
-                          result++;
-                      }
-                  }else if(j==0){
-                     if(board[i-1][j] !='X'){
-                          result++;
-                      } 
-                  }else{
-                      if(board[i][j-1] !='X' && board[i-1][j] !='X'){
-                           result++;
-                      }
-                  }
-              }
-          }
-      }
-        return result;
+        int count =0;
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) { //go for i=0 j=0
+                if(board[i][j]=='X') {
+                   if(i == 0 && j==0) {
+                       count++;
+                   }else if(i==0) {
+                       if(board[i][j-1]!='X') {
+                           count++;
+                       }
+                   }else if(j==0) {
+                       if(board[i-1][j]!='X') {
+                           count++;
+                       }
+                   }else if(board[i-1][j]!='X' && board[i][j-1]!='X') {
+                       count++;
+                   }
+                }
+            }
+        }
+        return count;
     }
- 
 }
