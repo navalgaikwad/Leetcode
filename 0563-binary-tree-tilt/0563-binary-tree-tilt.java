b619e854f12965aa7ffb40678a64c540b13d07db
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-12 12:52:45 -0500

    Time: 0 ms (100.00%), Space: 44.4 MB (35.53%) - LeetHub

--- 0563-binary-tree-tilt/0563-binary-tree-tilt.java
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.java
@@ -14,26 +14,20 @@
  * }
  */
 class Solution {
-    int global = 0;
+    int tilt =0;
     public int findTilt(TreeNode root) {
-        helper(root);
-        return global;
+         dfs(root);
+        return tilt;
     }
     
-    int helper(TreeNode root) {
+    int dfs(TreeNode root) {
         if(root == null) {
             return 0;
         }
         
-        //TreeNode newRoot = new TreeNode();
-        int gl =0;
-        int left = helper(root.left);
-        int right = helper(root.right);
-       
-        global += Math.abs(left - right);
-        gl +=Math.abs(left - right);
-        int result = root.val + left + right;
-        root.val = gl;
-        return result;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        tilt += Math.abs(left - right);
+        return root.val+left+right;
     }
 }
