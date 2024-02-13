e8aaeea5686a3edaf3bddab119ced7855f043d45
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-13 12:47:18 -0500

    Time: 2 ms (84.15%), Space: 45.5 MB (92.00%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left =0;
-       // int right =0;
-        
-        for(int i=0;i<nums.length; i++) {
-            if(nums[i]!=0) {
-               nums[left++] = nums[i];
+        int count =0;
+        int j =0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[j++] = nums[i];
             }
         }
-        for(int i =left; i<nums.length; i++) {
-             nums[i] = 0; 
+        
+        for(int k =j; k < nums.length; k++) {
+            nums[k] = 0;
         }
+        
     }
 }
+//take 2 pointer 
+//one moving with no another zero
+//[0,1,0,3,12]
+//[1,1,0,3,12]
+//[1,3, 0, 3, 13]
+//[1,3,12,3,12]
+//j =2 i =4
