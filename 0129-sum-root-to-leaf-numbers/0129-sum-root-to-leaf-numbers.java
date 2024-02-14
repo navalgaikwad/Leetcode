a2047c66db9d104d9b8e11b87994d81edd7c196a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-14 18:16:59 -0500

    Time: 0 ms (100.00%), Space: 41.5 MB (20.09%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -1,22 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int sumNumbers(TreeNode root) {
-        if(root==null) {
-            return 0;
-        }
-        int totalUptilNow = 0;
-        int sum = dfs(root, totalUptilNow);
+        int totalUptillNow = 0;
+        int sum = helper(root, totalUptillNow);
         return sum;
     }
     
-    private int dfs(TreeNode root, int totalUptilNow) {
-        if(root==null) {
-            return 0;
-        }
-        totalUptilNow = totalUptilNow*10+root.val;
-        if(root.left==null && root.right==null) {
-            return totalUptilNow;
-        }
-        return dfs(root.left, totalUptilNow) + dfs(root.right, totalUptilNow);
+    int helper(TreeNode root, int sum) {
+        if(root == null) return 0;
+        
+        sum = sum *10+root.val;
+        if(root.left == null && root.right==null) return sum;//leaf path vvimp
+        
+        int left = helper(root.left, sum);
+        int right = helper(root.right, sum);
+        
+        return left + right;
     }
-    
 }
+
+
