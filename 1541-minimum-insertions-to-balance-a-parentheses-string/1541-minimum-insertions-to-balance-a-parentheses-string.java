e13e803d74652064c11c8ea1a49a1b0f8cd6d2fe
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-15 12:48:09 -0500

    Time: 11 ms (64.22%), Space: 45.4 MB (40.99%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,38 +1,35 @@
 class Solution {
     public int minInsertions(String s) {
-        
-        int ans=0;       
-        int cnt=0;
-        for(char ch:s.toCharArray()) {     
-            if(ch == '(') { //opeing
-                cnt += 2;  //add 2
-                if(cnt%2 != 0){ //if not even add increment ans
+        int cnt =0;
+        int ans =0;
+        for(Character c : s.toCharArray()) {
+            if(c == '(') {
+                cnt+=2;
+                if(cnt%2!=0) {
+                    cnt--;
                     ans++;
-                    cnt--;//
                 }
-            } else {       
-                cnt -= 1;//closing
-                if(cnt < 0){//less than 0
+            }else {
+                cnt--;
+                if(cnt< 0) {
+                    cnt+=2;
                     ans++;
-                    cnt+=2;//add 2 
-                } 
-            }           
+                }
+            }
         }
-        return ans + cnt;
+        return ans+cnt;
     }
 }
-//  ))())(
-//  cnt = -1 < 0, 1,0, 2,1,0,2,1
-//  ans= 2
-//  cnt + ans
-//  
-//when open (
-//cnt+2// for closing
-    //cnt is odd
-        //incremnt ans++
-        //cnt--
-//cnt is less than 0
-    //increment ans++
-    //cnt+2
 
-//return ans+cnt
+//(()))
+//cnt=1
+//
+
+//())
+//cnt=2
+//
+
+//))())(
+//
+//cnt+=2 1, 0,2, 1,0, 2,1
+//ans=1,2
