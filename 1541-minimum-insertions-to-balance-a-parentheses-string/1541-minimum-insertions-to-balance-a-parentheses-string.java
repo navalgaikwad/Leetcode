39451ea3e4197f91b4ccd3b885fb9daa80cbdcbc
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-14 21:10:33 -0500

    Time: 7 ms (100.00%), Space: 45 MB (85.29%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
@@ -3,31 +3,36 @@ class Solution {
         
         int ans=0;       
         int cnt=0;
-        for(char ch:s.toCharArray()){     
+        for(char ch:s.toCharArray()) {     
             if(ch == '(') { //opeing
                 cnt += 2;  //add 2
-                if(cnt%2!=0){ //if not even add increment ans
+                if(cnt%2 != 0){ //if not even add increment ans
                     ans++;
                     cnt--;//
                 }
-            }
-            else {       
-                cnt-=1;//closing
-                if(cnt<0){//less than 0
+            } else {       
+                cnt -= 1;//closing
+                if(cnt < 0){//less than 0
                     ans++;
-                    cnt+=2;//add 2
+                    cnt+=2;//add 2 
                 } 
             }           
         }
-
-        return ans+cnt;
-        
+        return ans + cnt;
     }
 }
-//cnt is odd
-    //incremnt ans++
-    //cnt--
+//  ))())(
+//  cnt = -1 < 0, 1,0, 2,1,0,2,1
+//  ans= 2
+//  cnt + ans
+//  
+//when open (
+//cnt+2// for closing
+    //cnt is odd
+        //incremnt ans++
+        //cnt--
 //cnt is less than 0
     //increment ans++
-    //cnt++
+    //cnt+2
+
 //return ans+cnt
