6503d03d68bd74e259d62f1a3f09a768fea39640
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-16 16:19:41 -0500

    Time: 4 ms (99.72%), Space: 45.4 MB (31.59%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int left =0;
-        int right = s.length() - 1;
-        while(left < right) {
-            if(s.charAt(left)!=s.charAt(right)) {
-               return  isPalindrome(s, left +1, right) || isPalindrome(s, left , right-1);
+        int i = 0; int j = s.length() -1;
+        while(i < j) {
+            if(s.charAt(i)!=s.charAt(j)) {
+                return helper(s, i + 1, j) || helper(s, i , j-1) ;
             }
-            left++;
-            right--;
+            i++;
+            j--;
         }
         return true;
     }
     
-     private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+    boolean helper(String s, int i, int j) {
+        while(i<j) {
+            if(s.charAt(i)!=s.charAt(j)) {
                 return false;
             }
-            left++;
-            right--;
+            i++;
+            j--;
         }
         return true;
     }
