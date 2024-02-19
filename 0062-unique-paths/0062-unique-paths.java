798b3d34212c88f5c6a71e282b304708ad74e573
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-18 20:42:00 -0500

    Time: 1 ms (14.68%), Space: 40.4 MB (53.38%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,53 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return dfs(m-1, n-1, new HashMap<>());
+        int[][] dp = new int[m][n];
+       // return dfs(m-1, n-1, new HashMap<>());
+        // int[][] dp = new int[m+1][n+1];
+        // dp[0][0] = 1;
+        // for(int i=0; i<m; i++) {
+        //     for(int j=0; j<n; j++) {
+        //         if(i - 1 >= 0 ) {
+        //             dp[i][j] += dp[i-1][j];
+        //         }
+        //         if(j - 1 >= 0) {
+        //             dp[i][j] += dp[i][j-1];
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+        // for (int i = m-1; i >= 0; i--) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i - 1 >= 0 || j + 1 < n) {
+        //             if (i - 1 >= 0) dp[i - 1][j] += dp[i][j];
+        //             if (j + 1 < n) dp[i][j + 1] += dp[i][j];
+        //         }
+        //     }
+        // }
+        // int[][] dp = new int[m][n];
+        // dp[m-1][0] = 1;
+        // for(int i = m-1; i>=0; i--) {
+        //     for(int j=0; j<n; j++) {
+        //         if(i-1 >=0) {
+        //             dp[i-1][j] += dp[i][j];
+        //         }
+        //         if(j + 1 < n) {
+        //             dp[i][j+1] += dp[i][j];
+        //         }
+        //     }
+        // }
+        // return dp[0][n-1];
+        dp[m-1][0] = 1;
+         for (int i = m-1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                if (i + 1 < m || j - 1 >= 0) {
+                    if (i + 1 < m) dp[i][j] = dp[i][j] + dp[i + 1][j];
+                    if (j - 1 >= 0) dp[i][j] = dp[i][j]+ dp[i][j - 1];
+                    //grid chi value
+                }
+            }
+        }
+        return dp[0][n-1];
+        
     }
     int dfs(int m, int n, HashMap<String, Integer> memo) {
         if(memo.containsKey(m+"-"+n)) return memo.get(m+"-"+n); 
