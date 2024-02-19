85f996f071bf5b2b23ccae05697430685ef681e7
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-18 19:39:36 -0500

    Time: 3 ms (89.39%), Space: 49.4 MB (54.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,8 +2,8 @@ class Solution {
     public int numIslands(char[][] grid) {
         int count =0;
         for(int i=0; i<grid.length; i++) {
-            for(int j=0; j<grid[i].length; j++) {
-                if(grid[i][j]=='1'){
+            for(int j=0; j<grid[0].length; j++) {
+                if(grid[i][j]=='1') {
                     dfs(grid, i, j);
                     count++;
                 }
@@ -13,15 +13,13 @@ class Solution {
     }
     
     void dfs(char[][] grid, int i, int j) {
-        int row = grid.length;
-        int col = grid[0].length;
-        if(i >=row || i<0 || j>=col || j<0 || grid[i][j]=='0') {
+        if( i < 0 || i >= grid.length || j < 0 || j>=grid[0].length || grid[i][j] == '0') {
             return;
         }
         grid[i][j] = '0';
-        dfs(grid, i+1, j);
+        dfs(grid, i + 1, j);
         dfs(grid, i, j+1);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j-1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
     }
 }
