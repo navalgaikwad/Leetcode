f19374fe92ec2d38f157d62c9da7b6440d2b5860
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-21 10:48:49 -0500

    Time: 1 ms (95.87%), Space: 46.1 MB (7.75%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,22 +1,26 @@
 class Solution {
     public int countBattleships(char[][] board) {
         int count =0;
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board[0].length; j++) { //go for i=0 j=0
+        for(int i =0; i<board.length; i++) {
+            for(int j=0; j < board[0].length; j++) {
+                //check for not equal to X main idea
                 if(board[i][j]=='X') {
-                   if(i == 0 && j==0) {
-                       count++;
-                   }else if(i==0) {
-                       if(board[i][j-1]!='X') {
-                           count++;
-                       }
-                   }else if(j==0) {
-                       if(board[i-1][j]!='X') {
-                           count++;
-                       }
-                   }else if(board[i-1][j]!='X' && board[i][j-1]!='X') {
-                       count++;
-                   }
+                    if(i == 0 && j == 0) {
+                        count++;
+                     
+                    }else if(i == 0) {
+                        if(board[i][j-1] != 'X') {
+                            count++;
+                        }
+                    }else if(j ==0) {
+                        if(board[i-1][j]!='X') {
+                            count++;
+                        }
+                    }else if(i!=0 && j!=0){
+                        if(board[i-1][j]!='X' && board[i][j-1]!='X') {
+                            count++;
+                        }
+                    }
                 }
             }
         }
