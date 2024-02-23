02a1905ac23c95f11b97faa1e854df89728ed267
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-23 11:11:52 -0500

    Time: 0 ms (100.00%), Space: 42.4 MB (77.31%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] prefix = new int[nums.length];
-        prefix[0] = nums[0];
-        for(int i=1; i<nums.length; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
+        int[] result =new int[nums.length];
+        result[0] = nums[0];
+        for(int i =1; i<nums.length; i++) {
+            result[i] = result[i-1]+nums[i];
         }
-        return prefix;
+        return result;
     }
 }
