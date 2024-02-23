60c0a5babac3f622d85fe1fb809134d1188b5322
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-23 14:26:44 -0500

    Time: 3 ms (37.84%), Space: 46.2 MB (69.90%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,18 +1,22 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        helper(graph, 0, graph.length - 1, new ArrayList<>());
+        dfs(graph, 0, new ArrayList<>());
         return result;
     }
     
-    void helper(int[][] graph, int start, int target, List<Integer> runnningList) {
-        runnningList.add(start);
-        if(start == target) {
-            result.add(new ArrayList<>(runnningList));
+    void dfs(int[][] graph, int parent, ArrayList<Integer> ans) {
+        ans.add(parent);
+        if(parent == graph.length-1) {
+            result.add(new ArrayList<>(ans));
+            return;
+        }
+         
+        for(Integer neighbour: graph[parent]) {
+            //if(neighbour!=parent) {
+                dfs(graph, neighbour, new ArrayList<>(ans));
+           // }
         }
         
-        for(int neighbour: graph[start]) {
-            helper(graph, neighbour, target, new ArrayList<>(runnningList));
-        }
     }
 }
