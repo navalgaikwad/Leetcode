86ccde840dab98de5dc34fbc7421a2b33acad243
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-24 14:32:42 -0500

    Time: 18 ms (51.37%), Space: 45.1 MB (90.54%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,28 +1,29 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if (grid[0][0] != 0) return -1;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, 1});
-        int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1},{1,-1}, {-1,1},{1,1},{-1,-1}};
-       // int[][] direction = new int[][]{{0,-1}, {-1,0}, {1,1}, {-1, -1}, {1, 0}, {0, 1}, {-1, 1}, {1, -1}};
+         if (grid[0][0] != 0) return -1;
         int m = grid.length;
         int n = grid[0].length;
-        HashSet<String> visited = new HashSet<>();
-        visited.add(0+"-"+0);
+       // int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1},{1,1},{-1,-1},{-1, 1},{1,-1}};
+        int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1},{1,-1}, {-1,1},{1,1},{-1,-1}};
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+        visited[0][0]= true;
+        q.add(new int[]{0, 0, 1});
         while(!q.isEmpty()) {
             int[] current = q.remove();
-            int x = current[0];
-            int y= current[1];
+            int i = current[0];
+            int j = current[1];
             int level = current[2];
-            if(x == m-1 && y == n-1) {
+            
+            if(i == m-1 && j == n-1) {
                 return level;
             }
-            for(int[] dir :dirs) {
-                int i = x + dir[0];
-                int j = y + dir[1];
-                if(i>=0&&i<m&&j>=0&&j<n&&grid[i][j]==0 && !visited.contains(i+"-"+j)) {
-                    q.add(new int[]{i, j, level + 1});
-                    visited.add(i+"-"+j);
+            for(int[] dir : dirs) {
+                int x = i + dir[0];
+                int y = j + dir[1];
+                if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==0 && !visited[x][y]) {
+                    q.add(new int[]{x, y, level+1});
+                    visited[x][y]= true;
                 }
             }
         }
