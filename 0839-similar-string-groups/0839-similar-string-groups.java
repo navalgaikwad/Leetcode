af41646c2b0ae793f5b8df685149ee543bcf82b3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-23 20:14:20 -0500

    Time: 122 ms (21.73%), Space: 43 MB (63.23%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.java
+++ 0839-similar-string-groups/0839-similar-string-groups.java
@@ -1,37 +1,40 @@
 class Solution {
     public int numSimilarGroups(String[] strs) {
-         int group=0;
-        boolean[] visited=new boolean[strs.length];
-        for(int i=0; i<strs.length; i++){
-            if(visited[i]){
-                continue;
+        int group =0;
+        boolean[] visited = new boolean[strs.length];
+        for(int i=0; i<strs.length; i++) {
+            if(!visited[i]) {
+                dfs(strs, i, visited);
+                group++;
             }
-            dfs(strs, i, visited);
-            group++;
         }
-       return  group;
+        return group;
     }
     
-
     void dfs(String[] strs, int parent, boolean[] visited) {
-        visited[parent]= true;
+        visited[parent] = true;
         for(int i=0; i<strs.length; i++) {
+            //important is here check parent with all string
             if(!visited[i] && check(strs[i], strs[parent])) {
                 dfs(strs, i, visited);
             }
         }
     }
-
-
-    boolean check(String a,  String b) {
-        int i=0;
+    
+    boolean check(String a, String b) {
+        int i = 0; 
+        int j = 0;
         int count =0;
-        while(i < a.length()) {
-            if(a.charAt(i)!=b.charAt(i)) {
+        while(i < a.length() && j < b.length()) {
+            if(a.charAt(i)!=b.charAt(j)) {
                 count++;
             }
             i++;
+            j++;
         }
-        return count==2 || count==0;
+        return count==0 || count==2;
     }
 }
+
+//["tars","rats","arts","star"]
+//
