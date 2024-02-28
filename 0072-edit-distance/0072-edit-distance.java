d094b171e23fcb9c65c327f69c125b1c8d8bcb54
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-02-27 23:24:41 -0500

    Time: 20 ms (5.06%), Space: 45.5 MB (5.32%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,33 +1,46 @@
-public class Solution {
-
+class Solution {
     public int minDistance(String word1, String word2) {
-        int len1 = word1.length();
-        int len2 = word2.length();
-        int[][] memo = new int[len1][len2];
-        for (int i = 0; i < len1; i++) {
-            Arrays.fill(memo[i], -1);
-        }
-        return minDistanceHelper(word1, word2, len1 - 1, len2 - 1, memo);
+        //int[][] dp = new int[][];
+        return dfs(word1, word2, word1.length()-1, word2.length()-1, new HashMap<>());
     }
-
-    private int minDistanceHelper(String word1, String word2, int i, int j, int[][] memo) {
-        if (i < 0) return j + 1;
-        if (j < 0) return i + 1;
-
-        if (memo[i][j] != -1) {
-            return memo[i][j];
+    
+    int dfs(String word1, String word2, int i, int j, HashMap<String, Integer> memo) {
+        String key = i+"-"+j;
+        
+        if(i < 0) {
+            return j+1; // Insert all characters from word2
         }
-        //when palindrome we are incrementing here we are not doing anything
-        //two strings are given
-        if (word1.charAt(i) == word2.charAt(j)) {
-            memo[i][j] = minDistanceHelper(word1, word2, i - 1, j - 1, memo);
-        } else {
-            int insert = minDistanceHelper(word1, word2, i, j - 1, memo);
-            int delete = minDistanceHelper(word1, word2, i - 1, j, memo);
-            int replace = minDistanceHelper(word1, word2, i - 1, j - 1, memo);
-            memo[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
-            //minimum of all the operation
+        if(j < 0) {
+          return i+1;   // Delete all characters from word1
         }
-        return memo[i][j];
+        if(memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        //if both are same
+        //insert
+        //delete
+        //replace
+        int value;
+        if(word1.charAt(i) == word2.charAt(j)) {
+            value = dfs(word1, word2, i - 1, j - 1, memo);
+        }else {
+            int insert = dfs(word1, word2, i - 1, j, memo);
+            int delete = dfs(word1, word2, i, j - 1, memo);
+            int replace = dfs(word1, word2, i - 1, j - 1, memo);
+            value =  1 + Math.min(insert, Math.min(delete, replace));
+        }
+        memo.put(key, value);
+        return value;
     }
 }
+
+//Input: word1 = "horse", word2 = "ros"
+//Output: 3
+
+//"horse", word2 = "ros"
+//h = r
+//rorse
+//rorse
+//rosse
+//rose
+//ros
