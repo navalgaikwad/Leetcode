d9bf3acf55a9797b367131518cb6660e01b115e2
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-02 20:02:30 -0500

    Time: 21 ms (5.07%), Space: 45.6 MB (5.12%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,46 +1,29 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        //int[][] dp = new int[][];
-        return dfs(word1, word2, word1.length()-1, word2.length()-1, new HashMap<>());
+        return helper(word1, word2, word1.length()-1, word2.length()-1, new HashMap<>());
     }
     
-    int dfs(String word1, String word2, int i, int j, HashMap<String, Integer> memo) {
+    int helper(String word1, String word2, int i, int j, HashMap<String, Integer> memo) {
         String key = i+"-"+j;
-        
         if(i < 0) {
-            return j+1; // Insert all characters from word2
+            return j + 1;
         }
         if(j < 0) {
-          return i+1;   // Delete all characters from word1
+            return i + 1;
         }
         if(memo.containsKey(key)) {
             return memo.get(key);
         }
-        //if both are same
-        //insert
-        //delete
-        //replace
-        int value;
+        int total = 0;
         if(word1.charAt(i) == word2.charAt(j)) {
-            value = dfs(word1, word2, i - 1, j - 1, memo);
+            total = helper(word1, word2, i - 1, j - 1, memo);
         }else {
-            int insert = dfs(word1, word2, i - 1, j, memo);
-            int delete = dfs(word1, word2, i, j - 1, memo);
-            int replace = dfs(word1, word2, i - 1, j - 1, memo);
-            value =  1 + Math.min(insert, Math.min(delete, replace));
+            int insert = helper(word1, word2, i - 1, j, memo);
+            int delete = helper(word1, word2, i, j - 1, memo);
+            int replace = helper(word1, word2, i - 1, j - 1, memo);
+            total = 1 + Math.min(insert,  Math.min(delete, replace));
         }
-        memo.put(key, value);
-        return value;
+        memo.put(key, total);
+        return total;
     }
 }
-
-//Input: word1 = "horse", word2 = "ros"
-//Output: 3
-
-//"horse", word2 = "ros"
-//h = r
-//rorse
-//rorse
-//rosse
-//rose
-//ros
