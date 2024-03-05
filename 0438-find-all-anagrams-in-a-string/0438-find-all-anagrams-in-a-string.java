a27887d5f07eada6ca4c00c3bb68146143a8f510
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-05 13:10:10 -0500

    Time: 24 ms (53.08%), Space: 44.4 MB (95.53%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -4,20 +4,23 @@ class Solution {
         for(char c : p.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int left =0;
+        //System.out.print(map);
+        List<Integer> result = new ArrayList<>();
         int counter = map.size();
-        ArrayList<Integer> ans = new ArrayList<>(); 
-        for(int right =0; right<s.length(); right++) {
+        int left = 0;
+        for(int right=0; right<s.length(); right++) {
             char rightSide = s.charAt(right);
+            
             if(map.containsKey(rightSide)) {
                 map.put(rightSide, map.getOrDefault(rightSide, 0) - 1);
                 if(map.get(rightSide) == 0) {
                     counter--;
                 }
             }
+            
             while(counter == 0) {
                 if(right - left + 1 == p.length()) {
-                    ans.add(left);
+                    result.add(left);
                 }
                 char leftSide = s.charAt(left);
                 if(map.containsKey(leftSide)) {
@@ -29,6 +32,28 @@ class Solution {
                 left++;
             }
         }
-        return ans;
+        
+        return result;
     }
 }
+
+//first i will add value in the map
+//then take counter = map.size();
+//iterate over String s
+//when ever the counter is zero
+// i will add the left pointer
+// and increament left until we find count grater than 0
+
+//s = "cbaebabacd", p = "abc"
+/*
+a =1,0,-1,-2
+b = 1,0,-1,-2
+c= 1, 0, 1, 0
+result = 0
+left = 6
+counter = 1, 0
+
+
+
+
+*/
