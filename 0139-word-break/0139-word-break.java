e4f085ae0323705605b0c7ea96ff9d207cc2d944
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-06 14:23:40 -0500

    Time: 3 ms (84.48%), Space: 41.9 MB (82.79%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -3,23 +3,51 @@ class Solution {
         return dp(s, wordDict, new HashMap<>());
     }
     
-    boolean dp(String target, List<String> wordDict, HashMap<String, Boolean> memo) {
-        if(memo.containsKey(target)) {
-            return memo.get(target);
+    boolean dp(String s, List<String> wordDict, HashMap<String, Boolean> memo) {
+         if(memo.containsKey(s)) {
+            return memo.get(s);
         }
-        if(target == "") {
+        if(s.equals("")) {
             return true;
         }
-        for(String word : wordDict) {
-            if(target.startsWith(word)) {
-                String slice = target.substring(word.length());
-                if(dp(slice, wordDict, memo)) {
-                    memo.put(target, true);
-                    return true;
+       
+        boolean flag = false;
+        for(String word: wordDict) {
+            if(s.startsWith(word)) {
+                String slice = s.substring(word.length());
+                if(dp(slice, wordDict, memo)){
+                    flag = true;
                 }
             }
         }
-        memo.put(target, false);
-        return false;
+        memo.put(s, flag);
+        return flag;
     }
+    
 }
+
+// class Solution {
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//         return dp(s, wordDict, new HashMap<>());
+//     }
+    
+//     boolean dp(String target, List<String> wordDict, HashMap<String, Boolean> memo) {
+//         if(memo.containsKey(target)) {
+//             return memo.get(target);
+//         }
+//         if(target == "") {
+//             return true;
+//         }
+//         for(String word : wordDict) {
+//             if(target.startsWith(word)) {
+//                 String slice = target.substring(word.length());
+//                 if(dp(slice, wordDict, memo)) {
+//                     memo.put(target, true);
+//                     return true;
+//                 }
+//             }
+//         }
+//         memo.put(target, false);
+//         return false;
+//     }
+// }
