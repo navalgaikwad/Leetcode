92087391dd40432ec125e29418d8619d9c3c27bc
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-05 20:57:55 -0500

    Time: 9 ms (14.64%), Space: 43.2 MB (34.59%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -5,22 +5,24 @@ class Solution {
         return result;
     }
     
-    void helper(String s, int part, int index, String ans) {
-        if(part == 4 || index == s.length()) {
-            if(part == 4 && index == s.length()) {
-                result.add(ans.substring(0, ans.length() - 1));
+    void helper(String s, int level, int index, String ans) {
+        if(level == 4 || index == s.length()) {
+            if(level == 4 && index == s.length()) {
+                result.add(ans.substring(0, ans.length()-1));
                 return;
             }
             return;
         }
-        helper(s, part+1, index + 1, ans+s.charAt(index)+".");
+        helper(s, level+1, index + 1, ans+s.charAt(index)+".");
+        
         if(index + 2 <= s.length() && isValid(s.substring(index, index + 2))) {
-           helper(s, part+1, index+2, ans+s.substring(index, index + 2)+"."); 
+            helper(s, level + 1, index + 2, ans+s.substring(index, index + 2)+".");
         }
         if(index + 3 <= s.length() && isValid(s.substring(index, index + 3))) {
-           helper(s, part+1, index+3, ans+s.substring(index, index + 3) +".");   
+            helper(s, level + 1, index + 3, ans+s.substring(index, index + 3)+".");
         }
     }
+    
     boolean isValid(String s) {
         if(s.charAt(0) == '0') return false;
         return Integer.parseInt(s) <= 255;
