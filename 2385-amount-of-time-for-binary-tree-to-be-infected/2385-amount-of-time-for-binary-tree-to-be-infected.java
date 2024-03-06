305ff368597b8aeb2e57833bfa71e2689841fd8a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-06 16:19:41 -0500

    Time: 138 ms (31.53%), Space: 111.2 MB (47.89%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -14,48 +14,46 @@
  * }
  */
 class Solution {
-    Map<Integer, ArrayList<Integer>> map = new HashMap<>();
-    void buildList(TreeNode child, TreeNode parent) {
+    Map<Integer, ArrayList<Integer>> adj = new HashMap<>();
+    
+    void build(TreeNode child, TreeNode parent) {
         if(child == null) {
             return;
         }
-        //child nasel tar map madhye tar tak
-        if(!map.containsKey(child.val)) {
-            map.put(child.val, new ArrayList<>());
-        }//parent null nasel tar child and parent val takla ahe
-        if(parent != null) {
-            map.get(parent.val).add(child.val);
-            map.get(child.val).add(parent.val);
-            
+        if(!adj.containsKey(child.val)) {
+            adj.put(child.val, new ArrayList<>());
         }
-        buildList(child.left, child);
-        buildList(child.right, child);
+        
+        if(parent!=null) {
+            adj.putIfAbsent(child.val, new ArrayList<>());
+            adj.putIfAbsent(parent.val, new ArrayList<>());
+            adj.get(parent.val).add(child.val);
+            adj.get(child.val).add(parent.val);
+        }
+        build(child.left, child);
+        build(child.right, child);
+        
     }
     public int amountOfTime(TreeNode root, int start) {
-        buildList(root, null);
+        build(root, null);
+        
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{start, 0});
-        HashSet<Integer>visited = new HashSet<>();
+        HashSet<Integer> visited = new HashSet<>();
         visited.add(start);
         int max = 0;
-        int[] dist = new int[map.size()];
-        //dist[start] = 0;
         while(!q.isEmpty()) {
             int[] current = q.remove();
-            int i = current[0];
+            int src = current[0];
             int level = current[1];
-            max = Math.max(max, level);
-            for(Integer neighbour: map.get(i)) {
+            max = Math.max(level, max);
+            for(Integer neighbour :adj.get(src)) {
                 if(!visited.contains(neighbour)) {
-                    //dist[neighbour] = dist[i] + 1;
                     visited.add(neighbour);
                     q.add(new int[]{neighbour, level+1});
                 }
             }
         }
-        // for(int d : dist) {
-        //     System.out.print(d);
-        // }
         return max;
     }
 }
