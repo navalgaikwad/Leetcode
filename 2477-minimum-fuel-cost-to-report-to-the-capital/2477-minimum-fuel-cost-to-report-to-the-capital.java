e1447010b709acca751886825ed34732c6a5d14e
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-08 14:03:20 -0500

    Time: 51 ms (68.86%), Space: 103.7 MB (46.62%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
@@ -1,27 +1,30 @@
 class Solution {
-    //same as diameter formate
-    long daimeter =0;
+    long diameter = 0;
     public long minimumFuelCost(int[][] roads, int seats) {
-        ArrayList<Integer>[] adj=new ArrayList[roads.length+1];
-        for(int i=0; i<=roads.length; i++){
-            adj[i]=new ArrayList<>();
+        int n = roads.length;
+        ArrayList<Integer> adj[] = new ArrayList[n + 1];
+        for(int i=0; i <= n; i++) {
+            adj[i] = new ArrayList<>();
         }
-        for(int[] road: roads){
+        for(int[] road : roads) {
             adj[road[0]].add(road[1]);
             adj[road[1]].add(road[0]);
         }
-        dfs(adj, 0, -1, seats);
-        return daimeter;
+        boolean[] visited = new boolean[n + 1];
+        //visited[0] = true;
+        dfs(adj, 0, visited, seats);
+        return diameter;
     }
-    int dfs(ArrayList<Integer>[] adj, int current, int parent, int seats) {
+    long dfs(ArrayList<Integer> adj[], int parent, boolean[] visited,  int seats) {
         int total = 1;
-        for(int neighbour : adj[current]) {
-            if(neighbour != parent) {
-                total +=dfs(adj, neighbour, current, seats);
+        visited[parent] = true;
+        for(Integer neighbour : adj[parent]) {
+            if(!visited[neighbour]) {
+                total+= dfs(adj,neighbour, visited, seats);
             }
         }
-        if(current != 0) {
-          daimeter += Math.ceil((double)total/(double)seats);   
+        if(parent != 0) {
+            diameter+= Math.ceil((double)total/(double)seats);   
         }
         return total;
     }
