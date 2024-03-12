8aeb8aa885a5c1a7984d36577206b068d65efd9f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-12 16:06:42 -0400

    Time: 1653 ms (5.00%), Space: 45.8 MB (33.32%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int[] dp = new int[nums.length + 1];
-        int len = nums.length;
         Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        for(int i=0; i<len; i++) {
-            if(dp[i]!=Integer.MAX_VALUE) {
-                for(int coin=1; coin<=nums[i]; coin++) {
-                    int nextPosition = i + coin;
-                    if(nextPosition < len) {
-                        dp[nextPosition] = 1+ dp[i];
+        dp[0] = nums[0];
+        for(int i =0;i <=nums.length; i++) {
+            if(dp[i] != Integer.MAX_VALUE) {
+                for(int coin =0; coin <=nums[i]; coin++) {
+                    int next = i + coin;
+                    if(next < nums.length) {
+                        dp[next] = Math.min(dp[next], dp[i] + 1);
                     }
+                    
                 }
             }
-            
         }
-        return dp[len - 1] == Integer.MAX_VALUE ? false : true;
         
+        return dp[nums.length - 1] == Integer.MAX_VALUE ? false : true; 
     }
+    
 }
-
-// [2,3,1,1,4]
