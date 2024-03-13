d32738ced60623e661916076e4ea5f6828d30cf0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-13 15:00:31 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (78.16%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,24 +1,23 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] grid) {
-        int col = grid.length;
-        int row = grid[0].length;
-        int[][] dp = new int[col+1][row+1];
-        dp[0][0] = 1; 
-        for(int i = 0; i<col; i++) {
-            for(int j = 0; j<row; j++) {
-                if(grid[i][j]==1) {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        dp[0][0] =1; 
+        for(int i =0; i < m; i++) {
+            for(int j=0; j< n; j++) {
+                if(obstacleGrid[i][j] == 1) {
                     dp[i][j] = 0;
                 }else {
-                    if(i-1 >= 0) {
-                     dp[i][j]+= dp[i-1][j];   
+                    if(i -1  >= 0){
+                        dp[i][j] += dp[i-1][j] ;
                     }
-                    if(j-1 >= 0) {
-                     dp[i][j]+=dp[i][j-1];    
+                    if(j - 1 >= 0){
+                        dp[i][j] += dp[i][j-1];
                     }
                 }
-                
             }
         }
-        return dp[col-1][row-1];
+        return dp[m-1][n-1];
     }
 }
