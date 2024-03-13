9fdcff49ebd3392b60a9ae682a864e42186a6674
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-12 21:06:49 -0400

    Time: 1 ms (75.76%), Space: 41.4 MB (31.79%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,24 +1,29 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] arr = s.split(" ");
-        if(pattern.length() != arr.length) {
-            return false;
-        }
         
-        HashMap<Character, String> map = new HashMap<>();
-        for(int i =0 ;i<pattern.length(); i++) {
-            char c = pattern.charAt(i);//a
-            String str = arr[i];//dog
-            if(map.containsKey(c)) {
-                String value = map.get(c);
-                if(!value.equals(str)) {
+        String[] arr = s.split(" ");
+        
+        if(pattern.length() != arr.length) return false;
+        
+        Map<Character, String> map = new HashMap<>();
+        Set<String> visited = new HashSet<>();
+        int len = pattern.length();
+        
+        for(int i = 0; i< len; i++) {
+            char ch = pattern.charAt(i); //a
+            String value = arr[i];//dog
+            
+            if(!map.containsKey(ch)) {
+                if(visited.contains(value)) {
                     return false;
                 }
-            }else {
-                if(map.containsValue(str)) {
+                visited.add(value);//dog, cat
+                map.put(ch, value);//a - dog, b- cat
+            } else {
+                String existingValue = map.get(ch);// dog
+                if(!existingValue.equals(value)) {
                     return false;
                 }
-                map.put(c, str);
             }
         }
         return true;
@@ -26,6 +31,41 @@ class Solution {
 }
 
 
-//pattern = "abba", s = "dog cat cat dog"
-//"abba"   "dog dog dog dog"
+//bijection
+//"abba", s = "dog cat cat dog"
 
+//pattern = "abba", s = "dog cat cat fish"
+/**
+pattern = "aaaa", s = "dog cat cat dog"
+
+//both lower case
+
+1. both should be same
+2. take a map particular elemnt is prent or not
+3. if it is not presnet i w
+4. if it match
+
+Set
+
+a - dog
+b - dog
+
+ 
+"aaaa", s = "dog cat cat dog"
+
+
+
+// a - dog, cat
+// b
+
+//add both in seta
+// setb
+//a - dog
+// a - dog
+// b - cat
+//
+a - dog
+b
+
+
+**/
