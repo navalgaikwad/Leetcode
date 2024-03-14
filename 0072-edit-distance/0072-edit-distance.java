0d08c4fbb7b2f4886b44d66f3a0cbf032ef461f7
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-14 12:59:47 -0400

    Time: 19 ms (5.06%), Space: 45.3 MB (9.13%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,9 +1,8 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        return helper(word1, word2, word1.length()-1, word2.length()-1, new HashMap<>());
+        return helper(word1.length()-1, word2.length()-1, word1, word2, new HashMap<>());
     }
-    
-    int helper(String word1, String word2, int i, int j, HashMap<String, Integer> memo) {
+    int helper(int i, int j, String word1, String word2, HashMap<String, Integer> memo) {
         String key = i+"-"+j;
         if(i < 0) {
             return j + 1;
@@ -16,12 +15,12 @@ class Solution {
         }
         int total = 0;
         if(word1.charAt(i) == word2.charAt(j)) {
-            total = helper(word1, word2, i - 1, j - 1, memo);
+            total = helper(i-1, j-1, word1, word2, memo);
         }else {
-            int insert = helper(word1, word2, i - 1, j, memo);
-            int delete = helper(word1, word2, i, j - 1, memo);
-            int replace = helper(word1, word2, i - 1, j - 1, memo);
-            total = 1 + Math.min(insert,  Math.min(delete, replace));
+            int insert = helper(i-1, j, word1, word2, memo);
+            int delete = helper(i, j-1, word1, word2, memo);
+            int replace = helper(i-1, j-1, word1, word2, memo);
+            total =1+ Math.min(insert, Math.min(delete, replace));
         }
         memo.put(key, total);
         return total;
