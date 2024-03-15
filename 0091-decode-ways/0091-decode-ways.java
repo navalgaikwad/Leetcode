278002d46d60f7dead87d1c59c100f41f98a4ebb
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-15 00:33:45 -0400

    Time: 1 ms (80.98%), Space: 41.9 MB (25.76%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -8,15 +8,15 @@ class Solution {
         if (index == s.length()) {
             return 1;
         }
-        if (s.charAt(index) == '0') {
+        if (s.charAt(index) == '0') { //zero asel tar return 0
             return 0;
         }
         if (memo[index] != null) {
             return memo[index];
         }
         int count = dfs(s, index + 1, memo);
-        if (index + 1 < s.length() && Integer.parseInt(s.substring(index, index + 2)) <= 26) {
-            count += dfs(s, index + 2, memo);
+        if (index + 1 < s.length() && Integer.parseInt(s.substring(index, index + 2)) <= 26) {//index + 1 < len && index, index + 2 <=26
+            count += dfs(s, index + 2, memo); //add the count
         }
         memo[index] = count;
         return count;
