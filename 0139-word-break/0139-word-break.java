08b6b8f17565ba444a4f8ffa4a911ba2578e688d
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-15 11:33:39 -0400

    Time: 2 ms (91.07%), Space: 41.6 MB (90.58%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return dp(s, wordDict, new HashMap<>());
+        return helper(s, wordDict, new HashMap<>());
     }
     
-    boolean dp(String s, List<String> wordDict, HashMap<String, Boolean> memo) {
-         if(memo.containsKey(s)) {
-            return memo.get(s);
-        }
+    boolean helper(String s, List<String> wordDict, HashMap<String, Boolean> memo) {
         if(s.equals("")) {
             return true;
         }
-       
+        if(memo.containsKey(s)) {
+            return memo.get(s);
+        }
         boolean flag = false;
-        for(String word: wordDict) {
+        for(String word : wordDict) {
             if(s.startsWith(word)) {
                 String slice = s.substring(word.length());
-                if(dp(slice, wordDict, memo)){
+                if(helper(slice, wordDict, memo)) {
                     flag = true;
                 }
             }
@@ -23,31 +22,4 @@ class Solution {
         memo.put(s, flag);
         return flag;
     }
-    
 }
-
-// class Solution {
-//     public boolean wordBreak(String s, List<String> wordDict) {
-//         return dp(s, wordDict, new HashMap<>());
-//     }
-    
-//     boolean dp(String target, List<String> wordDict, HashMap<String, Boolean> memo) {
-//         if(memo.containsKey(target)) {
-//             return memo.get(target);
-//         }
-//         if(target == "") {
-//             return true;
-//         }
-//         for(String word : wordDict) {
-//             if(target.startsWith(word)) {
-//                 String slice = target.substring(word.length());
-//                 if(dp(slice, wordDict, memo)) {
-//                     memo.put(target, true);
-//                     return true;
-//                 }
-//             }
-//         }
-//         memo.put(target, false);
-//         return false;
-//     }
-// }
