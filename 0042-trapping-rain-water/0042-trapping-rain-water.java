9e13791e2eab03e78b4c60647161f79fbc65b5d3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-15 10:41:21 -0400

    Time: 0 ms (100.00%), Space: 46.1 MB (58.10%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,6 +2,32 @@ class Solution {
     public int trap(int[] height) {
         int left =0;
         int right = height.length - 1;
+        int result = 0;
+        int leftMax = 0;
+        int rightMax =0;
+        while(left < right) {
+            if(height[left] <= height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                result+=leftMax - height[left];
+                left++;
+            }else {
+                rightMax = Math.max(rightMax, height[right]);
+                result+=rightMax - height[right];
+                right--;
+            }
+        }
+        return result;
+    }
+}
+
+//take two pointer
+//leftMax
+//rightMax
+//same as stock problem
+/*class Solution {
+    public int trap(int[] height) {
+        int left =0;
+        int right = height.length - 1;
         int result =0;
         int leftMax =0;
         int rightMax =0;
@@ -18,4 +44,4 @@ class Solution {
         }
         return result;
     }
-}
+}*/
