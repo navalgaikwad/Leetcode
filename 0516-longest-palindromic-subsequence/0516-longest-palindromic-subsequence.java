f72754fb820510e49ac845e8b44187695630dd71
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-16 18:43:16 -0400

    Time: 55 ms (44.31%), Space: 61.5 MB (5.06%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,36 +1,33 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
+        String rev ="";
+        for(int i = 0; i<s.length(); i++) {
+            rev = s.charAt(i) + rev;
+        }
         Integer[][] memo = new Integer[s.length()][s.length()];
-         if(s.length()<=1){
-            return 1;
-        }
-        String rev="";
-        for(int i=0; i<s.length() ; i++){
-            rev=s.charAt(i)+rev+"";
-        }
-        return helper(s, rev, rev.length()-1, rev.length()-1, memo);
-        
+       // System.out.print(rev);
+        return helper(s.length() - 1, s.length() - 1, s, rev, memo);
     }
     
-    int helper(String s, String rev, int i, int j, Integer[][] memo) {
-        if(i <0) {
+    int helper(int i, int j, String s, String rev, Integer[][] memo) {
+        if( i < 0) {
             return 0;
         }
-        if(j <0) {
+        if(j < 0) {
             return 0;
         }
-        if(memo[i][j]!=null) {
+        if(memo[i][j] != null) {
             return memo[i][j];
         }
-        int total =0;
+        int total = 0;
         if(s.charAt(i) == rev.charAt(j)) {
-          total =1+ helper(s,rev, i-1, j-1, memo);  
+            total = 1 + helper(i - 1, j - 1, s, rev, memo);
         }else {
-            int top = helper(s,rev, i-1, j, memo);
-            int right = helper(s,rev, i, j-1, memo);
-            total =Math.max(top, right);
+            int one = helper(i , j - 1, s, rev, memo);
+            int two = helper(i - 1, j, s, rev, memo);
+            total = Math.max(one, two);
         }
-        memo[i][j]= total;
+        memo[i][j] = total;
         return total;
     }
 }
