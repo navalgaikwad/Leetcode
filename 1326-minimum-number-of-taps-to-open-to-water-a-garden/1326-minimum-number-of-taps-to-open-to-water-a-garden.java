3a3a4463bd45cb9fd5343cd03f5ecd2a6435e393
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-17 15:29:02 -0400

    Time: 6 ms (55.60%), Space: 43.9 MB (94.34%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minTaps(int n, int[] ranges) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, (int) 1e9);
+        dp[0] = 0;
+        for(int i =0;i<=n; i++) {
+            
+            int left = Math.max(0, i - ranges[i]);//check in left side
+            int right = Math.min(n, i + ranges[i]);//check in right side
+            
+            for(int start =left; start<=right; start++) {//go from left to right
+                dp[right] = Math.min(dp[right], dp[start] + 1); //sames as min coin
+            }
+               
+    }
+       
+        return dp[n] == (int) 1e9 ? -1 : dp[n];
+    }
+}
