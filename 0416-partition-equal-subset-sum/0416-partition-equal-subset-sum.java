f8f0da9b8d12d106c03cfa43fe42cfd4036cb60b
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-16 22:37:35 -0400

    Time: 32 ms (71.33%), Space: 41.8 MB (94.51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,23 +1,27 @@
 class Solution {
       public boolean canPartition(int[] nums) {
-        int sum = Arrays.stream(nums).sum();
-        
-        if(sum % 2 != 0) {
-            return false;
-        }
-    
-        sum /= 2;
-        boolean[] dp = new boolean[sum + 1];
-        dp[0] = true;
-        for(int i=0; i<nums.length; i++) {
-            for(int j = sum; j>=nums[i]; j--) {
-                boolean take = dp[j];
-                boolean notTake = dp[j - nums[i]];
-                dp[j] = take || notTake;
-            }
-        }
-        return dp[sum];
+          int sum = Arrays.stream(nums).sum();
+          if(sum%2 != 0) {
+              return false;
+          }
+          sum /= 2;
+          boolean[] dp = new boolean[sum + 1];
+          dp[0] = true;
+          for(int i=0; i<nums.length; i++) {
+              for(int j = sum; j>=nums[i]; j--) {
+                  boolean notTake = dp[j - nums[i]];
+                  boolean take = dp[j];
+                  
+                  dp[j] =  take || notTake; 
+              }
+          }
+          return dp[sum];
     }
+    //find total sum
+    // if not even return false
+    //define dp of sum+1 of boolean type
+    //for from i to nums.lenght
+    //for from sum to nums[i] j--
 
     // static boolean tabulation(int target, int[] num) {
     //     boolean[] table = new boolean[target + 1];
