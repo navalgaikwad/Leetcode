8261923fdea8d3c5973c89fd7bc800ede7da1da0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-18 07:40:14 -0400

    Time: 14 ms (7.81%), Space: 42.3 MB (35.79%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,21 +1,18 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         List<Integer> result = new ArrayList<>();
-        
         HashMap<Character, Integer> map = new HashMap<>();
-        for(int i =0 ;i<s.length(); i++) {
+        for(int i = 0; i<s.length(); i++) {
             map.put(s.charAt(i), Math.max(i, map.getOrDefault(s.charAt(i), 0)));
-        }// to find last index of character
-        
+        }
         int left = 0;
-        int max = -1;
+        int maxi = -1;
         for(int right = 0; right<s.length(); right++) {
-            
-            max = Math.max(max, map.get(s.charAt(right)));
-            if(right == max) {
+            maxi = Math.max(maxi, map.get(s.charAt(right)));
+            if(maxi == right) {
                 result.add(right - left + 1);
-                left = right+1;
-                max = -1;
+                left = right + 1;
+                maxi = -1;
             }
         }
         return result;
