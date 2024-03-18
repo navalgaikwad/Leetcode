6aa675e301e03a40fdd9b4cbee9946ba7c5ce668
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-18 08:08:13 -0400

    Time: 4 ms (93.65%), Space: 45.4 MB (20.74%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
@@ -1,18 +1,48 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
         int[] arr = new int[26];
-        int n=s.length();
-        if( k > n ) return false;
+        int len = s.length();
+        if( k > len) {
+            return false;
+        }
         for(char c : s.toCharArray()) {
             arr[c-'a']++;
         }
-        int res =0;
-        for(int i=0;i<26;i++) {
-            if(arr[i]%2!=0) {
-                res++;
+        int count = 0;
+        for(int i=0; i<26; i++) {
+            if(arr[i]%2 != 0) {
+                count++;
             }
         }
-        return res <=k;
+        return count <= k;
     }
 }
+
+// class Solution {
+//     public boolean canConstruct(String s, int k) {
+//         int[] arr = new int[26];
+//         int n=s.length();
+//         if( k > n ) return false;
+//         for(char c : s.toCharArray()) {
+//             arr[c-'a']++;
+//         }
+//         int res =0;
+//         for(int i=0;i<26;i++) {
+//             if(arr[i]%2!=0) {
+//                 res++;
+//             }
+//         }
+//         return res <=k;
+//     }
+// }
 //count odd occurences of character, here it is 'b' in annabelle for k = 2
+
+//annabelle
+/*
+a - 2
+b - 1
+n - 2
+e = 2
+l = 2
+
+*/
