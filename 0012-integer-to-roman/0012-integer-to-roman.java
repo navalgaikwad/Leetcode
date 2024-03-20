53360e91bbe8ce45b8b90af07b740ebb12b7061a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-20 16:00:02 -0400

    Time: 3 ms (97.71%), Space: 44.6 MB (40.97%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,17 +1,29 @@
 class Solution {
     public String intToRoman(int num) {
-        int []values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String []notations={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        StringBuilder sb=new StringBuilder();
-
-        for(int i=0;i<values.length;i++){
-            while(num>=values[i])
-            {
-                num-=values[i];
-                sb.append(notations[i]);
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40,  10,9, 5,4, 1};
+        
+        String[] notation = {"M",  "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<values.length; i++) {
+            while(num >= values[i]) {
+                num=num - values[i];
+                sb.append(notation[i]);
             }
         }
-
         return sb.toString();
     }
 }
+
+ // valuesToSymbols.put(1000, "M", );
+ //        valuesToSymbols.put(900, "CM");
+ //        valuesToSymbols.put(500, "D");
+ //        valuesToSymbols.put(400, "CD");
+ //        valuesToSymbols.put(100, "C");
+ //        valuesToSymbols.put(90, "XC");
+ //        valuesToSymbols.put(50, "L");
+ //        valuesToSymbols.put(40, "XL");
+ //        valuesToSymbols.put(10, "X");
+ //        valuesToSymbols.put(9, "IX");
+ //        valuesToSymbols.put(5, "V");
+ //        valuesToSymbols.put(4, "IV");
+ //        valuesToSymbols.put(1, "I");
