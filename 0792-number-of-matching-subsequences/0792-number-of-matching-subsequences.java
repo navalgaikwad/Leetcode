3a412f784a7675196a7b85e6d526b0849264d795
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-21 16:25:23 -0400

    Time: 72 ms (71.25%), Space: 47 MB (44.32%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
@@ -1,26 +1,32 @@
 class Solution {
-     //O(m * k * n)
     public int numMatchingSubseq(String s, String[] words) {
-        int max = 0;
+        Map<Character, Deque<String>> map = new HashMap<>();
+        for(char c = 'a'; c <= 'z'; c++) {
+            map.putIfAbsent(c, new LinkedList<>());
+        }
         for(String word : words) {
-            if(helper(s, word)) {
-                max++;
+            map.get(word.charAt(0)).addLast(word);
+        }
+        //System.out.print(map);
+        int count = 0;
+        for(char c : s.toCharArray()) {
+            Deque<String> q = map.get(c);
+            int size = q.size();
+            for(int i = 0; i<size; i++) {
+                String word = q.removeFirst();
+                if(word.length() == 1) {
+                    count++;
+                }else {
+                    map.get(word.charAt(1)).addLast(word.substring(1));
+                }
             }
         }
-        return max;
-    }
-    
-    
-    boolean helper(String s, String word) {
-        int index = 0;
-        
-       for(char c : word.toCharArray()) {
-           int idx = s.indexOf(c, index);
-           if(idx == -1) {
-               return false;
-           }
-           index = idx + 1;
-       }
-        return true;
+        return count;
     }
 }
+
+/*
+The time complexity of this algorithm is O(S + W), where S is the length of the string S and W is the total number of characters in 
+all words in the words array. The space complexity is O(W), where W is the total number of characters in all words in the words array, 
+due to the storage of words in deques.
+*/
