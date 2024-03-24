012f00f8eb59f4e0ad83b30bcef29705b890b52b
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-24 14:01:08 -0400

    Time: 6 ms (84.73%), Space: 42.8 MB (77.14%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,19 +1,12 @@
-import java.util.Stack;
-
-public class Solution {
+class Solution {
     public String reverseWords(String s) {
-        Stack<String> st = new Stack<>();
-        String[] arr = s.trim().split("\\s+"); //remove trailing space : \\s+
-        for (String a : arr) {
-            st.push(a);
-        }
-
+        String[] arr = s.trim().split("\\s+");
         StringBuilder result = new StringBuilder();
-        while (st.size()>1) {
-            result.append(st.pop().trim());
+        for(int i = arr.length - 1; i > 0; i--) {
+            result.append(arr[i]);
             result.append(" ");
         }
-        result.append(st.pop());
+        result.append(arr[0]);
         return result.toString();
     }
 }
