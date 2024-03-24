99459cdc7656a4f7b0beec6e9573656d5b8aac24
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-24 13:54:55 -0400

    Time: 5 ms (69.51%), Space: 45.5 MB (24.88%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,26 +1,24 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1 || numRows >= s.length()) {
+          if (numRows == 1 || numRows >= s.length()) {
             return s;
         }
         int len = s.length();
         ArrayList<Character> arr[] = new ArrayList[len];
-        for(int i=0;i<len; i++) {
+        for(int i=0; i<len; i++) {
             arr[i] = new ArrayList<>();
         }
         int j =0;
-        while(j<len) {
-            for(int i=0; i< numRows && j < len; i++) {
-                arr[i].add(s.charAt(j));
-                j++;
+        while(j < len) {
+            for(int i =0; i<numRows&& j < len; i++) {
+                arr[i].add(s.charAt(j++));
             }
-            for(int i = numRows - 2; i>=1&& j < len; i--) {
-                arr[i].add(s.charAt(j));
-                j++;
+            for(int i = numRows-2; i>=1&& j < len; i--) {
+                arr[i].add(s.charAt(j++));
             }
         }
         
-         StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for (int k = 0; k < arr.length; k++) {
             for (char c : arr[k]) {
                 sb.append(c);
