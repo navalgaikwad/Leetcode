0e15b92270b8aa9f91d65781e657893b7fead1e6
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-25 12:42:30 -0400

    Time: 8 ms (87.98%), Space: 46.4 MB (66.25%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,49 +1,23 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        LinkedList<int[]> mergedList = new LinkedList<>();
-        
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]); 
-        
-        for(int[]  interval: intervals){
-            if(mergedList.isEmpty() || mergedList.getLast()[1] < interval[0]){
-                mergedList.add(interval);
-            }else if(interval[0] <= mergedList.getLast()[1]){
-                mergedList.getLast()[1] =  Math.max(interval[1], mergedList.getLast()[1]);
-                //take max from intervals
+        LinkedList<int[]> merger = new LinkedList<>();
+        Arrays.sort(intervals, (a, b)-> a[0]-b[0]);
+        merger.add(intervals[0]);
+        for(int i =0; i<intervals.length; i++) {
+            if(!merger.isEmpty()&&intervals[i][0] <= merger.getLast()[1]) {
+                merger.getLast()[1] = Math.max(intervals[i][1], merger.getLast()[1]);
+            }else {
+                merger.addLast(intervals[i]);
             }
         }
-        
-        int[][] result = new int[mergedList.size()][2];
-        int i = 0;
-        for (int[] interval : mergedList){
-            result[i] = interval;
-            i += 1;
-        }
-        
-        return result;
+         return merger.toArray(new int[merger.size()][]);
     }
 }
 
-//[[1,4],[3,5]]
-//[1, 5] end < satrt
+/*
+there will be negative no
+they will be sorted
+1 size input
+2 size i
 
-
-// class Solution {
-//     public int[][] insert(int[][] intervals, int[] newInterval) {
-//         for(int[] interval : intervals) {
-//             if(newInterval[0] < interval[1]) {
-//                 interval[1] = Math.max(interval[1], newInterval[0]);
-//                 interval[0] = Math.min(interval[0], newInterval[0]);
-//             }
-//         }
-//         return intervals;
-//     }
-// }
-
-// // intervals = [[1,3],[6,9]], newInterval = [4,5]
-// //[1, ]
-// //2<3 merge start2 < end1
-// //4<3 not merge 
-
-// //intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
-// //[1,2]
+*/
