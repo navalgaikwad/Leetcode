9ce60e74ad47cb53ca42698b39a1715c442ba513
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-25 16:06:55 -0400

    Time: 8 ms (60.95%), Space: 57.4 MB (52.07%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,39 +1,24 @@
 class Solution {
-    int[][] dp;
-    
     public int maximalSquare(char[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int ans = Integer.MIN_VALUE;
-        dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+        if (matrix == null || matrix.length < 1) {
+            return 0;
         }
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    ans = Math.max(ans, helperMethod(i, j, matrix));
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int[][] dp = new int[rows + 1][cols + 1];
+        int maxSide = 0;
+        
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (matrix[r][c] == '1') {
+                    dp[r + 1][c + 1] = Math.min(dp[r][c], Math.min(dp[r + 1][c], dp[r][c + 1])) + 1; // Be careful of the indexing since dp grid has additional row and column
+                    maxSide = Math.max(maxSide, dp[r + 1][c + 1]);
                 }
             }
         }
         
-        return ans == Integer.MIN_VALUE ? 0 : ans * ans;
-    }
-    
-    private int helperMethod(int i, int j, char[][] matrix) {
-        if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length || matrix[i][j] == '0') {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        
-        return dp[i][j] = 1 + Math.min(helperMethod(i + 1, j, matrix), Math.min(helperMethod(i + 1, j + 1, matrix),
-                            helperMethod(i, j + 1, matrix)));
+        return maxSide * maxSide;
     }
 }
-
-// i+1. j+1
-// i+1, j
-// i, j+1
