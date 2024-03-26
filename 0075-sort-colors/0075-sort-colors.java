d78bdf617020abc03adebd21245b4e410755ec21
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-26 16:57:14 -0400

    Time: 0 ms (100.00%), Space: 41.8 MB (52.06%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,18 +1,24 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 0);
-        map.put(1, 0);
-        map.put(2, 0);
-        for(int i =0 ;i<nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        int j =0;
-        for(int i=0; i<3; i++) {
-            while(map.get(i) > 0) {
-                nums[j++] = i;
-                map.put(i, map.get(i) - 1);
+     public void sortColors(int[] nums) {
+        int lt = 0, i = 0, gt = nums.length - 1;
+        while (i <= gt) {
+            if (nums[i] == 0) {
+                swap(nums, lt, i);
+                lt++;
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, gt);
+                gt--;
+            } else { // nums[i] == 1
+                i++;
             }
         }
     }
+    private void swap(int[] nums, int p1, int p2) {
+        int temp = nums[p1];
+        nums[p1] = nums[p2];
+        nums[p2] = temp;
+    }
+
 }
+
