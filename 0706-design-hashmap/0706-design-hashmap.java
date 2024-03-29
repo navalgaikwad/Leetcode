fe968f763e7367479cff1d6c9057c295f0e2c995
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-29 09:03:03 -0400

    Time: 424 ms (5.02%), Space: 85.7 MB (6.10%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,16 +1,27 @@
 class MyHashMap {
-    int[] data;
+    int[] arr;
     public MyHashMap() {
-        data = new int[1000001];
-        Arrays.fill(data, -1);
+        arr = new int[10000001];
+        Arrays.fill(arr, -1);
     }
-    public void put(int key, int val) {
-        data[key] = val;
+    
+    public void put(int key, int value) {
+        arr[key] = value; 
     }
+    
     public int get(int key) {
-        return data[key];
+        return arr[key];
     }
+    
     public void remove(int key) {
-        data[key] = -1;
+        arr[key] = -1;
     }
 }
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
