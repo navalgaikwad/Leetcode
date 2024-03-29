bf9157bd6533e767cd8b0da2b53a245f18408f01
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-29 19:23:42 -0400

    Time: 170 ms (58.44%), Space: 40.5 MB (89.80%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,22 +1,20 @@
 class Solution {
     int res = Integer.MAX_VALUE;
     public int distributeCookies(int[] cookies, int k) {
-        
         int[] child = new int[k];
-        backTracking(child, cookies, 0, 0);
+        helper(child, cookies, 0, 0);
         return res;
     }
     
-    void backTracking(int[] child, int[] cookies, int index, int max) {
+    void helper(int[] child, int[] cookies, int index, int max) {
         if(index == cookies.length) {
             res = Math.min(res, max);
             return;
         }
-        //int c = cookies[index];
-        for(int i = 0; i<child.length; i++) {
-            child[i] +=cookies[index];;
-            backTracking(child, cookies, index + 1, Math.max(max, child[i] ));
-            child[i] -=cookies[index];;
+        for(int i=0; i < child.length; i++) {
+            child[i]+=cookies[index];
+            helper(child, cookies, index + 1, Math.max(max, child[i]));
+            child[i]-=cookies[index];
         }
     }
 }
