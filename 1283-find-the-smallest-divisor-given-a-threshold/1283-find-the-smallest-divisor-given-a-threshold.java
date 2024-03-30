427bcd049bbb72c159157fb2d62542d7e905c9fe
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-29 20:04:44 -0400

    Time: 30 ms (54.91%), Space: 48 MB (64.07%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -2,30 +2,30 @@ class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
         int left = 1;
         int right = 0;
-        for (int num : nums) {
-            right = Math.max(right, num);
+        for(int num : nums) {
+            right = Math.max(num, right);
         }
-        //find max
-        int result =right;
+        int result = right;
         while(left <= right) {
-            int mid = left + (right - left)/2;
-            if(helper(nums, threshold, mid)) {
+            int mid = left + (right-left)/2;
+            if(helper(nums, mid, threshold)) {
                 result = mid;
                 right = mid - 1;
             }else {
-                
                 left = mid + 1;
             }
         }
         return result;
     }
-    //division must be ceil
-     boolean helper(int[] nums, int threshold, int divisor) {
+    
+    boolean helper(int[] nums, int mid, int threshold) {
         int sum = 0;
-        for (int num : nums) {
-            sum += Math.ceil((double) num / divisor);
+        for(int num : nums) {
+            sum += Math.ceil((double) num/ mid);
         }
-        return sum <= threshold;
+        return sum <=threshold;
     }
 }
-//rest is the same
+//do binary search
+//take right max value from array
+//and left = 0
