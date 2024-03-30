894439d5a5207d42e3fe4379bb54f62102f7e8ad
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-30 17:39:59 -0400

    Time: 1 ms (97.05%), Space: 45.6 MB (28.60%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,23 +1,18 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int n = nums.length;
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-        prefix[0] = nums[0];
-        for(int i=1; i<n;i++) {
-            prefix[i] = prefix[i-1] + nums[i];
-        }
-        suffix[n-1] = nums[n-1];
-        for(int i = n-2; i>=0; i--) {
-            suffix[i] = suffix[i+1] + nums[i];
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
         }
         
-        for(int i=0; i<n; i++) {
-            if(suffix[i] == prefix[i]) {
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
                 return i;
             }
+            leftSum += nums[i];
         }
-        return -1;
         
+        return -1;
     }
 }
