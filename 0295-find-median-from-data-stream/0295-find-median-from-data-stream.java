dcb63c3c6f05b7bf21a55e56fffd36b6fb99d1b5
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-29 20:08:50 -0400

    Time: 98 ms (64.17%), Space: 63.3 MB (87.74%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,39 +1,33 @@
 class MedianFinder {
-    PriorityQueue<Double> max = new PriorityQueue<>((a, b) -> Double.compare(b, a));
-    PriorityQueue<Double> min = new PriorityQueue<>();
+    PriorityQueue<Double> minHeap = new PriorityQueue<>();
+    PriorityQueue<Double> maxHeap = new PriorityQueue<>((a, b) -> Double.compare(b, a));
     public MedianFinder() {
         
     }
     
     public void addNum(int num) {
-      if(max.isEmpty() || num < max.peek()) {
-          max.add((double)num);
-      }else {
-          min.add((double) num);
-      }
+        if(maxHeap.isEmpty() || (long)num <= maxHeap.peek()){
+            maxHeap.add((double)num);
+        }else{
+            minHeap.add((double)num);
+        }
         
-        if(max.size() > min.size()+1) {
-            min.add(max.remove());
-        }else if(max.size() < min.size()) {
-            max.add(min.remove());
+        if(maxHeap.size() > minHeap.size()+1){
+            minHeap.add(maxHeap.remove());
+        }else if(maxHeap.size() < minHeap.size()){
+             maxHeap.add(minHeap.remove());
         }
     }
     
     public double findMedian() {
-        int size1 = max.size();
-        int size2 = min.size();
+        int size1 = maxHeap.size();
+        int size2 = minHeap.size();
         if(size1 == size2) {
-            double maxKey = max.peek();
-            double minKey = min.peek();
-            return (maxKey+minKey)/2;
+           double max = maxHeap.peek();
+           double min = minHeap.peek();
+            double val = (max + min)/2;
+            return val;
         }
-        return max.peek();
+       return maxHeap.peek();
     }
 }
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
