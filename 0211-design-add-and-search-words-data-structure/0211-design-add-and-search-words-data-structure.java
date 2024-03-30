c974e4f636d906ec44a1ac36f915bbffb287a3c8
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-30 17:25:16 -0400

    Time: 288 ms (28.48%), Space: 118.2 MB (5.24%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,4 +1,5 @@
-class Node {
+class WordDictionary {
+    class Node {
         char c;
         Map<Character, Node> children;
         boolean isEnd;
@@ -8,12 +9,9 @@ class Node {
             this.isEnd = false;
         }
     }
-class WordDictionary {
-    
-    private Node root;
-    
+    Node root = new Node('\0');
     public WordDictionary() {
-        root = new Node('\0');
+        
     }
     
     public void addWord(String word) {
@@ -24,28 +22,28 @@ class WordDictionary {
             }
             curr = curr.children.get(c);
         }
-        curr.isEnd = true; 
+        curr.isEnd = true;
     }
     
     public boolean search(String word) {
-        Node curr = root;
+         Node curr = root;
         return searchInNode(word, curr);
     }
     //.ad
-    private boolean searchInNode(String word, Node node) {
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            if (!node.children.containsKey(ch)) {//
-               if(ch == '.') {
-                   for(Node c: node.children.values()) {//pair with other combination
-                     if(searchInNode(word.substring(i+1), c)){
-                       return true;
-                   }  
-                 } 
-               }
-             return false;
-            } 
-           node = node.children.get(ch);   
+    private boolean searchInNode(String word, Node node) { 
+        for(int i = 0; i<word.length(); i++) {
+            char c = word.charAt(i);
+            if(!node.children.containsKey(c)) {
+                if(c == '.') {
+                    for(Node ch : node.children.values()) {
+                        if(searchInNode(word.substring(i + 1), ch)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            node = node.children.get(c);
         }
         return node.isEnd;
     }
