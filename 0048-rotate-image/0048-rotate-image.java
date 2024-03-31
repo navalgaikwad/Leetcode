de576f890fc0e1c3f09903ab6992d0a7cc46dea4
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-03-31 11:07:11 -0400

    Time: 0 ms (100.00%), Space: 42.5 MB (7.67%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,21 +1,33 @@
 class Solution {
+    
     /*
- * clockwise rotate
- * first reverse up to down, then swap the symmetry 
- * 1 2 3     7 8 9     7 4 1
- * 4 5 6  => 4 5 6  => 8 5 2
- * 7 8 9     1 2 3     9 6 3
-*/
+    [[1,2,3],
+    [4,5,6],
+    [7,8,9]]
+    
+    [7,8,9],
+    [4,5,6],
+    [[1,2,3]
+    
+    [7,4,1]
+    [8,5,2]
+    [9,6,3]
+    
+[[7,8,1],
+[4,5,2],
+[9,6,3]]
+    
+    */
     public void rotate(int[][] matrix) {
-        // Reverse the matrix
-       for(int i=0; i<matrix.length/2; i++) {
-           int[] temp = matrix[i];
-           matrix[i] = matrix[matrix.length - i - 1];
-           matrix[matrix.length - i - 1] = temp;
-       }
+        int len = matrix.length-1;
+        for(int i=0; i<=len/2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[len - i ];
+            matrix[len - i ] = temp;
+        }
         
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=i+1; j<matrix[i].length; j++) {
+        for(int i =0; i<len; i++) {
+            for(int j = i+1; j<matrix[0].length; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
