7198f2616cb18dccb3aca4bb08781e934373d39c
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-01 00:11:21 -0400

    Time: 4 ms (71.74%), Space: 43.9 MB (96.24%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,29 +11,21 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length == 0) return null; 
-        PriorityQueue<ListNode> pq= new PriorityQueue<>((a,b)->(a.val - b.val));
+        PriorityQueue<ListNode> pq= new PriorityQueue<>((a,b)-> a.val-b.val);
         for(ListNode list : lists) {
-            if(list!=null) pq.add(list);
+            if(list != null) pq.add(list);
         }
-       // System.out.print(pq);
+        //System.out.print(pq);
         ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+        ListNode curr = dummy;
         while(!pq.isEmpty()) {
-            // ListNode list = 
-            // if(list != null) {
-                ListNode list = pq.remove();
-                current.next = list;
-                current = current.next;
-                if(list.next != null) {
-                   pq.add(list.next); 
-                }
-            //}
-            
+            ListNode list = pq.remove();
+            curr.next = list;
+            curr = curr.next;
+            if(list.next!= null) {
+                pq.add(list.next);
+            }
         }
-        
         return dummy.next;
     }
 }
-//[[1,4,5],[1,3,4],[2,6]]
-//keep the head in priority Queue
-//accordingly i will create 
