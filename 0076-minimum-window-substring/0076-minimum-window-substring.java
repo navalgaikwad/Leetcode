7f04af7017557da3092d241aea4f695fd93da6a7
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-01 14:10:29 -0400

    Time: 68 ms (14.24%), Space: 45.5 MB (14.45%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,53 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int left =0;
-       // int right =0;
+        int left = 0;
+        int len = s.length();
+        //int right = 0;
+        int minLen = Integer.MAX_VALUE;
+        String ans = "";
         Map<Character, Integer> map = new HashMap<>();
         for(char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int counter = map.size();
-        int len = Integer.MAX_VALUE;
-        String ans = "";
-        for(int right = 0; right < s.length(); right++) {
+        int count = map.size();
+         for(int right = 0; right < s.length(); right++) {
             char rightChar = s.charAt(right);
             if(map.containsKey(rightChar)) {
                 map.put(rightChar, map.getOrDefault(rightChar, 0) - 1);
                 if(map.get(rightChar) == 0) {
-                    counter--;
+                    count--;
                 }
             }
             
-            while(counter == 0) {
-                if(right - left + 1 < len) {
-                    len = right - left + 1;
+            while(count == 0) {
+                if(right - left + 1 < minLen) {
+                    minLen = right - left + 1;
                     ans = s.substring(left, right + 1);
                 }
-                
-                char leftChar = s.charAt(left);
-                if(map.containsKey(leftChar)) {
-                    map.put(leftChar, map.getOrDefault(leftChar, 0) + 1);
-                    if(map.get(leftChar) >0) {
-                        counter++;
+                char cl = s.charAt(left);
+                if(map.containsKey(cl)) {
+                    map.put(cl, map.getOrDefault(cl, 0) + 1);
+                    if(map.get(cl) > 0) {
+                      count++;
                     }
                 }
                 left++;
             }
+            
         }
         return ans;
     }
 }
-//1. first i will take count
-//2. i will go chacryer by character to find the each charcter untill that value in the map count =0
-//3. onces i found i will take length and string
+
+/*
+s = "ADOBECODEBANC", t = "ABC"
+A:1
+B:1
+C:1
+
+Jwhen ever it is 0
+if min len updadet the window
+get left char check if present in map then increament
+
+
+*/
