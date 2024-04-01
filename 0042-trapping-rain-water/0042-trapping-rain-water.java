7d622873f2626bb24af0f0a8f657c47a0294c52a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-01 01:00:32 -0400

    Time: 0 ms (100.00%), Space: 45.4 MB (95.48%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,47 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        int left =0;
-        int right = height.length - 1;
-        int result = 0;
-        int leftMax = 0;
-        int rightMax =0;
+        int left = 0;
+        int right = height.length-1;
+        int leftMax = Integer.MIN_VALUE;
+        int rightMax = Integer.MIN_VALUE;
+        int total = 0;
         while(left < right) {
             if(height[left] <= height[right]) {
                 leftMax = Math.max(leftMax, height[left]);
-                result+=leftMax - height[left];
+                total+=leftMax - height[left];
                 left++;
             }else {
                 rightMax = Math.max(rightMax, height[right]);
-                result+=rightMax - height[right];
+                total+=rightMax - height[right];
                 right--;
             }
         }
-        return result;
+        return total;
     }
 }
-
-//take two pointer
-//leftMax
-//rightMax
-//same as stock problem
-/*class Solution {
-    public int trap(int[] height) {
-        int left =0;
-        int right = height.length - 1;
-        int result =0;
-        int leftMax =0;
-        int rightMax =0;
-        while(left < right) {
-            if(height[left] <= height[right]) {
-                leftMax = Math.max(height[left], leftMax);
-                result += leftMax - height[left];
-                left++;
-            }else {
-                rightMax = Math.max(height[right], rightMax);
-                result += rightMax - height[right];
-                right--;
-            }
-        }
-        return result;
-    }
-}*/
