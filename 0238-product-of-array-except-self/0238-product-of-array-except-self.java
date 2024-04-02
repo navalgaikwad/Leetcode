e33901fc7b4a88a7a30ee70bddc2e754ccb2216e
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-02 14:54:54 -0400

    Time: 2 ms (75.11%), Space: 54.8 MB (23.39%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,43 +2,27 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int len = nums.length;
         int[] prefix = new int[len];
-        int[] suffix = new int[len];
-        
-        
-        //[1, 2,6,24]
-        //[ 6,6,3,1]
-        
-        //[1, 1,2, 6]
-        //[1,2,3,4]
-        //[ 24,12,4,1]
+
         prefix[0] = 1;
         for(int i = 1; i<len; i++) {
             prefix[i] = prefix[i-1] * nums[i-1];
         }
-        print(prefix);
-        suffix[len - 1] = 1;
-        for(int i = len-2; i>=0; i--) {
-            suffix[i] = suffix[i+1] * nums[i+1];
+       
+        int suffix = 1;
+        for(int i = len-1; i>=0; i--) {
+            prefix[i] *= suffix;
+            suffix*=nums[i];
+            
         }
-       // print(suffix);
-        
-         int[] result = new int[len];
-        for(int i=0; i<len; i++) {
-            result[i] = suffix[i] * prefix[i];
-        }
-        return result;
+        return prefix;
     }
     
-    void print(int[] arr) {
-        for(int ar : arr) {
-            System.out.print(ar+" ");
-        }
-    }
 }
 
 /*
 
 //[1,2,3,4]
+1, 1,2, 6
 //[1,2,6,12]
 //[6,6,3,1]
 
