48ce41d7250f6863c369feccf73937561f8a7e5d
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-02 14:47:37 -0400

    Time: 349 ms (6.22%), Space: 56.5 MB (5.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,24 +1,49 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefix = new int[nums.length];
-        int[] postFix = new int[nums.length];
-        int n = nums.length;
-        postFix[n - 1] = 1;////vv important
-        for(int i = n-2; i>=0; i--) {
-            postFix[i] = postFix[i + 1] * nums[i + 1];
+        int len = nums.length;
+        int[] prefix = new int[len];
+        int[] suffix = new int[len];
+        
+        
+        //[1, 2,6,24]
+        //[ 6,6,3,1]
+        
+        //[1, 1,2, 6]
+        //[1,2,3,4]
+        //[ 24,12,4,1]
+        prefix[0] = 1;
+        for(int i = 1; i<len; i++) {
+            prefix[i] = prefix[i-1] * nums[i-1];
         }
-        prefix[0] = 1;//vv important
-        for(int i=1; i<n; i++) {
-            prefix[i] = prefix[i - 1]*nums[i - 1];
+        print(prefix);
+        suffix[len - 1] = 1;
+        for(int i = len-2; i>=0; i--) {
+            suffix[i] = suffix[i+1] * nums[i+1];
         }
-        int[] result = new int[n];
-        for(int i=0; i<n; i++) {
-            result[i] = postFix[i] * prefix[i];
+       // print(suffix);
+        
+         int[] result = new int[len];
+        for(int i=0; i<len; i++) {
+            result[i] = suffix[i] * prefix[i];
         }
         return result;
     }
+    
+    void print(int[] arr) {
+        for(int ar : arr) {
+            System.out.print(ar+" ");
+        }
+    }
 }
 
+/*
+
 //[1,2,3,4]
-//prefix 1, 1, 2, 6
-//suffix 24,12,4, 1
+//[1,2,6,12]
+//[6,6,3,1]
+
+[1,2,3,4]
+[6,6,3,1]
+[1,2,6,12]
+[]
+*/
