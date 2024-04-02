05f868136015a5f5e0960be06ce6e04b360e8a71
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-02 13:26:22 -0400

    Time: 2 ms (90.50%), Space: 41.5 MB (91.75%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         return helper(s, wordDict, new HashMap<>());
     }
-    
-    boolean helper(String s, List<String> wordDict, HashMap<String, Boolean> memo) {
+    boolean helper(String s, List<String> wordDict, Map<String, Boolean> memo) {
         if(s.equals("")) {
             return true;
         }
@@ -13,8 +12,9 @@ class Solution {
         boolean flag = false;
         for(String word : wordDict) {
             if(s.startsWith(word)) {
-                String slice = s.substring(word.length());
-                if(helper(slice, wordDict, memo)) {
+                String remainder = s.substring(word.length());
+                if(helper(remainder, wordDict, memo)) {
+                   
                     flag = true;
                 }
             }
