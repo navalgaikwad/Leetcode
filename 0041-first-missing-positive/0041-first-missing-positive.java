56495dd634ce07812a42ed7011388aabad505009
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-03 08:54:21 -0400

    Time: 1 ms (95.97%), Space: 57 MB (21.14%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,25 +1,18 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-        boolean[] seen = new boolean[n + 1]; // Array for lookup
-
-        // Mark the elements from nums in the lookup array
-        for (int num : nums) {
-            if (num > 0 && num <= n) {
-                seen[num ] = true;
+        boolean[] seen = new boolean[n+1];
+        for(int i=0; i<n; i++) {
+            if(nums[i] > 0 && nums[i] <= n){
+                seen[nums[i]] = true;
             }
         }
-
-        // Iterate through integers 1 to n
-        // return smallest missing positive integer
-        for (int i = 1; i <= n; i++) {
-            if (!seen[i]) {
-                return i;
+        
+        for(int i = 1; i<=n; i++) {
+            if(!seen[i]) {
+               return i; 
             }
         }
-
-        // If seen contains all elements 1 to n
-        // the smallest missing positive number is n + 1
         return n + 1;
     }
 }
