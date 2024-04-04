b886aab5e8cfc7caf8a9ef9a0ccb664686dfe23a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-04 07:52:07 -0400

    Time: 11 ms (34.39%), Space: 44.4 MB (60.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set<String> row = new HashSet<>();
-        Set<String>col = new HashSet<>();
-        Set<String>boxes = new HashSet<>();
         for(int i =0; i < board.length; i++) {
             for(int j=0; j< board[0].length; j++) {
                 
@@ -10,16 +8,17 @@ class Solution {
                  int box = (i/3) + (j/3)*3;
                 
                 if(board[i][j] != '.') {
-                    String c = value+"-"+i;
-                    String v = value+"-"+j;
-                    String boxi = value+"-"+box;
-                    if(row.contains(c) || col.contains(v) || boxes.contains(boxi)) {
+                    String r = value+"r"+i;
+                    String c = value+"c"+j;
+                    String boxi = value+"b"+box;
+                    if(row.contains(r) || row.contains(c) || row.contains(boxi)) {
                         return false;
                     }
                         
                     row.add(c);
-                    col.add(v);
-                    boxes.add(boxi);
+                    row.add(r);
+                    row.add(boxi);
+                    
                 }
             }
         }
