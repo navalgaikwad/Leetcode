a02eef57f3e3ed39cc985bd6b47a35363e704b96
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-05 18:32:10 -0400

    Time: 24 ms (52.50%), Space: 44.7 MB (61.09%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -4,56 +4,34 @@ class Solution {
         for(char c : p.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        //System.out.print(map);
+        int size = map.size();
+        int left =0;
+        int min = Integer.MAX_VALUE;
         List<Integer> result = new ArrayList<>();
-        int counter = map.size();
-        int left = 0;
-        for(int right=0; right<s.length(); right++) {
-            char rightSide = s.charAt(right);
-            
-            if(map.containsKey(rightSide)) {
-                map.put(rightSide, map.getOrDefault(rightSide, 0) - 1);
-                if(map.get(rightSide) == 0) {
-                    counter--;
+        for(int right =0; right <s.length(); right++) {
+            char cright = s.charAt(right);
+            if(map.containsKey(cright)) {
+                
+                map.put(cright, map.getOrDefault(cright, 0) - 1);
+                if(map.get(cright)==0) {
+                    size--;
                 }
             }
-            
-            while(counter == 0) {
+            while(size == 0) {
                 if(right - left + 1 == p.length()) {
                     result.add(left);
+                    //min = right - left + 1;
                 }
-                char leftSide = s.charAt(left);
-                if(map.containsKey(leftSide)) {
-                    map.put(leftSide, map.getOrDefault(leftSide, 0) + 1);
-                    if(map.get(leftSide) > 0) {
-                        counter++;
+                char cleft = s.charAt(left);
+                if(map.containsKey(cleft)) {
+                    map.put(cleft, map.getOrDefault(cleft, 0) + 1);
+                    if(map.get(cleft)>0) {
+                        size++;
                     }
                 }
                 left++;
             }
         }
-        
         return result;
     }
 }
-
-//first i will add value in the map
-//then take counter = map.size();
-//iterate over String s
-//when ever the counter is zero
-// i will add the left pointer
-// and increament left until we find count grater than 0
-
-//s = "cbaebabacd", p = "abc"
-/*
-a =1,0,-1,-2
-b = 1,0,-1,-2
-c= 1, 0, 1, 0
-result = 0
-left = 6
-counter = 1, 0
-
-
-
-
-*/
