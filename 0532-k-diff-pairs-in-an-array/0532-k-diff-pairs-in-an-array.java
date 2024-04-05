d7350462118a82a78eb9ee9ac068029a754223c0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-05 18:03:10 -0400

    Time: 6 ms (97.21%), Space: 44.8 MB (59.33%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,24 +1,34 @@
+import java.util.Arrays;
+
 class Solution {
     public int findPairs(int[] nums, int k) {
-        int i =0;
+        int i = 0;
         int j = 1;
-        int count = 0;
-        int sum = Integer.MIN_VALUE;
+        int m = 0;
         Arrays.sort(nums);
-        while(i < nums.length && j < nums.length) {
-            while(nums[j] - nums[i] == k && nums[i] + nums[j] != sum) {
-                sum =nums[i] + nums[j];
-                count++;
-            }
-            if(nums[j] - nums[i] < k ) {
+        while (i < nums.length - 1 && j < nums.length) {
+            if (Math.abs(nums[i] - nums[j]) == k) {
+                m++;
+                int val1 = nums[i];
+                int val2 = nums[j];
+                // Move to the next different value of nums[i]
+                while (i < nums.length && nums[i] == val1) {
+                    i++;
+                }
+                // Move to the next different value of nums[j]
+                while (j < nums.length && nums[j] == val2) {
+                    j++;
+                }
+            } else if ((nums[j] - nums[i]) < k) {
                 j++;
-            }else {
+            } else {
                 i++;
             }
-            if(i == j) {
+            // Ensure i and j are not the same to avoid duplicates
+            if (i == j) {
                 j++;
             }
         }
-        return count;
+        return m;
     }
 }
