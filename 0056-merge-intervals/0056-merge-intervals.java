9eeff21ec8ffa847ab197506504c8bb211863712
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-06 10:38:57 -0400

    Time: 8 ms (88.22%), Space: 46.6 MB (36.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,25 +1,16 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        LinkedList<int[]> merger = new LinkedList<>();
-         Arrays.sort(intervals, (a, b) ->Integer.compare(a[0], b[0]));
-        for(int[] interval :intervals) {
-            if(merger.isEmpty() || merger.getLast()[1] < interval[0]) {
-                merger.add(interval);
+        Arrays.sort(intervals, (a, b) ->a[0] - b[0]);
+        LinkedList<int[]> merge = new LinkedList<>();
+        merge.add(intervals[0]);
+        for(int[] interval : intervals) {
+            if(merge.getLast()[1] >= interval[0]) {
+                merge.getLast()[1] = Math.max(merge.getLast()[1], interval[1]);
             }else {
-                merger.getLast()[1] = Math.max(interval[1], merger.getLast()[1]);
+                merge.addLast(interval);
             }
         }
-         return merger.toArray(new int[ merger.size() ][ ]);
+        //return result.toArray(new int[result.size()][]);
+        return merge.toArray(new int[merge.size()][]);
     }
 }
-
-// LinkedList<int[]> merger = new LinkedList<>();
-// Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
-// for(int[] interval :intervals) {
-//     if(merger.isEmpty() || merger.getLast()[1] < interval[0]) {
-//         merger.add(interval);
-//     }else {
-//         merger.getLast()[1] = Math.max(interval[1], merger.getLast()[1]);
-//     }
-// }
-// return merger.toArray(new int[merger.size()][]);
