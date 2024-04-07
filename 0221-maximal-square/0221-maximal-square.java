a107b20b72dfcfaac2b274b20b89a0bbc3920bbb
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-07 19:20:29 -0400

    Time: 10 ms (25.06%), Space: 57.5 MB (39.19%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,24 +1,35 @@
 class Solution {
+    int[][] dp;
     public int maximalSquare(char[][] matrix) {
-        if (matrix == null || matrix.length < 1) {
-            return 0;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        dp = new int[row][col];
+        for(int i=0; i<row; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        
-        int[][] dp = new int[rows + 1][cols + 1];
-        int maxSide = 0;
-        
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (matrix[r][c] == '1') {
-                    dp[r + 1][c + 1] = Math.min(dp[r][c], Math.min(dp[r + 1][c], dp[r][c + 1])) + 1; // Be careful of the indexing since dp grid has additional row and column
-                    maxSide = Math.max(maxSide, dp[r + 1][c + 1]);
+        int ans = Integer.MIN_VALUE;
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(matrix[i][j] == '1') {
+                    int result = dfs(matrix, i, j);
+                    ans = Math.max(result, ans);
                 }
             }
         }
-        
-        return maxSide * maxSide;
+        return ans == Integer.MIN_VALUE ? 0 : ans*ans; 
     }
+    
+    int dfs(char[][] matrix, int row, int col) {
+        if(row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || matrix[row][col] == '0') {
+            return 0;
+        }
+        if(dp[row][col] != -1) {
+            return dp[row][col];
+        }
+        int min = 1 + Math.min(dfs(matrix, row + 1, col), Math.min(dfs(matrix, row, col + 1), 
+                                                                  dfs(matrix, row + 1, col + 1)));
+        dp[row][col] = min;
+        return min;
+    }
+    
 }
