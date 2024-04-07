ee2d1e91a5906aa12423b421dcba7c2c6cba243d
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-07 19:01:07 -0400

    Time: 5 ms (41.14%), Space: 44.2 MB (96.57%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,28 +1,26 @@
 class MinStack {
-    Stack<Integer>minStack = new Stack<>();
-    Stack<Integer>stack = new Stack<>();
+    Stack<Integer> mainStack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
     public MinStack() {
         
     }
-
+    
     public void push(int val) {
-         stack.push(val);
-        if(minStack.isEmpty() || val<= minStack.peek()){
+        mainStack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()) {
             minStack.push(val);
         }
-       
     }
     
     public void pop() {
-         int val = stack.pop();
-        if(val == minStack.peek()) {
+        int value = mainStack.pop();
+        if(value == minStack.peek()) {
             minStack.pop();
         }
-       // stack.pop();
     }
     
     public int top() {
-        return stack.peek();
+        return mainStack.peek();
     }
     
     public int getMin() {
