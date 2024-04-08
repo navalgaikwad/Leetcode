fca6c548c2f6e0037b180f85b3bd6d47465e375a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-07 21:23:29 -0400

    Time: 1 ms (99.43%), Space: 57.2 MB (28.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,20 +3,13 @@ class Solution {
         int sum = 0;
         int max = Integer.MIN_VALUE;
         for(int i=0; i<nums.length; i++) {
-            sum +=nums[i];
+            sum += nums[i];
             max = Math.max(max, sum);
-            
             if(sum < 0) {
                 sum = 0;
             }
+            
         }
         return max;
     }
 }
-/*
-[5,4,-1,7,8]
-5+4+-1+7+8
-[-2,1,-3,4,-1,2,1,-5,4]
-4-1+2+1 
-
-*/
