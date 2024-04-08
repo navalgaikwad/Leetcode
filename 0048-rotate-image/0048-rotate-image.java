ba663b6384c8492596b590a86266d48621fe7fde
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-08 10:31:36 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (91.00%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,36 +1,36 @@
 class Solution {
-    
     /*
     [[1,2,3],
     [4,5,6],
     [7,8,9]]
     
-    [7,8,9],
-    [4,5,6],
-    [[1,2,3]
     
-    [7,4,1]
-    [8,5,2]
-    [9,6,3]
+    [7,8,9]
+    [4,5,6]
+    [1,2,3]
     
-[[7,8,1],
-[4,5,2],
-[9,6,3]]
+    7,4,1
+    8,5,2
+    9,6,3
+    
+[[7,4,1],
+[8,5,6],
+[9,2,3]]
     
     */
     public void rotate(int[][] matrix) {
         int len = matrix.length-1;
-        for(int i=0; i<=len/2; i++) {
-            int[] temp = matrix[i];
-            matrix[i] = matrix[len - i ];
-            matrix[len - i ] = temp;
+        for(int i =0; i<=len/2; i++) {
+            int[] value = matrix[len - i];
+            matrix[len - i] = matrix[i];
+            matrix[i] = value;
         }
-        
-        for(int i =0; i<len; i++) {
-            for(int j = i+1; j<matrix[0].length; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        //System.out.print(matrix[0][0]);
+        for(int i = 0; i<matrix.length; i++) {
+            for(int j=i+1; j<matrix[0].length; j++) {
+                int value = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = value;
             }
         }
     }
