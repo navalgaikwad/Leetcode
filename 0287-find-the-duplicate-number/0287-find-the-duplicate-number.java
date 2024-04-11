1116461150a2d5a25029c727106ae1ce7f00a0b0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-11 08:29:58 -0400

    Time: 4 ms (81.78%), Space: 58.1 MB (50.05%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,24 +2,16 @@ class Solution {
     public int findDuplicate(int[] nums) {
         int slow = nums[0];
         int fast = nums[0];
-
-        // Find the intersection point of the two pointers
-        do {
+         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        // Find the "entrance" to the cycle
+        } while(slow != fast);
+        
         slow = nums[0];
-        while (slow != fast) {
+        while(slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
         }
-
         return slow;
     }
 }
-/*
-Once the intersection point is found, the algorithm resets the slow pointer to the starting position (nums[0]) and keeps the fast pointer at the intersection point. Then, both pointers move at the same pace (one step at a time) until they meet again. The meeting point will be the "entrance" to the cycle, which corresponds to the index of the duplicate number in the array.
-
-*/
