98b952e1bd4b3275a11661d3faf6cf6e0e5d9015
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-11 06:57:09 -0400

    Time: 1 ms (9.61%), Space: 40.8 MB (61.99%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,46 +1,30 @@
 class Solution {
     public int rob(int[] nums) {
-        int len = nums.length;
+         int len = nums.length;
         if(len<2){
             return nums[0];
         }
-      
-
-        // Copy the array from index 2 to the last index
-        int[] copy1 = Arrays.copyOfRange(nums, 1, nums.length);
-        int[] copy2 = Arrays.copyOfRange(nums, 0, nums.length - 1);
-        
-        int firstRob = dp(copy1, len - 2, new HashMap<>());
-        int secondRob = dp(copy2, len - 2, new HashMap<>());
-        return Math.max(firstRob, secondRob);
+        int[] a = Arrays.copyOfRange(nums, 1, nums.length);
+        int[] b = Arrays.copyOfRange(nums, 0, nums.length - 1);
+        int first = dp(a, len - 2, new HashMap<>());
+        int second = dp(b, len - 2, new HashMap<>());
+        return Math.max(first, second);
     }
     
-    int dp(int[] nums, int currentSteps, HashMap<Integer, Integer> memo){
-        if(currentSteps == 0){
-            return nums[currentSteps];
+    int dp(int[] nums, int start, HashMap<Integer, Integer> memo) {
+        if(start == 0) {
+            return nums[start];
         }
-        if(currentSteps < 0){
-            //return Math.max(nums[0], nums[1]);
+        if(start < 0) {
             return 0;
         }
-        if(memo.containsKey(currentSteps)) return memo.get(currentSteps);
-        
-        int currentRob = dp(nums, currentSteps - 2, memo) + nums[currentSteps];
-        int previousRob = dp(nums, currentSteps - 1, memo);
-        int val = Math.max(currentRob, previousRob);
-        memo.put(currentSteps, val);
-        return val;
+        if(memo.containsKey(start)) {
+            return memo.get(start);
+        }
+        int current = dp(nums, start - 2, memo) + nums[start];
+        int previous = dp(nums, start - 1, memo);
+        int max = Math.max(current, previous);
+        memo.put(start, max);
+        return max;
     }
-    
-    //  int dp(int[] nums, int start){
-    //     if(start == ){
-    //         return nums[start];
-    //     }
-    //     if(start == 2){
-    //         return Math.max(nums[1], nums[2]);
-    //     }
-    //     int currentRob = dp(nums, start - 2) + nums[start - 2];
-    //     int previousRob = dp(nums, start - 1);
-    //     return Math.max(currentRob, previousRob);
-    // }
 }
