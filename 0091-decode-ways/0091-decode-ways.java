f5230e740e33a99032f26929c024d22ffdb7d991
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-11 07:53:38 -0400

    Time: 1 ms (80.81%), Space: 41.8 MB (32.17%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,29 +1,23 @@
 class Solution {
     public int numDecodings(String s) {
-        Integer[] memo = new Integer[s.length()];
-        return dfs(s, 0, memo);
+        return helper(s, 0, new HashMap<>());
     }
     
-    int dfs(String s, int index, Integer[] memo) {
-        if (index == s.length()) {
+    int helper(String s, int start, HashMap<Integer, Integer> memo) {
+        if(start == s.length()) {
             return 1;
         }
-        if (s.charAt(index) == '0') { //zero asel tar return 0
+        if(s.charAt(start) == '0') {
             return 0;
         }
-        if (memo[index] != null) {
-            return memo[index];
+        if(memo.containsKey(start)) {
+            return memo.get(start);
         }
-        int count = dfs(s, index + 1, memo);
-        if (index + 2 <= s.length() && Integer.parseInt(s.substring(index, index + 2)) <= 26) {//index + 1 < len && index, index + 2 <=26
-            count += dfs(s, index + 2, memo); //add the count
+        int count = helper(s, start + 1, memo);
+        if(start + 2 <= s.length() && Integer.parseInt(s.substring(start, start + 2)) <= 26) {
+            count+= helper(s, start + 2, memo);
         }
-        memo[index] = count;
+        memo.put(start, count);
         return count;
     }
 }
-
-//either i can select one or 2
-//if select 1 need to check not 0
-//if select 2 need to check not 0 and not 7
-//need to take a count
