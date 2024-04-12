06ecc57d5f81d05a1e04dac0796d45a21400a8b1
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-12 15:18:26 -0400

    Time: 52 ms (5.42%), Space: 60.5 MB (5.59%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,18 +1,20 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        boolean[] seen = new boolean[n+1];
-        for(int i=0; i<n; i++) {
-            if(nums[i] > 0 && nums[i] <= n){
-                seen[nums[i]] = true;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++) {
+            if( nums[i] <0 || nums[i] > nums.length) {
+                //return false;
+                continue;
             }
+            set.add(nums[i]);
         }
+        System.out.print(set);
         
-        for(int i = 1; i<=n; i++) {
-            if(!seen[i]) {
-               return i; 
+        for(int i=1; i<=nums.length; i++) {
+            if(!set.contains(i)) {
+                return i;
             }
         }
-        return n + 1;
+        return nums.length + 1;
     }
 }
