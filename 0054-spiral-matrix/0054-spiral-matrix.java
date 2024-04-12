512b03c0f9a5a158314e8b0eba2690d6d1ea8bfd
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-12 01:18:40 -0400

    Time: 0 ms (100.00%), Space: 41.4 MB (53.97%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -4,35 +4,29 @@ class Solution {
          if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return result;
         }
-        
-        int left = 0; int right = matrix[0].length-1; int top = 0; int bottom = matrix.length-1;
+        int left = 0; int right = matrix[0].length - 1; int top = 0; int bottom = matrix.length - 1;
         while(left <= right && top <= bottom) {
-            for(int i = left; i<=right; i++) {
+            for(int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
             }
             top++;
-            for(int i = top; i<=bottom; i++) {
-                 result.add(matrix[i][right]);
+            for(int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]); 
             }
             right--;
-            
-            
             if(top <= bottom) {
-               for(int i = right; i>=left; i--) {
-                result.add(matrix[bottom][i]);
-               } 
-              bottom--;
+                for(int i = right; i>=left; i--) {
+                   result.add(matrix[bottom][i]);  
+                }
+                bottom--;
             }
-            
             if(left <= right) {
                for(int i = bottom; i>=top; i--) {
-                 result.add(matrix[i][left]);
+                   result.add(matrix[i][left]);  
+                }
+                left++; 
             }
-            left++; 
-            }
-            
         }
         return result;
-        
     }
 }
