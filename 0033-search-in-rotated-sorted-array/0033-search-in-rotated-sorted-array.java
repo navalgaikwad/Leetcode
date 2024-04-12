afd5d3677c1a11c9f7f7264f1597afa8e55a45a6
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-12 12:22:34 -0400

    Time: 0 ms (100.00%), Space: 42.3 MB (10.53%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -10,23 +10,23 @@ class Solution {
         if (nums[mid] == target) {
             return mid;
         }
-
-        if (nums[start] <= nums[mid]) {
-            if (target >= nums[start] && target < nums[mid]) {
-                end = mid - 1;
-            } else {
+        if(nums[start] <= nums[mid]) {
+            if(target >= nums[start] && target < nums[mid]) {
+                end = mid -1;
+            }else {
                 start = mid + 1;
             }
-        } else {
-             if(nums[end] >= nums[mid]) {
-                if (target <= nums[end] && target > nums[mid]) {
-                    start = mid + 1;
-
-                } else {
-                    end = mid - 1;
-              }
+        }else {
+            if(nums[end] >= nums[mid]) {
+            if(target <= nums[end] && target > nums[mid]) {
+                start = mid + 1;
+                
+            }else {
+                end = mid -1;
             }
         }
+      }
+        
     }
 
     return -1;
