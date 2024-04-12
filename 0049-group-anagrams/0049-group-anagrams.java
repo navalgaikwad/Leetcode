2574f892d57f7fbef70f7722ad05190af04dc01c
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-12 00:02:51 -0400

    Time: 8 ms (38.92%), Space: 47.9 MB (39.42%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -14,3 +14,8 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+
+/*
+The time complexity of this algorithm is O(n * k), where n is the number of strings in strs and k is the average length of a string. This is because, for each string, we iterate through its characters to create the character array, which has a constant size (26). The space complexity is also O(n * k) due to the use of the map to store the grouped anagrams.
+
+*/
