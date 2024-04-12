c56e65689dfa4dbc20ed966ab1e0c23f517e1f17
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-12 12:37:00 -0400

    Time: 1 ms (98.59%), Space: 41.7 MB (12.52%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() == 1) return false;
-        Map<Character, Character> map =new HashMap<>();
-        map.put('(', ')');
-        map.put('{', '}');
-        map.put('[', ']');
-        
-        Stack<Character> st = new Stack<>();
-        for(char c : s.toCharArray()) {
-            if(!st.isEmpty () && (c == ')' || c =='}' || c == ']')) {
-                char ch = map.getOrDefault(st.peek(), '0');
-                if(ch != c || ch == '0'){
-                    return false;
-                }
-                st.pop();
-            }else {
-                st.push(c);
-            }
-        }
-        if(!st.isEmpty()) {
-            return false;
-        }
-        return true;
+        Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
     }
 }
