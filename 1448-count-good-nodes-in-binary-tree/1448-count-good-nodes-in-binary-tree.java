8612ab64165b14daa031af66277929923787f092
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-14 14:20:24 -0400

    Time: 2 ms (82.24%), Space: 54.1 MB (9.07%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,23 +14,21 @@
  * }
  */
 class Solution {
-    int prev= Integer.MIN_VALUE;
-    int count=0;
+    //int min = Integer.MIN_VALUE;
+    int count =0;
     public int goodNodes(TreeNode root) {
-        helper(root, prev);
+        helper(root, Integer.MIN_VALUE);
         return count;
-        
     }
-    void helper(TreeNode root, int prev) {
-        if(root == null) {
-            return;
-        }
-        
-        if(root.val >= prev) {
-            count++;
-            prev = root.val;
-        }
-        helper(root.left, prev);
-        helper(root.right, prev);
-    }
+     void helper(TreeNode root, int prev) {
+         if(root == null) {
+             return;
+         }
+         if(root.val >= prev) {
+             count++;
+             prev = root.val;
+         }
+         helper(root.left, prev);
+         helper(root.right, prev);
+     }
 }
