50855a4065a53187edc6be8c36bffb1fbfd1d38b
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-14 18:29:24 -0400

    Time: 3 ms (73.95%), Space: 47.2 MB (30.83%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,38 +1,40 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
+        int row = 0, col = 0;
+        int m = mat.length;
+        int n = mat[0].length;
+        int index =0;
+        int[] result = new int[m*n];
         boolean up = true;
-        int row =0, col =0;
-        int i=0;
-        int[] arr = new int[m*n];
-        while(row < m && col <n) {
+        while(row < m && col < n) {
             if(up) {
-                while(row >0 && col<n-1) {
-                    arr[i++] = mat[row][col];
-                    row--;
+               while(row > 0 && col < n - 1) {
+                   result[index++] = mat[row][col];
+                   row--;
+                   col++;
+               }
+                result[index++] = mat[row][col];
+                if(col == n-1) {
+                    row++;
+                }else {
                     col++;
                 }
-                 arr[i++] = mat[row][col];
-                 if(col == n-1) {
-                     row++;
-                 }else {
-                     col++;
-                 }
+                
             }else {
-                while(col >0 && row<m-1) {
-                    arr[i++] = mat[row][col];
+                 while(col > 0 && row < m - 1) {
+                   result[index++] = mat[row][col];
+                   row++;
+                   col--;
+               }
+                result[index++] = mat[row][col];
+                if(row == m-1) {
+                    col++;
+                }else {
                     row++;
-                    col--;
                 }
-                 arr[i++] = mat[row][col];
-                 if(row == m-1) {
-                     col++;
-                 }else {
-                     row++;
-                 }
             }
-            up=!up;
+            up = !up;
         }
-        return arr;
+        return result;
     }
 }
