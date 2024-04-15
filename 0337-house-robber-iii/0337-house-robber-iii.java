00d697419b16e4885fb0982e6aa3bb54582235ce
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-15 15:42:11 -0400

    Time: 0 ms (100.00%), Space: 44.1 MB (85.94%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -14,25 +14,22 @@
  * }
  */
 class Solution {
+    
     public int rob(TreeNode root) {
-        return Math.max(robHouses(root), root.val);
-    }
+    int[] res = robSub(root);
+    return Math.max(res[0], res[1]);
+}
 
-    // return non adjacent values
-    // update each root with values that represents the max value till that root possible if the root house is also robbed
-    private int robHouses(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }
+private int[] robSub(TreeNode root) {
+    if (root == null) return new int[2];
+    
+    int[] left = robSub(root.left);
+    int[] right = robSub(root.right);
+    int[] res = new int[2];
 
-        int left = robHouses(root.left);
-        int right = robHouses(root.right);
-
-        int nonAdjacentValues = (root.left != null) ? root.left.val : 0;
-        nonAdjacentValues += (root.right != null) ? root.right.val : 0;
-
-        root.val = Math.max(root.val + left + right, nonAdjacentValues);
-
-        return nonAdjacentValues;
-    }
+    res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+    res[1] = root.val + left[0] + right[0];
+    
+    return res;
+}
 }
