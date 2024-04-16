aca49fb320045d5a7928252a500fac710e49f179
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-16 16:01:43 -0400

    Time: 38 ms (75.24%), Space: 64.1 MB (97.69%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,27 +1,23 @@
 class Solution {
-    public int minReorder(int n, int[][] con) {
+    public int minReorder(int n, int[][] connections) {
         ArrayList<int[]> adj[] = new ArrayList[n];
         for(int i=0; i<n; i++) {
             adj[i] = new ArrayList<>();
         }
-        for(int[] c : con) {
-            adj[c[0]].add(new int[]{c[1], 1});
-            adj[c[1]].add(new int[]{c[0], 0});
+        for(int[] connection : connections) {
+            adj[connection[0]].add(new int[]{connection[1], 1});
+            adj[connection[1]].add(new int[]{connection[0], 0});
         }
-        return dfs(adj, 0, new HashSet<>());
+        boolean[] visiting = new boolean[n];
+        return helper(adj, 0, visiting);
     }
-    
-    int dfs(ArrayList<int[]> adj[], int parent, HashSet<Integer> visited) {
+    int helper(ArrayList<int[]> adj[], int parent, boolean[] visiting) {
+        visiting[parent] = true;
         int total = 0;
-        visited.add(parent);
-        for(int[] neighbours: adj[parent]) {
-            int neighbour = neighbours[0];
-            if(!visited.contains(neighbour)) {
-                int cost = neighbours[1];
-                total+= cost + dfs(adj, neighbour, visited);
-           
+        for(int[] neighbour: adj[parent]) {
+            if(!visiting[neighbour[0]]) {
+                total+=neighbour[1] + helper(adj, neighbour[0], visiting);
             }
-            
         }
         return total;
     }
