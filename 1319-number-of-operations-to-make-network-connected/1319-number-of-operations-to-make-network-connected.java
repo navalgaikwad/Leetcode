942203f32224debf605becdc4b8b8b5e60bee075
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-16 16:26:17 -0400

    Time: 13 ms (50.25%), Space: 73 MB (7.70%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,55 +1,36 @@
 class Solution {
+   
     public int makeConnected(int n, int[][] connections) {
-        if(n - 1 > connections.length) {
+        if (connections.length < n - 1) {
             return -1;
         }
-        ArrayList<Integer>[] adj = new ArrayList[n]; //O(n)
-        for(int i =0; i<n;i++) {        //O(n)
-            adj[i] = new ArrayList<>(); // O(1)
+    ArrayList<Integer>adj[] = new ArrayList[n];
+        for(int i=0;i<n;i++){
+        adj[i]=new ArrayList<>();    
         }
-        for(int[] con : connections) {//O() lenght of conenction
-            adj[con[0]].add(con[1]);
-            adj[con[1]].add(con[0]);
+        for(int[] connection:connections){
+            adj[connection[0]].add(connection[1]);
+            adj[connection[1]].add(connection[0]);
         }
-        boolean[] visited = new boolean[n]; // O(n)
-        int count =0;//O(1)
-        for(int i = 0; i < n; i++) { //O(n)
-            if( !visited[i] ) {
-               dfs(adj, i, visited);
+        int count=0;
+        boolean[] visited=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+               dfs(adj, i, visited);  
                 count++;
             }
-            
+           
         }
-        return count -1;
+        return count-1;
     }
-    //O(n*m) 
     
-    void dfs(ArrayList<Integer>[] adj, int i, boolean[] visited) {
-        if(visited[i]) {
-            return;
-        }
-        visited[i] =true;
-        for(int neighbours: adj[i]) {
-            if(!visited[neighbours]) {
-               dfs(adj, neighbours, visited); 
+    void dfs(ArrayList<Integer> adj[], int src, boolean[] visited){
+        visited[src]=true;
+        for(Integer neighbour: adj[src]){
+            if(!visited[neighbour]){
+                dfs(adj, neighbour, visited );
             }
         }
     }
+    
 }
-//[[0,1],[0,2],[1,2]]
-//visted [0, 1, 2, 3]
-//        t, t, t
-//n - visted.length;
-
-//i will create an adj list they are bidrectional
-//i will iterate on each node
-//i will visit adjeacet node
-// then i will mark it true
-//oncess all node are viisted i will take the length of viisted and i will minus it from n
-
-// 
-// 0 - 1
-// 1 - 0, 2
-// 4 = 4 - 1 = 3 - 2 = 1
-// 6 -1 = 5- 3 =2 
-//6 -1 = 5 - 
