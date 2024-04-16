d4aa703f24241048e270b72093e227d4ce5bdae9
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-15 22:16:51 -0400

    Time: 44 ms (90.01%), Space: 59.6 MB (84.00%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,34 +1,25 @@
 class Solution {
-    int maxTime =0;
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
         ArrayList<Integer> adj[] = new ArrayList[n];
         for(int i=0; i<n; i++) {
             adj[i] = new ArrayList<>();
         }
         for(int i=0; i<n; i++) {
-            if(manager[i] != -1) {
-               adj[manager[i]].add(i); 
+            if(manager[i]!=-1) {
+                adj[manager[i]].add(i);
             }
         }
-        
-        return dfs(adj, headID, informTime);
+        boolean[] visited = new boolean[n];
+        return helper(adj, headID, informTime, visited);
     }
-    
-//     void dfs(ArrayList<Integer> adj[], int parent, int[] informTime, int time) {
-        
-//         int total = Integer.MIN_VALUE;
-//         for(int neighbour :adj[parent]) {
-//             dfs(adj, neighbour, informTime, time + informTime[parent]);
-//         }
-//         maxTime = Math.max(maxTime, time);
-//     }
-    
-    int dfs(ArrayList<Integer> adj[], int parent, int[] informTime) {
-        
+    int helper(ArrayList<Integer> adj[], int parent, int[] informTime, boolean[] visited) {
+        visited[parent] = true;
         int total = Integer.MIN_VALUE;
-        for(int neighbour :adj[parent]) {
-            int totalMax = dfs(adj, neighbour, informTime);
-            total = Math.max(total, totalMax + informTime[parent]);
+        for(Integer neighbour : adj[parent]) {
+            if(!visited[neighbour]) {
+                int totalChildTime = helper(adj, neighbour, informTime, visited);
+                total = Math.max(total, totalChildTime + informTime[parent]);
+            }
         }
         return Math.max(total, informTime[parent]);
     }
