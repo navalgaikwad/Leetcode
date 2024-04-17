8f61550cb8cae9b697edd6f930d599ba5a9d0abe
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-17 15:27:11 -0400

    Time: 4 ms (98.09%), Space: 44.5 MB (71.18%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
@@ -1,45 +1,39 @@
 class Solution {
     public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
-        ArrayList<int[]> adj[] = new ArrayList[n];
-        int[] result=new int[n];
-        for(int i=0; i<n; i++) {
-            adj[i] = new ArrayList<>();
-            result[i] = -1;
+        ArrayList[] adj = new ArrayList[n];
+        for(int i=0; i<n; i++){
+       adj[i]=new ArrayList<>();     
+       }
+        for(int[] redEdge: redEdges){
+            adj[redEdge[0]].add(new int[]{redEdge[1], 0});
         }
-        for(int[] red : redEdges) {
-            adj[red[0]].add(new int[]{red[1], 1});
-        }
-        for(int[] blue : blueEdges) {
-            adj[blue[0]].add(new int[]{blue[1], 0});
+        
+         for(int[] blueEdge: blueEdges){
+            adj[blueEdge[0]].add(new int[]{blueEdge[1], 1});
         }
         
         boolean[][] visited = new boolean[n][2];
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, -1});
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, -1});//current, dist, color
          result[0]=0;
-        while(!q.isEmpty()) {
-            int[] current = q.remove();
-            int dist = current[0];//dist
-            int node = current[1];//node
-            int col = current[2];//color
-            for(int[] neighbours: adj[node]) {
-                int next = neighbours[0];
-                int colr = neighbours[1];
-                if(!visited[next][colr] && col!=colr) {
-                    if(result[next] == -1) {
-                       result[next] = dist + 1; 
+        while(!queue.isEmpty()){
+            int[] currents = queue.remove();
+            ArrayList<int[]>current= adj[currents[0]]; //get value from adj list for current node
+            for(int[] neighbour: current){
+                if(!visited[neighbour[0]][neighbour[1]] && neighbour[1]!= currents[2]){
+                    if(result[neighbour[0]] ==-1){
+                        result[neighbour[0]] = 1+  currents[1];
+                        
                     }
-                    visited[next][colr] = true; //mark it is visited
-                   q.add(new int[]{1+ dist, next, colr });
-                    
+                   visited[neighbour[0]][neighbour[1]]=true;
+                   queue.add(new int[]{neighbour[0], currents[1] + 1, neighbour[1]});
                 }
             }
         }
         return result;
     }
 }
-//dist
-//node
-//color
-//red =0;
-//blue 1
+
+//neighbour[1] = color
