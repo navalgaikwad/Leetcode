81254ac6ad5869170f26e61b52ada8357e9e6b51
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-17 09:50:52 -0400

    Time: 1 ms (98.26%), Space: 42 MB (69.34%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,15 +1,11 @@
 class Solution {
-    class Pair {
-        String c;
-        double val;
-        Pair(String c, double val) {
-            this.c = c;
-            this.val = val;
-        }
-    //     @Override
-    // public String toString() {
-    //     return "(" + c + ", " + val + ")";
-    // }
+        class Pair {
+            String c;
+            double val;
+            Pair(String c, double val) {
+                this.c = c;
+                this.val = val;
+            }
     }
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         Map<String, ArrayList<Pair>> adj = new HashMap<>();
@@ -21,42 +17,37 @@ class Solution {
             adj.get(equation.get(1)).add(new Pair(equation.get(0), 1/values[j]));
             j++;
         }
-        //System.out.print(adj);
-         double[] result = new double[queries.size()];
+       double[] result = new double[queries.size()];
         for(int i=0; i<queries.size(); i++){
           String node1 =  queries.get(i).get(0);
           String node2 =  queries.get(i).get(1);
-            if(!adj.containsKey(node1) || !adj.containsKey(node2)){
+            if(!adj.containsKey(node1) || !adj.containsKey(node2)) {
                 result[i] =-1.0;
-            }else{
+            } else {
                 double value = dfs(adj, node1, node2, new HashSet<>());
                 result[i] =value;
             }
             
         }
         return result;
-       // return new double[]{};
+       
     }
     
-    double dfs(Map<String, ArrayList<Pair>> adj, String src, String dst, HashSet<String> visited) {
+    double dfs(Map<String, ArrayList<Pair>> adj, String src, String dest, HashSet<String> visited) {
         visited.add(src);
-        if(src.equals(dst)) {
+        if(src.equals(dest)) {
             return 1.0;
         }
         double total = -1;
-        for(Pair pair: adj.get(src)) {
-            String neighbour = pair.c;
-            double value = pair.val;
-            if(!visited.contains(neighbour)) {
-                double sum = dfs(adj, neighbour, dst, visited);
-                if(sum != -1) {
-                   sum = sum*value; 
-                   total = sum;
-                   //return sum;
+        for(Pair  neighbour:adj.get(src)) {
+            if(!visited.contains(neighbour.c)) {
+                double value = dfs(adj, neighbour.c, dest, visited);
+                if(value != -1) {
+                    total = neighbour.val * value;
                 }
-                
             }
         }
         return total;
     }
+    
 }
