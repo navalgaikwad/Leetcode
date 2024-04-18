b7fb25a48046f9e09aeb015fd5f43aca055456e4
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-18 19:53:56 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (17.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,32 +3,30 @@ class Solution {
         return helper(nums, target, 0, nums.length - 1);
     }
     
-   int helper(int[] nums, int target, int start, int end) {
-    while (start <= end) {
-        int mid = start + (end - start) / 2;
-
-        if (nums[mid] == target) {
-            return mid;
-        }
-        if(nums[start] <= nums[mid]) {
-            if(target >= nums[start] && target < nums[mid]) {
-                end = mid -1;
-            }else {
-                start = mid + 1;
+    int helper(int[] nums, int target, int start, int end) {
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target) {
+                return mid;
             }
-        }else {
-            if(nums[end] >= nums[mid]) {
-            if(target <= nums[end] && target > nums[mid]) {
-                start = mid + 1;
-                
+            if(nums[start] <= nums[mid]) {
+                if(nums[start] <= target && nums[mid] > target) {
+                    end = mid -1;
+                    
+                }else {
+                    start = mid + 1;
+                }
             }else {
-                end = mid -1;
+                if(nums[end] >= nums[mid]) {
+                    if(nums[end] >= target && nums[mid] < target) {
+                        start = mid + 1;
+                    }else {
+                        end = mid -1;
+                    }
+                } 
             }
         }
-      }
-        
+        return -1;
     }
-
-    return -1;
-  }
 }
+
