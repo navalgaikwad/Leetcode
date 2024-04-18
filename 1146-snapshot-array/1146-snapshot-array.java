b1094ae02c79e07469e283eb25a49bc977f0e788
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-18 19:02:21 -0400

    Time: 62 ms (65.40%), Space: 78.8 MB (50.68%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.java
+++ 1146-snapshot-array/1146-snapshot-array.java
@@ -1,16 +1,18 @@
 class SnapshotArray {
     TreeMap<Integer, Integer> map[];
-    int snap_id;
+    int snap_id = 0;
     public SnapshotArray(int length) {
         map = new TreeMap[length];
-        for(int i =0; i< length; i++){
+        for(int i=0; i<length; i++) {
             map[i] = new TreeMap<>();
             map[i].put(0, 0);
         }
     }
     
     public void set(int index, int val) {
+       
         map[index].put(snap_id, val);
+       
     }
     
     public int snap() {
@@ -18,16 +20,10 @@ class SnapshotArray {
     }
     
     public int get(int index, int snap_id) {
-       // return map[index].get(snap_id);
-        return map[index].floorEntry(snap_id).getValue();
+         return map[index].floorEntry(snap_id).getValue();
     }
 }
-/*
-The floorKey() method is used to return the greatest key less than or equal to given key.
-1. Maintain global snap_id
-2. I have Array of TreeMap  update with default value
-3. 
-*/
+
 /**
  * Your SnapshotArray object will be instantiated and called as such:
  * SnapshotArray obj = new SnapshotArray(length);
