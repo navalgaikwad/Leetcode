95a0628333e17b06edc81e3223ce454b92f419b8
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-19 07:46:46 -0400

    Time: 9 ms (78.92%), Space: 80.9 MB (46.54%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.java
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -1,37 +1,34 @@
 class Solution {
     public int[] platesBetweenCandles(String s, int[][] queries) {
         int len = s.length();
+        int[] plates = new int[len];
         int[] leftSideCandles = new int[len];
         int[] rightSideCandles = new int[len];
-        int[] plates = new int[len];
-        
-        //1. count stars
-        //2.count candles from left and right side
-        //plates[i]
-        int count =0;
-        for (int i = 0; i < len; i++) {
-            if (s.charAt(i) == '*') {
-                count++;
+        int count = 0;
+        for(int i=0; i<len; i++) {
+            if(s.charAt(i) == '*') {
+                 count++;
             }
             plates[i] = count;
         }
         
-         count=-1;
+        count = -1;
         for(int i=0; i<len; i++) {
-            if(s.charAt(i)=='|') {//candel asel tar count kar
-                count = i;
+            if(s.charAt(i) == '|') {
+                 count = i;
             }
             rightSideCandles[i] = count;
         }
         
-        count =-1;//vv imp -1
-        for(int i=len-1; i>=0; i--) {//candel asel tar count kar
-            if(s.charAt(i)=='|') {
-                count = i;
+        count = -1;
+        for(int i=len - 1; i>=0; i--) {
+            if(s.charAt(i) == '|') {
+                 count = i;
             }
             leftSideCandles[i] = count;
         }
-         int[] result = new int[queries.length];
+        
+        int[] result = new int[queries.length];
         int k =0;
         for(int[] query : queries) {
             int left = leftSideCandles[query[0]];
