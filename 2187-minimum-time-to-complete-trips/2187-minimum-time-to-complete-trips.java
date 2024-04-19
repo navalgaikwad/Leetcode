12a28e439bf6eb47736035f72d175ada5a4a5a36
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-19 14:07:41 -0400

    Time: 43 ms (96.42%), Space: 57.6 MB (71.97%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -2,9 +2,10 @@ class Solution {
     public long minimumTime(int[] time, int totalTrips) {
         long left = 1;
         long right = 100000000000000L;
+        
         while(left < right) {
-            long mid = left + (right-left)/2;
-            if(compelteTheTrip(time, mid, totalTrips) >= totalTrips) {
+            long mid = left + (right - left)/2;
+            if(helper(time, totalTrips, mid) >= totalTrips) {
                 right = mid;
             }else {
                 left = mid + 1;
@@ -13,15 +14,26 @@ class Solution {
         return left;
     }
     
-    long compelteTheTrip(int[] times, long currentTime, int totalTrips) {
+    long helper(int[] times, int totalTrips, long mid) {
         long count = 0;
         for(int time : times) {
-            long t = (long)time;
-            count += currentTime / t;
-            if(count >= totalTrips ){//count is greater than totalTrips break it
+            //long t = ;
+            count+= mid/(long)time;
+            if(count >= totalTrips) {
                 break;
             }
         }
         return count;
     }
+    // long compelteTheTrip(int[] times, long currentTime, int totalTrips) {
+    //     long count = 0;
+    //     for(int time : times) {
+    //         long t = (long)time;
+    //         count += currentTime / t;
+    //         if(count >= totalTrips ){//count is greater than totalTrips break it
+    //             break;
+    //         }
+    //     }
+    //     return count;
+    // }
 }
