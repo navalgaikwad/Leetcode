7d422837bf5cdc9fecb18c5ee429bdda5997cf03
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-19 17:51:13 -0400

    Time: 3 ms (98.25%), Space: 45.3 MB (67.99%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        List<int[]> list = new ArrayList<>();
-        int m = firstList.length, n = secondList.length, p = 0, q = 0;
-        while (p < m && q < n) {
-            if (firstList[p][0] > secondList[q][1]) {
-                q++;
-            } else if (firstList[p][1] < secondList[q][0]) {
-                p++;
-            } else {
-                list.add(new int[] {Math.max(firstList[p][0], secondList[q][0]), Math.min(firstList[p][1], secondList[q][1])});
-                if (firstList[p][1] < secondList[q][1]) {
-                    p++;
-                } else {
-                    q++;
-                }
+        ArrayList<int[]> list = new ArrayList<>();
+        int len1 = firstList.length;
+        int len2 = secondList.length;
+        int i = 0, j =0;
+        while( i< len1 && j < len2) {
+            int low =Math.max(firstList[i][0], secondList[j][0]);
+            int high =Math.min(firstList[i][1], secondList[j][1]);
+            
+            if(low <= high) {
+                list.add(new int[]{low, high});
+            }
+            if(firstList[i][1] < secondList[j][1]) {
+                i++;
+            }else {
+                j++;
             }
         }
-        int[][] res = new int[list.size()][2];
-        for (int i = 0; i < res.length; i++) {
-            int[] cur = list.get(i);
-            res[i][0] = cur[0];
-            res[i][1] = cur[1];
-        }
-
-        return res;
+        return list.toArray(new int[list.size()][]);
     }
 }
