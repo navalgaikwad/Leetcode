a8cfbe4f0e12ed5b88529164f463a958a871c49d
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-20 07:09:55 -0400

    Time: 6 ms (90.67%), Space: 45.3 MB (51.99%) - LeetHub

---
+++ 0817-linked-list-components/0817-linked-list-components.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(Integer num : nums) {
+            set.add(num);
+        }
+        //ListNode curr = head;
+        int count = 0;
+        while(head != null) {
+            ListNode curr = head;
+            boolean found = false;
+            
+            while(curr!=null && set.contains(curr.val)) {
+                found = true;
+                curr = curr.next;
+            }
+            if(found) {
+                count++;
+            }
+            head = curr == null ? curr : curr.next;
+        }
+        
+        return count;
+    }
+}
