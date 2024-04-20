096653b806286920278fbd061148e758052fa987
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-19 20:51:16 -0400

    Time: 1 ms (99.42%), Space: 57.1 MB (26.95%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,31 +1,20 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        return solve(nums, 0, nums.length - 1);
-    }
-    
-    int solve(int[] nums, int lo, int hi) {
-        if(lo >= hi) {
-            return nums[lo];
+         if(nums.length == 1) {
+            return nums[0];
         }
-        int mid = lo + (hi - lo) / 2;
-        int L = solve(nums, lo, mid - 1);
-        int R = solve(nums, mid + 1, hi);// normal binary search
-        
-        int C = cross(nums, lo, mid, hi);
-        return Math.max(L, Math.max(R, C));
-    }
-    
-    int cross(int[] nums, int lo, int mid, int hi) {
-        int left = 0, maxLeft = 0;
-        int right = 0, maxRight = 0;
-        for(int i = mid - 1; i >=lo; i--) {
-            left += nums[i];
-            maxLeft = Math.max(left, maxLeft);
+        int sum = 0;
+        int max = Integer.MIN_VALUE;;
+        for(int i=0; i<nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+            if(sum < 0) {
+                sum = 0;
+            }
         }
-        for(int i = mid + 1; i <= hi; i++) {
-            right += nums[i];
-            maxRight = Math.max(right, maxRight);
-        }
-        return maxLeft + maxRight + nums[mid];
+        return max;
     }
 }
+/*
+
+*/
