ae78fc6ede7b5cf6fa2d2ef16512df82bd86bf86
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-22 07:33:30 -0400

    Time: 1 ms (54.98%), Space: 41.7 MB (51.78%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,10 +1,10 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         Map<Character, Integer> map = new HashMap<>();
-        for(char c :stones.toCharArray()) {
+        for(char c : stones.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int count =0;
+        int count = 0;
         for(char c : jewels.toCharArray()) {
             if(map.containsKey(c)) {
                 count+=map.get(c);
