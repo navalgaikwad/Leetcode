07708c70f15a9e14e473a120b5ab6da96aa3feaa
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-22 09:54:29 -0400

    Time: 3 ms (79.43%), Space: 43 MB (82.83%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
@@ -1,35 +1,24 @@
 class Solution {
     public String sortString(String s) {
         int[] arr = new int[26];
-
-        for(int i=0; i<s.length(); i++){
-            int index = s.charAt(i)-'a';
-            arr[index] = arr[index]+1;
+        for(int i=0; i<s.length(); i++) {
+            arr[s.charAt(i)-'a']++;
         }
-
-        // System.out.print(Arrays.toString(arr));
-
         StringBuilder sb = new StringBuilder();
-        while(sb.length() < s.length()){
-        
-        for(int i=0; i<26; i++){
-            if (arr[i]>0){
-                char ch = (char)('a'+i);
-                sb.append(ch);
-                arr[i]--;
+        while(sb.length() < s.length()) {
+            for(int i = 0; i<26; i++) {
+                if(arr[i] > 0) {
+                    sb.append((char)('a'+i));
+                    arr[i]--;
+                }
+            }
+            for(int i = 25; i>=0; i--) {
+                if(arr[i] > 0) {
+                    sb.append((char)('a'+i));
+                     arr[i]--;
+                }
             }
         }
-
-        for(int i=25; i>=0; i--){
-            if (arr[i]>0){
-                char ch = (char)('a'+i);
-                sb.append(ch);
-                arr[i]--;
-            }
-        }
-
-    }
-
         return sb.toString();
     }
 }
