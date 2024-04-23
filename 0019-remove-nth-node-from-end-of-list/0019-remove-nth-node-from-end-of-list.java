a05dd57e108d9a62a23ca993891012690733e0c3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-23 17:45:31 -0400

    Time: 0 ms (100.00%), Space: 41.3 MB (90.54%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -3,25 +3,31 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode first = dummy;
-        ListNode second = dummy;
-        // Advances first pointer so that the gap between first and second is n nodes apart
-        for (int i = 1; i <= n + 1; i++) {
-            first = first.next;
-        }
-        // Move first to the end, maintaining the gap
-        while (first != null) {
-            first = first.next;
-            second = second.next;
-        }
-        second.next = second.next.next;
-        return dummy.next;
+     public ListNode removeNthFromEnd(ListNode head, int n) {
+         //[1,2,3,4,5] n=5
+         //         c
+        ListNode current = head;//5
+         for(int i=0; i < n; i++) {
+             current = current.next;
+         }
+         ListNode slow = head;//1
+         ListNode fast = current;//5
+         ListNode previous = null;//1
+         while(fast != null) {
+             previous = slow;
+             slow = slow.next;
+             fast = fast.next;
+         }
+         if(previous == null) {
+             return head.next;
+         }
+         previous.next = slow.next;
+         return head;
     }
 }
