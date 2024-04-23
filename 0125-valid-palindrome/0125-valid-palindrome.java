a155c939208eb3fba1f3135a025b3434636f3849
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-23 09:56:44 -0400

    Time: 3 ms (74.76%), Space: 42.5 MB (95.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(null == s) {
-			System.out.println("Input string is false");
-			return false;
-		}
-		
-		if(1 > s.length() || s.length() > 200000) {
-			System.out.println("Input string is very small OR big" + s);
-			return false;
-		}
-		
-		s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-		if(s.isEmpty()) {
-			return true;
-		}
-		
-		char[] chArray = s.toCharArray();
-		for(int i=0, j=chArray.length-1; i<=j; i++, j--) {
-			if(chArray[i] != chArray[j]){
-				return false;
-			}
-		}
-		return  true;
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int start =0;
+        int end = s.length()-1;
+        
+        while(start <= end) {
+            char startChar = s.charAt(start);
+            char endChar = s.charAt(end);
+            if(!Character.isLetterOrDigit(startChar)) {
+                start++;
+            }
+            else if(!Character.isLetterOrDigit(endChar)) {
+                end--;
+            }else {
+                if(Character.toLowerCase(startChar) != Character.toLowerCase(endChar)) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
     }
 }
