920ca7a8d516b12f888640f613a837998b9930a6
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-24 13:26:05 -0400

    Time: 1 ms (92.61%), Space: 45.2 MB (71.71%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,8 +10,11 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if (head == null) return null;
-        head.next = removeElements(head.next, val);
-        return head.val == val ? head.next : head;
-}
+        if(head == null) return null;
+         head.next = removeElements(head.next, val);
+        if(head!=null && head.val == val) {
+            return head.next;
+        }
+        return head;
+    }
 }
