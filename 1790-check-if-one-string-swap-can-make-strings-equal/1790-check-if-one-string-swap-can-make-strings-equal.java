5f163b29ca341e7aeb45b02e89cc780e1e8c8401
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-25 11:21:49 -0400

    Time: 0 ms (100.00%), Space: 41.1 MB (93.59%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -5,12 +5,26 @@ class Solution {
         if(s1.equals(s2))
             return true;
         else{//one swap one char from 1 and secod character from another so 2
-             List<Integer> list = new ArrayList<>();
-            for (int i = 0;i<s1.length();i++)
-                if (s1.charAt(i) != s2.charAt(i)) list.add(i);
-            return list.size() == 2 && s1.charAt(list.get(0)) == s2.charAt(list.get(1))
-                && s2.charAt(list.get(0)) == s1.charAt(list.get(1));
+             //List<Integer> list = new ArrayList<>();
+            int diff1 = -1;
+            int diff2 = -1;
+            for (int i = 0;i<s1.length();i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    if(diff1 == -1) {
+                        diff1 = i;
+                    }else if(diff2 == -1) {
+                        diff2 = i;
+                    }else {
+                        return false;
+                    }
+                }
+            }
+            
+            return diff1 !=-1 && diff2!=-1 && s1.charAt(diff1) == s2.charAt((diff2))
+                && s2.charAt((diff1)) == s1.charAt((diff2));
 
         }
     }
 }
+
+//string buddy
