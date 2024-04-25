6beeafacc9d6a2e941fe9974587ea90c54384aa5
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-25 10:28:58 -0400

    Time: 1 ms (73.07%), Space: 41.7 MB (8.65%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,71 +1,25 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        
         String[] arr = s.split(" ");
-        
-        if(pattern.length() != arr.length) return false;
-        
+        if(pattern.length() != arr.length) {
+            return false;
+        }
         Map<Character, String> map = new HashMap<>();
-        Set<String> visited = new HashSet<>();
-        int len = pattern.length();
-        
-        for(int i = 0; i< len; i++) {
-            char ch = pattern.charAt(i); //a
-            String value = arr[i];//dog
-            
-            if(!map.containsKey(ch)) {
-                if(visited.contains(value)) {
+        for(int i=0; i<arr.length; i++) {
+            char character = pattern.charAt(i);
+            String word = arr[i];
+            if(map.containsKey(character)) {
+                String value = map.get(character);
+                if(!value.equals(word)) {
                     return false;
                 }
-                visited.add(value);//dog, cat
-                map.put(ch, value);//a - dog, b- cat
-            } else {
-                String existingValue = map.get(ch);// dog
-                if(!existingValue.equals(value)) {
+            }else {
+                if(map.containsValue(word)) {
                     return false;
                 }
+                map.put(character, word);
             }
         }
         return true;
     }
 }
-
-
-//bijection
-//"abba", s = "dog cat cat dog"
-
-//pattern = "abba", s = "dog cat cat fish"
-/**
-pattern = "aaaa", s = "dog cat cat dog"
-
-//both lower case
-
-1. both should be same
-2. take a map particular elemnt is prent or not
-3. if it is not presnet i w
-4. if it match
-
-Set
-
-a - dog
-b - dog
-
- 
-"aaaa", s = "dog cat cat dog"
-
-
-
-// a - dog, cat
-// b
-
-//add both in seta
-// setb
-//a - dog
-// a - dog
-// b - cat
-//
-a - dog
-b
-
-
-**/
