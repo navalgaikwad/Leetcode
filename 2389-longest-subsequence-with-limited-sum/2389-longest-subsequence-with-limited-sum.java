2017becfa79c0ec2e4586cd291b15c5558f6c654
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-25 18:48:52 -0400

    Time: 5 ms (98.69%), Space: 45.3 MB (12.59%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -3,30 +3,30 @@ class Solution {
         Arrays.sort(nums);
         int[] prefix = new int[nums.length];
         prefix[0] = nums[0];
-        for(int i =1; i<nums.length; i++){
-            prefix[i] = nums[i] + prefix[i - 1];
+        for(int i = 1; i<nums.length; i++) {
+           prefix[i] = prefix[i-1] + nums[i]; 
         }
-        
         int[] result = new int[queries.length];
         int i =0;
         for(int querie:queries) {
-            result[i++] =findCount(prefix, querie);
+            result[i++] = helper(prefix, querie);
         }
         return result;
     }
-    
-    int findCount(int[] prefix, int q) {
-        int count =0;
-        for(int i =0; i<prefix.length; i++){
-            if(prefix[i]>q) {
-                break;
+    int helper(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+        while(left <=right) {
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target) {
+                return mid + 1;
             }
-            count++;
+            if(nums[mid] > target) {
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
         }
-        return count;
+        return left;
     }
 }
-// nums = [4,5,2,1],
-//1,2,4,5 1.sort
-//3 . //do prefix sum
-    // and find the no
