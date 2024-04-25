5acd6d9ec95aff27f4dfbee5a462f4954d4bf731
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-04-25 10:57:39 -0400

    Time: 5 ms (28.17%), Space: 42.4 MB (36.68%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -1,28 +1,30 @@
 class Solution {
     public boolean buddyStrings(String s, String goal) {
-        int diff1 = -1;
-        int diff2 = -1;
+        int stringDiff1 = -1;
+        int stringDiff2 = -1;
         if(s.length()!=goal.length()) {
             return false;
         }
-        Set<Character> set = new HashSet<>();
-        for(int i=0; i< s.length(); i++) {
-            if(s.charAt(i)!=goal.charAt(i)) {
-                if(diff1 == -1) {
-                    diff1 = i;
-                }else if(diff2 == -1) {
-                    diff2 = i;
+         Set<Character> set = new HashSet<>();
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) != goal.charAt(i)) {
+                if(stringDiff1 == -1) {
+                    stringDiff1 = i;
+                }else if(stringDiff2 == -1) {
+                    stringDiff2 = i;
                 }else {
                     return false;
                 }
             }
             set.add(s.charAt(i));
         }
-        if(diff1!=-1 && diff2!=-1) {
-            return s.charAt(diff1) == goal.charAt(diff2) && 
-                s.charAt(diff2) == goal.charAt(diff1);
+        if(stringDiff1 != -1 && stringDiff2 != -1) {
+         return s.charAt(stringDiff1) == goal.charAt(stringDiff2) && 
+             s.charAt(stringDiff2) == goal.charAt(stringDiff1);
         }
-        if(diff1!=-1) return false;
+        if(stringDiff1 != -1 || stringDiff2 != -1) {
+            return false;
+        }
         return set.size() < goal.length();
     }
 }
