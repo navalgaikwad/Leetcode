ec1c51c1731ad955d6b1a114913f652d33babae4
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-02 17:01:40 -0400

    Time: 24 ms (77.77%), Space: 46 MB (84.22%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,21 +1,16 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        int sum = 0;
         map.put(0, 1);
-        for(Integer num : nums) {
-            sum+=num;
+        int sum =0;
+        int count =0;
+        for(int num : nums) {
+            sum +=num;
             if(map.containsKey(sum - k)) {
-                count+=map.get(sum -k);
+                count+=map.get(sum - k);
             }
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
 }
-
-/*
-1. two sum sarkha ahe problem ahe
-
-*/
