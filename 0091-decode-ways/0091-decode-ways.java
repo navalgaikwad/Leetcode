a773fde63a199ab01d949c8c6f6c5ec31c99ebe0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-04 18:14:22 -0400

    Time: 2 ms (21.35%), Space: 42.1 MB (16.66%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,47 +1,47 @@
-// class Solution {
-//     public int numDecodings(String s) {
-//         return dfs(s, 0, new HashMap<>());
-//     }
-    
-//     int dfs(String s, int start, HashMap<Integer, Integer> memo) {
-//         if(s.length() == start) {
-//             return 1;
-//         }
-        
-//         if(s.charAt(start) == '0') {
-//             return 0;
-//         }
-//         if(memo.containsKey(start)) {
-//             memo.get(start);
-//         }
-//         int count = dfs(s, start  +1, memo);
-//         if(start + 2 <= s.length() && Integer.parseInt(s.substring(start , start + 2)) <=26) {
-//             count+=dfs(s, start+2, memo);
-//         }
-//         memo.put(start, count);
-//         return count;
-//     }
-// }
 class Solution {
     public int numDecodings(String s) {
-        return helper(s, 0, new HashMap<>());
+        return dfs(s, 0, new HashMap<>());
     }
     
-    int helper(String s, int start, HashMap<Integer, Integer> memo) {
-        if(start == s.length()) {
+    int dfs(String s, int start, HashMap<Integer, Integer> memo) {
+        if(s.length() == start) {
             return 1;
         }
+        
         if(s.charAt(start) == '0') {
             return 0;
         }
         if(memo.containsKey(start)) {
             return memo.get(start);
         }
-        int count = helper(s, start + 1, memo);
-        if(start + 2 <= s.length() && Integer.parseInt(s.substring(start, start + 2)) <= 26) {
-            count+= helper(s, start + 2, memo);
+        int count = dfs(s, start  +1, memo);
+        if(start + 2 <= s.length() && Integer.parseInt(s.substring(start , start + 2)) <=26) {
+            count+=dfs(s, start+2, memo);
         }
         memo.put(start, count);
         return count;
     }
 }
+// class Solution {
+//     public int numDecodings(String s) {
+//         return helper(s, 0, new HashMap<>());
+//     }
+    
+//     int helper(String s, int start, HashMap<Integer, Integer> memo) {
+//         if(start == s.length()) {
+//             return 1;
+//         }
+//         if(s.charAt(start) == '0') {
+//             return 0;
+//         }
+//         if(memo.containsKey(start)) {
+//             return memo.get(start);
+//         }
+//         int count = helper(s, start + 1, memo);
+//         if(start + 2 <= s.length() && Integer.parseInt(s.substring(start, start + 2)) <= 26) {
+//             count+= helper(s, start + 2, memo);
+//         }
+//         memo.put(start, count);
+//         return count;
+//     }
+// }
