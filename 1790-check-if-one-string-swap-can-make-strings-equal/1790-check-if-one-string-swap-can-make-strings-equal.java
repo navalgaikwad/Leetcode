e6103aa07c3baeb2be06c5bb4747377d3020cf45
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-03 21:14:58 -0400

    Time: 0 ms (100.00%), Space: 41.4 MB (53.48%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,30 +1,26 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-          if(s1.length()!=s2.length())
-           return false;
-        if(s1.equals(s2))
-            return true;
-        else{//one swap one char from 1 and secod character from another so 2
-             //List<Integer> list = new ArrayList<>();
-            int diff1 = -1;
-            int diff2 = -1;
-            for (int i = 0;i<s1.length();i++) {
-                if (s1.charAt(i) != s2.charAt(i)) {
-                    if(diff1 == -1) {
-                        diff1 = i;
-                    }else if(diff2 == -1) {
-                        diff2 = i;
-                    }else {
-                        return false;
-                    }
-                }
-            }
-            
-            return diff1 !=-1 && diff2!=-1 && s1.charAt(diff1) == s2.charAt((diff2))
-                && s2.charAt((diff1)) == s1.charAt((diff2));
-
+        if(s1.length() != s2.length()) {
+            return false;
         }
+        if(s1.equals(s2)) {
+            return true;
+        }
+        int diff1 = -1;
+        int diff2 = -1;
+        for(int i=0; i<s1.length(); i++) {
+            if(s1.charAt(i)!=s2.charAt(i)) {
+                if(diff1 == -1) {
+                    diff1 = i;
+                }else if(diff2 == -1){
+                    diff2 = i;
+                }else {
+                    return false;
+                }
+                
+            }
+        }
+        return diff1 != -1 && diff2 != -1 && s1.charAt(diff1) == s2.charAt(diff2) 
+            && s1.charAt(diff2) == s2.charAt(diff1);
     }
 }
-
-//string buddy
