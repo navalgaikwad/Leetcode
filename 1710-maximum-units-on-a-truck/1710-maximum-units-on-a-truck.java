39423d539ee54e72d44575b88af4392c31557828
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-05 16:25:53 -0400

    Time: 10 ms (46.21%), Space: 45 MB (54.68%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,32 +1,13 @@
 class Solution {
-    public int maximumUnits(int[][] boxTypes, int t) {
-        int sum=0;
-       Arrays.sort(boxTypes, (a,b)->(b[1]-a[1]));
-//         for(int[]  box: boxTypes){
-//             int val1 = box[0];
-//             int val2 = box[1];
-             
-//             if(val1<=t){
-                
-//                 if(val1<t){
-//                    sum=sum+ (val1*val2); 
-//                 }
-//                  t=t-val1;
-                
-//             }else{
-                     
-//                 sum=sum+ (t*val2); 
-//                 t=val1-t;
-//                 }
-//         }
-//         return sum;
-//     }
-        int i=0;
-        while(t>0 && i<boxTypes.length){
-            int units = boxTypes[i][1];
-            int boxesToPick = Math.min(t, boxTypes[i][0]);
-            sum = sum + units * boxesToPick;
-            t = t - boxesToPick;
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (a, b)->b[1] - a[1]);
+        int i =0;
+        int sum =0;
+        while(truckSize > 0 && i < boxTypes.length) {
+            int noBoxes = boxTypes[i][0];
+            int boxToPick=Math.min(noBoxes, truckSize);
+            sum = sum + (boxToPick * boxTypes[i][1]);
+            truckSize = truckSize -noBoxes;
             i++;
         }
         return sum;
