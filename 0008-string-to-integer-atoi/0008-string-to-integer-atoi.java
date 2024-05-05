2b8273daf6670e2b5586dc6440fa5a8667b7a755
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-05 17:34:26 -0400

    Time: 1 ms (100.00%), Space: 41.8 MB (97.84%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,37 +1,33 @@
 class Solution {
     public int myAtoi(String str) {
-        int index = 0;
-        int total = 0;
-        int sign = 1;
-        
-        // Check if empty string
-        if(str.length() == 0)
-            return 0;
-        
-        // remove white spaces from the string
-        while(index < str.length() && str.charAt(index) == ' ')
-            index++;
-        
-        if (index == str.length()) return 0;
-        
-        // get the sign
-        if(str.charAt(index) == '+' || str.charAt(index) == '-') {
-            sign = str.charAt(index) == '+' ? 1 : -1;
-            index++;
-        }
-        
-        // convert to the actual number and make sure it's not overflow
-        while(index < str.length()) {
-            int digit = str.charAt(index) - '0';
-            if(digit < 0 || digit > 9) break;
-            
-            // check for overflow
-            if(Integer.MAX_VALUE / 10 < total || Integer.MAX_VALUE / 10 == total && Integer.MAX_VALUE % 10 < digit)
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            
-            total = total*10 + digit;
-            index++; // don't forget to increment the counter
-        }
-        return total*sign;
+  
+    int index = 0, sign = 1, total = 0;
+    //1. Empty string
+    if(str.length() == 0) return 0;
+
+    //2. Remove Spaces
+    while(index < str.length() && str.charAt(index) == ' ' )
+        index ++;
+
+    if (index == str.length()) return 0;
+    //3. Handle signs
+    if(str.charAt(index) == '+' || str.charAt(index) == '-'){
+        sign = str.charAt(index) == '+' ? 1 : -1;
+        index ++;
     }
+    
+    //4. Convert number and avoid overflow
+    while(index < str.length()){
+        int digit = str.charAt(index) - '0';
+        if(digit < 0 || digit > 9) break;
+
+        //check if total will be overflow after 10 times and add digit
+        if(Integer.MAX_VALUE/10 < total || Integer.MAX_VALUE/10 == total && Integer.MAX_VALUE %10 < digit)
+            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+
+        total = 10 * total + digit;
+        index ++;
+    }
+    return total * sign;
+}
 }
