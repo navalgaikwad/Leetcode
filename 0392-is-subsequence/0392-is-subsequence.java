74f62030dfe8d45c7c7af3378333a3a79a70bf61
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-06 13:27:20 -0400

    Time: 1 ms (90.81%), Space: 41.1 MB (78.32%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int len = s.length();
-        int lent = t.length();
-        if(len == 0) {
+        int lenOfS = s.length();
+        int lenOfT = t.length();
+        if(lenOfS == 0) {
             return true;
         }
-        int indexS = 0; int indexT = 0;
-        while(indexT < lent) {
-            if(s.charAt(indexS)== t.charAt(indexT)) {
-                indexS++;
-                if(indexS == len) {
-                    return true;
+        int sIndex =0;
+        for(int i=0; i<t.length(); i++) {
+            if(s.charAt(sIndex) == t.charAt(i)) {
+                sIndex++;
+                if(sIndex == lenOfS) {
+                return true;
                 }
             }
-            indexT++;
+            
         }
         return false;
     }
