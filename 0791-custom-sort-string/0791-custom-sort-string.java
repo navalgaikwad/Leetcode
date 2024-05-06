d44c8b0a196015f18f258fb57f89deb8bf84d017
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-06 18:56:47 -0400

    Time: 6 ms (18.28%), Space: 42.4 MB (7.66%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,25 +1,25 @@
 class Solution {
-    public String customSortString(String order, String s) {
-        String a = "";
-        String b = "";
-        int[] arr = new int[26];
-        for(char c : order.toCharArray()) {
-            arr[c - 'a']++;
+    public String customSortString(String od, String s) {
+        String a ="";
+        String b ="";
+        int[] order = new int[26];
+        for(char c  :od.toCharArray()) {
+            order[c - 'a']++;
+        }
+        for(char c  :s.toCharArray()) {
+            if(order[c - 'a'] == 0) {
+                b+=c;
+            }else {
+                order[c - 'a']++;
+            }
         }
         
-        for(char c : s.toCharArray()) {
-            if(arr[c - 'a'] == 0) {
+        for(char c  :od.toCharArray()) {
+            while(order[c - 'a'] > 1) {
                 a+=c;
-            }else {
-                arr[c - 'a']++;
+                order[c - 'a']--;
             }
         }
-        for(char c : order.toCharArray()) {
-            while(arr[c- 'a']>1) {//here count is 2 because we have traverse it on both order and s
-                b+=c;
-                arr[c- 'a']--;
-            }
-        }
-        return b+a;
+        return a+b;
     }
 }
