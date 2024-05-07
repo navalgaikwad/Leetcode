e27a08f22c01b33031c01872921cfaff9e8f1d08
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-07 15:20:05 -0400

    Time: 1 ms (100.00%), Space: 41.7 MB (98.98%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,33 +1,31 @@
 class Solution {
-    public int myAtoi(String str) {
-  
-    int index = 0, sign = 1, total = 0;
-    //1. Empty string
-    if(str.length() == 0) return 0;
-
-    //2. Remove Spaces
-    while(index < str.length() && str.charAt(index) == ' ' )
-        index ++;
-
-    if (index == str.length()) return 0;
-    //3. Handle signs
-    if(str.charAt(index) == '+' || str.charAt(index) == '-'){
-        sign = str.charAt(index) == '+' ? 1 : -1;
-        index ++;
+    public int myAtoi(String s) {
+        int index = 0;
+        int sign = 1;
+        int len = s.length();
+        int total = 0;
+        if(s.length() == 0) return 0;
+        while(index < len && s.charAt(index) == ' ') {
+            index++;
+        }
+         if (index == s.length()) return 0;
+        if(s.charAt(index) == '-' || s.charAt(index) == '+') {
+           
+            sign = s.charAt(index) == '+'?1:-1;
+            
+            index++;
+        }
+        while(index < len) {
+            int digit = Character.getNumericValue(s.charAt(index));
+            if(digit < 0 || digit > 9) {
+                break;
+            }
+            if(Integer.MAX_VALUE/10 < total || Integer.MAX_VALUE/10 == total && Integer.MAX_VALUE %10 < digit) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            total = 10* total + digit;
+            index++;
+        }
+        return total * sign;
     }
-    
-    //4. Convert number and avoid overflow
-    while(index < str.length()){
-        int digit = str.charAt(index) - '0';
-        if(digit < 0 || digit > 9) break;
-
-        //check if total will be overflow after 10 times and add digit
-        if(Integer.MAX_VALUE/10 < total || Integer.MAX_VALUE/10 == total && Integer.MAX_VALUE %10 < digit)
-            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-
-        total = 10 * total + digit;
-        index ++;
-    }
-    return total * sign;
-}
 }
