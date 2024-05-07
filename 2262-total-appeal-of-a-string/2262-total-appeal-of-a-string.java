38db0065b02389c42e1cce82eb64bac45280899e
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-06 20:03:19 -0400

    Time: 17 ms (35.29%), Space: 45.4 MB (21.98%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
@@ -1,15 +1,17 @@
 class Solution {
     public long appealSum(String s) {
-        int last[] = new int[26];
-        long res = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            last[s.charAt(i) - 'a'] = i + 1;
-            for (int j: last)
-                res += j;
+        int[] lists = new int[26];
+        long result = 0;
+        for(int i=0; i<s.length(); ++i) {
+            lists[s.charAt(i) - 'a']= i + 1;
+            for(int list : lists) {
+                result+=list;
+            }
         }
-        return res;
+        return result;
     }
 }
+
 
  // public long appealSum(String s) {
  //        int last[] = new int[26];
