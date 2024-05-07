46036a8416a2da71cbe406fd7bc353633b6b49fb
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-07 17:27:21 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (21.37%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -1,19 +1,15 @@
 class Solution {
     public int maxDepth(String s) {
-        Map<Integer, Integer> map = new TreeMap<>();
-        for(int i=0; i < s.length(); i++) {
-            if(s.charAt(i) == '(') {
-                map.put(i, 1);
-            }else if(s.charAt(i) == ')'){
-                map.put(i, -1);
+        int current = 0;
+        int max =0;
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                current++;
+                max = Math.max(max, current);
+            }else if(c == ')') {
+                current--;
             }
-        }
-        int sum = 0;
-        int max = 0;
-        System.out.print(map);
-        for(Integer values : map.values()) {
-            sum+=values;
-            max= Math.max(max, sum);
+            
         }
         return max;
     }
