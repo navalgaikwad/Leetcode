178d25dbde4edaab992c1da9ae9bf296c76f9960
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-06 20:22:20 -0400

    Time: 2 ms (97.39%), Space: 43.7 MB (70.75%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean makeEqual(String[] words) {
-        Map<Character, Integer> map = new HashMap<>();
-        for(String word : words) {
-            for(char c : word.toCharArray()) {
-                map.put(c, map.getOrDefault(c, 0) + 1);
+        int[] count = new int[26];
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                count[c - 'a']++;
             }
         }
-        
+
         int n = words.length;
-        for(char c : map.keySet()) {
-            int value = map.get(c);
-            if(value%n != 0) {
+        for (int value : count) {
+            if (value % n != 0) {
                 return false;
             }
         }
