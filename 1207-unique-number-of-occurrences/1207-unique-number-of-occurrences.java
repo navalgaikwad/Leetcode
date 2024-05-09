07e82f0d2eeb84ad9178049c490f4901e411188a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-09 16:44:03 -0400

    Time: 2 ms (96.65%), Space: 41.6 MB (64.24%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+    //Arrays.sort(arr);
+        Map<Integer, Integer> map= new HashMap<>();
+        for(int i=0;i<arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+        Set<Integer> set=new HashSet<>(map.values());
+        return set.size()==map.size();
+    }
+}
