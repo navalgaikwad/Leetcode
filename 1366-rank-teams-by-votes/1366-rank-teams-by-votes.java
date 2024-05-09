4a562ebbd8156ac7cc1d34dbba48352ff215a478
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-09 18:15:49 -0400

    Time: 17 ms (36.17%), Space: 44.7 MB (28.38%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
@@ -1,31 +1,30 @@
 class Solution {
     public String rankTeams(String[] votes) {
-      Map<Character, int[]> map = new HashMap<>();
-        
-      int col = votes[0].length();
-        
-      for(String vote : votes){
-        for(int i = 0; i < col; i++){
-          char c = vote.charAt(i);
-          map.putIfAbsent(c, new int[col]);
-          map.get(c)[i]++;
+        Map<Character, int[]> map = new HashMap<>();
+        int col = votes[0].length();
+        for(String vote : votes) {
+            for(int i=0; i<col; i++) {
+                map.putIfAbsent(vote.charAt(i), new int[col]);
+                map.get(vote.charAt(i))[i]++;
+            }
         }
-      }
-      
-      List<Character> list = new ArrayList<>(map.keySet());
-      Collections.sort(list, (a,b) -> {
-        for(int i = 0; i < col; i++){
-          if(map.get(a)[i] != map.get(b)[i]){
-            return map.get(b)[i] - map.get(a)[i];
-          }
+        //System.out.print(map);
+        List<Character> listOfVotes = new ArrayList<>(map.keySet());
+        Collections.sort(listOfVotes, (a, b) -> {
+            for(int i=0; i<col; i++) {
+                if(map.get(a)[i]!=map.get(b)[i]) {
+                    return map.get(b)[i] - map.get(a)[i];
+                }
+            }
+            return 0;
+        });
+        StringBuilder sb = new StringBuilder();
+        for(char c:  listOfVotes) {
+            sb.append(c);
         }
-        return 0;
-      });
-      
-      StringBuilder sb = new StringBuilder();
-      for(char c : list){
-        sb.append(c);
-      }
-      return sb.toString();
+        return sb.toString();
     }
 }
+
+//O(n * m * log(m)).
+//O(m^2).
