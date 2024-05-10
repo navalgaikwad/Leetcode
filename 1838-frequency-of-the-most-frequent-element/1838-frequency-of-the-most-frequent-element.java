e3e318466dc649b4d97c471edf1cb2c02b724215
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-09 21:03:59 -0400

    Time: 30 ms (63.57%), Space: 56.1 MB (92.24%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,22 +1,19 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
-        int left = 0;
-        int ans = 0;
-        long curr = 0;
-        
-        for (int right = 0; right < nums.length; right++) {
+        int left =0;
+        long sum = 0;
+        int len = 0;
+        for(int right =0; right < nums.length; right++) {
             int target = nums[right];
-            curr += target;
-            
-            while (((right - left + 1) * (long)target)  > k + curr) {
-                curr -= nums[left];
+            sum+=target;
+          
+            while(((right - left + 1) * (long)target) > k + sum) {
+                sum -=nums[left];
                 left++;
             }
-            
-            ans = Math.max(ans, right - left + 1);
+            len = Math.max(len, right - left + 1);
         }
-        
-        return ans;
+        return len;
     }
 }
