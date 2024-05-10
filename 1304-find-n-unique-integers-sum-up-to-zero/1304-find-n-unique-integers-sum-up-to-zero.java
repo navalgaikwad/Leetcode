217892cceba1594b100fb75df8125cd8a6d34b9a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-10 17:48:29 -0400

    Time: 0 ms (100.00%), Space: 41.4 MB (76.36%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -1,18 +1,10 @@
-
 class Solution {
     public int[] sumZero(int n) {
-        int[] result = new int[n];
-        for (int i = 0; i < n - 1; i += 2) {
-            result[i] = i + 1;
-            result[i + 1] = -(i + 1);
+        int[] array = new int[n];
+        for(int i=0; i<n - 1; i+=2) {
+            array[i] = (i + 1);
+            array[i+1] = -(i + 1);
         }
-        return result;
+        return array;
     }
-    
 }
-
-/*
-i = i+1
-i+1 = -(i+1)
-
-*/
