19b37407da8a16369dfb897e01a3d0f9b4d140e8
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-09 21:30:21 -0400

    Time: 79 ms (86.97%), Space: 97.4 MB (67.96%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
@@ -3,40 +3,38 @@ class Solution {
     public int longestPath(int[] parent, String s) {
         int len = parent.length;
         ArrayList<Integer> adj[] = new ArrayList[len];
-        for(int i=0; i < len; i++) {
+        for(int i=0; i<len; i++) {
             adj[i] = new ArrayList<>();
         }
-        for(int i = 0;i <len; i++) {
-            if(parent[i] != - 1) {
+        for(int i=0; i<len; i++) {
+            if(parent[i] != -1) {
                 adj[parent[i]].add(i);
             }
-            
         }
         boolean[] visited = new boolean[len];
-        helper(adj, 0, visited, s);
+        dfs(adj, 0, s, visited);
         return diameter;
     }
     
-    int helper(ArrayList<Integer> adj[], int parent, boolean[] visited, String s) {
+    int dfs(ArrayList<Integer> adj[], int parent, String s, boolean[] visited) {
+        int first = 0; int second = 0;
         visited[parent] = true;
-        int left =0; int right = 0;
-        for(Integer neighbour : adj[parent]) {
+        for(Integer neighbour:adj[parent]) {
             if(!visited[neighbour]) {
-                int count = helper(adj, neighbour, visited, s);
-                if(s.charAt(neighbour)!= s.charAt(parent)) {
-                    
-                    if(left < count) {
-                        right = left;
-                        left = count;
-                    }else if(right < count){
-                        right = count;
+                int count = dfs(adj, neighbour, s, visited);
+                if(s.charAt(neighbour)!=s.charAt(parent)) {
+                    if(first < count) {
+                        second = first;
+                        first = count;
+                    }else if(second < count) {
+                        second = count;
                     }
                 }
+                
             }
+            
         }
-        diameter = Math.max(diameter, 1 + left + right);
-        return 1 + left;
+        diameter = Math.max(first+second  +1, diameter);
+        return 1 + first;
     }
-    
-    
 }
