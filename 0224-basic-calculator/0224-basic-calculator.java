9a2b728c837d0dab27ecb381d310e442a03ef0bb
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-10 10:26:12 -0400

    Time: 9 ms (64.80%), Space: 43.5 MB (61.03%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,36 +1,37 @@
 class Solution {
     public int calculate(String s) {
         Stack<Integer> st = new Stack<>();
-        int number = 0;
         int sign = 1;
         int result = 0;
-        for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            
+        int number = 0;
+         for(int i=0; i<s.length(); i++) {
+             char c = s.charAt(i);
             if(Character.isDigit(c)) {
-                number = 10 * number + (int)(c- '0');
+                //while( i < s.length() && Character.isDigit(c)) {
+                    number = number * 10 + Character.getNumericValue(c);
+                //}
             }else if(c == '+') {
-                result = result + sign * number;
+                result = result  + sign * number;
                 sign = 1;
-                number =0;
+                number = 0;
             }else if(c == '-') {
-                result = result + sign * number;
+                result = result  + sign * number;
                 sign = -1;
-                number =0;
-            }else if(c == '(') {
+                number = 0;
+            }else if(c == '('){
                 st.push(result);
                 st.push(sign);
-                result =0;
-                sign =1;
-            }else if(c == ')') {
-                result = result + sign * number;
-                number =0;
-                result*=st.pop();//sign 
-                result+=st.pop();//no
+                result = 0;
+                sign = 1;
+            }else if( c == ')') {
+                result = result  + sign * number;
+                number = 0;
+                result*=st.pop();
+                result+=st.pop();
             }
-            
         }
         if(number != 0) result += sign * number;
         return result;
     }
+        
 }
