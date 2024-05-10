4026711818e01db051f10c1152d080fb6bc619fb
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-10 13:04:02 -0400

    Time: 3 ms (98.78%), Space: 45.8 MB (39.07%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,42 +1,42 @@
 class Solution {
-    Stack<Integer> st = new Stack<>();
+    Stack<Integer> ans = new Stack<>();
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<Integer> adj[] = new ArrayList[numCourses];
-        for(int i =0; i<numCourses; i++) {
+        for(int i=0; i<numCourses; i++) {
             adj[i] = new ArrayList<>();
         }
-        for(int[] pre : prerequisites) {
-            adj[pre[1]].add(pre[0]);
+        for(int[] prerequisite:prerequisites) {
+            adj[prerequisite[1]].add(prerequisite[0]);
         }
-        int[] visited = new int[numCourses ];
-        int[] ans = new int[numCourses];
-       for (int i=0; i< numCourses; i++){
+        int[] visited = new int[numCourses];
+         for (int i=0; i< numCourses; i++){
            if(!dfs(adj, i, visited)){
                  return new int[]{};
            }
         }
-        int i = 0;
-        while (!st.isEmpty()) {
-            ans[i++] = st.peek();
-            st.pop();
+         int i = 0;
+        int[] result = new int[numCourses];
+        while (!ans.isEmpty()) {
+            result[i++] = ans.peek();
+            ans.pop();
         }
-        return ans;
+        return result;
     }
     boolean dfs(ArrayList<Integer> adj[], int parent, int[] visited) {
         if(visited[parent] == 1) {
             return false;
-        } 
+        }
         if(visited[parent] == 2) {
             return true;
         }
         visited[parent] = 1;
-        for(Integer neigbhour : adj[parent]) {
-            if(!dfs(adj, neigbhour, visited)) {
+        for(Integer neighbour :adj[parent]) {
+            if(!dfs(adj, neighbour, visited)) {
                 return false;
             }
         }
         visited[parent] = 2;
-        st.push(parent);
+        ans.push(parent);
         return true;
     }
 }
