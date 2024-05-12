9d64c7e22300b5f460a68a70c6ab0ae5013a455f
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-12 18:37:11 -0400

    Time: 0 ms (100.00%), Space: 41.3 MB (68.56%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,7 +1,9 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
+        int left = 0; int right = matrix[0].length - 1;
+        int top = 0; int bottom = matrix.length - 1;
         List<Integer> result = new ArrayList<>();
-        int left = 0, right = matrix[0].length - 1, top = 0, bottom = matrix.length - 1;
+        
         while(left <= right && top <= bottom) {
             for(int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
@@ -12,18 +14,17 @@ class Solution {
             }
             right--;
             if(top <= bottom) {
-               for(int i = right; i >= left; i--) {
-                result.add(matrix[bottom][i]);
-               }
+                for(int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
                 bottom--;
             }
             if(left <= right) {
-               for(int i = bottom; i >= top; i--) {
-                result.add(matrix[i][left]);
-               }
+                for(int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
                 left++;
             }
-            
         }
         return result;
     }
