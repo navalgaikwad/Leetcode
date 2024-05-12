8e542a5e45d6539179b0a431d51c8b104ccbda88
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-12 17:26:18 -0400

    Time: 54 ms (22.78%), Space: 114.9 MB (41.74%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -9,14 +9,14 @@ class Node {
         this.value = value;
     }
 }
+    int _capacity =0;
+    Map<Integer, Node> map = new HashMap<>();
     Node head = new Node(0, 0);
     Node tail = new Node(0, 0);
-    int _capacity = 0;
-    Map<Integer, Node> map = new HashMap<>();
     public LRUCache(int capacity) {
         _capacity = capacity;
-        head.next = tail;
-        tail.prev = head;
+        head.next= tail;
+        tail.next = head;
     }
     
     public int get(int key) {
@@ -25,38 +25,43 @@ class Node {
         }
         Node node = map.get(key);
         remove(node);
-        add(node);
+        add(new Node(node.key, node.value));
         return node.value;
     }
     
     public void put(int key, int value) {
-         if(map.containsKey(key)) {
+        if(map.containsKey(key)) {
             Node node = map.get(key);
             remove(node);
         }
         if(_capacity == map.size()) {
             remove(tail.prev);
         }
-       
+        
         add(new Node(key, value));
     }
-
-    void add(Node node) {
+    
+    void add(Node node) {//map madhye add kar
         map.put(node.key, node);
         Node next = head.next;
+        //Node prev = head;
+        
         head.next = node;
         node.prev = head;
         node.next = next;
         next.prev = node;
-        
     }
-    void remove(Node node) {
+    
+    void remove(Node node) {//map madhun remove kar
         map.remove(node.key);
+        
         Node next = node.next;
         Node prev = node.prev;
+        
         prev.next = next;
         next.prev = prev;
     }
+    
 }
 
 /**
