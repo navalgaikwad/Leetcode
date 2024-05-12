eb8a562ad471aa28abaa2bc61b6205670e78925b
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-12 18:14:57 -0400

    Time: 9 ms (33.19%), Space: 48.1 MB (24.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,8 +3,9 @@ class Solution {
         Map<String, ArrayList<String>> map = new HashMap<>();
         for(String str : strs) {
             char[] charArray = new char[26];
-            for(char c : str.toCharArray()) {
-                charArray[c - 'a']++;
+           
+            for(char c: str.toCharArray()) {
+                charArray[c -'a']++;
             }
             if(!map.containsKey(String.valueOf(charArray))) {
                 map.put(String.valueOf(charArray), new ArrayList<>());
@@ -14,8 +15,3 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
-
-/*
-The time complexity of this algorithm is O(n * k), where n is the number of strings in strs and k is the average length of a string. This is because, for each string, we iterate through its characters to create the character array, which has a constant size (26). The space complexity is also O(n * k) due to the use of the map to store the grouped anagrams.
-
-*/
