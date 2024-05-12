c5a4e9902f4456af05bfa9f5d4af01b952acdad3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-12 16:36:02 -0400

    Time: 0 ms (100.00%), Space: 44.5 MB (5.11%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,15 +1,14 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int countNegativeNumbers = 0;
-        for (int num : nums) {
-            if (num == 0) {
+        int countNegative = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 0) {
                 return 0;
             }
-            if (num < 0) {
-                countNegativeNumbers++;
+            if(nums[i] < 0) {
+                countNegative++;
             }
         }
-
-        return countNegativeNumbers % 2 == 0 ? 1 : -1;
+        return countNegative%2 == 0 ? 1 : -1;
     }
 }
