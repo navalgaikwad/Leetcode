b386d5bc0f665f97078ee43be434ce57bf33c872
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-13 18:19:59 -0400

    Time: 12 ms (5.66%), Space: 43 MB (6.96%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -4,23 +4,28 @@ class Solution {
         for(char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        PriorityQueue<Character> pq = new PriorityQueue<>((a,b)->map.get(b) - map.get(a));
+        PriorityQueue<Character> pq = new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
         pq.addAll(map.keySet());
+        
         String result = "";
-        while(pq.size()>1) {
-            char one = pq.remove();
-            char two = pq.remove();
-            result+=one;
-            result+=two;
-            map.put(one, map.getOrDefault(one, 0) - 1);
-            map.put(two, map.getOrDefault(two, 0) - 1);
-            if(map.get(one) != 0) {
-                pq.add(one);
+        while(pq.size() > 1) {
+            char characterOne = pq.remove();
+            char characterTwo = pq.remove();
+            
+            result+=characterOne;
+            result+=characterTwo;
+            
+            map.put(characterOne, map.getOrDefault(characterOne, 0) - 1);
+            map.put(characterTwo, map.getOrDefault(characterTwo, 0) - 1);
+            if(map.get(characterOne) != 0) {
+                pq.add(characterOne);
             }
-            if(map.get(two) != 0) {
-                pq.add(two);
+            
+            if(map.get(characterTwo) != 0) {
+                pq.add(characterTwo);
             }
         }
+        
         if(!pq.isEmpty()) {
             Character c= pq.remove(); // if there is remaining if it greater than 1 return null
            if(map.get(c)>1){
