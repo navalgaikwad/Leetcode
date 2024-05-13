2e2b7fce0ed40b5e5ed1febbbcbbd587d2232861
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-13 14:02:17 -0400

    Time: 1 ms (93.78%), Space: 41.3 MB (35.59%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,47 +1,10 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-//         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
-//         for(int num : nums){
-//             if(num!=0 && !pq.contains(num)){
-//               pq.add(num);  
-//             }
-            
-//         }
-         if(nums[0] == 0 && nums.length == 1){
-            return 0;
-        }
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums){
-            if(num!=0) {
-            set.add(num);
-            }
-        }
-       // int count =0;
-       // while(!pq.isEmpty()){
-       //      helper(nums, pq.remove());
-       //      count++;
-       //  }
-        
-        return set.size();
-    }
-    void helper(int[] nums, int val){
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]!=0){
-               nums[i] = Math.abs( val - nums[i] ); 
-            }
-            
-        }
+      HashSet<Integer> set = new HashSet<>();
+      for(int a : nums) {
+        if(a > 0)
+          set.add(a);
+      }
+      return set.size();
     }
 }
-
-//[1,5,0,3,5]
-// [4, 0, 2, 0]
-// [0,0,2,0]
-// [0,0,0,0]
-
-
-//[1,5,0,3,5, 6]
-//[5,1,0,3,1,0]
-//[0,4,0,2,4,0]
-//[0,0,0,2,0,0]
-//[0,0,0,0,0,0]
