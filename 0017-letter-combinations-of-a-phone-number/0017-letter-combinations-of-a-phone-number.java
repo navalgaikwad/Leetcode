bda3a81a28912ca13d83d2684c8aa2c291c49f97
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-12 20:17:55 -0400

    Time: 5 ms (38.38%), Space: 42.6 MB (5.59%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,11 +1,8 @@
 class Solution {
-    List<String> result = new ArrayList<>();
     Map<Character, String> map = new HashMap<>();
-    
+    List<String> result = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        if(digits.length() == 0) return result;
-        
-        
+        if(digits.length() == 0) return result; 
         map.put('2', "abc");
         map.put('3', "def");
         map.put('4', "ghi");
@@ -14,39 +11,19 @@ class Solution {
         map.put('7', "pqrs");
         map.put('8', "tuv");
         map.put('9', "wxyz");
-        
-        helper(digits, "", 0);
-        
+        dfs(digits, "", 0);
         return result;
     }
     
-    void helper(String digits, String ans, int index) {
-        if(index == digits.length()) {
-            result.add(new String(ans));//ad
+    void dfs(String digits, String ans, int index) {
+        if(digits.length() == ans.length()) {
+            result.add(new String(ans));
             return;
         }
-        String value = map.get(digits.charAt(index));//abc // def
-        
-        for(char c: value.toCharArray()) {//def
-           helper(digits, ans+c, index + 1); //ab
+        String value = map.get(digits.charAt(index));
+        for(char c : value.toCharArray()) {
+            dfs(digits, ans+c, index+1);
         }
-    }   
+    }
+    
 }
-
-/*
-Input: digits = "23"
-Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
-
-2 :abc
-3: def
-
-2
-ad
-
-2: abc
-3: def
-ad, ae, af, b
-
-
-
-*/
