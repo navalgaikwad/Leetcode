1eb9428bb64813f4dc0e2ed33cf195ce5a314cc1
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-14 21:08:26 -0400

    Time: 8 ms (46.97%), Space: 40.3 MB (33.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,7 +2,7 @@ class Solution
 {
     public int fib(int N)
     {
-        if(N <= 1)
+        if(N == 1 || N == 0)
             return N;
         else
             return fib(N - 1) + fib(N - 2);
