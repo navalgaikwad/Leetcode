846d39f697b54610524a74a4b7360b768032aff1
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-16 20:28:19 -0400

    Time: 118 ms (64.31%), Space: 66 MB (20.23%) - LeetHub

---
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.java
@@ -0,0 +1,13 @@
+class Solution {
+    int minAbsoluteDifference(List<Integer> nums, int x) {
+    int ans = Integer.MAX_VALUE;
+    TreeSet<Integer> s = new TreeSet<>();
+    for(int i = x; i < nums.size(); ++i){
+        s.add(nums.get(i-x));
+        Integer high = s.ceiling(nums.get(i)), low = s.floor(nums.get(i));
+        if(high != null) ans = Math.min(ans, Math.abs(nums.get(i) - high));
+        if(low != null) ans = Math.min(ans, Math.abs(nums.get(i) - low));
+    }
+    return ans;
+}
+}
