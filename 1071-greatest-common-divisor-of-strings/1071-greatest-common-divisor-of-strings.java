d5c5e8fe7dce36c2e4c6ec99a96b56fb5c574733
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-18 20:37:52 -0400

    Time: 1 ms (69.12%), Space: 42.4 MB (23.26%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -3,10 +3,24 @@ class Solution {
         if(!(str1+str2).equals(str2+str1)) {
             return "";
         }
-        int gcd = gcd(str1.length(), str2.length());
-        return str1.substring(0, gcd);
+        int value = gcd(str1.length(), str2.length());
+        return str1.substring(0, value);
     }
+    
     int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a%b);
+        return b==0?a :gcd(b, a%b);
     }
 }
+
+// class Solution {
+//     public String gcdOfStrings(String str1, String str2) {
+//         if(!(str1+str2).equals(str2+str1)) {
+//             return "";
+//         }
+//         int gcd = gcd(str1.length(), str2.length());
+//         return str1.substring(0, gcd);
+//     }
+//     int gcd(int a, int b) {
+//         return b == 0 ? a : gcd(b, a%b);//vvimp
+//     }
+// }
