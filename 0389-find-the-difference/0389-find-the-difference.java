de9a01c66060f5a075243d5fcf02646f4c9bda54
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-18 22:30:25 -0400

    Time: 1 ms (100.00%), Space: 41.8 MB (24.65%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,24 +1,17 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         int[] a = new int[26];
-        
-        // Increment the count for each character in string s
         for(char c : s.toCharArray()) {
             a[c - 'a']++;
         }
-        
-        // Decrement the count for each character in string t
         for(char c : t.toCharArray()) {
             a[c - 'a']--;
         }
-        
-        // Find the character that has a count of -1
-        for(int i = 0; i < 26; i++) {
-            if (a[i] == -1) {
+        for(int i=0; i<26 ;i++) {
+            if( a[i] == -1) {
                 return (char)(i + 'a');
             }
         }
-        
         return ' ';
     }
 }
