a2bc8cd60d4a820a29f2c1a55bfb132c948abe81
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-21 15:00:18 -0400

    Time: 127 ms (63.03%), Space: 54.3 MB (41.38%) - LeetHub

--- 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
@@ -1,31 +1,26 @@
 class Solution {
-    private boolean isValidNum(String num) {
-        for (int i = 0; i < num.length(); ++i) {
-            char c = num.charAt(i);
-            if (c < '0' || c > '9') {
+     //character number
+    boolean isValid(String word) {
+        for(int i=0; i<word.length(); i++) {
+            char c = word.charAt(i);
+            if(c < '0' || c >'9') {
                 return false;
+                
             }
         }
         return true;
     }
-    
     public String discountPrices(String sentence, int discount) {
-        String[] words = sentence.split(" ");
-        for (int i = 0; i < words.length; ++i) {
-            String word  = words[i];
-            String num = word.substring(1);
-            
-            if (word.length() > 1 && word.charAt(0) == '$' && isValidNum(num)) {
-                words[i] = String.format("$%.2f", Long.parseLong(num) * (1 - discount / 100d));
+        String[] arr = sentence.split(" ");
+        for(int i=0; i<arr.length; i++ ){
+            String word = arr[i];
+            String num = word.substring(1);//next word check karyla
+            if(word.length() > 1 &&  word.charAt(0)=='$' && isValid(num)) {
+                arr[i] = String.format("$%.2f", Long.parseLong(num) * (1 - discount/100d));
             }
         }
-        return String.join(" ", words);
-    }
     
+        return String.join(" ", arr);
+            
+    }
 }
-//seperate by  " "
-/*
-check for 1 charcter is $ ani num valid ahe ka
-ahe tar String.formate("$%.2f", formula);
-String.join(" ", words);
-*/
