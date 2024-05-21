3dcbd594285c7a109a3d20862045f66a9064dd96
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-21 19:44:24 -0400

    Time: 2 ms (57.40%), Space: 42.6 MB (41.35%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,38 @@
 class Solution {
-    HashSet<List<Integer>> result = new HashSet<>();
+    Set<List<Integer>> result = new HashSet<>();
     public List<List<Integer>> subsets(int[] nums) {
-        backTracking(nums, new ArrayList<>(), 0);
-       // System.out.print(result);
+        dp(nums, 0, new ArrayList<>());
         return new ArrayList<>(result);
     }
     
-    void backTracking(int[] nums, List<Integer> runningList, int index) {
-        
-        result.add(new ArrayList<>(runningList));
+    void dp(int[] nums, int index, ArrayList<Integer> answer) {
+        result.add(new ArrayList<>(answer));
         for(int i=index; i<nums.length; i++) {
-            runningList.add(nums[i]);
-            backTracking(nums, runningList, i + 1);
-            runningList.remove(runningList.size() - 1);
-        }
+            //if(!answer.contains(nums[i])) {
+                answer.add(nums[i]);
+                dp(nums, i + 1, answer);
+                answer.remove(answer.size() - 1); 
+            }
+            
+       // }
     }
 }
+
+// class Solution {
+//     HashSet<List<Integer>> result = new HashSet<>();
+//     public List<List<Integer>> subsets(int[] nums) {
+//         backTracking(nums, new ArrayList<>(), 0);
+//        // System.out.print(result);
+//         return new ArrayList<>(result);
+//     }
+    
+//     void backTracking(int[] nums, List<Integer> runningList, int index) {
+        
+//         result.add(new ArrayList<>(runningList));
+//         for(int i=index; i<nums.length; i++) {
+//             runningList.add(nums[i]);
+//             backTracking(nums, runningList, i + 1);
+//             runningList.remove(runningList.size() - 1);
+//         }
+//     }
+// }
