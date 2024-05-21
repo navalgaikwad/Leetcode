cd5a14ffbfa219a5d062cb6f0fe57260e3700dcb
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-21 10:47:53 -0400

    Time: 1 ms (93.68%), Space: 40.5 MB (98.85%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,10 +1,11 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-      HashSet<Integer> set = new HashSet<>();
-      for(int a : nums) {
-        if(a > 0)
-          set.add(a);
-      }
-      return set.size();
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(num > 0) {
+                set.add(num);
+            }
+        }
+        return set.size();
     }
 }
