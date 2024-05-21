648792ff6bf289ec1395a6e7d886422149f54286
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-21 09:52:26 -0400

    Time: 30 ms (59.46%), Space: 46.2 MB (9.44%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,14 +1,14 @@
 class Solution {
     public int partitionString(String s) {
         Set<Character> set = new HashSet<>();
-        int result = 1;
+        int count =1;
         for(char c : s.toCharArray()) {
             if(set.contains(c)) {
-                result++;
+                count++;
                 set = new HashSet<>();
             }
             set.add(c);
         }
-        return result;
+        return count;
     }
 }
