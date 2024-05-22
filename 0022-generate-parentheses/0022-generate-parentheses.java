f08da3b5bc33d342e116fc756ef9baa0f70d8e9a
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-22 11:55:47 -0400

    Time: 2 ms (51.16%), Space: 42.9 MB (66.22%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -4,22 +4,17 @@ class Solution {
         helper(n, 0, 0, "");
         return result;
     }
+    
     void helper(int n, int open, int close, String ans) {
         if(2*n == open+close) {
             result.add(new String(ans));
             return;
         }
         if(open < n) {
-           helper(n, open + 1, close, ans+"("); 
+            helper(n, open+1, close, ans+"(");
         }
         if(close < open) {
-           helper(n, open, close + 1, ans+")"); 
+            helper(n, open, close + 1, ans+")");
         }
     }
 }
-/*
-The number of nodes in a full binary tree of depth 2n is 2^(2n+1) - 1, which is approximately equal to 4^n.
-
-However, not all paths in the recursion tree are valid parentheses strings (some may violate the balance condition). The number of valid paths is the nth Catalan number, which is asymptotically bounded by 4^n / n^(3/2) or 4^n / sqrt(n).
-O(4^n / sqrt(n))
-*/ 
