d62d74b188bddf1f2125d0cd02d12547afb432b2
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-22 14:36:42 -0400

    Time: 1 ms (99.44%), Space: 56.9 MB (50.83%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,36 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        return solve(nums, 0, nums.length - 1);
-    }
-    
-    int solve(int[] nums, int lo, int hi) {
-        if(lo >= hi) {
-            return nums[lo];
+         if(nums.length == 1) {
+            return nums[0];
         }
-        int mid = lo + (hi-lo)/2;
-        
-        int left = solve(nums, lo, mid - 1);
-        int right = solve(nums, mid + 1, hi);
-        int cross = cross(nums, lo, hi, mid);
-        return Math.max(left, Math.max(right, cross));
-        
-    }
-    
-    int cross(int[] nums, int lo, int hi, int mid) {
-        int rightValue = 0;
-        int leftValue =0;
-        int left = 0;
-        int right = 0;
-        for(int i=mid - 1; i>=lo; i--) {
-            leftValue+=nums[i];
-            left = Math.max(left, leftValue);
+        int sum =0;
+        int max =Integer.MIN_VALUE;;
+        for(int i=0; i<nums.length; i++) {
+            sum+=nums[i];
+            max= Math.max(sum, max);
+            if(sum < 0) {
+                sum =0;
+            }
+               
         }
-       // int leftValue = 0;
-        for(int i=mid+1; i<=hi; i++) {
-            rightValue+=nums[i];
-            right = Math.max(right, rightValue);
-        }
-        
-        return nums[mid] + left + right;
+        return max;
     }
 }
