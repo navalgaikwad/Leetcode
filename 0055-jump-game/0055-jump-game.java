1fea8ab812874d9edb11de1704ec71aeda4beed0
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-23 18:53:21 -0400

    Time: 1575 ms (5.01%), Space: 45.7 MB (27.23%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,39 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int len = nums.length+1;
-        int[] dp = new int[len];
+        int length = nums.length;
+        int[] dp = new int[length + 1];
         Arrays.fill(dp, -1);
         dp[0] = 1;
-        for(int i=0; i< nums.length; i++) {
+        for(int i=0; i<=length; i++) {
             if(dp[i] != -1) {
-                for(int j=1; j<=nums[i]; j++) {
-                    int next = j + i;//use j not dp[j] or dp[i]
-                    if(next < len) {
-                        dp[next] =  dp[i] + 1;
-                    }
+            for(int j=1; j<=nums[i]; j++) {
+                int next = j + i;
+                if(next < length) {
+                    dp[next] = dp[i] + 1;
                 }
             }
+          }
         }
-         return dp[nums.length - 1] != -1; 
+        return dp[length-1]!=-1;
     }
 }
-
-// class Solution {
-//     public boolean canJump(int[] nums) {
-//         int len = nums.length;
-//         int[] dp = new int[len];
-//         Arrays.fill(dp, -1);
-//         dp[0] = 1;
-//         for (int i = 0; i < nums.length; i++) {
-//             if (dp[i] != -1) {
-//                 for (int j = 1; j <= nums[i]; j++) {
-//                     int next = j + i;
-//                     if (next < len) {
-//                         dp[next] = dp[i] + 1;
-//                     }
-//                 }
-//             }
-//         }
-//         return dp[len - 1] != -1;
-//     }
-// }
