a55f6c1006a0623da3dc6bb9032f6457d676f980
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-27 20:00:21 -0400

    Time: 14 ms (7.91%), Space: 42.7 MB (10.29%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,26 +1,21 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         List<Integer> result = new ArrayList<>();
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i<s.length(); i++) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i =0; i < s.length(); i++) {
             map.put(s.charAt(i), Math.max(i, map.getOrDefault(s.charAt(i), 0)));
         }
         int left = 0;
-        int maxi = -1;
-        for(int right = 0; right<s.length(); right++) {
-            maxi = Math.max(maxi, map.get(s.charAt(right)));
-            if(maxi == right) {
+        int maximum =-1;
+        
+        for(int right =0; right < s.length(); right++) {
+            maximum = Math.max(map.get(s.charAt(right)), maximum);
+            if(right == maximum) {
+                maximum = -1;
                 result.add(right - left + 1);
                 left = right + 1;
-                maxi = -1;
             }
         }
         return result;
     }
 }
-
-//find the last elemnt of the character
-//find max from map take it in max
-// check for max == right
-// thn update left = right +1
-//then update length
