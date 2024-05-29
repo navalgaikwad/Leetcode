016e63830485a9c84949fa015f4f29536d0cea21
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-29 11:54:36 -0400

    Time: 31 ms (17.69%), Space: 46.4 MB (32.03%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -2,26 +2,27 @@ class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
         int currentNo =0;
-        char operation = '+';
+        char sign = '+';
+        
         for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if(Character.isDigit(c)) {
-                currentNo = (currentNo * 10) + (c - '0');
+            if(Character.isDigit(s.charAt(i))) {
+                currentNo = (currentNo *10) + (s.charAt(i) - '0');
             }
-            
-            if(!Character.isDigit(c) && !Character.isWhitespace(c) || i == s.length() - 1) {
-                if(operation == '+') {
+            //if(!Character.isDigit(c) && !Character.isWhitespace(c) || i == s.length() - 1) {
+            if(!Character.isDigit(s.charAt(i)) && !Character.isWhitespace(s.charAt(i)) || i == s.length() - 1) {
+                if(sign == '+') {
                     stack.push(currentNo);
-                }else if(operation == '-') {
+                }else if(sign == '-') {
                     stack.push(-currentNo);
-                }else if(operation == '*') {
+                }else if(sign == '*') {
                     stack.push(stack.pop() * currentNo);
-                }else if(operation == '/') {
+                }else if(sign == '/') {
                     stack.push(stack.pop() / currentNo);
                 }
-                operation = c;
-                currentNo =0;
+                sign = s.charAt(i);
+                currentNo =0;  
             }
+            
         }
         int result =0;
         while(!stack.isEmpty()) {
@@ -30,13 +31,3 @@ class Solution {
         return result;
     }
 }
-
-//"3+2*2"
-//3 check for digit
-//then check for operation +
-//push 3 on the stcak
-//operation +
-//current = 2
-//push  2
-//3
-//4
