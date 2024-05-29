7ccbda1a6cfacb43b7cd81f371998037644d9b47
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-29 13:04:32 -0400

    Time: 2 ms (96.72%), Space: 44 MB (75.16%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -13,25 +13,19 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null) {
-            return false;
-        }
-        if(isSame(root, subRoot)) {
-            return true;
-        }
-        
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+public class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
     
-    boolean isSame(TreeNode root, TreeNode subRoot) {
-        if(root == null || subRoot == null) {
-            return root == subRoot;
-        }
-        if(root.val != subRoot.val) {
-            return false;
-        }
-        return isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right);
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        
+        if (s.val != t.val) return false;
+        
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
     }
 }
