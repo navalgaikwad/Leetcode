ff19840be99a509184956b70ced5613103b16ce3
Anantnaval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-29 17:34:38 -0400

    Time: 1 ms (78.91%), Space: 40.7 MB (63.25%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        long reverse = 0;
+        int temp;
+
+        while (x != 0) {
+            temp = x % 10;
+            reverse = reverse * 10 + temp;
+            x = x / 10;
+
+            if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+
+        return (int)reverse;
+    }
+}
