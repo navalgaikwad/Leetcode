5c478d0fcbc9fc04a3ee87545108ca219184a6f9
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-31 07:50:41 -0400

    Time: 1 ms (87.54%), Space: 44.6 MB (63.00%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int prefix=1;
-        int suffix=1;
-        int maxi=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(prefix==0){
-                prefix=1;
+        int prefix =1;
+        int suffix =1;
+        int len = nums.length;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length; i++) {
+            if(prefix == 0) {
+                prefix = 1;
             }
-            if(suffix==0){
-                suffix=1;
+            if(suffix == 0) {
+                suffix = 1;
             }
-            prefix=prefix*nums[i];
-            suffix=suffix*nums[nums.length-i-1];
-            maxi=Math.max(maxi,Math.max(prefix,suffix));
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[len - i - 1];
+            max = Math.max(max, Math.max(prefix, suffix));
         }
-        return maxi;
-        
+        return max;
     }
 }
