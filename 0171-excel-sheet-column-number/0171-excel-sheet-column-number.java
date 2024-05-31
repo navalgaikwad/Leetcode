d61e6b2d928de313e237bb975272c3479c93cdd0
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-31 11:48:11 -0400

    Time: 1 ms (92.76%), Space: 41.7 MB (96.06%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -1,12 +1,9 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
-        if(columnTitle == " ") {
-            return 0;
-        }
         int sum =0;
-        for(Character c : columnTitle.toCharArray()) {
-            sum = sum * 26;
-            sum = sum + c - 'A' + 1;
+        for(char c : columnTitle.toCharArray()) {
+            sum = sum *26;
+            sum+= c -'A' + 1;
         }
         return sum;
     }
