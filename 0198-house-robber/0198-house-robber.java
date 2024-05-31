41730e04a331d2003ee71f0054db43f8e17e780f
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-05-31 13:00:28 -0400

    Time: 1 ms (5.40%), Space: 41 MB (37.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,36 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
-//         int n = nums.length;
-        
-//         if(n<2){
-//             return nums[0];
-//         }
-//         int[] dp = new int[n +1];
-//         dp[0] = nums[0];
-//         dp[1] = Math.max(nums[0], nums[1]);
-//         for(int i=2; i<n; i++) {
-//             int alternate = dp[i - 2] + nums[i];
-//             int previous = dp[i - 1];
-//             dp[i] = Math.max(alternate, previous);
-//         }
-        
-//         return dp[n - 1];
-        int n = nums.length;
-        return dp(nums, n - 1, new HashMap<>());
+      int n = nums.length;
+      return dp(nums, n - 1, new HashMap<>());  
     }
     
-    int dp(int[] nums, int i, HashMap<Integer, Integer> memo) {
-        if(memo.containsKey(i)) return memo.get(i); 
-        if(i == 0) {
-            return nums[i];
+    int dp(int[] nums, int index, HashMap<Integer, Integer> memo) {
+        if(memo.containsKey(index)) {
+            return memo.get(index);
         }
-        if(i == 1) {
+        if(index == 0) {
+            return nums[0];
+        }
+        if(index == 1) {
             return Math.max(nums[0], nums[1]);
         }
-        int current = nums[i] + dp(nums, i -2, memo);
-        int previous = dp(nums, i -1, memo);
+        int current = nums[index] + dp(nums, index - 2, memo);
+        int previous = dp(nums, index - 1, memo);
+        
         int result = Math.max(current, previous);
-        memo.put(i, result);
+        memo.put(index, result);
         return result;
     }
 }
