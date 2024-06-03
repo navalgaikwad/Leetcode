8a92b64b56e1efaaf58a04d765e591a3d302d227
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-03 17:21:03 -0400

    Time: 1 ms (99.15%), Space: 44.4 MB (82.09%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,16 +1,21 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] count1 = new int[26];
+       // int[] ransomNoteCount= new int[26];
+        int[] magazineCount= new int[26];
         
-        for(char c : magazine.toCharArray()) {
-            count1[c-'a']++;
+        // for(char c: magazine.toCharArray()) {
+        //     ransomNoteCount[c - 'a']++;
+        // }
+        for(char c: magazine.toCharArray()) {
+            magazineCount[c - 'a']++;
         }
         
         for(char c: ransomNote.toCharArray()) {
-            count1[c-'a']--;
-            if(count1[c-'a'] < 0){
-               return false; 
+             magazineCount[c - 'a']--;
+            if(magazineCount[c - 'a'] < 0) {
+                return false;
             }
+            //magazineCount[c - 'a']--;
         }
         return true;
     }
