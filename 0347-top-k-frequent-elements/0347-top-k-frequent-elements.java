c3ea069782ecd62d55909a472039d59a972a6161
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-03 16:59:16 -0400

    Time: 13 ms (73.67%), Space: 48.4 MB (52.59%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,27 +1,18 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int num : nums) {
+        for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        ArrayList<Integer> bucket[] = new ArrayList[nums.length + 1];
-        for(int num : map.keySet()) {//vvimp
-            int freq = map.get(num);
-            if(bucket[freq] == null) {
-               bucket[freq] = new ArrayList<>(); 
-            }
-            bucket[freq].add(num);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        pq.addAll(map.keySet());
+        
+        int[] num = new int[k];
+        int i = 0;
+        while(k-- > 0){
+            num[i++] = pq.remove();
         }
-        int[] res = new int[k];
-        int index =0;
-        for(int i = bucket.length-1; i>=0; i--) {
-            if(bucket[i] != null) {
-                for(int val : bucket[i]) {
-                    res[index++] = val;
-                    if(index == k) return res;
-                }
-            }
-        }
-        return res;
+        return num;
     }
 }
