80d672bdcc97b1529df5f30ae8e1a5cfe5f76868
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-06 15:18:17 -0400

    Time: 3 ms (13.14%), Space: 41.4 MB (27.04%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,30 +1,26 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
         int left = Arrays.stream(nums).max().getAsInt();
-        // The maximum possible value for the largest sum is the sum of all numbers in the array,
-        // which would be the case if all numbers are in one subarray.
-        int right = Arrays.stream(nums).sum();
-        int ans = 0;
+        int right =  Arrays.stream(nums).sum();
+        int ans =0;
         while(left <= right) {
             int mid = left + (right - left)/2;
-            
-            int sum = 0;
-            int count =1;
-            for(int i=0; i<nums.length; i++) {
-                sum = sum + nums[i];
+            int sum =0;
+            int count = 1;
+            for(int num : nums) {
+                sum = sum + num;
                 if(sum > mid) {
                     count++;
-                    sum = nums[i];
+                    sum = num;
                 }
             }
-            if(count<=k) {
+            if(count <= k) {
                 ans = mid;
-                right = mid -1;
+                right = mid - 1;
             }else {
                 left = mid + 1;
             }
         }
         return ans;
     }
-    
 }
