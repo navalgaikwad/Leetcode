0de3476f47b2994128f94c31086240fa48b0b1b3
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-06 20:40:09 -0400

    Time: 32 ms (19.08%), Space: 44.4 MB (24.83%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,45 +1,21 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         Map<Character, Integer> map = new HashMap<>();
-        int leftSide =0;
-        int len = 0;
-        int frequency =0;
-        for(int right=0; right<s.length(); right++) {
-            char rightSideChar = s.charAt(right);
-            
-            map.put(rightSideChar, map.getOrDefault(rightSideChar, 0) + 1);
-            frequency = Math.max(frequency, map.get(rightSideChar));
-            int replacement = (right - leftSide + 1) -  frequency;
-            
-            if(replacement > k) {
-                char leftSideChar = s.charAt(leftSide);
-                map.put(leftSideChar, map.getOrDefault(leftSideChar, 0) - 1);
-                leftSide++;
+        int left =0;
+        int maxLen =0;
+        int freq = 0;
+        for(int right = 0; right<s.length(); right++) {
+            char rightSide = s.charAt(right);
+            map.put(rightSide, map.getOrDefault(rightSide, 0) + 1);
+            freq = Math.max(freq, map.get(rightSide));
+            int replacementsNeeded = (right - left + 1) - freq;
+            if(replacementsNeeded > k) {
+                char leftSide = s.charAt(left);
+                map.put(leftSide, map.getOrDefault(leftSide, 0) - 1);
+                left++;
             }
-            len = Math.max(len, right - leftSide + 1);
-        }
-        return len;
+            maxLen = Math.max(maxLen, right - left + 1);
+        } 
+        return maxLen;
     }
 }
-// class Solution {
-//     public int characterReplacement(String s, int k) {
-//         Map<Character, Integer> map = new HashMap<>();
-//         int left =0;
-//         int len = 0;
-//         int freq =0;
-//         for(int right =0; right < s.length(); right++) {
-//             char rightChar = s.charAt(right);
-//             map.put(rightChar, map.getOrDefault(rightChar, 0) + 1);
-//             freq =Math.max(freq, map.get(rightChar));
-//             int replacement = (right - left  +1) - freq;
-//             if(replacement > k) {
-//                 char leftChar = s.charAt(left);
-//                 map.put(leftChar, map.getOrDefault(leftChar, 0) - 1);
-//                 left++;
-//             }
-            
-//             len = Math.max(len, right - left  +1);
-//         }
-//         return len;
-//     }
-// }
