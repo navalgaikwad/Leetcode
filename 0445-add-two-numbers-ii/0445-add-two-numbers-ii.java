ba4bac97b74ccd03d894802bc6330cf0fe470d57
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-07 10:54:29 -0400

    Time: 4 ms (13.08%), Space: 44.2 MB (76.35%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -1,35 +1,42 @@
-public class Solution {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> s1 = new Stack<Integer>();
-        Stack<Integer> s2 = new Stack<Integer>();
+        Stack<Integer> st1 = new Stack<>();
+        Stack<Integer> st2 = new Stack<>();
         
-        while(l1 != null) {
-            s1.push(l1.val);
+        while(l1!=null) {
+            st1.push(l1.val);
             l1 = l1.next;
-        };
-        while(l2 != null) {
-            s2.push(l2.val);
+        }
+        while(l2!=null) {
+            st2.push(l2.val);
             l2 = l2.next;
         }
-        
-        int totalSum = 0, carry = 0;
-        ListNode ans = new ListNode();
-        while (!s1.empty() || !s2.empty()) {
-            if (!s1.empty()) {
-                totalSum += s1.pop();
+        ListNode ans= new ListNode();
+        int total =0, carry = 0;
+        while(!st1.isEmpty() || !st2.isEmpty()) {
+            if(!st1.isEmpty()) {
+                total+=st1.pop();
             }
-            if (!s2.empty()) {
-                totalSum += s2.pop();
+            if(!st2.isEmpty()) {
+                total+=st2.pop();
             }
-            
-            ans.val = totalSum % 10;
-            carry = totalSum / 10;
-            ListNode head = new ListNode(carry);
+             ans.val = total % 10;
+            carry = total / 10;
+            ListNode head= new ListNode(carry);
             head.next = ans;
             ans = head;
-            totalSum = carry;
+            total =carry;
         }
-
-        return carry == 0 ? ans.next: ans;
+         return carry == 0 ? ans.next: ans;
     }
 }
