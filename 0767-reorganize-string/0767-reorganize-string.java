e73f4e9dbb9a4e4706c9f86a0f8d4636fb0c02aa
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-07 19:55:33 -0400

    Time: 6 ms (32.30%), Space: 41.6 MB (54.59%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,38 +1,39 @@
 class Solution {
-    public String reorganizeString(String s) {
+     public String reorganizeString(String s) {
+         if(s.length() == 0) {
+             return "";
+         }
         Map<Character, Integer> map = new HashMap<>();
-        for(char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        PriorityQueue<Character> pq = new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
-        pq.addAll(map.keySet());
-        
-        String result = "";
-        while(pq.size() > 1) {
-            char characterOne = pq.remove();
-            char characterTwo = pq.remove();
-            
-            result+=characterOne;
-            result+=characterTwo;
-            
-            map.put(characterOne, map.getOrDefault(characterOne, 0) - 1);
-            map.put(characterTwo, map.getOrDefault(characterTwo, 0) - 1);
-            if(map.get(characterOne) != 0) {
-                pq.add(characterOne);
-            }
-            
-            if(map.get(characterTwo) != 0) {
-                pq.add(characterTwo);
-            }
-        }
-        
-        if(!pq.isEmpty()) {
-            Character c= pq.remove(); // if there is remaining if it greater than 1 return null
-           if(map.get(c)>1){
-               return "";
-           }
-           result+=c;
-        }
-        return result;
+         for(char c : s.toCharArray()) {
+             map.put(c, map.getOrDefault(c, 0)  +1);
+         }
+         PriorityQueue<Character> pq = new PriorityQueue<>((a,b)->map.get(b) - map.get(a));
+         pq.addAll(map.keySet());//a, b
+         StringBuilder sb = new StringBuilder();
+         while(pq.size() > 1) {
+             char value1 = pq.remove();//a
+             char value2 = pq.remove();//b
+             
+             map.put(value1, map.getOrDefault(value1, 0) - 1);//a = 2
+             map.put(value2, map.getOrDefault(value2, 0) - 1);//b = 0
+             
+             sb.append(value1);//ab
+             sb.append(value2);
+             if(map.get(value1) > 0) {//
+                 pq.add(value1);//a
+             }
+             if(map.get(value2) > 0) {
+                 pq.add(value2);
+             }
+         }
+         
+         if(!pq.isEmpty()) {
+             char value = pq.remove();//a
+             if(map.get(value) > 1) { //2 >1
+                 return "";
+             }
+             sb.append(value);//
+         }
+         return sb.toString();
     }
 }
