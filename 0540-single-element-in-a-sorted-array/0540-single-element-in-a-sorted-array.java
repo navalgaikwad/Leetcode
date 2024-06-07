30d07028da359cdb321e9a91a7ed2b8b1ec790c9
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-07 17:40:18 -0400

    Time: 0 ms (100.00%), Space: 50.4 MB (29.60%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,23 +1,19 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
+        if(nums.length == 0) return 0;
+        int left =0;
+        int right = nums.length -1;
+        while(left < right) {
             int mid = left + (right - left)/2;
-            if(mid%2 == 1) {
+            if( mid%2 == 1) {
                 mid--;
             }
-            if(nums[mid]!=nums[mid+1]) {
-                right = mid-1;
+            if(nums[mid]!=nums[mid + 1]) {
+                right = mid -1;
             }else {
                 left = mid + 2;
             }
-            
         }
         return nums[left];
     }
 }
-//check on odd and even position
-//if i get mid which is even check next elemnt is is not equal
-// if not equal value is at left side
-//if not then left side increamnet left side by 2 becuae there are 2 element
