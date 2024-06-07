37122e1951fb1b58554a5a30803bff0c1bb59246
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-06 22:36:38 -0400

    Time: 1 ms (99.54%), Space: 43.7 MB (66.38%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,11 +1,10 @@
 class Solution {
     public int compress(char[] chars) {
         StringBuilder sb = new StringBuilder();
-        
-        for(int i =0; i< chars.length; i++) {
-            char c = chars[i];
+        for(int i=0; i<chars.length; i++) {
             int count = 1;
-            while(i+1 < chars.length && c == chars[i+1]) {
+            char c = chars[i];
+            while( i + 1< chars.length && c == chars[i + 1]) {
                 i++;
                 count++;
             }
@@ -13,12 +12,12 @@ class Solution {
             if(count > 1) {
                 sb.append(count);
             }
+           
         }
-        int j =0;
-        for(int i=0; i<sb.length();i++) {
-            chars[i] = sb.charAt(i);
-        }
-        
+         int j =0;
+            for(int i=0; i<sb.length();i++) {
+                chars[i] = sb.charAt(i);
+            } 
         return sb.length();
     }
 }
