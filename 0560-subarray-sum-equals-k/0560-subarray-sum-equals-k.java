2c257055198141c872baa3522081284a5b5f3471
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-07 17:54:55 -0400

    Time: 23 ms (91.94%), Space: 45.7 MB (94.12%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -2,11 +2,11 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
-        int sum =0;
-        int count = 0;
+        int sum = 0;
+        int count =0;
         for(int num : nums) {
-            sum+=num;
-            if(map.containsKey(sum -k)) {
+            sum= sum + num;
+            if(map.containsKey(sum - k)) {
                 count+=map.get(sum - k);
             }
             map.put(sum, map.getOrDefault(sum, 0) + 1);
@@ -14,4 +14,3 @@ class Solution {
         return count;
     }
 }
-
