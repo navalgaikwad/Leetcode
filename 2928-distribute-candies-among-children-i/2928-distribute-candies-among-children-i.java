20135182798c6bd45ced89ecd41cce340ec293d5
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-08 19:52:39 -0400

    Time: 14 ms (43.69%), Space: 40.3 MB (93.20%) - LeetHub

--- 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.java
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.java
@@ -1,26 +1,21 @@
 class Solution {
-    public int waysToDistributeCandies(int remainingCandies, int candyLimit, int remainingChildren) {
-        // Base case: If there are no candies left, return 1 way (valid distribution)
-        if (remainingCandies == 0) {
+    
+    int helper(int totalCandies, int limit, int children) {
+        if(totalCandies == 0) {
             return 1;
         }
-
-        // Base case: If there are no children left or the limit is exceeded, return 0 ways
-        if (remainingChildren == 0 || candyLimit == 0) {
+        if( children == 0) {
             return 0;
         }
-
-        int ways = 0;
-
-        // Try distributing candies to the current child within the limit
-        for (int i = 0; i <= Math.min(candyLimit, remainingCandies); ++i) {
-            ways += waysToDistributeCandies(remainingCandies - i, candyLimit, remainingChildren - 1);
+        int ways =0;
+        for(int i =0; i<=Math.min(limit, totalCandies); i++) {
+            ways += helper(totalCandies - i, limit, children - 1);
         }
-
         return ways;
     }
-
-    public int distributeCandies(int totalCandies, int candyDistributionLimit) {
-        return waysToDistributeCandies(totalCandies, candyDistributionLimit, 3);
+    
+    
+    public int distributeCandies(int n, int limit) {
+        return helper(n, limit, 3);
     }
 }
