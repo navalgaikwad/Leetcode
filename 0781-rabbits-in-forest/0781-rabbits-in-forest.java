7e63f2a9acc764495535e38ddffcdb51b111bd3f
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-11 19:08:01 -0400

    Time: 3 ms (77.14%), Space: 42.4 MB (16.63%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numRabbits(int[] answers) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        int count = 0;
+
+        for (int answer : answers) {
+            if (answer == 0) {
+                // This must be the only rabbit of its color.
+                count++;
+            } else if (counts.getOrDefault(answer, 0) == 0) {
+                // This is the first time the color appears.
+                counts.put(answer, 1);
+                // Add all rabbits having this color.
+                count += answer + 1;
+            } else {
+                // Add one to how many times answer occurred.
+                counts.put(answer, counts.get(answer) + 1);
+                if (counts.get(answer) > answer) {
+                    // n+1 rabbits have said n, so color done.
+                    counts.put(answer, 0);
+                }
+            }
+        }
+        return count;
+    }
+}
