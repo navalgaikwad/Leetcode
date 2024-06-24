1300f4ef9baa673bedda6761df0a21709aade50f
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-23 20:59:25 -0400

    Time: 1 ms (79.59%), Space: 41.9 MB (23.45%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,22 +1,21 @@
 class Solution {
     public int numDecodings(String s) {
-        return dfs(s, 0, new HashMap<>());
+        return helper(s, 0, new HashMap<>());
     }
     
-    int dfs(String s, int start, HashMap<Integer, Integer> memo) {
-        if(s.length() == start) {
+    int helper(String s, int start, HashMap<Integer, Integer> memo) {
+        if(start == s.length()) {
             return 1;
         }
-        
         if(s.charAt(start) == '0') {
             return 0;
         }
         if(memo.containsKey(start)) {
             return memo.get(start);
         }
-        int count = dfs(s, start  +1, memo);
-        if(start + 2 <= s.length() && Integer.parseInt(s.substring(start , start + 2)) <=26) {
-            count+=dfs(s, start+2, memo);
+        int count = helper(s, start + 1, memo);
+        if(start + 2 <= s.length() && Integer.parseInt(s.substring(start, start + 2)) <= 26) {
+            count+= helper(s, start + 2, memo);
         }
         memo.put(start, count);
         return count;
@@ -24,22 +23,23 @@ class Solution {
 }
 // class Solution {
 //     public int numDecodings(String s) {
-//         return helper(s, 0, new HashMap<>());
+//         return dfs(s, 0, new HashMap<>());
 //     }
     
-//     int helper(String s, int start, HashMap<Integer, Integer> memo) {
-//         if(start == s.length()) {
+//     int dfs(String s, int start, HashMap<Integer, Integer> memo) {
+//         if(s.length() == start) {
 //             return 1;
 //         }
+        
 //         if(s.charAt(start) == '0') {
 //             return 0;
 //         }
 //         if(memo.containsKey(start)) {
 //             return memo.get(start);
 //         }
-//         int count = helper(s, start + 1, memo);
-//         if(start + 2 <= s.length() && Integer.parseInt(s.substring(start, start + 2)) <= 26) {
-//             count+= helper(s, start + 2, memo);
+//         int count = dfs(s, start  +1, memo);
+//         if(start + 2 <= s.length() && Integer.parseInt(s.substring(start , start + 2)) <=26) {
+//             count+=dfs(s, start+2, memo);
 //         }
 //         memo.put(start, count);
 //         return count;
