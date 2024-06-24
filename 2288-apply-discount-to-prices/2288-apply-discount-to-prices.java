b4afe14ff44ca69d5e9275b7ea19be66621bf4a5
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-24 18:59:29 -0400

    Time: 161 ms (39.85%), Space: 51.3 MB (69.65%) - LeetHub

--- 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
@@ -1,26 +1,23 @@
 class Solution {
-     //character number
-    boolean isValid(String word) {
-        for(int i=0; i<word.length(); i++) {
-            char c = word.charAt(i);
-            if(c < '0' || c >'9') {
+    boolean isValid(String num) {
+        for(char c : num.toCharArray()) {
+            if( c < '0' || c > '9') {
                 return false;
-                
             }
         }
         return true;
     }
     public String discountPrices(String sentence, int discount) {
-        String[] arr = sentence.split(" ");
-        for(int i=0; i<arr.length; i++ ){
+        String[] arr = sentence.split("\\s+");
+        for(int i=0; i<arr.length; i++) {
             String word = arr[i];
-            String num = word.substring(1);//next word check karyla
-            if(word.length() > 1 &&  word.charAt(0)=='$' && isValid(num)) {
+            char dollar = word.charAt(0);
+            String num = word.substring(1);
+            if( word.length() > 1 && dollar == '$' && isValid(num)) {
                 arr[i] = String.format("$%.2f", Long.parseLong(num) * (1 - discount/100d));
             }
         }
-    
+        
         return String.join(" ", arr);
-            
     }
 }
