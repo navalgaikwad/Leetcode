e328b786d2ee6c975dd81e4bc920993c62614b3a
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-24 15:44:18 -0400

    Time: 134 ms (78.71%), Space: 41.2 MB (90.59%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,30 +2,28 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i=0; i<board.length; i++) {
             for(int j=0; j<board[0].length; j++) {
-                if(board[i][j] == word.charAt(0)) {
-                    if(dfs(board, word, i, j, 0)) return true;
+                if(board[i][j] == word.charAt(0) && helper(board, word, i, j, 0)) {
+                    return true;
                 }
             }
         }
         return false;
     }
     
-    boolean dfs(char[][] board, String word, int i, int j, int index) {
-        if(index == word.length()) {
+    boolean helper(char[][] board, String word, int i, int j, int index) {
+        if(word.length() == index) {
             return true;
         }
-        if( i < 0 ||  i >=board.length || j < 0 || j >=board[0].length || board[i][j] != word.charAt(index) || board[i][j] == '$') {
+        if( i < 0 || i >= board.length || j < 0 || j >=board[0].length || board[i][j] == '$' || word.charAt(index)!= board[i][j]) {
             return false;
         }
         char temp = board[i][j];
         board[i][j] = '$';
-        boolean bottom = dfs(board, word, i+1, j, index+1);
-        boolean right = dfs(board, word, i, j+1, index+1);
-        boolean top = dfs(board, word, i-1, j, index+1);
-        boolean left = dfs(board, word, i, j-1, index+1);
+        boolean left = helper(board, word, i + 1, j, index  +1);
+        boolean right = helper(board, word, i, j + 1, index  +1);
+        boolean up = helper(board, word, i - 1, j, index  +1);
+        boolean down = helper(board, word, i , j - 1, index  +1);
         board[i][j] = temp;
-        return left || right || top || bottom;
+        return left || right || up || down;
     }
 }
-//Therefore, the overall time complexity is O(N * M * 4^L).
-//O(L)
