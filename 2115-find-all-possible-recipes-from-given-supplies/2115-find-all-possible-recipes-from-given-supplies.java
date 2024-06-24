82a238e14a40e173fc71b65dcf833fdb1e90a6dc
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-24 19:54:59 -0400

    Time: 458 ms (11.85%), Space: 46.4 MB (71.10%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,36 +1,34 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        Set<String> set = new HashSet<>();
-        for(String supplie: supplies) {
+        HashSet<String> set = new HashSet<>();
+        for(String supplie:supplies) {
             set.add(supplie);
         }
-        List<String> result = new ArrayList<>();
         boolean found = true;
+        List<String> result = new ArrayList<>();
         while(found) {
             found = false;
             for(int i=0; i<recipes.length; i++) {
-                if(set.contains(recipes[i])) {//check set in recipes
+                if(set.contains(recipes[i])) { //it will start checking for bread which we have already added
                     continue;
                 }
-                List<String>ingredient =  ingredients.get(i);
+                
+                List<String> ingredient = ingredients.get(i);
                 boolean flag = true;
-                for(String ingred :ingredient) {
-                    if(!set.contains(ingred)) {//check set in ingredident
+                for(String ingred:ingredient) {
+                    if(!set.contains(ingred)) {//not important
                         flag = false;
                         break;
                     }
                 }
                 if(flag) {
                     found = true;
-                    result.add(recipes[i]);
                     set.add(recipes[i]);
-                    
+                    result.add(recipes[i]);
                 }
             }
-            
         }
-        return result; 
+        return result;
     }
 }
-//check karnar suplies ahet ka recipes madhye
-//mg ingrendent ghe mg check set mahdye item ahet ka, nahi ahet atr add noko karus
+
