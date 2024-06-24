1c754c9ee5d2bf78e04ba74b93ff92a3e3d0e805
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-24 18:48:33 -0400

    Time: 0 ms (100.00%), Space: 41.4 MB (25.80%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
@@ -1,36 +1,37 @@
 class Solution {
     public String tictactoe(int[][] moves) {
         int n = 3;
-        int[] row = new int[n];
-        int[] col = new int[n];
+        int[] rowArray = new int[n];
+        int[] colArray = new int[n];
         
-        int d1 = 0;
-        int d2 = 0;
+        int diagonal = 0;
+        int diagonalCross = 0;
         int player = 1;
         for(int[] move : moves) {
-            int r = move[0];
-            int c = move[1];
+            int row = move[0];
+            int col = move[1];
             
-            row[r]+=player;
-            col[c]+=player;
+            rowArray[row]+= player;
+            colArray[col]+=player;
             
-            if(r == c) {
-                d1+=player;
+            if(row == col) {
+                diagonal+=player;
             }
-            if(r + c == n - 1) {
-                d2+=player;
+            if(row + col == n - 1) {
+                diagonalCross+=player;
             }
-            if(Math.abs(row[r]) == n || Math.abs(col[c]) == n ||  Math.abs(d1) == n || Math.abs(d2) == n ) {
+            
+            if(Math.abs(rowArray[row] )== n || Math.abs(colArray[col]) == n || Math.abs(diagonal) == n || Math.abs(diagonalCross) == n) {
                 if(player == 1) {
                     return "A";
                 }else {
                     return "B";
                 }
             }
-            
-            player*=-1;
+            player= player*-1;
         }
-        if(moves.length == n * n) {
+        
+        if(moves.length == n*n) {
             return "Draw";
         }else {
             return "Pending";
