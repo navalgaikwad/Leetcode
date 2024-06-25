24065d4bf03cac712b20494cb544b990f3e89a7a
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-24 20:31:53 -0400

    Time: 4 ms (12.14%), Space: 42.2 MB (44.92%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,27 +1,23 @@
-public class Solution {
+class Solution {
     public int maxFrequencyElements(int[] nums) {
-        Map<Integer, Integer> frequencies = new HashMap<>();
-        int maxFrequency = 0;
-        int totalFrequencies = 0;
-
-        // Find the frequency of each element
-        // Determine the maximum frequency
-        // Calculate the total frequencies of elements with the maximum frequency
-        for (int num : nums) {
-            frequencies.put(num, frequencies.getOrDefault(num, 0) + 1);
-            int frequency = frequencies.get(num);
-
-            // If we discover a higher frequency element
-            // Update maxFrequency
-            // Re-set totalFrequencies to the new max frequency
-            if (frequency > maxFrequency) {
-                maxFrequency = frequency;
-                totalFrequencies = frequency;
-                // If we find an element with the max frequency, add it to the total
-            } else if (frequency == maxFrequency) {
-                totalFrequencies += frequency;
+        Map<Integer, Integer> map = new HashMap<>();
+        int totalCount =0;
+        int max = 0;
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0)  + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        pq.addAll(map.values());
+        while(!pq.isEmpty()) {
+            int num = pq.remove();
+            
+            if(num > max) {
+                totalCount = num;
+                max = num;
+            }else if(max == num) {
+                totalCount+=num;
             }
         }
-        return totalFrequencies;
+        return totalCount;
     }
 }
