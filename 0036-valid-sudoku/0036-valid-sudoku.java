3d4398f419ead6b8eb1740836f62755f629b9459
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-25 13:47:39 -0400

    Time: 10 ms (34.88%), Space: 44.3 MB (63.19%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> row = new HashSet<>();
-        for(int i =0; i < board.length; i++) {
-            for(int j=0; j< board[0].length; j++) {
-                
-                 char value = board[i][j];
-                 int box = (i/3) + (j/3)*3;
-                
-                if(value != '.') {
-                    String r = value+"r"+i;
-                    String c = value+"c"+j;
-                    String boxi = value+"b"+box;
-                    if(row.contains(r) || row.contains(c) || row.contains(boxi)) {
+        Set<String> set  =new HashSet<>();
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
+                int value = board[i][j];
+                int box = (i/3) + (j/3) * 3;
+                if(value!='.') {
+                    String row= value+"r"+i;
+                    String col = value+"c"+j;
+                    String boxes = value+"b"+box;
+                    if(set.contains(row) || set.contains(col) || set.contains(boxes)) {
                         return false;
                     }
-                        
-                    row.add(c);
-                    row.add(r);
-                    row.add(boxi);
-                    
+                    set.add(row);
+                    set.add(col);
+                    set.add(boxes);
                 }
             }
         }
