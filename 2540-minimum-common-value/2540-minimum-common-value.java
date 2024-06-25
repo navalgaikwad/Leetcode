498847b5286bf7cb5c4bbd628da1e659324596e6
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-25 11:08:37 -0400

    Time: 1 ms (95.22%), Space: 62.5 MB (51.28%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,22 +1,16 @@
-public class Solution {
+class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int first = 0;
+        int first =0;
         int second = 0;
-
-        // Traverse through both arrays with two pointers
-        // Increment the pointer with a smaller value at that index
-        // Return the first common element found
-        while (first < nums1.length && second < nums2.length) {
-            if (nums1[first] < nums2[second]) {
+        while(first < nums1.length && second < nums2.length) {
+            if(nums1[first] < nums2[second]) {
                 first++;
-            } else if (nums1[first] > nums2[second]) {
+            }else if(nums1[first] > nums2[second]) {
                 second++;
-            } else {
+            }else {
                 return nums1[first];
             }
         }
-
-        // Return -1 if there are no common elements
         return -1;
     }
 }
