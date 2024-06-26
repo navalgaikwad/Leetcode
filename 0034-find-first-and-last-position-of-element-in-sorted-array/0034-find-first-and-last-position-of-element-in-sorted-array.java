9b2ef95c898b7758b166dd4be3d09cffe56fd89f
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-26 16:04:10 -0400

    Time: 0 ms (100.00%), Space: 45.7 MB (56.41%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,44 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int val1 = binarySearch1(nums, target);
-        int val2 = binarySearch2(nums, target);
-        return new int[]{val2, val1};
+         int start = firstPosition(nums, target, 0, nums.length - 1);
+         int end = LastPosition(nums, target, 0, nums.length - 1);
+       // System.out.print(end);
+        return new int[]{end, start};
     }
     
-    int binarySearch1(int[] nums, int target) {
-        int start =0;
-        int end = nums.length-1;
-         int position = -1;
+    int firstPosition(int[] nums, int target, int start, int end) {
+        int ans = -1;
         while(start <= end) {
             int mid = start + (end - start)/2;
-            if(nums[mid]==target) position = mid;
-            if(nums[mid] <= target) start = mid + 1;
-            if(nums[mid] > target) end = mid - 1; 
+            
+            if(nums[mid] == target) {
+                ans = mid;
+            }
+            if(nums[mid] <= target) {
+               // ans = start;
+                start = mid + 1;
+            }else {
+                end = mid - 1;
+            }
         }
-        return position;
+        return ans;
     }
-    int binarySearch2(int[] nums, int target) {
-        int start =0;
-        int end = nums.length-1;
-        int position = -1;
+    
+     int LastPosition(int[] nums, int target, int start, int end) {
+        int ans = -1;
         while(start <= end) {
             int mid = start + (end - start)/2;
-            if(nums[mid]==target) position = mid;
-            if(nums[mid] >= target) end = mid - 1;
-            if(nums[mid] < target) start = mid + 1; 
+            
+            if(nums[mid] == target) {
+                ans = mid;
+            }
+            if(nums[mid] >= target) {
+                //ans = start;
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
         }
-        return position;
+        return ans;
     }
 }
