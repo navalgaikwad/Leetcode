5a0062ea7d9b55aad866d6b96ddb9fa31161620b
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-27 11:38:33 -0400

    Time: 36 ms (97.32%), Space: 44.9 MB (72.10%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,40 +1,41 @@
 class Solution {
+    int m =0;
+    int n=0;
     public int minimumEffortPath(int[][] heights) {
-        int row = heights.length;
-        int col = heights[0].length;
-        int[][] dist = new int[row][col];
-        PriorityQueue<int[]> min = new PriorityQueue<>((a,b)->a[2] - b[2]);
-        min.add(new int[]{0,0,0});
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
-                dist[i][j] = Integer.MAX_VALUE;
+        m = heights.length;
+        n = heights[0].length;
+        int left =0;
+        int right=0;
+        for (int[] row: heights) {
+            for (int num: row) {
+                right = Math.max(right, num);
             }
         }
-        dist[0][0] = 0;
-        int[][] dirs= {{0,1}, {1,0}, {0,-1}, {-1, 0}};
-        while(!min.isEmpty()) {
-            int[] current = min.remove();
-            int x = current[0];
-            int y = current[1];
-            int level = current[2];
-           
-            if(x == row - 1 && y == col - 1) {
-                return level;
+        while(left<=right) {
+            int mid = left + (right - left)/2;
+            if(check(heights, mid, 0, 0, new boolean[m][n])) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
             }
-            for(int[] dir : dirs) {
-                int i = dir[0] + x;
-                int j = dir[1] + y;
-                if(i>=0 && i<row && j>=0 && j<col) {
-                    int effort = Math.max(level, Math.abs(heights[i][j]-heights[x][y]));
-                    if(effort < dist[i][j]) {
-                        dist[i][j] = effort;
-                        min.add(new int[]{i, j, effort});
-                    }
-                    
+        }
+        return left;
+    }
+    int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+    boolean check(int[][] heights, int mid, int i, int j, boolean[][] visited) {
+        if( i == m - 1 && j == n - 1) {
+            return true;
+        }
+        visited[i][j] = true;
+        for(int[] dir:dirs) {
+            int x = dir[0]+i;
+            int y = dir[1] + j;
+            if( x>=0 && x < m && y >=0 && y < n && !visited[x][y] && Math.abs(heights[i][j] - heights[x][y]) <= mid) {
+                if(check(heights, mid, x, y, visited)) {
+                    return true;
                 }
             }
         }
-        return -1;
+        return false;
     }
 }
-
