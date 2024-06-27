dfbdce20a19f9a7a90d17c81ff296663f545be34
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-06-26 20:06:48 -0400

    Time: 1 ms (99.21%), Space: 44 MB (83.72%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -6,22 +6,22 @@ class Solution {
         while(left<=right) {
             boolean visited[][] = new boolean[grid.length][grid.length];
             int mid = left + (right - left)/2;
-            if(dfs(grid, 0, 0, mid, visited)) {
+            if(dfs(grid, 0, 0, mid, visited)) {//true ass right = mid -1
                 right = mid - 1;
             }else {
-                left = mid + 1;
+                left = mid + 1; //left = mid + 1
             }
         }
         return left;
     }
     
-    boolean dfs(int[][] grid, int i, int j, int mid, boolean visited[][]) {
+    boolean dfs(int[][] grid, int i, int j, int mid, boolean visited[][]) {//less than mid
         
         int len = grid.length;
         if(i<0 || i>=len || j<0 || j>=grid[0].length || grid[i][j] > mid|| visited[i][j]) {
             return false;
         }
-        if(i == len - 1 && j ==len - 1) {
+        if(i == len - 1 && j ==len - 1) {//if i reach the end i == len -1 and j == len -1
             return true;
         }
        visited[i][j] = true;
