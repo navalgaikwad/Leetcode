a3338c0fa495ff032db83eb6471d3bf074aef1e9
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-07-29 20:50:17 -0400

    Time: 38 ms (77.90%), Space: 60.4 MB (26.52%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumBeauty(int[] nums, int k) {
+        Arrays.sort(nums);
+        int min = Integer.MIN_VALUE;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.length; ++right) {
+            // Keep increasing minimum threashold
+            min = Math.max(min, nums[right] - k);
+
+            while (nums[left] + k < min) {
+                // Reduce window size and bring left within
+                // acceptable range
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+}
