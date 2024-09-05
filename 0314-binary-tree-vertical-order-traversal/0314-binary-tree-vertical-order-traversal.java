065674ac68fcb2b007e37b950f25e59f0e4ea2bb
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-09-05 16:00:00 -0400

    Time: 4 ms (24.73%), Space: 42.7 MB (56.23%) - LeetPush

---
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, ArrayList<int[]>> map = new TreeMap<>();
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        dfs(root, 0, 0);
+        List<List<Integer>> result = new ArrayList<>();
+        for(ArrayList<int[]> li : map.values()) {
+            List<Integer> ans = new ArrayList<Integer>();
+            Collections.sort(li, (a,b)->a[0] - b[0]);
+            for(int[] l:li) {
+                ans.add(l[1]);
+            }
+            result.add(ans);
+        }
+        return result;
+    }
+    
+    private void dfs(TreeNode root, int dept, int index) {
+        if(root == null) {
+            return;
+        }
+        if(!map.containsKey(index)) {
+            map.put(index, new ArrayList<>());
+        }
+        map.get(index).add(new int[]{dept, root.val});
+        dfs(root.left, dept+1, index - 1);
+        dfs(root.right, dept+1, index + 1);
+    }
+}
