9d282414d0818dc8c9957188a9c3efc697bd8509
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-09-08 18:56:27 -0400

    Time: 30 ms (33.75%), Space: 44.8 MB (30.37%) - LeetPush

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -0,0 +1,26 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Set<Node> set = new HashSet<>();
+        while(p!=null) {
+            set.add(p);
+            p = p.parent;
+        }
+        while(q!=null) {
+            if(set.contains(q)) {
+                return q;
+            }
+            q = q.parent;
+        }
+        return null;
+    }
+}
