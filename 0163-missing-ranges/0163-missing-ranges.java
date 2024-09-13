5efe3fbee2a381c0f7aa9d21d95617bfa787b566
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-09-13 13:24:33 -0400

    Time: 0 ms (100.00%), Space: 41.3 MB (54.60%) - LeetPush

--- 0163-missing-ranges/0163-missing-ranges.java
+++ 0163-missing-ranges/0163-missing-ranges.java
@@ -1,26 +1,24 @@
 class Solution {
     public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
-        int n = nums.length;
-        List<List<Integer>> missingRanges = new ArrayList<>();
-        if(n == 0) {
-             missingRanges.add(Arrays.asList(lower, upper));
-            return missingRanges;
+        List<List<Integer>> missRange = new ArrayList<>();
+        if(nums.length == 0){
+            missRange.add(Arrays.asList(lower, upper));
+            return  missRange;
+        } 
+        if(lower < nums[0] ) {
+            missRange.add(Arrays.asList(lower, nums[0]- 1));
         }
-        if(lower < nums[0]) {
-            
-            missingRanges.add(Arrays.asList(lower, nums[0] - 1));
+        int n = nums.length;
+        for(int i = 0; i < n-1; i++) {
+            if(nums[i + 1] - nums[i]>=2){
+                missRange.add(Arrays.asList(nums[i] + 1, nums[i+1] - 1));
+            }
         }
         
-        for(int i=0; i<n -1; i++) {
-            if(nums[i + 1] - nums[i] <= 1) {
-                continue;
-            }
-            
-            missingRanges.add(Arrays.asList(nums[i] + 1, nums[i + 1] - 1));
+        
+        if(nums[n - 1] < upper) {
+            missRange.add(Arrays.asList(nums[n - 1] + 1, upper ));
         }
-        if(upper > nums[n-1]) {
-            missingRanges.add(Arrays.asList(nums[n-1] + 1, upper));
-        }
-        return missingRanges;
+        return missRange;
     }
 }
