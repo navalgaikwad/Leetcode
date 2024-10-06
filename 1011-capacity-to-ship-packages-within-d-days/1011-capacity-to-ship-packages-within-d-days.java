5da6b4959bfab8eb10bcc4251a9363fca64444fa
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-06 19:19:11 -0400

    Time: 14 ms (12.83%), Space: 47.3 MB (29.58%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,26 +1,25 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-           int left = Arrays.stream(weights).max().getAsInt();//10
-           int right = Arrays.stream(weights).sum();//sum
+        int left = Arrays.stream(weights).max().getAsInt();
+        int right = Arrays.stream(weights).sum();
         int ans =0;
         while(left <= right) {
             int mid = left + (right - left)/2;
-            if(check(weights, days, mid)) {
+            if(helper(weights, mid, days)) {
                 ans = mid;
-                right = mid -1;
-                
+                right = mid - 1;
             }else {
-                left = mid + 1;
+                left = mid + 1; 
             }
         }
         return ans;
     }
     
-    boolean check(int[] weights, int days, int mid) {
+    boolean helper(int[] weights, int mid, int days) {
         int sum =0;
-        int count=1;
+        int count =1;
         for(int weight : weights) {
-            sum+=weight;
+            sum += weight;
             if(sum > mid) {
                 count++;
                 sum = weight;
