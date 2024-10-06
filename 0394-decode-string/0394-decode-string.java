6686df8e5216d628f722756bd657bf61698c01de
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-06 18:05:29 -0400

    Time: 4 ms (19.28%), Space: 41.7 MB (27.00%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,42 +1,36 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<String> characterStack = new Stack<>();
         Stack<Integer> numberStack = new Stack<>();
-        int i =0;
-        int number = 0;
-        String ans = "";
-        String result = "";
-        for(;i < s.length(); i++) {
+        Stack<String> aplhaStack = new Stack<>();
+        
+        int len = s.length();
+        String val ="";
+        for(int i=0; i<len; i++) {
             char c = s.charAt(i);
-            
             if(Character.isDigit(c)) {
-                while( i < s.length() && Character.isDigit(s.charAt(i))) {
-                    number = (number * 10) + s.charAt(i) - '0';
+                int number =0;
+                while(Character.isDigit(s.charAt(i)) && i < len) {
+                    number =number *10 +( s.charAt(i) - '0');
                     i++;
                 }
-                i--;
                 numberStack.push(number);
-               
+                i--;
             }else if(c == '[') {
-                characterStack.push(ans);
-                ans = "";
-                number = 0;
+                aplhaStack.push(val);
+                val = "";
             }else if(c == ']') {
-                StringBuilder sb = new StringBuilder(characterStack.pop());
-                int value = numberStack.pop();
-                while(value > 0) {
-                    sb.append(ans);
-                    value--;
+                StringBuilder sb = new StringBuilder(aplhaStack.pop());
+                int num = numberStack.pop();
+                while(num > 0) {
+                    sb.append(val);
+                    num--;
                 }
-                ans =sb.toString();
+                
+                val=sb.toString();
             }else {
-                ans+=s.charAt(i);
-                //i++;
+                val+=c;
             }
-            
         }
-       // System.out.print(ans);
-        return ans;
+        return val;
     }
 }
-
