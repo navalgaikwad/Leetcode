c9cd741089b4d9df03ef2d9d92c6d1099c4eee57
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-17 01:44:32 -0400

    Time: 17 ms (93.30%), Space: 76.7 MB (20.22%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,28 +1,28 @@
 class Solution {
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
         ArrayList<Integer> adj[] = new ArrayList[n];
-        for(int i=0; i<n; i++) {
+        for(int i=0; i < n; i++) {
             adj[i] = new ArrayList<>();
         }
         for(int[] edge : edges) {
             adj[edge[0]].add(edge[1]);
             adj[edge[1]].add(edge[0]);
         }
-        return helper(adj, 0, new HashSet<>(), hasApple);
+        boolean[] visited = new boolean[n];
+        return helper(adj, 0, hasApple, visited);
     }
     
-    int helper(ArrayList<Integer> adj[], int parent, HashSet<Integer> visited,List<Boolean> hasApple) {
+    int helper(ArrayList<Integer> adj[], int parent, List<Boolean> hasApple, boolean[] visited) {
         int total = 0;
-        visited.add(parent);
-        for(Integer neighbour : adj[parent]) {
-            if(!visited.contains(neighbour)) {
-                int count = helper(adj, neighbour, visited, hasApple);
-                total = total + count;
-               
+        visited[parent] = true;
+        for(Integer neighbour: adj[parent]) {
+            if(!visited[neighbour]) {
+                int count = helper(adj, neighbour, hasApple, visited);
+                total +=count;
             }
         }
-         if(parent != 0 && (hasApple.get(parent) || total>0)) {
-            total = total +  2;
+        if(parent!=0 && (hasApple.get(parent) || total > 0)) {
+            total+=2;
         }
         return total;
     }
