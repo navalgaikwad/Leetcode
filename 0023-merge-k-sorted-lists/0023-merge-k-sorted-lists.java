c9d5f4174f3d9af5aca40ab9499c4c14ee4aff0c
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-18 17:49:25 -0400

    Time: 4 ms (76.22%), Space: 44.4 MB (53.31%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,29 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
-        
-        // Add the head of each list to the priority queue
-        for (ListNode list : lists) {
-            if (list != null) {
+        PriorityQueue<ListNode> pq =new PriorityQueue<>((a,b)->a.val - b.val);
+        for(ListNode list : lists) {
+            if(list!=null) {
                 pq.add(list);
             }
         }
         
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
-        
-        // Process the priority queue
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()) {
             ListNode node = pq.remove();
             current.next = node;
             current = current.next;
-            
-            // If there are more nodes in the current list, add the next one
-            if (node.next != null) {
+            if(node.next!=null) {
                 pq.add(node.next);
             }
         }
-        
         return dummy.next;
+        
     }
 }
