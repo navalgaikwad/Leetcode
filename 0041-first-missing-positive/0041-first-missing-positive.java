c792dd23137548e7a6541d279ffd9661d82ed997
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-18 15:12:36 -0400

    Time: 1 ms (100.00%), Space: 56.6 MB (84.46%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,18 +1,17 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        boolean[] seen = new boolean[n + 1];
-        for(int i=0; i<n; i++) {
-            if(nums[i] >0 && nums[i] <= n) {
+        int length = nums.length;
+        boolean seen[] = new boolean[length + 1];
+        for(int i=0; i<length; i++) {
+            if(nums[i] > 0 && nums[i] <= length) {
                 seen[nums[i]] = true;
             }
         }
-        
-        for(int i = 1; i <= n; i++) {
+        for(int i =1; i<=length; i++) {
             if(!seen[i]) {
                 return i;
             }
         }
-        return n + 1;
+        return length + 1;
     }
 }
