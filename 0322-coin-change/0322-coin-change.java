7847ab4b083cfa39612e6768bec4badd1bbc8d3f
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-19 16:07:14 -0400

    Time: 165 ms (5.13%), Space: 46.3 MB (5.13%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,31 +1,34 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-         Integer value = dp(coins, amount, new HashMap<>());
-        return value == null? -1 : value;
+        Integer value = dp(coins, amount, new HashMap<>());
+        return value == null ? -1 : value; 
     }
-    Integer dp(int[] coins, int target, Map<Integer, Integer> memo) {
-        if(memo.containsKey(target)) {
-            return memo.get(target);
-        }
-        if(target == 0) {
+    
+    Integer dp(int[] coins, int amount, HashMap<Integer, Integer> memo) {
+        if(amount == 0) {
             return 0;
+            
         }
-        if(target < 0) {
+        if(amount < 0) {
             return null;
         }
-        Integer maxCount = null;
+        if(memo.containsKey(amount)) {
+            return memo.get(amount);
+        }
+        Integer maxAmount = null;
         for(int coin : coins) {
-            int remainder = target - coin;
-            Integer count = dp(coins, remainder, memo);
-            
-            if(count!= null) {
-                int newCount = count + 1;
-                if(maxCount == null || newCount <  maxCount) {
-                    maxCount = newCount;
+            int remainder = amount - coin;
+            if(remainder <= amount) {
+                Integer count = dp(coins, remainder, memo);
+                if(count!=null) {
+                     int newCount = count + 1;
+                    if(maxAmount == null || newCount <= maxAmount) {
+                        maxAmount = newCount;
+                    }
                 }
             }
         }
-        memo.put(target, maxCount);
-        return maxCount;
+        memo.put(amount, maxAmount);
+        return maxAmount;
     }
 }
