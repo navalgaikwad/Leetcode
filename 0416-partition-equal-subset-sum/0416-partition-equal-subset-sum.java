803152e75a993e7f62da0ed45abcd74db49c7d46
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-20 13:34:30 -0400

    Time: 182 ms (6.09%), Space: 45.9 MB (72.20%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,53 +1,22 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-
-        int sum = 0;
-        for(int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-        }
-
-        if(sum %2 != 0) return false; //odd sum will not yield two partition
-
-        //target sum in each partition
+        int sum = Arrays.stream(nums).sum();
+        if(sum%2!=0) return false;
         int target = sum/2;
-
-        //initialize with zero
         Set<Integer> set = new HashSet<>();
         set.add(0);
-
-        for(int n : nums) {
-            Set<Integer> sumsToCurrentNum = new HashSet<>();
-
-            for(int s : set) {
-
-                if(s + n == target) return true;
-                if(s + n < target) sumsToCurrentNum.add(s+n);
+        for(int num : nums) {
+            Set<Integer> currentSum = new HashSet<>();
+            for(Integer s : set) {
+                if(num + s == target) {
+                    return true;
+                }
+                if(num + s < target) {
+                    currentSum.add(num+s);
+                }
             }
-
-            //collect the current sums to parent set
-            set.addAll(sumsToCurrentNum);
+            set.addAll(currentSum);
         }
-
         return false;
     }
 }
-    //find total sum
-    // if not even return false
-    //define dp of sum+1 of boolean type
-    //for from i to nums.lenght
-    //for from sum to nums[i] j--
-
-    // static boolean tabulation(int target, int[] num) {
-    //     boolean[] table = new boolean[target + 1];
-    //     table[0] = true;
-    //     for (int i = 0; i < target; i++) {  // Fix: Change i <= target to i < target
-    //         if (table[i]) {
-    //             for (int nu : num) {
-    //                 if (i + nu <= target) {
-    //                     table[i + nu] = true;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return table[target];
-    // }
