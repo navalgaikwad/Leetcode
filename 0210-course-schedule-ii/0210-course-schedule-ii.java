f436bc82941b3263e79fb85ae2ffc4b01f88587d
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-20 14:57:15 -0400

    Time: 3 ms (98.86%), Space: 46 MB (39.06%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,42 +1,58 @@
 class Solution {
-    Stack<Integer> ans = new Stack<>();
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList<Integer> adj[] = new ArrayList[numCourses];
-        for(int i=0; i<numCourses; i++) {
+       public int[] findOrder(int numCourses, int[][] pres) {
+         // if(numCourses>1&&pres.length==0){
+         //    int[] val= new int[numCourses];
+         //     val[0]=1;
+         //     val[1]=0;
+         //     return val;
+         // }
+        ArrayList<Integer>[]adj=new ArrayList[numCourses];
+        for(int i=0;i<numCourses; i++){
             adj[i] = new ArrayList<>();
         }
-        for(int[] prerequisite:prerequisites) {
-            adj[prerequisite[1]].add(prerequisite[0]);
+
+        for(int[] pre: pres){
+            adj[pre[1]].add(pre[0]);
         }
-        int[] visited = new int[numCourses];
-         for (int i=0; i< numCourses; i++){
-           if(!dfs(adj, i, visited)){
+
+        int[] ans = new int[numCourses];
+        int[] visited=new int[numCourses];
+        Stack<Integer> stack=new Stack<>();
+        for (int i=0; i< numCourses; i++){
+           if(!dfs(adj, stack, i, visited)){
                  return new int[]{};
            }
         }
-         int i = 0;
-        int[] result = new int[numCourses];
-        while (!ans.isEmpty()) {
-            result[i++] = ans.peek();
-            ans.pop();
+        
+        int i = 0;
+        while (!stack.isEmpty()) {
+            ans[i++] = stack.peek();
+            stack.pop();
         }
-        return result;
+        return ans;
     }
-    boolean dfs(ArrayList<Integer> adj[], int parent, int[] visited) {
-        if(visited[parent] == 1) {
+    
+  boolean dfs(ArrayList<Integer>[]adj,Stack<Integer> stack, int node, int[] visited){
+        if(visited[node]==1) {
             return false;
         }
-        if(visited[parent] == 2) {
+        if(visited[node]==2){
             return true;
         }
-        visited[parent] = 1;
-        for(Integer neighbour :adj[parent]) {
-            if(!dfs(adj, neighbour, visited)) {
-                return false;
-            }
+        visited[node]=1;
+
+        for(int neighbour: adj[node]){
+           
+                if (!dfs(adj, stack, neighbour, visited)){
+                    return false;
+                }
+            
         }
-        visited[parent] = 2;
-        ans.push(parent);
+        stack.push(node);
+        visited[node] = 2;
+
         return true;
+
     }
+    
 }
