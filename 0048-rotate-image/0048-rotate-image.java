ff4865d2ed82e6a4e21dfdbc00fda6633500c146
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-21 15:36:54 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (27.76%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,29 +2,33 @@ class Solution {
     public void rotate(int[][] matrix) {
         int len = matrix.length;
         for(int i=0; i<len/2; i++) {
-            int[] tmp = matrix[i];
+            int[] temp = matrix[i];
             matrix[i] = matrix[len - i - 1];
-            matrix[len - i - 1]= tmp;
+            matrix[len - i - 1] = temp;
         }
-        
-        for(int i=0; i<matrix.length; i++) {
-            for(int j = i + 1; j<matrix[0].length; j++) {
-                int tmp = matrix[j][i];
-                matrix[j][i] = matrix[i][j];
-                matrix[i][j] = tmp; 
+        for(int i =0; i<matrix.length; i++) {//i =0
+            for(int j =i+1; j<matrix[0].length; j++) {//j =i+1
+                int temp =matrix[j][i];
+                matrix[j][i] =  matrix[i][j];
+                matrix[i][j] = temp;
             }
         }
     }
 }
-//matrix = [[1,2,3],[4,5,6],[7,8,9]]
 
 /*
 [[1,2,3],
 [4,5,6],
 [7,8,9]]
 
-khalach warti ghe
-mg 0 ani 1 pasun start kar
+[7,8,9]
+[4,5,6]
+[1,2,3]
+
+
+[[7,4,1],
+[8,5,6],
+[9,2,3]]
 
 
 */
