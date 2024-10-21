419ee7377cea188eae0017ff7e8f3bd43dfaecf7
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-21 15:18:23 -0400

    Time: 1 ms (100.00%), Space: 44.5 MB (41.82%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,26 +10,25 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode original = new ListNode(0);
-        ListNode dummy = original;
-        
+        ListNode current = new ListNode(0);
+        ListNode dummy = current;
         int carry = 0;
-        while(l1!=null || l2!=null || carry != 0) {
+        while(l1 != null || l2!=null  || carry != 0) {
             int sum =0;
             if(l1!=null) {
-                sum = sum + l1.val;
+                sum+=l1.val;
                 l1 = l1.next;
             }
-            if(l2!=null) {
-                sum = sum + l2.val;
+            if(l2 != null) {
+                sum+=l2.val;
                 l2 = l2.next;
             }
             int total = sum + carry;
-            carry = total/10;
-            ListNode root = new ListNode(total%10);
-            dummy.next = root;
+            carry = total / 10;
+            ListNode node = new ListNode(total % 10);
+            dummy.next = node;
             dummy = dummy.next;
         }
-        return original.next;
+        return current.next;
     }
 }
