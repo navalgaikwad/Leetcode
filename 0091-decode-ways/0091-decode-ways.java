54824edce1d4921ff58a42d99bacebe3cc20b963
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-22 13:20:43 -0400

    Time: 2 ms (19.72%), Space: 41.7 MB (50.19%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -2,7 +2,6 @@ class Solution {
     public int numDecodings(String s) {
         return helper(s, 0, new HashMap<>());
     }
-    
     int helper(String s, int start, HashMap<Integer, Integer> memo) {
         if(start == s.length()) {
             return 1;
@@ -14,8 +13,9 @@ class Solution {
             return memo.get(start);
         }
         int count = helper(s, start + 1, memo);
-        if(start + 2 <= s.length() && Integer.parseInt(s.substring(start, start + 2)) <= 26) {
+        if(start + 2 <= s.length() && Integer.parseInt(s.substring(start, start + 2)) <=26) {
             count+= helper(s, start + 2, memo);
+            
         }
         memo.put(start, count);
         return count;
