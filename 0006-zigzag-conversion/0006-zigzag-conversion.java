0f7ec6de44c0923be6d17473a4d17c3c034cdef2
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-22 14:49:04 -0400

    Time: 5 ms (69.34%), Space: 45.4 MB (37.41%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -13,7 +13,7 @@ class Solution {
             for(int i =0; i<numRows&& j < len; i++) {
                 arr[i].add(s.charAt(j++));
             }
-            for(int i = numRows-2; i>=1&& j < len; i--) {
+            for(int i = numRows-2; i>=1&& j < len; i--) {//numrow -2 && i>=1
                 arr[i].add(s.charAt(j++));
             }
         }
