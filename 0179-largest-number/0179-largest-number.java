85af2b26f3c527db9943d79d7712760e3ab26847
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-22 11:56:23 -0400

    Time: 6 ms (52.51%), Space: 44.1 MB (18.43%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,35 +1,28 @@
 class Solution {
-    private class LargerNumberComparator implements Comparator<String> {
+    class LargestNumber implements Comparator<String> {
         @Override
         public int compare(String a, String b) {
             String order1 = a + b;
             String order2 = b + a;
-           return order2.compareTo(order1);
+            return order2.compareTo(order1);
         }
     }
-
     public String largestNumber(int[] nums) {
-        // Get input integers as strings.
-        String[] asStrs = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            asStrs[i] = String.valueOf(nums[i]);
+        String[] arr = new String[nums.length];
+        for(int i=0; i<nums.length; i++) {
+            arr[i] = String.valueOf(nums[i]);
         }
-
-        // Sort strings according to custom comparator.
-        Arrays.sort(asStrs, new LargerNumberComparator());
-
-        // If, after being sorted, the largest number is `0`, the entire number
-        // is zero.
-        if (asStrs[0].equals("0")) {
+        Arrays.sort(arr, new LargestNumber());
+        
+        if(arr[0].equals("0")) {
             return "0";
         }
-
-        // Build largest number from sorted array.
         String largestNumberStr = new String();
-        for (String numAsStr : asStrs) {
+        for (String numAsStr : arr) {
             largestNumberStr += numAsStr;
         }
 
         return largestNumberStr;
+        
     }
 }
