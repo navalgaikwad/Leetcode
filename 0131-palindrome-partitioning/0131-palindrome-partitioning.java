42ff7ae6b06cc0eb1182dbc5c4f8dbaf8fea9adc
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-22 12:07:08 -0400

    Time: 7 ms (98.48%), Space: 57.3 MB (14.68%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,35 +1,38 @@
 class Solution {
-    List<List<String>> result = new ArrayList<>();
     public List<List<String>> partition(String s) {
-        helper(s, new ArrayList<>(), 0);
+        List<List<String>> result = new ArrayList<>();
+        Boolean[][] memo = new Boolean[s.length()][s.length()];
+        
+        helper(0, s, result, new ArrayList<>(), memo);
         return result;
     }
     
-    void helper(String s, List<String> ans, int index) {
-        if(s.length() == index) {
-            result.add(new ArrayList<>(ans));
+    void helper(int start, String s, List<List<String>> result, List<String> runningList, Boolean[][] memo) {
+        if(start >= s.length()) {
+            result.add(new ArrayList<>(runningList));
             return;
         }
-        
-        for(int i = index; i <s.length(); i++) {
-            if(isValid(s, index, i)) {
-                ans.add(s.substring(index, i+1));
-                helper(s, ans, i + 1);
-                ans.remove(ans.size()-1);
+        for(int i = start; i<s.length(); i++) {
+            if(palindrome(start, i, s, memo)) {
+                runningList.add(s.substring(start, i + 1));
+                helper(i + 1, s, result, runningList, memo);
+                runningList.remove(runningList.size() - 1);
             }
         }
     }
     
-    
-    
-    boolean isValid(String s, int start, int end) {
-        while(start <= end) {
-            if(s.charAt(start) != s.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
+    boolean palindrome(int start, int end, String s, Boolean[][] memo) {
+        if(start >= end) {
+            return true;
         }
-        return true;
+        if(memo[start][end]!=null) {
+            return memo[start][end];
+        }
+        if(s.charAt(start)==s.charAt(end)) {
+            memo[start][end] = palindrome(start + 1, end - 1, s, memo);
+            return memo[start][end];
+        }
+        memo[start][end] = false;
+        return false;
     }
 }
