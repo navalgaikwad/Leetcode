0cb17d01688227e3d75acf2fbe60d81eebd45077
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-22 12:20:05 -0400

    Time: 1138 ms (8.65%), Space: 67.1 MB (35.84%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -4,46 +4,19 @@ class Solution {
         for(int num : nums) {
             set.add(num);
         }
-    
-         int max =0;
-         int largestStreak = 0;
-        
-        for(int i =0; i<nums.length; i++) {
-            if(!set.contains(nums[i] - 1)) {
-                //set.add(num[i]);
-                int currentStreak = 1;
-                int value = nums[i] + 1;
-                while(set.contains(value)) {
-                    value = value + 1;
-                    currentStreak++;
+        int max = 0;
+        //set.add(0);
+        for(int i= 0; i< nums.length; i++) {
+            if(!set.contains(nums[i] - 1)) {//not in set
+                int count = 1;
+                int number = nums[i] + 1;
+                while(set.contains(number)) {
+                    count++;
+                    number = number + 1;
                 }
-                 largestStreak = Math.max(largestStreak, currentStreak); 
+                max = Math.max(max, count);
             }
-          
         }
-        return largestStreak;
+        return max;
     }
 }
-
-// class Solution {
-//     public int longestConsecutive(int[] nums) {
-//         Set<Integer> set = new HashSet<>();
-//         for(int num : nums) {
-//             set.add(num);
-//         }
-//         int largestStreak = 0;
-//         for(int i=0; i<nums.length; i++) {
-//             int num = nums[i];
-//             if(!set.contains(num - 1)) {
-//                 int currentStreak = 1;
-//                 int nextNo = num;
-//                 while(set.contains(nextNo+1)) {
-//                     nextNo++;
-//                     currentStreak++;
-//                 }
-//                 largestStreak = Math.max(largestStreak, currentStreak);
-//             }
-//         }
-//         return largestStreak;
-//     }
-// }
