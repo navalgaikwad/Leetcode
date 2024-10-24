91596695758914f6513068a7c97bafde75cd8002
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-24 10:47:56 -0400

    Time: 22 ms (22.89%), Space: 52.4 MB (80.19%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,33 +1,17 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
-        int n = time.length;
-        int count = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        //just like two sum
-        for (int t : time) {
-            int remainder = t % 60;
-            int complement = (60 - remainder) % 60;
-
-            if (map.containsKey(complement)) {
-                count += map.get(complement);
-            }
-
-            map.put(remainder, map.getOrDefault(remainder, 0) + 1);
+        int n = time.length, ans = 0;
+        HashMap<Integer, Integer> len = new HashMap<>();
+        for(int i  = 0; i < n; i++){
+            int rem = time[i] % 60;
+            int target = 60 - (rem);
+            if(len.containsKey(target))
+                ans += len.get(target);
+            if(rem != 0)
+                len.put(rem, len.getOrDefault(rem, 0) + 1);
+            else
+                len.put(60, len.getOrDefault(60, 0) + 1);
         }
-
-        return count;
+        return ans;
     }
 }
-// 1st element (t = 30):
-// remainder = 30 % 60 = 30
-// complement = (60 - 30) % 60 = 30
-// map = {30: 1}
-// 2nd element (t = 20):
-// remainder = 20 % 60 = 20
-// complement = (60 - 20) % 60 = 40
-// No complement found in the map.
-// map = {30: 1, 20: 1}
-// 3rd element (t = 150):
-// remainder = 150 % 60 = 30
-// complement = (60 - 30) % 60 = 30
-// Complement found in the map for remainder = 30.
