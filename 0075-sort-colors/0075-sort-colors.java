cd78b40f8a5403a9392fe29a3768646292557d40
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-24 13:16:23 -0400

    Time: 0 ms (100.00%), Space: 41.8 MB (48.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,20 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
-       Map<Integer, Integer> map = new HashMap<>();
-        for(int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        int i =0;
-        for(Integer key :map.keySet()) {
-            int value = map.get(key);
-            while(value > 0) {
-                nums[i++] = key;
-                value--;
+        int p1 =0;
+        int p2 =nums.length - 1;
+        for(int i =0; i<=p2; i++) {
+            if(nums[i] == 0) {
+                nums[i] =  nums[p1];
+                nums[p1] = 0;
+                p1++;
+            }else if(nums[i] == 2) {
+                nums[i] = nums[p2];
+                nums[p2] = 2;
+                p2--;
+                i--;
             }
         }
     }
 }
-
-//[2,0,2,1,1,0]
-//[0,        2]
-//[0,0,]
