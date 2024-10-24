b2566c1411d3ce3e4da8585a84f980e27188cf9c
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-24 11:55:17 -0400

    Time: 22 ms (22.89%), Space: 52.4 MB (84.36%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,17 +1,39 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
-        int n = time.length, ans = 0;
-        HashMap<Integer, Integer> len = new HashMap<>();
-        for(int i  = 0; i < n; i++){
-            int rem = time[i] % 60;
-            int target = 60 - (rem);
-            if(len.containsKey(target))
-                ans += len.get(target);
-            if(rem != 0)
-                len.put(rem, len.getOrDefault(rem, 0) + 1);
-            else
-                len.put(60, len.getOrDefault(60, 0) + 1);
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < time.length; i++) {
+            int value = time[i] % 60;
+            int complement = (60 - value) % 60; 
+            
+            // If complement exists in the map, it means we found a valid pair
+            if (map.containsKey(complement)) {
+                ans += map.get(complement);
+            }
+            
+            // Store the count of the current remainder in the map
+            map.put(value, map.getOrDefault(value, 0) + 1);
         }
+        
         return ans;
     }
 }
+
+/*
+30 %60 = 30
+60 -30 = 30
+30, 1
+20 %60 = 20
+60-20 = 40
+40, 1
+150%60 = 30 
+60 -30 =30
+ans =1
+100 %60 = 40
+60 - 40 =20
+20, 1
+40
+
+
+*/
