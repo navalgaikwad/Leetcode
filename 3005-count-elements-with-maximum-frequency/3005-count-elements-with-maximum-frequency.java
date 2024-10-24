7fb743507cd75e0cf83a7f59504cd27f86b579a8
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-24 14:14:42 -0400

    Time: 1 ms (99.58%), Space: 42.6 MB (9.92%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,26 +1,17 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        //Map<Integer, Integer> map = new HashMap<>();
+        int max =0;
         int[] map = new int[101];
-        int totalCount =0;
-        int max = 0;
         for(int num : nums) {
             map[num]++;
+            max = Math.max(max, map[num]);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
-        for(int num : map) {
-            pq.add(num);
-        }
-        while(!pq.isEmpty()) {
-            int num = pq.remove();
-            
-            if(num > max) {
-                totalCount = num;
-                max = num;
-            }else if(max == num) {
-                totalCount+=num;
+        int ans =0;
+        for(Integer value: map) {
+            if(max == value) {
+                ans+=value;
             }
         }
-        return totalCount;
+        return ans;
     }
 }
