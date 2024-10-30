2bd8159ae6f9b543a40705cb176113016cf6e9f1
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-30 16:47:51 -0400

    Time: 7 ms (32.45%), Space: 44.9 MB (62.31%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,30 +1,24 @@
 class Solution {
     public String convert(String s, int numRows) {
-          if (numRows == 1 || numRows >= s.length()) {
-            return s;
-        }
-        int len = s.length();
-        ArrayList<Character> arr[] = new ArrayList[len];
-        for(int i=0; i<len; i++) {
-            arr[i] = new ArrayList<>();
+        ArrayList<Character>list[] = new ArrayList[numRows];
+        for(int i=0; i<numRows; i++) {
+            list[i] = new ArrayList<>();
         }
         int j =0;
-        while(j < len) {
-            for(int i =0; i<numRows&& j < len; i++) {
-                arr[i].add(s.charAt(j++));
+        for(int i =0; i<s.length(); i++) {
+            for(int k =0; k<numRows && j < s.length(); k++) {
+                list[k].add(s.charAt(j++));
             }
-            for(int i = numRows-2; i>=1&& j < len; i--) {//numrow -2 && i>=1
-                arr[i].add(s.charAt(j++));
+            for(int k =numRows-2; k>=1 && j < s.length(); k--) {
+                list[k].add(s.charAt(j++));
             }
         }
-        
         StringBuilder sb = new StringBuilder();
-        for (int k = 0; k < arr.length; k++) {
-            for (char c : arr[k]) {
-                sb.append(c);
+        for(ArrayList<Character> li : list) {
+            for(Character val: li) {
+                sb.append(val);
             }
         }
-        
         return sb.toString();
     }
 }
