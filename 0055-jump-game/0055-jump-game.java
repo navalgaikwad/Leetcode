acfdd745809d0388dda7cad708863675dc452564
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-30 16:10:28 -0400

    Time: 1131 ms (5.02%), Space: 46 MB (14.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int len = nums.length+1;
-        int[] dp = new int[len];
+        int len = nums.length;
+        int[] dp = new int[len+1];
         Arrays.fill(dp, -1);
         dp[0] = 1;
         for(int i=0; i<nums.length; i++) {
             if(dp[i] != -1) {
-                for(int j=1; j<=nums[i]; j++) {
-                    int next = j + i;
+                for(int j =1; j<= nums[i]; j++) {
+                    int next = i + j;
                     if(next < len) {
-                        dp[next] = dp[i] + 1;
+                        dp[next] =  dp[i] + 1;
                     }
                 }
             }
