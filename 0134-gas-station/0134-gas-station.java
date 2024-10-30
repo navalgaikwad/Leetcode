a094713fc44253feab40b1d2f494d2c5f8333834
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-29 20:58:21 -0400

    Time: 2 ms (97.61%), Space: 56.2 MB (55.82%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,22 +1,21 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        //determine total which is -1 start from zero
-        //then by find start point
-        int total =0;
-        for(int i = 0; i < gas.length; i++) {
-            total+=gas[i] - cost[i];
+        int total = 0;
+        for(int i=0; i<gas.length; i++) {
+            total+=(gas[i] - cost[i]);
         }
-        if(total < 0){
+        if(total < 0) {
             return -1;
         }
         int start =0;
         int tank = 0;
-        for(int i = 0; i < gas.length; i++) {
-            tank+=gas[i] - cost[i];
-            if(tank < 0){
-                start = i+1;
+        for(int i=0; i<gas.length; i++) { 
+            tank+=(gas[i] - cost[i]);
+            if(tank < 0) {
                 tank =0;
+                start = i + 1;
             }
+            
         }
         return start;
     }
