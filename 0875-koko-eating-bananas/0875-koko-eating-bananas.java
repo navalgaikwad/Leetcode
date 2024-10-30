df69a49b4c9c9e6966a7892a54ab35984787e469
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-30 17:21:29 -0400

    Time: 28 ms (18.26%), Space: 46.1 MB (6.70%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -2,25 +2,21 @@ class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int left =1;
         int right = Arrays.stream(piles).max().getAsInt();
-        int ans = 0;
-        while(left <= right) {
+        while(left<=right) {
             int mid = left + (right - left)/2;
-            if(helper(piles, mid) <= h) {
-                ans = mid;
+            if(helper(piles, h, mid)) {
                 right = mid - 1;
             }else {
                 left = mid + 1;
             }
         }
-        return ans;
+        return left;
     }
-    
-    int helper(int[] piles, int mid) {
+    boolean helper(int[] piles, int h, int mid) {
         int sum =0;
-        int count =0;
-        for(int pile : piles) {
-           count += Math.ceil((double) pile / mid);
+        for(Integer plie : piles) {
+            sum+= Math.ceil((double) plie / mid);;
         }
-        return count;
+        return sum<=h;
     }
 }
