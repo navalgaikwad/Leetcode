97c907e110ba8926bd80ece150ba387dbd6875bb
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-30 14:19:51 -0400

    Time: 94 ms (58.99%), Space: 75.7 MB (85.87%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -1,14 +1,12 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->a[0] - b[0]);
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int count =0;
-        int endTime =0;
-        for(int[] interval:intervals) {
+        Arrays.sort(intervals, (a,b)->a[0] - b[0]);
+        PriorityQueue<Integer> pq =new PriorityQueue<>();
+        for(int[] interval :intervals) {
             if(!pq.isEmpty() && pq.peek() < interval[0]) {
                 pq.remove();
             }
-            pq.add( interval[1]);
+            pq.add(interval[1]);
         }
         return pq.size();
     }
