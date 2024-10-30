4d81c2cca6f65f6f2259fceaa3d5cde7d6141d03
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-29 20:42:51 -0400

    Time: 2 ms (51.76%), Space: 43.3 MB (30.29%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -4,17 +4,15 @@ class Solution {
         helper(n, 0, 0, "");
         return result;
     }
-    
     void helper(int n, int open, int close, String ans) {
-        if(2*n == open+close) {
+        if(2*n == open + close) {
             result.add(new String(ans));
             return;
         }
         if(open < n) {
             helper(n, open+1, close, ans+"(");
-        }
-        if(close < open) {
-            helper(n, open, close + 1, ans+")");
-        }
+        } if(close < open) {
+            helper(n, open, close+1, ans+")");
+        } 
     }
 }
