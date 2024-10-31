dc48973f01b34777db5b46e3280de88326cc90e4
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-30 21:55:42 -0400

    Time: 24 ms (84.69%), Space: 42.1 MB (87.74%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = Arrays.stream(nums).sum();
-        if(sum%2!=0) return false;
-        int target = sum/2;
-        Set<Integer> set = new HashSet<>();
-        set.add(0);
-        for(int num : nums) {
-            Set<Integer> currentSum = new HashSet<>();
-            for(Integer s : set) {
-                if(num + s == target) {
-                    return true;
-                }
-                if(num + s < target) {
-                    currentSum.add(num+s);
-                }
-            }
-            set.addAll(currentSum);
+        
+        // If the total sum is odd, it cannot be divided into two equal subsets
+        if (sum % 2 != 0) {
+            return false;
         }
-        return false;
+        
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true; // Base case: a subset with sum 0 is always possible
+
+        for (int num : nums) {
+            // Iterate backwards to prevent overwriting dp values in the same iteration
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        
+        return dp[target];
     }
 }
