ca8c1cab03da3e7b77b56567affd3d20177e414a
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-30 22:57:03 -0400

    Time: 114 ms (17.85%), Space: 45.2 MB (36.76%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,34 +2,31 @@ class Solution {
     public String minWindow(String s, String t) {
         int left =0;
         Map<Character, Integer> map = new HashMap<>();
-        for(char c: t.toCharArray()) {
+        for(char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
-            
         }
-        int count =map.size();
-        int len = Integer.MAX_VALUE;
+        int size = map.size();
+        int min = Integer.MAX_VALUE;
         String result = "";
-        for(int right =0; right < s.length(); right++) {
-            char rightSide = s.charAt(right);
-            if(map.containsKey(rightSide)) {
-                map.put(rightSide, map.getOrDefault(rightSide, 0) - 1);
-                if(map.getOrDefault(rightSide, 0) == 0) {
-                    count--;
+        for(int right =0; right<s.length(); right++) {
+            char rightChar = s.charAt(right);
+            if(map.containsKey(rightChar)) {
+                map.put(rightChar, map.getOrDefault(rightChar, 0) - 1);
+                if(map.get(rightChar) == 0) {
+                    size--;
+                 
                 }
-                
             }
-            
-            
-            while(count == 0) {
-                if(right - left + 1 < len) {
-                    len = right - left + 1;
-                    result = s.substring(left, right+1);
+            while(size == 0) {
+                if(right - left + 1 < min) {
+                    min = right - left + 1;
+                    result = s.substring(left, right + 1);
                 }
-                char leftSide = s.charAt(left);
-                if(map.containsKey(leftSide)) {
-                    map.put(leftSide, map.getOrDefault(leftSide, 0) + 1);
-                     if(map.get(leftSide) >0) {
-                        count++;
+                 char leftChar = s.charAt(left);
+                if(map.containsKey(leftChar)) {
+                    map.put(leftChar, map.getOrDefault(leftChar, 0) + 1);
+                    if(map.get(leftChar) > 0) {
+                        size++;
                     }
                 }
                 left++;
