faa9a4e82ec32bed7b833ba023e4ab8bec322909
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-10-30 23:35:40 -0400

    Time: 57 ms (25.71%), Space: 44.3 MB (38.97%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,16 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length + 1];
-        Arrays.fill(dp, 1);
-        
+         Arrays.fill(dp ,1);
         for(int i=0; i<nums.length; i++) {
-            for(int j=0; j< i; j++) {
+            
+            for(int j=0; j<i; j++) {
                 int count = dp[j] + 1;
-                if(nums[i] > nums[j] && count > dp[i]) {
+                if(nums[i]> nums[j] && dp[i] < count) {
                     dp[i] = count;
                 }
             }
         }
-        
         return Arrays.stream(dp).max().getAsInt();
     }
 }
