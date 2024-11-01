24b14fea79273823346b482d4ab571f66de7e21f
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-01 02:51:24 -0400

    Time: 0 ms (100.00%), Space: 40.8 MB (76.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-      int n = nums.length;
-      return dp(nums, n - 1, new HashMap<>());  
+        int len = nums.length - 1;
+        Integer[] memo = new Integer[nums.length + 1];
+        return helper(nums, nums.length - 1, memo);
     }
-    
-    int dp(int[] nums, int index, HashMap<Integer, Integer> memo) {
-        if(memo.containsKey(index)) {
-            return memo.get(index);
-        }
-        if(index == 0) {
-            return nums[0];
-        }
-        if(index == 1) {
-            return Math.max(nums[0], nums[1]);
-        }
-        int current = nums[index] + dp(nums, index - 2, memo);
-        int previous = dp(nums, index - 1, memo);
+    int helper(int[] nums, int i, Integer[] memo) {
+        if(i == 0) return nums[0];
+        if(i ==1) return Math.max(nums[0], nums[1]);
+        if(memo[i] !=null) return memo[i];
         
-        int result = Math.max(current, previous);
-        memo.put(index, result);
-        return result;
+        int currentToNextToNext = nums[i] + helper(nums, i - 2, memo);
+        int next =  helper(nums, i - 1, memo);
+        
+        int max = Math.max(currentToNextToNext, next);
+        memo[i] = max;
+        return max;
     }
 }
