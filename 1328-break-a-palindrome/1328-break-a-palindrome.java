a04b5c9810aa3d785e9a49911d26431a4387831c
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-01 15:11:39 -0400

    Time: 0 ms (100.00%), Space: 41.3 MB (82.23%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,19 +1,18 @@
 class Solution {
     public String breakPalindrome(String palindrome) {
         if(palindrome.length() <= 1) return "";
-        char[] palindromeArrray = palindrome.toCharArray();
-        int left = 0;
-        int right = palindrome.length()-1;
+        int left =0, right = palindrome.length() - 1;
+        char[] c = palindrome.toCharArray();
         while(left < right) {
-            if(palindromeArrray[left]!='a') {
-                palindromeArrray[left] = 'a';
-                return String.valueOf(palindromeArrray);
+            if(c[left] != 'a') {
+                c[left] = 'a';
+                return String.valueOf(c);
             }else {
                 left++;
                 right--;
             }
         }
-        palindromeArrray[palindrome.length()-1] = 'b';
-        return String.valueOf(palindromeArrray);
+        c[palindrome.length() - 1] = 'b';
+        return String.valueOf(c); 
     }
 }
