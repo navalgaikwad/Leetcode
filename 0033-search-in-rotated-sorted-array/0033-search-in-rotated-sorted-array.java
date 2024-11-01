e5dbd687984d053f7196829910614da5cf1a88a0
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-01 14:52:39 -0400

    Time: 0 ms (100.00%), Space: 42 MB (41.19%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,12 +1,8 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return helper(nums, target);
-    }
-    int helper(int[] nums, int target) {
-        int left =0;
-        int right = nums.length-1;
+        int left =0, right = nums.length - 1;
         while(left <= right) {
-            int mid = left + (right -left)/2;
+            int mid = left + (right - left)/2;
             if(nums[mid] == target) {
                 return mid;
             }
@@ -19,7 +15,6 @@ class Solution {
             }else if(nums[right] >= nums[mid]) {
                 if(nums[right] >= target && nums[mid] < target) {
                     left = mid + 1;
-                    
                 }else {
                     right = mid - 1;
                 }
@@ -27,4 +22,5 @@ class Solution {
         }
         return -1;
     }
+    
 }
