33da9a6a185baf52d9d612c30404e2c22afda0df
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-02 15:48:04 -0400

    Time: 7 ms (65.55%), Space: 45.2 MB (76.17%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,24 +1,18 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         List<List<Integer>> ans = new ArrayList<>();
-        
-        // A map from group size to the list of indices that are there in the group.
-        Map<Integer, List<Integer>> szToGroup = new HashMap<>();
-        for (int i = 0; i < groupSizes.length; i++) {
-            if (!szToGroup.containsKey(groupSizes[i])) {
-                szToGroup.put(groupSizes[i], new ArrayList<>());
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for(int i=0; i< groupSizes.length; i++) {
+            if(!map.containsKey(groupSizes[i])) {
+                map.put(groupSizes[i], new ArrayList<>());
             }
-            
-            List<Integer> group = szToGroup.get(groupSizes[i]);
-            group.add(i);
-
-            // When the list size equals the group size, empty it and store it in the answer.
-            if (group.size() == groupSizes[i]) {
-                ans.add(group);
-                szToGroup.remove(groupSizes[i]);
+            ArrayList<Integer> list = map.get(groupSizes[i]);
+            list.add(i);
+            if(list.size()== groupSizes[i]) {
+                ans.add(list);
+                map.remove(groupSizes[i]);
             }
         }
-
         return ans;
     }
 }
