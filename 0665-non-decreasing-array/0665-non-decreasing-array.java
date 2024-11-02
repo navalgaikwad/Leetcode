10568089ef32b664f458687bc40cf0178c34fa1e
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-02 17:50:57 -0400

    Time: 0 ms (100.00%), Space: 45.2 MB (40.00%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.java
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
-        int cnt = 0;                                                                    //the number of changes
-        for(int i = 1; i < nums.length && cnt<=1 ; i++){
-            if(nums[i-1] > nums[i]){
-                cnt++;
-                if(i-2<0 || nums[i-2] <= nums[i])nums[i-1] = nums[i];                    //modify nums[i-1] of a priority
-                else nums[i] = nums[i-1];                                                //have to modify nums[i]
+        int count = 0;
+        for(int i=1; i<nums.length && count<=1; i++) {
+            if(nums[i-1] > nums[i]) {
+                count++;
+                if(i - 2 < 0 || nums[i - 2] <= nums[i]) {
+                    nums[i-1] = nums[i];
+                }else {
+                    nums[i] = nums[i-1];
+                }
             }
         }
-        return cnt<=1; 
+        return count<=1;
     }
 }
