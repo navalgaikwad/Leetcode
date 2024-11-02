b680a2e402db22ce888e1170222d878924153f03
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-02 18:45:43 -0400

    Time: 4 ms (90.25%), Space: 45.1 MB (62.00%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -2,8 +2,8 @@ class Solution {
     public String reverseWords(String s) {
         char[] arr = s.toCharArray();
         int lastSpace = -1;
-        int len = s.length();
-        for(int i =0; i<= s.length(); i++) {
+        int len = s.length();//no -1 vvimp
+        for(int i =0; i<= s.length(); i++) {//vimp i<=s.lenght
             if(i == len || arr[i] == ' ') {
                 int start = lastSpace + 1;
                 int end = i - 1;
