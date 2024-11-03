384885971d60b5c0e8aaca272cfb89e26ceff168
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-02 21:10:45 -0400

    Time: 27 ms (25.15%), Space: 46.3 MB (47.59%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,38 +1,39 @@
 class Solution {
     public int calculate(String s) {
-       // Stack<Character> stSign = new Stack<>();
-        Stack<Integer> stNumber = new Stack<>(); //integer array
+        Stack<Integer> stack = new Stack<>();
         char sign = '+';
-        int number =0;
-        for(int i=0; i<s.length(); i++) {
-            if(Character.isDigit(s.charAt(i))) {
-                
-                //while(Character.isDigit(s.charAt(i)) && i < s.length()) {
-                    number =number*10 +( s.charAt(i) - '0');
-                    //i++;
-               // }
-               
-            }
+        int number = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if((!Character.isWhitespace(c)&& !Character.isDigit(c))|| i == s.length() -1) {
-                if(sign == '+') {
-                    stNumber.push(number);
-                }else if(sign == '-') {
-                    stNumber.push(-number);
-                }else if(sign == '*') {
-                    stNumber.push(stNumber.pop() * number);
-                }else if(sign == '/') {
-                    stNumber.push(stNumber.pop() / number);
-                }
-                sign = c;
-                number = 0;
+
+            // Accumulate the number if it's a digit
+            if (Character.isDigit(c)) {
+                number = number * 10 + (c - '0');
             }
+            
+            // Process the accumulated number when encountering an operator or at the end of the string
+            if ((!Character.isDigit(c) && !Character.isWhitespace(c)) || i == s.length() - 1) {
+                if (sign == '+') {
+                    stack.push(number);
+                } else if (sign == '-') {
+                    stack.push(-number);
+                } else if (sign == '*') {
+                    stack.push(stack.pop() * number);
+                } else if (sign == '/') {
+                    stack.push(stack.pop() / number);
+                }
+                sign = c; // Update the sign to the current operator
+                number = 0; // Reset number for the next round
+            }
+        }
+
+        // Sum up all values in the stack for the final result
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
         }
         
-        int result = 0;
-        while(!stNumber.isEmpty()) {
-            result+=stNumber.pop();
-        }
         return result;
     }
 }
