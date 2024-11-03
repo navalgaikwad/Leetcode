554d56a39b0dfd7372deab79fea2169e2eda104d
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-03 15:51:32 -0500

    Time: 447 ms (10.63%), Space: 46.2 MB (88.32%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,34 +1,33 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        HashSet<String> set = new HashSet<>();
-        for(String supplie:supplies) {
+        HashSet<String>set = new HashSet<>();
+       
+        for(String supplie: supplies){
             set.add(supplie);
         }
-        boolean found = true;
         List<String> result = new ArrayList<>();
+        boolean found =true;
         while(found) {
-            found = false;
-            for(int i=0; i<recipes.length; i++) {
-                if(set.contains(recipes[i])) { //it will start checking for bread which we have already added
-                    continue;
-                }
+            found =false;
+            for(int i=0;i<recipes.length; i++){
+                if(set.contains(recipes[i])) continue; // already presnet in the set
                 
-                List<String> ingredient = ingredients.get(i);
-                boolean flag = true;
-                for(String ingred:ingredient) {
-                    if(!set.contains(ingred)) {//not important
+              List<String>ingredient = ingredients.get(i);
+                boolean flag =true;
+                for(String ingred: ingredient){
+                    if(!set.contains(ingred)){
                         flag = false;
                         break;
                     }
                 }
-                if(flag) {
-                    found = true;
-                    set.add(recipes[i]);
-                    result.add(recipes[i]);
+                if(flag){
+                   found=true;
+                   result.add(recipes[i]);
+                   set.add(recipes[i]);
                 }
             }
         }
-        return result;
+        return result;   
     }
 }
-
+//run loop on receopeos use i 
