be656cf63d045cc566cea44cc4797364f99afe24
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-03 00:53:22 -0400

    Time: 15 ms (77.70%), Space: 44.7 MB (39.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        long[] dp = new long[amount + 1];
-        Arrays.fill(dp, Long.MAX_VALUE); // Filling the array with max value instead of -1
+        long[] dp = new long[amount+1];
+        Arrays.fill(dp, Long.MAX_VALUE);
         dp[0] = 0;
-
-        for (int i = 0; i <= amount; i++) {
-            if (dp[i] != Long.MAX_VALUE) {
-                for (int coin : coins) {
-                    if (i <= amount - coin) { // Check for potential overflow
-                        dp[i + coin] = Math.min(dp[i + coin], dp[i] + 1);
+        for(int i=0; i<=amount; i++) {
+            if(dp[i]!=Long.MAX_VALUE) {
+                for(int coin : coins) {
+                    if( i <= amount - coin) {
+                        dp[i + coin] = Math.min(dp[i+coin], dp[i] + 1);
                     }
                 }
             }
         }
-
         return dp[amount] > amount ? -1 : (int) dp[amount];
     }
 }
