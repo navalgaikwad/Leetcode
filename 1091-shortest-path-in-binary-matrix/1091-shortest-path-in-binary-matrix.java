58899397285381d15f3bd77eb6ee3c8675f8e1b1
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-03 18:12:49 -0500

    Time: 117 ms (5.11%), Space: 45.6 MB (33.74%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,29 +1,31 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-         if (grid[0][0] != 0) return -1;
+        int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}, {-1,-1}, {1, 1}, {-1, 1}, {1, -1}};
+         if (grid[0][0] == 1) {
+            return -1; 
+        }
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, 1});
+        HashSet<String> visited = new HashSet<>();
+        //visited.add("0-0");
         int m = grid.length;
         int n = grid[0].length;
-       // int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1},{1,1},{-1,-1},{-1, 1},{1,-1}};
-        int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1},{1,-1}, {-1,1},{1,1},{-1,-1}};
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[m][n];
-        visited[0][0]= true;
-        q.add(new int[]{0, 0, 1});
         while(!q.isEmpty()) {
+            int size = q.size();
+            
             int[] current = q.remove();
             int i = current[0];
             int j = current[1];
             int level = current[2];
-            
-            if(i == m-1 && j == n-1) {
+            if(i == m - 1 && j == n - 1) {
                 return level;
             }
-            for(int[] dir : dirs) {
+            for(int[] dir :dirs ) {
                 int x = i + dir[0];
                 int y = j + dir[1];
-                if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==0 && !visited[x][y]) {
+                if(x >=0 && x<m && y>=0 && y<n && grid[x][y] !=1 && !visited.contains(x+"-"+y)) {
+                    visited.add(x+"-"+y);
                     q.add(new int[]{x, y, level+1});
-                    visited[x][y]= true;
                 }
             }
         }
