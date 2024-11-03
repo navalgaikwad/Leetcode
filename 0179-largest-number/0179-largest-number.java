8c1a844e38c17ae68dac32f9e0d9071a098d58ab
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-02 21:21:37 -0400

    Time: 6 ms (54.48%), Space: 44.5 MB (9.42%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,28 +1,27 @@
 class Solution {
-    class LargestNumber implements Comparator<String> {
+    class LargestNo implements Comparator<String> {
+        
         @Override
         public int compare(String a, String b) {
-            String order1 = a + b;
-            String order2 = b + a;
-            return order2.compareTo(order1);
+            String val1 = a+ b;
+            String val2 = b + a;
+            return val2.compareTo(val1);
         }
     }
     public String largestNumber(int[] nums) {
-        String[] arr = new String[nums.length];
-        for(int i=0; i<nums.length; i++) {
-            arr[i] = String.valueOf(nums[i]);
+          String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
         }
-        Arrays.sort(arr, new LargestNumber());
-        
-        if(arr[0].equals("0")) {
+        Arrays.sort(asStrs, new LargestNo());
+        if(asStrs[0].equals("0")) {
             return "0";
         }
         String largestNumberStr = new String();
-        for (String numAsStr : arr) {
+        for (String numAsStr : asStrs) {
             largestNumberStr += numAsStr;
         }
 
         return largestNumberStr;
-        
     }
 }
