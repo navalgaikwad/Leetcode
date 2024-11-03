016df6033f208d57d9a00a33a38eaeda34aed4e2
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-03 13:56:55 -0500

    Time: 60 ms (66.20%), Space: 118.7 MB (15.12%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
@@ -1,31 +1,31 @@
 class Solution {
     long result = 0;
     public long minimumFuelCost(int[][] roads, int seats) {
-        int length = roads.length;
-        ArrayList<Integer>adj[] = new ArrayList[length + 1];
-        for(int i=0; i<=length; i++) {
+        int len = roads.length;
+        ArrayList<Integer> adj[] = new ArrayList[len+1];
+        for(int i=0; i<=len; i++) {
             adj[i] = new ArrayList<>();
         }
         for(int[] road : roads) {
             adj[road[0]].add(road[1]);
             adj[road[1]].add(road[0]);
         }
-        boolean[] visited = new boolean[length+1];
-        findNumberOfFeulRequired(adj, 0, visited, seats);
+        boolean[] visited = new boolean[len+1];
+        helper(adj, 0, visited, seats);
         return result;
     }
     
-    int findNumberOfFeulRequired(ArrayList<Integer>adj[], int parent, boolean[] visited, int seats) {
+    int helper(ArrayList<Integer> adj[], int parent, boolean[] visited, int seats) {
         visited[parent] = true;
         int total = 1;
         for(Integer neighbour : adj[parent]) {
             if(!visited[neighbour]) {
-                total+=findNumberOfFeulRequired(adj, neighbour, visited, seats);
+                total+=helper(adj, neighbour, visited, seats);
             }
         }
-        if(parent!=0) {
-            result += (long)Math.ceil((double)(total)/(double)(seats));
-        }
+         if(parent != 0) {
+            result += Math.ceil((double)total/ (double)seats); 
+         }
         return total;
     }
 }
