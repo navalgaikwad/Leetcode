16a154e4fcaf43706f5c3781500b7b9b5eaf2abe
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-03 16:10:03 -0500

    Time: 1 ms (71.25%), Space: 41.9 MB (88.23%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,3 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
@@ -25,6 +40,6 @@ class Solution {
             res.add(level);
             zigzag = !zigzag;
         }
-        return res;
+      return res;
     }
 }
