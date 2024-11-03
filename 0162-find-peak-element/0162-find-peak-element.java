082d80b49894475b0ec586b39c5aa74b1c1a4987
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-02 23:56:04 -0400

    Time: 0 ms (100.00%), Space: 42.5 MB (32.53%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -20,10 +20,10 @@ class Solution {
            if(nums[mid -1] < nums[mid] && nums[mid] > nums[mid + 1]) {
                return mid;
            }
-           if(nums[mid] < nums[mid + 1]) {
-               start = mid + 1;
+           if(nums[mid] > nums[mid + 1]) {
+               end = mid - 1;     
            }else  {
-               end = mid - 1;   
+               start = mid + 1;   
            }
        }
         return start;
