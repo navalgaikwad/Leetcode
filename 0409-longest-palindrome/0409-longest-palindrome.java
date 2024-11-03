7d754b66adee6646b726b33611cfc9f276f26f0c
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-02 20:23:36 -0400

    Time: 2 ms (81.58%), Space: 41.5 MB (86.54%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,17 +1,17 @@
 class Solution {
     public int longestPalindrome(String s) {
         int oddNo = -1;
-        int[] counts = new int[128];
+        int[] count = new int[128];
+        int ans =0;
         for(int i=0; i<s.length(); i++) {
-            counts[s.charAt(i)]++;
+            count[s.charAt(i)]++;
         }
-        for(int count: counts) {
-            if(count%2!=0) {
+        for(int i =0; i<128; i++) {
+            if(count[i]%2!=0) {
                 oddNo++;
             }
         }
-        return oddNo> 0? s.length() - oddNo : s.length();
+        return oddNo>0? s.length() - oddNo : s.length();
     }
 }
-//abccccdd
-//odd - len = 
+//128
