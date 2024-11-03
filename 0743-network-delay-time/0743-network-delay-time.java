34298fd949d2efd14ffba3f7e0a45b97dd5a8de3
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-03 02:47:13 -0500

    Time: 24 ms (25.72%), Space: 46.7 MB (95.89%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,55 +1,69 @@
 class Solution {
-    class Pair {
+      class Pair{
         int dest;
         int weight;
-        Pair(int dest, int weight) {
-            this.dest = dest;
+        Pair(int weight, int dest){
+            this.dest =dest;
             this.weight = weight;
+        }
+
+        public int getDest() {
+            return dest;
+        }
+
+        public int getWeight() {
+            return weight;
         }
     }
     Map<Integer, List<Pair>> adj = new HashMap<>();
     public int networkDelayTime(int[][] times, int n, int k) {
-        for(int[] time: times) {
-            int src = time[0];
+        for(int[] time: times){
+            int source = time[0];
             int dest = time[1];
-            int weight = time[2];
-            adj.putIfAbsent(src, new ArrayList<>());
-            adj.get(src).add(new Pair(dest, weight));
+            int timeT =time[2];
+
+            adj.putIfAbsent(source, new ArrayList<>());
+            adj.get(source).add(new Pair(timeT, dest));
         }
-        int[] signaltravel = new int[n+1];
-        // int[] signalReceivedAt = new int[n + 1];
-        Arrays.fill(signaltravel, Integer.MAX_VALUE);
-        bfs(adj, k, signaltravel);
+        int[] signalReceivedAt = new int[n + 1];
+        Arrays.fill(signalReceivedAt, Integer.MAX_VALUE);
+        dijkstra(signalReceivedAt, k, n);
 
         int answer = Integer.MIN_VALUE;
         for (int i = 1; i <= n; i++) {
-            answer = Math.max(answer, signaltravel[i]);
+            answer = Math.max(answer, signalReceivedAt[i]);
         }
 
         // INT_MAX signifies atleat one node is unreachable
         return answer == Integer.MAX_VALUE ? -1 : answer;
-        
+
     }
-    
-    void bfs(Map<Integer, List<Pair>> adj, int k, int[] signaltravel) {
-        //PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparing(Pair::Pair.weight));
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(pair -> pair.weight));
-        pq.add(new Pair(k, 0));
-        signaltravel[k] = 0;
-        while(!pq.isEmpty()) {
-            Pair p = pq.remove();
-            int src = p.dest;
-            int cost = p.weight;
-            
-            if(!adj.containsKey(src)) {
+
+    private void dijkstra(int[] signalReceivedAt, int source, int n) {
+
+        PriorityQueue<Pair>pq=new PriorityQueue<Pair>(Comparator.comparing(Pair::getWeight));
+
+        pq.add(new Pair(0, source));
+        signalReceivedAt[source]=0;
+
+        while(!pq.isEmpty()){
+            Pair pair=pq.remove();
+            int cuurNode=pair.getDest();
+            int currTime=pair.weight;
+
+         
+            //if not present in adj list skip it
+            if(!adj.containsKey(cuurNode)){
                 continue;
             }
-            for(Pair neigbhour : adj.get(src)) {
-                int dest = neigbhour.dest;
-                int weight = neigbhour.weight;
-                if(cost + weight < signaltravel[dest]) {
-                    signaltravel[dest] = cost + weight;
-                    pq.add(new Pair( dest, cost + weight));
+
+            for(Pair edge : adj.get(cuurNode)){
+                int time = edge.weight;
+                int neighborNode = edge.getDest();
+
+                if(currTime + time<signalReceivedAt[neighborNode] ){
+                    signalReceivedAt[neighborNode] = currTime + time;
+                    pq.add(new Pair(signalReceivedAt[neighborNode], neighborNode));
                 }
             }
         }
