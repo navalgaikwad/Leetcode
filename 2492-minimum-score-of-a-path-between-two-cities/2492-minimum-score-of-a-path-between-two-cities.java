4ed077721f2a87837e614211ac7f1481b9dadd1d
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-03 03:38:50 -0500

    Time: 28 ms (75.93%), Space: 120.3 MB (33.33%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,30 +1,36 @@
+import java.util.ArrayList;
+
 class Solution {
     public int minScore(int n, int[][] roads) {
         ArrayList<int[]> adj[] = new ArrayList[n+1];
-        for(int i=1; i <=n; i++) {
+        for(int i=1; i<=n; i++) {
             adj[i] = new ArrayList<>();
         }
         for(int[] road : roads) {
             adj[road[0]].add(new int[]{road[1], road[2]});
             adj[road[1]].add(new int[]{road[0], road[2]});
         }
-        Queue<Integer> q = new LinkedList<>();
-        q.add(1);
         
-        int min = Integer.MAX_VALUE;
         boolean[] visited = new boolean[n+1];
         visited[1] = true;
-        while(!q.isEmpty()) {
-            int current = q.remove();
-            for(int[] neighbour : adj[current]) {
-                int dst = neighbour[0];
-                min = Math.min(min, neighbour[1]);
-                if(!visited[dst]) {
-                    visited[dst] = true;
-                    q.add(dst);
-                }
+        return dfs(adj, 1, visited);
+    }
+    
+
+    int dfs(ArrayList<int[]> adj[], int parent, boolean[] visited) {
+        int total = Integer.MAX_VALUE;
+
+        for(int[] neighbours : adj[parent]) {
+            int neighbour = neighbours[0];
+            int cost = neighbours[1];
+             total = Math.min(total, cost);
+            if (!visited[neighbour]) {
+                visited[neighbour] = true; 
+                int min = dfs(adj, neighbour, visited);
+                total = Math.min(total, min); 
             }
-        }    
-        return min;
+        }
+       
+        return total;
     }
 }
