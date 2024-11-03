bae9fb30b5f9d033c69086bda91d28e7a4abf135
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-03 17:42:19 -0500

    Time: 4 ms (23.65%), Space: 47 MB (79.10%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,42 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
+   
+     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
-     ArrayList<Integer> [] adj =new ArrayList[n];
+        //At first we are creating our adjacency list from given adjacency matrix;
+        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
         for(int i=0;i<n;i++){
-            adj[i]=new ArrayList<>();
+            adj.add(new ArrayList<>());
         }
+
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j] == 1){
-                    adj[i].add(j);
-                    adj[j].add(i);
+                if(isConnected[i][j]==1 && i!=j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
                 }
             }
         }
-        int count = 0;
-        boolean[] visited = new boolean[n+1];
-        for(int i=0; i<n; i++) {
-            if(!visited[i]) {
-                helper(adj, i, visited);
+        
+
+
+        int visited[]=new int[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
                 count++;
+                dfs(i,adj,visited);
             }
         }
         return count;
-        
     }
-        
-    void helper(ArrayList<Integer> [] adj, int parent, boolean[] visited) {
-        visited[parent] = true;
-        for(Integer neighbour: adj[parent]) {
-            if(!visited[neighbour]) {
-                helper(adj, neighbour, visited);
+
+    public void dfs(int i,ArrayList<ArrayList<Integer>>adj,int visited[]){
+        visited[i]=1;
+        for(int e:adj.get(i)){
+            if(visited[e]==0){
+                dfs(e,adj,visited);
             }
         }
     }
+    
 }
