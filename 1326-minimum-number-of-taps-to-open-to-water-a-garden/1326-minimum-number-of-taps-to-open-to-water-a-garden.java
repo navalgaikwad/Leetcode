d020801c499281330dd4e9c48d2c840b053169fd
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-15 21:09:37 -0500

    Time: 6 ms (48.28%), Space: 44.4 MB (54.74%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -5,7 +5,7 @@ class Solution {
         dp[0] = 0;
         for(int i =0;i<=n; i++) {
             
-            int left = Math.max(0, i - ranges[i]);//check in left side
+            int left = Math.max(0, i - ranges[i]);//check in left side 
             int right = Math.min(n, i + ranges[i]);//check in right side
             
             for(int start =left; start<=right; start++) {//go from left to right
