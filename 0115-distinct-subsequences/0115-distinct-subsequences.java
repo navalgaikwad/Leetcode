42360a69bfbc0f3b969ee015e8f7a4de3cb61994
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-15 20:46:38 -0500

    Time: 17 ms (77.31%), Space: 41.4 MB (96.04%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numDistinct(String S, String T) {
+        int sl = S.length();
+	int tl = T.length();
+	
+	int [] preComb = new int[sl+1];
+	int [] comb = new int[sl+1];
+	
+	
+	for(int i=0; i<=sl; i++)
+		preComb[i] = 1;		
+
+	for(int t=1; t<=tl; ++t){
+		for(int s=1; s<=sl; ++s){
+			if(T.charAt(t-1) != S.charAt(s-1)){
+				comb[s] = comb[s-1];
+			}else{
+				comb[s] = comb[s-1] + preComb[s-1];
+			}
+		}
+		
+		for(int i=0; i<=sl; ++i){
+			preComb[i] = comb[i];
+		}
+	}
+	
+	return preComb[sl];
+    }
+}
+
