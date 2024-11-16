71ca1e94613406b925559c2f2647039ca44bb5f2
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-15 19:44:34 -0500

    Time: 2 ms (6.04%), Space: 42.1 MB (13.18%) - LeetPush

---
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> runningList = new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        while(root != null) {
+            runningList = new ArrayList<>();
+            TreeNode value = dfs(root);
+            if(value == null) {
+                root = null;
+            }
+            result.add(new ArrayList<>(runningList));
+        }
+        
+        System.out.print(runningList);
+        return result;
+    }
+    
+    public TreeNode dfs(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        if(root.left == null && root.right == null) {
+            runningList.add(root.val);
+            return null;
+        }
+        root.left = dfs(root.left);
+        root.right = dfs(root.right);
+        
+       
+        return root;
+    }
+}
