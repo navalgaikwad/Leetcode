822735fa718bf7e58c9922b3befc0edd84e43d44
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-15 19:09:12 -0500

    Time: 44 ms (67.61%), Space: 48.4 MB (22.72%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
@@ -1,48 +1,20 @@
 class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
         Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i + minSize - 1 < s.length(); i++) {
-            String subString = s.substring(i, i + minSize);
-            map.put(subString, map.getOrDefault(subString, 0) + 1);
+        for(int i=0; i + minSize -1 < s.length(); i++) {
+            map.put(s.substring(i, i + minSize), map.getOrDefault(s.substring(i, i + minSize), 0) + 1);
         }
-        int res =0;
-        for (Map.Entry<String,Integer> entry : map.entrySet())  {
+        int result = 0;
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
             Set<Character> set = new HashSet<>();
-            for(char c :entry.getKey().toCharArray()) {
+            for(char c : entry.getKey().toCharArray()) {
                 set.add(c);
             }
-            if(set.size() <=maxLetters) {
-                //res = Math.max(res,e.getValue());
-                res = Math.max(res, entry.getValue());
+            if(set.size() <= maxLetters) {
+                result = Math.max(result, entry.getValue());
             }
-            
         }
-        return res;
+       // System.out.print(map);
+        return result;
     }
 }
-
-// class Solution {
-//     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
-//         Map<String, Integer> map = new HashMap();
-        
-//         int res = 0;
-        
-//         for (int i = 0; i + minSize - 1 < s.length(); i++) {
-//             String subs = s.substring(i, i + minSize);
-//             map.put(subs, map.getOrDefault(subs, 0) + 1);
-//         }
-        
-//         for (Map.Entry<String, Integer> e : map.entrySet()) {
-//             Set<Character> chrs = new HashSet();
-//             for (char c : e.getKey().toCharArray()) {
-//                 chrs.add(c);
-//             }
-            
-//             if (chrs.size() <= maxLetters) {
-//                 res = Math.max(res, e.getValue());
-//             }
-//         }
-        
-//         return res;
-//     }
-// }
