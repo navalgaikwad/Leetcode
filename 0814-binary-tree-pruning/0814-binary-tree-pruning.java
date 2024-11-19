327abf78184022b82b92e4850582dd85c6c043f4
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-19 18:55:49 -0500

    Time: 0 ms (100.00%), Space: 41.1 MB (53.27%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.java
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.java
@@ -15,18 +15,10 @@
  */
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
-        return helper(root);
-    }
-    
-    TreeNode helper(TreeNode root) {
-        if(root == null) {
-            return null;
-        }
-        //TreeNode newRoot = new TreeNode(root.val);
-        root.left = helper(root.left);
-        root.right = helper(root.right);
-        
-         if(root.left == null && root.val == 0 && root.right == null) {
+        if(root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if(root.val == 0 && root.left == null && root.right == null) {
             return null;
         }
         return root;
