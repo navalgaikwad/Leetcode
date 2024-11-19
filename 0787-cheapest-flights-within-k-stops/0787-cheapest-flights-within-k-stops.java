6f6e8ada40fee658405613286322e47085de571e
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-19 18:30:09 -0500

    Time: 5 ms (97.50%), Space: 44 MB (92.45%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,45 +1,54 @@
 class Solution {
-    class Pair {
-        int dest;
+    class Tuple{
+        int stop;
+        int src;
         int weight;
-        Pair(int dest, int weight) {
-            this.dest = dest;
-            this.weight = weight;
+        Tuple(int stop, int src, int weight){
+            this.stop = stop;
+            this.src = src;
+            this.weight=weight;
+        }
+    }
+    class Pair{
+        int dest;
+        int cost;
+        Pair(int dest, int cost){
+            this.dest=dest;
+            this.cost=cost;
         }
     }
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<Pair>adj[] = new ArrayList[n];
-         int[] dest = new int[n];
-        for(int i=0; i<n; i++) {
-            adj[i] = new ArrayList<>();
-            dest[i] = Integer.MAX_VALUE;
+        int dist[] =new int[n];
+        ArrayList<Pair>adj[] =new ArrayList[n];
+        for(int i=0;i<n;i++){
+            adj[i]=new ArrayList<>();
+            dist[i]=Integer.MAX_VALUE;
+           
         }
-        for(int[] flight: flights) {
-            adj[flight[0]].add(new Pair(flight[1], flight[2]));
+        for(int[] flight: flights){
+            adj[flight[0]].add(new Pair(flight[1], flight[2])); 
         }
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{src, 0, 0});
-        int result = -1;
-        while(!q.isEmpty()) {
-            int[] current = q.remove();
-            int sc = current[0];
-            int dist = current[1];
-            int stop = current[2];
-            if(stop > k) {
-                continue;
-            }
-            for(Pair neighbour : adj[sc]) {
-                int destination = neighbour.dest;
-                int weight = neighbour.weight;
-                if(dist+weight < dest[destination] && stop <=k) {
-                    dest[destination] = dist+weight;
-                    q.add(new int[]{destination, dist+weight, stop+1});
+        Queue<Tuple>q=new LinkedList<>();
+        q.add(new Tuple(0, src, 0));
+        dist[src]=0;
+        while(!q.isEmpty()){
+            Tuple t = q.remove();
+            int stop = t.stop;
+            int sc =t.src;
+            int weight =t.weight;
+            for(Pair p: adj[sc]){
+              int dest=  p.dest;
+              int cost=  p.cost;
+               
+                if(weight+cost<dist[dest]&& stop<=k){
+                    dist[dest]=weight+cost;
+                    q.add(new Tuple(stop+1,dest, weight+cost ));
                 }
             }
         }
-         if(dest[dst] == Integer.MAX_VALUE) {
+        if(dist[dst]==Integer.MAX_VALUE){
             return -1;
         }
-        return dest[dst];
+        return dist[dst];
     }
 }
