db730626e8be3ba7a007ec3d411481b63fa33265
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-19 16:57:57 -0500

    Time: 1 ms (96.55%), Space: 41.7 MB (49.71%) - LeetPush

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
@@ -1,26 +1,28 @@
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
         int i =0;
-        int j=0;
-        while(i < word.length() && j<abbr.length()) {
-            if(Character.isLetter(abbr.charAt(j))) {//check for character in abbr
+        int j =0;
+        while(i < word.length() && j < abbr.length()) {
+            int number = 0;
+            if(Character.isDigit(abbr.charAt(j))) {
+                while(j < abbr.length() &&Character.isDigit(abbr.charAt(j))) {
+                    number = number * 10 + (abbr.charAt(j) - '0');
+                     if(number == 0) return false;
+                    j++;
+                }
+                i+=number;
+            }else {
                 if(word.charAt(i)!=abbr.charAt(j)) {
                     return false;
                 }
                 i++;
                 j++;
-            }else {
-                //check for digit in word
-                int number =0;
-                while(j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
-                    number= number *10 + (abbr.charAt(j++) - '0');
-                    if(number == 0) return false;
-                }
-                i+=number;
             }
         }
-        
-        return i == word.length() && j == abbr.length();
-        
+        return i == word.length() && j == abbr.length();    
+      }
+                      
+                  
     }
-}
+
+                      
