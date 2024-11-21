a66f215a37140dc0aba7283c0f37b25b0339ec72
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-21 18:52:20 -0500

    Time: 0 ms (100.00%), Space: 45.9 MB (63.78%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,24 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         return dfs(root, key);
     }
+    
+    
     TreeNode dfs(TreeNode root, int key) {
-        if(root == null) {
-            return null;
-        }
-        if(root.val > key) {
-            root.left = dfs(root.left, key);
-        }else if(root.val < key){
-            root.right = dfs(root.right, key);
+        if(root == null) return null;
+        if(key < root.val) {
+            root.left =  dfs(root.left, key);
+        }else if(key > root.val) {
+            root.right= dfs(root.right, key);
         }else {
-            if(root.left != null && root.right==null && root.val == key) {
-                return root.left;
-            }else if(root.left == null && root.right!=null && root.val == key) {
+            if(root.left == null && root.right!=null && root.val == key) {
                 return root.right;
-            }else if(root.left == null && root.right==null && root.val == key) {
+            }else  if(root.left != null && root.right ==null && root.val == key) {
+                return root.left;
+            }else  if(root.left == null && root.right==null && root.val == key) {
                 return null;
-            }else if(root.left != null && root.right!=null && root.val == key) {
-                TreeNode temp = lastNode(root.right);
+            }else if(root.left!=null && root.right!=null && root.val == key){
+                TreeNode temp = helper(root.right);
                 root.val = temp.val;
                 root.right = dfs(root.right, temp.val);
             }
@@ -26,10 +41,11 @@ class Solution {
         return root;
     }
     
-    TreeNode lastNode(TreeNode node) {
-        while(node.left != null) {
-            node = node.left;
+    TreeNode helper(TreeNode root) {
+       
+        while(root.left!=null) {
+            root = root.left;
         }
-        return node;
+        return root;
     }
 }
