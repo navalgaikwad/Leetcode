0d2c5c63ffe14a917d4f3f63d22190bbe0ec38b6
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-21 18:03:40 -0500

    Time: 0 ms (100.00%), Space: 43.9 MB (97.51%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -14,22 +14,18 @@
  * }
  */
 class Solution {
-    
     public int rob(TreeNode root) {
-    int[] res = robSub(root);
-    return Math.max(res[0], res[1]);
-}
-
-private int[] robSub(TreeNode root) {
-    if (root == null) return new int[2];
+        int[] value = helper(root);
+        return Math.max(value[0], value[1]);
+    }
     
-    int[] left = robSub(root.left);
-    int[] right = robSub(root.right);
-    int[] res = new int[2];
-
-    res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);//max of both tree left and right 0: rob 1 not rob
-    res[1] = root.val + left[0] + right[0];//sum of root+left+right
-    
-    return res;
-}
+    int[] helper(TreeNode root) {
+        if(root == null) return new int[2];
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
+        int[] result = new int[2];
+        result[0] =Math.max(left[0], left[1])+ Math.max(right[0], right[1]);
+        result[1] = root.val + left[0] +  right[0];
+        return result;
+    }
 }
