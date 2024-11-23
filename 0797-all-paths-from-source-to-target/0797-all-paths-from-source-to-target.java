683de53a2603b941b57325903be38c426ae830ad
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-23 14:55:24 -0500

    Time: 3 ms (27.72%), Space: 46.3 MB (39.04%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -2,21 +2,19 @@ class Solution {
     List<List<Integer>>  result = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         ArrayList<Integer> runingList = new ArrayList<>();
-        runingList.add(0);
+        //runingList.add(0);
         dfs(graph, 0, graph.length-1, runingList);
         return result;
     }
     void dfs(int[][] graph, int strt, int dest, List<Integer> runningList) {
-       
+        runningList.add(strt);
         if(strt == dest) {
             result.add(new ArrayList<>(runningList));
             return;
         }
         
         for(int  neighbour:graph[strt]) {
-            runningList.add(neighbour);
-            dfs(graph, neighbour, dest, runningList);
-            runningList.remove(runningList.size()-1);
+            dfs(graph, neighbour, dest, new ArrayList<>(runningList));
         }
     }
 }
