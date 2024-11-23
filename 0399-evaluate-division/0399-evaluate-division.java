086127334bd6fd77dedeb279994d45061ce46dfa
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-23 11:23:21 -0500

    Time: 1 ms (98.90%), Space: 42.3 MB (20.98%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,52 +1,53 @@
 class Solution {
-    class Pair {
-        String st;
-        double weight;
-        Pair(String st, double weight) {
-            this.st = st;
-            this.weight = weight;
-        }
+        class Pair {
+            String c;
+            double val;
+            Pair(String c, double val) {
+                this.c = c;
+                this.val = val;
+            }
     }
-    
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        Map<String, List<Pair>> adj = new HashMap<>();
-        int index =0;
-        for(List<String> equation :equations) {
+        Map<String, ArrayList<Pair>> adj = new HashMap<>();
+        int j =0;
+        for(List<String>  equation: equations) {
             adj.putIfAbsent(equation.get(0), new ArrayList<>());
             adj.putIfAbsent(equation.get(1), new ArrayList<>());
-            adj.get(equation.get(0)).add(new Pair(equation.get(1), values[index]));
-            adj.get(equation.get(1)).add(new Pair(equation.get(0), 1/values[index]));
-            index++;
+            adj.get(equation.get(0)).add(new Pair(equation.get(1), values[j]));
+            adj.get(equation.get(1)).add(new Pair(equation.get(0), 1/values[j]));
+            j++;
         }
-        index = 0;
-        double[] result = new double[queries.size()];
-        for(List<String> querie : queries) {
-            if(!adj.containsKey(querie.get(0)) || !adj.containsKey(querie.get(1))) {
-                result[index] = -1;
-            }else {
-                double value = dfs(adj, querie.get(0), querie.get(1), new HashSet<>());
-                result[index] = value;
+       double[] result = new double[queries.size()];
+        for(int i=0; i<queries.size(); i++){
+          String node1 =  queries.get(i).get(0);
+          String node2 =  queries.get(i).get(1);
+            if(!adj.containsKey(node1) || !adj.containsKey(node2)) {
+                result[i] =-1.0;
+            } else {
+                double value = dfs(adj, node1, node2, new HashSet<>());
+                result[i] =value;
             }
-            index++;
+            
         }
         return result;
+       
     }
-    
-    
-    double dfs(Map<String, List<Pair>> adj, String start, String end, Set<String> visited) {
-        if(start.equals(end)) {
+    //use visited and total = -1
+    double dfs(Map<String, ArrayList<Pair>> adj, String src, String dest, HashSet<String> visited) {
+        visited.add(src);
+        if(src.equals(dest)) {
             return 1.0;
         }
         double total = -1;
-        visited.add(start);
-        for(Pair neighbour: adj.get(start)) {
-            if(!visited.contains(neighbour.st)) {
-                double value = dfs(adj, neighbour.st,  end, visited);
-                if(value != -1) {
-                    total = value * neighbour.weight;
-                } 
+        for(Pair  neighbour:adj.get(src)) {
+            if(!visited.contains(neighbour.c)) {
+                double value = dfs(adj, neighbour.c, dest, visited);
+                if(value != -1) { //vvimp
+                    total = neighbour.val * value;
+                }
             }
         }
         return total;
     }
+    
 }
