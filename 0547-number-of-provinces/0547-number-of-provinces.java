9e7496a0d0104f1350f22319bcd22fffe7068ed2
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-23 13:34:02 -0500

    Time: 3 ms (39.14%), Space: 47.4 MB (66.42%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,35 +1,34 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-     ArrayList<Integer> [] adj =new ArrayList[n];
-        for(int i=0;i<n;i++){
-            adj[i]=new ArrayList<>();
+        int n = isConnected.length;
+        ArrayList<Integer>adj[] = new ArrayList[n+1];
+        for(int i=0; i<=n; i++) {
+            adj[i] = new ArrayList<>();
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j] == 1){
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<isConnected[0].length; j++) {
+                if(isConnected[i][j] == 1) {
                     adj[i].add(j);
                     adj[j].add(i);
                 }
             }
         }
-        int count = 0;
-        boolean[] visited = new boolean[n+1];
+        boolean[] visited = new boolean[n];
+        int count =0;
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
-                helper(adj, i, visited);
+                dfs(adj, i, visited);
                 count++;
             }
         }
         return count;
-        
     }
-        
-    void helper(ArrayList<Integer> [] adj, int parent, boolean[] visited) {
-        visited[parent] = true;
-        for(Integer neighbour: adj[parent]) {
+    
+    void dfs(ArrayList<Integer>adj[], int start, boolean[] visited) {
+        visited[start] = true;
+        for(Integer neighbour: adj[start]) {
             if(!visited[neighbour]) {
-                helper(adj, neighbour, visited);
+                dfs(adj, neighbour, visited);
             }
         }
     }
