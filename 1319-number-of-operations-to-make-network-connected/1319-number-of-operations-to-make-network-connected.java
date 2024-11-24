8de8f98acdb1165b91fb63b3f094d42611389010
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-24 17:32:45 -0500

    Time: 12 ms (55.16%), Space: 72.4 MB (7.52%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,36 +1,38 @@
 class Solution {
-   
     public int makeConnected(int n, int[][] connections) {
-        if (connections.length < n - 1) {
+        if(n - 1 > connections.length) {
             return -1;
         }
-    ArrayList<Integer>adj[] = new ArrayList[n];
-        for(int i=0;i<n;i++){
-        adj[i]=new ArrayList<>();    
+        ArrayList<Integer>[] adj = new ArrayList[n]; //O(n)
+        for(int i =0; i<n;i++) {        //O(n)
+            adj[i] = new ArrayList<>(); // O(1)
         }
-        for(int[] connection:connections){
-            adj[connection[0]].add(connection[1]);
-            adj[connection[1]].add(connection[0]);
+        for(int[] con : connections) {//O() lenght of conenction
+            adj[con[0]].add(con[1]);
+            adj[con[1]].add(con[0]);
         }
-        int count=0;
-        boolean[] visited=new boolean[n];
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-               dfs(adj, i, visited);  
+        boolean[] visited = new boolean[n]; // O(n)
+        int count =0;//O(1)
+        for(int i = 0; i < n; i++) { //O(n)
+            if( !visited[i] ) {
+               dfs(adj, i, visited);
                 count++;
             }
-           
+            
         }
-        return count-1;
+        return count -1;
     }
+    //O(n*m) 
     
-    void dfs(ArrayList<Integer> adj[], int src, boolean[] visited){
-        visited[src]=true;
-        for(Integer neighbour: adj[src]){
-            if(!visited[neighbour]){
-                dfs(adj, neighbour, visited );
+    void dfs(ArrayList<Integer>[] adj, int i, boolean[] visited) {
+        if(visited[i]) {
+            return;
+        }
+        visited[i] =true;
+        for(int neighbours: adj[i]) {
+            if(!visited[neighbours]) {
+               dfs(adj, neighbours, visited); 
             }
         }
     }
-    
 }
