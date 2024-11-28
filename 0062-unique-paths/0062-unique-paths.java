3bbf24949288a330592c59518347764ac547e29a
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-28 15:27:02 -0500

    Time: 7 ms (8.98%), Space: 42.2 MB (7.45%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,53 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        //return helper(m-1, n - 1, new HashMap<>());
-        int[][] dp = new int[m+1][n+1];
-        dp[0][0] = 1;
-        for(int i=0; i<m; i++) {
-            for(int j =0; j<n; j++) {
-                if(i-1 >=0) {
-                  dp[i][j] += dp[i-1][j];
-                }
-                 if(j-1 >=0) {
-                  dp[i][j] += dp[i][j-1];
-                }
-            }
-        }
-    
-        return dp[m-1][n-1];
+        return dp(m - 1, n -1, new HashMap<>());
     }
     
-    
-     // int[][] dp = new int[m+1][n+1];
-        // dp[0][0] = 1;
-        // for(int i=0; i<m; i++) {
-        //     for(int j=0; j<n; j++) {
-        //         if(i - 1 >= 0 ) {
-        //             dp[i][j] += dp[i-1][j];
-        //         }
-        //         if(j - 1 >= 0) {
-        //             dp[i][j] += dp[i][j-1];
-        //         }
-        //     }
-        // }
-        // return dp[m-1][n-1];
-    /*
-    int helper(int m, int n, HashMap<String, Integer> memo) {
+    int dp(int m, int n, HashMap<String, Integer> memo) {
         String key = m+"-"+n;
-        if( m== 0 && n == 0) {
+        if(m == 0 && n == 0) {
             return 1;
         }
-        if( m < 0 || n < 0) {
+        if(m < 0 || n < 0) {
             return 0;
         }
         if(memo.containsKey(key)) {
             return memo.get(key);
         }
-        int one = helper(m - 1, n, memo);
-        int two = helper(m, n - 1, memo);
-        int total = one + two;
-        memo.put(key, total);
-        return total;
+        int one  = dp(m - 1, n, memo);
+        int two = dp(m, n-1, memo);
+        int answer = one + two;
+        memo.put(key, answer);
+        return answer;
     }
-    */
 }
