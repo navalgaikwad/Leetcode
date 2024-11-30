ea6f14c3581dc16893a72b63ae35d2206d05ab3c
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-30 07:18:38 -0500

    Time: 54 ms (54.02%), Space: 69.2 MB (30.71%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,34 +1,15 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a,b)->Integer.compare(a[1],b[1]));
-        int[] interval = points[0];
-        int count =points.length;
-        for(int i = 1; i<points.length; i++) {
-            if(points[i][0] <= interval[1]) {
+       Arrays.sort(points, (a,b)->Integer.compare(a[1],b[1]));
+        int intervalEnd = points[0][1];
+        int count = points.length;;
+        for(int i=1; i<points.length;i++) {
+            if(points[i][0] <= intervalEnd) {
                 count--;
             }else {
-                interval = points[i];
+                intervalEnd = points[i][1];
             }
         }
         return count;
     }
 }
-// [[10,16],[2,8],[1,6],[7,12]]
-//  [[1,6],[2,8],[7,12][10,16]]
-// class Solution {
-//     public int findMinArrowShots(int[][] points) {
-//         Arrays.sort(points, (a,b)->Integer.compare(a[1],b[1]));
-//         int[] prev = new int[points.length];
-//         prev = points[0];
-//         int count =points.length;
-//         for(int i=1; i<points.length; i++) {
-//             int[] interval = points[i];
-//             if(interval[0] <= prev[1]) {
-//                 count--;
-//             }else {
-//                 prev = interval;
-//             }
-//         }
-//         return count;
-//     }
-// }
