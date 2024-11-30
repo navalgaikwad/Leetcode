0b97c36c97e1f7c3992566ef58ad49f6bfca102d
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-30 05:52:28 -0500

    Time: 1 ms (76.51%), Space: 46.1 MB (5.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,28 +1,32 @@
 class Solution {
+
     public void setZeroes(int[][] matrix) {
-        int col0 = 1;
         int row = matrix.length;
-        int col = matrix[0].length;
-        //1. first add 0 to top and left
-        //2. when ever i find top zero it will add in bottom 
-        //3. when ever i find left zero add it in right side
-        for(int i=0; i<row; i++) {
-            if(matrix[i][0] == 0) col0 = 0;
-            for(int j =1; j<col; j++) {
-                if(matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        int column = matrix[0].length;
+        int[][] arr = new int[row][column];
+
+        // copy arr
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arr[i][j] = matrix[i][j];
+            }
+        }
+
+        // start
+
+        for (int a = 0; a < row; a++) {
+            for (int b = 0; b < column; b++) {
+                if (arr[a][b] == 0) {
+                    for (int i = 0; i < column; i++) {
+                        matrix[a][i] = 0;
+                    }
+                    for (int i = 0; i < row; i++) {
+                        matrix[i][b] = 0;
+                    }
+
                 }
             }
         }
-        
-        for(int i=row-1; i>=0; i--) {
-            for(int j=col-1; j>=1; j--) {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-            if (col0 == 0) matrix[i][0] = 0;
-        }
+
     }
 }
