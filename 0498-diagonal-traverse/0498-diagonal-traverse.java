ede94c4c2dfc9a969207146195616c09716dadf1
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-30 08:50:04 -0500

    Time: 2 ms (99.32%), Space: 47.1 MB (45.25%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,37 +1,37 @@
 class Solution {
-    public int[] findDiagonalOrder(int[][] mat) {  
-        int m = mat.length, n = mat[0].length;
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length; int n = mat[0].length;
+        int row =0, col = 0;
         boolean up = true;
-        int row = 0, col = 0;
-        int i = 0;
-        int[] arr = new int[m * n];
-        while( row < m && col < n  ) {
+        int i =0;
+        int[] arr = new int[m*n];
+        while(row<m && col <n) {
             if(up) {
                 while(row > 0 && col < n-1) {
                     arr[i++] = mat[row][col];
                     row--;
                     col++;
                 }
-                arr[i++] = mat[row][col];
-                if(col == n- 1) {
+                arr[i++] =  mat[row][col];
+                if(col == n-1) {
                     row++;
                 }else {
                     col++;
                 }
             }else {
-                while(col > 0 && row < m-1) {
+                 while(col > 0 && row < m-1) {
                     arr[i++] = mat[row][col];
-                    col--;
                     row++;
+                    col--;
                 }
-                arr[i++] = mat[row][col];
-                if(row == m- 1) {
+                arr[i++] =  mat[row][col];
+                if(row == m-1) {
                     col++;
                 }else {
                     row++;
                 }
             }
-             up = !up;
+            up=!up;
         }
         return arr;
     }
