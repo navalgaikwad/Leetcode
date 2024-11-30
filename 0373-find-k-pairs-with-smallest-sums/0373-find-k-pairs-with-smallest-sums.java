97f78e55e76ca6d01692c0973ac8e4e3085bb1e1
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-30 06:35:26 -0500

    Time: 82 ms (15.63%), Space: 63.7 MB (9.78%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,26 +1,24 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        int m = nums1.length;
-        int n = nums2.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)-> a[0] - b[0]);
-        pq.add(new int[]{nums1[0]+nums2[0], 0, 0});
-        
+        int m = nums1.length - 1;
+        int n = nums2.length - 1;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0] - b[0]);
+        pq.add(new int[]{nums1[0] + nums2[0], 0, 0});
         Set<String> visited = new HashSet<>();
         visited.add(0+"-"+0);
         List<List<Integer>> result = new ArrayList<>();
-        while(k-- >0 && !pq.isEmpty()) {
+        while(k-- >0 &&!pq.isEmpty()) {
             int[] top = pq.remove();
+           
             int i = top[1];
             int j = top[2];
-            result.add(List.of(nums1[i], nums2[j]));
-            
-            if(i+1 < nums1.length && !visited.contains((i+1)+"-"+j)) {
-                pq.add(new int[]{nums1[i+1]+nums2[j], i+1, j});
-                visited.add((i+1)+"-"+j);
+             result.add(List.of(nums1[i], nums2[j]));
+            while(i + 1 < nums1.length && !visited.contains((i + 1)+"-"+ j)) {
+                pq.add(new int[]{nums1[i+1] + nums2[j], i + 1, j});
+                 visited.add((i + 1)+"-"+j);
             }
-            
-            if(j+1 < nums2.length && !visited.contains(i+"-"+(j+1))) {
-                pq.add(new int[]{nums1[i]+nums2[j+1], i, j+1});
+            while(j + 1 < nums2.length && !visited.contains((i)+"-"+ (j+1))) {
+                pq.add(new int[]{nums1[i] + nums2[j+1], i, j+1});
                 visited.add((i)+"-"+(j+1));
             }
         }
