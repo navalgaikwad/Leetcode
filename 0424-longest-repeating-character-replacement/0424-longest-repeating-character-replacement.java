8b484e7d79852b100ed889d951891a9bba2bbcbb
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-11-29 21:06:44 -0500

    Time: 38 ms (14.44%), Space: 45.5 MB (6.91%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -5,10 +5,12 @@ class Solution {
         int maxLen =0;
         int freq = 0;
         for(int right = 0; right<s.length(); right++) {
+            
             char rightSide = s.charAt(right);
             map.put(rightSide, map.getOrDefault(rightSide, 0) + 1);
             freq = Math.max(freq, map.get(rightSide));
             int replacementsNeeded = (right - left + 1) - freq;
+            
             if(replacementsNeeded > k) {
                 char leftSide = s.charAt(left);
                 map.put(leftSide, map.getOrDefault(leftSide, 0) - 1);
