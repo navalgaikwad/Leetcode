216d780361d0d368a283c0dc4ab5494ac283bc2f
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-12-01 12:41:22 -0500

    Time: 60 ms (84.37%), Space: 41.5 MB (25.70%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,31 +1,29 @@
 class Solution {
     public int getMaximumGold(int[][] grid) {
-        int max = 0;
-        //Integer[][] memo = new Integer[grid.length][grid.length];
+        int result = 0;
         for(int i=0; i<grid.length; i++) {
             for(int j=0; j<grid[0].length; j++) {
                 if(grid[i][j] != 0) {
-                    max = Math.max(max, helper(grid, i, j)); 
+                    int max = dp(grid, i, j);
+                    result = Math.max(result, max);
                 }
             }
         }
-        return max;
+        return result;
     }
     
-    int helper(int[][] grid, int i, int j) {
-        if(i < 0 || i >= grid.length || j < 0 || j>=grid[0].length || grid[i][j] == 0) {
+    int dp(int[][] grid, int i, int j) {
+        if(i < 0 || i>= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
             return 0;
         }
-        int value = grid[i][j];
+        int temp = grid[i][j];
         grid[i][j] = 0;
-
-        int top = helper(grid, i - 1, j);
-        int right = helper(grid, i, j + 1);
-        int bottom = helper(grid, i + 1, j);
-        int left = helper(grid, i, j - 1);
-        
-        grid[i][j] = value;
-        int max = grid[i][j] + Math.max(top, Math.max(left, Math.max(right, bottom)));
-        return max;
+        int top = dp(grid, i - 1, j);
+        int down = dp(grid, i, j-1);
+        int up = dp(grid, i+1, j);
+        int bottom = dp(grid, i, j + 1);
+        grid[i][j] = temp;
+        int value = grid[i][j] + Math.max(top, Math.max(down, Math.max(up, bottom)));
+        return value;
     }
 }
