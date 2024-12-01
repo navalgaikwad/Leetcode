80d94d572528d56d12e29c547128c01beb62dbbc
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-12-01 11:24:56 -0500

    Time: 3 ms (95.98%), Space: 49.1 MB (70.22%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,28 +1,27 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int noncyclic = maxSum(nums);
+        int nonCyclic = maxSum(nums);
         int totalSum = 0;
-        for(int i=0; i<nums.length ;i++) {
-            totalSum +=nums[i]; 
-            nums[i] = -nums[i];//minus swatashi karnar
+        for(int i=0; i<nums.length; i++) {
+            totalSum+=nums[i];
+            nums[i]=-nums[i];//vvimp invert karnar by -
         }
-        int circular  = totalSum + maxSum(nums);
-        if(circular == 0 ) {
-            return noncyclic;
+        int cyclic = totalSum + maxSum(nums);
+         if(cyclic == 0 ) {
+            return nonCyclic;
         }
-        return Math.max(noncyclic, circular); // max of both cyclic and non cyclic
+        return Math.max(cyclic, nonCyclic);
     }
     
     int maxSum(int[] nums) {
-        int sum =nums[0];
+         int sum =nums[0];
         int max = nums[0];
-        for(int i =1; i<nums.length; i++) {
-            if(sum < 0){
-                sum =0;
+        for(int i=1; i<nums.length; i++) {
+            if(sum < 0) {
+                sum = 0;
             }
-            sum = sum + nums[i];
-            max =Math.max(max, sum);
-            
+            sum+=nums[i];
+            max = Math.max(max, sum);
         }
         return max;
     }
