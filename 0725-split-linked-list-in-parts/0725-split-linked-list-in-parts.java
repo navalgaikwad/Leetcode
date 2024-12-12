ac61d0c6a88e7a3d567ce3afcb191d13cf332f41
Naval Gaikwad <navalgaikwad1991@outlook.com>
2024-12-12 02:15:18 -0500

    Time: 0 ms (100.00%), Space: 43.3 MB (71.62%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -9,46 +9,46 @@
  * }
  */
 class Solution {
-public ListNode[] splitListToParts(ListNode head, int k) {
-    int length = 0;
-    ListNode current = head;
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int length = 0;
+        ListNode current = head;
 
-    // Calculate the total length of the list
-    while (current != null) {
-        current = current.next;
-        length++;
-    }
 
-    // Calculate partition size and remainder
-    int partition = length / k;
-    int remainder = length % k;
+        while (current != null) {
+            current = current.next;
+            length++;
+        }
 
-    ListNode[] result = new ListNode[k];
-    current = head;
 
-    for (int i = 0; i < k; i++) {
-        if (current == null) {
-            result[i] = null; // For empty partitions
-        } else {
-            result[i] = current; // Start of the current partition
-            int currentPartitionSize = partition + (remainder-- > 0 ? 1 : 0);
+        int partition = length / k;
+        int remainder = length % k;
 
-            // Move current to the end of the current partition
-            for (int j = 1; j < currentPartitionSize; j++) {
-                if (current != null) current = current.next;
-            }
+        ListNode[] result = new ListNode[k];
+        current = head;
 
-            // Break the current partition and move to the next
-            if (current != null) {
-                ListNode next = current.next;
-                current.next = null;
-                current = next;
+        for (int i = 0; i < k; i++) {
+            if (current == null) {
+                result[i] = null; 
+            } else {
+                result[i] = current; 
+                int currentPartitionSize = partition + (remainder-- > 0 ? 1 : 0);
+
+
+                for (int j = 1; j < currentPartitionSize; j++) {
+                    if (current != null) current = current.next;
+                }
+
+
+                if (current != null) {
+                    ListNode next = current.next;
+                    current.next = null;
+                    current = next;
+                }
             }
         }
-    }
 
-    return result;
-}
+        return result;
+    }
 
 
 }
