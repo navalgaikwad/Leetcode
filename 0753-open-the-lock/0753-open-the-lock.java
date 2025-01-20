c4c652f88ee6a3e2e195192d02392e005ecf5188
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-01-20 18:24:09 -0500

    Time: 98 ms (55.56%), Space: 53.5 MB (17.11%) - LeetHub

---
+++ 0753-open-the-lock/0753-open-the-lock.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    private static final String START = "0000";
+    
+    public int openLock(String[] deadends, String target) {
+        if (target == null || target.length() == 0) return -1;
+        Set<String> visited = new HashSet<>(Arrays.asList(deadends));
+        Queue<String> queue = new LinkedList<>();
+        int level = 0;
+        queue.offer(START);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String currentLock = queue.poll();
+                if (!visited.add(currentLock)) continue;
+                if (currentLock.equals(target)) return level;
+                
+                for (String nextLock : getNextStates(currentLock)) {
+                    if (!visited.contains(nextLock)) queue.offer(nextLock);
+                }
+            }
+            level++;
+        }
+        
+        return -1;
+    }
+    
+    private List<String> getNextStates(String lock) {
+        List<String> locks = new LinkedList<>();
+        char[] arr = lock.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            arr[i] = c == '9' ? '0' : (char) (c + ((char) 1));
+            locks.add(String.valueOf(arr));
+            arr[i] = c == '0' ? '9' : (char) (c - ((char) 1));
+            locks.add(String.valueOf(arr));
+            arr[i] = c;
+        }
+        return locks;
+    }
+}
