24d76eb3968bb1125686a25ed2b535039b1d5030
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-01-31 21:55:51 -0500

    Time: 22 ms (47.64%), Space: 45.1 MB (46.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,59 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String result = "";
-        int len = 0;
-        boolean isPalindromeFound = false;  // Flag to check if we found any palindrome
+        int  max = 0;
+        String ans = "";
 
-        for (int k = 0; k < s.length(); k++) {
-            // Odd length palindromes (center is a single character)
-            int left = k;
-            int right = k;
-            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-                if (right - left + 1 > len) {
-                    len = right - left + 1;
-                    result = s.substring(left, right + 1);
-                      // Found at least one palindrome
-                }
-                if (right - left + 1 > 1) {
-                    isPalindromeFound = true;
+        for(int i=0; i<s.length(); i++) {
+            int left = i;
+            int right = i;
+
+            while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                if(right - left + 1 > max) {
+                    max = right - left + 1;
+                    ans = s.substring(left, right + 1);
                 }
                 left--;
                 right++;
+
             }
+             left = i;
+             right = i + 1;
 
-            // Even length palindromes (center is between two characters)
-            left = k;
-            right = k + 1;
-            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-                if (right - left + 1 > len) {
-                    len = right - left + 1;
-                    result = s.substring(left, right + 1);
-                    //isPalindromeFound = true;  // Found at least one palindrome
-                }
-                 if (right - left + 1 > 1) {
-                    isPalindromeFound = true;
+            while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                if(right - left + 1 > max) {
+                    max = right - left + 1;
+                    ans = s.substring(left, right + 1);
                 }
                 left--;
                 right++;
+
             }
         }
-        // Print true/false depending on whether a palindrome was found
-       // System.out.println(isPalindromeFound);
-        return result;
+        return ans;
+
     }
 }
-
-
-// lowercase letter with "aabaac" return true or false 
-
-// aabaa
-// bcaabaa
-
-// aabc
-
-// abca
-    
-// a 
-// a 
-// b
-// c
