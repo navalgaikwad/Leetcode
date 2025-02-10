f9ce7e84613574f3abd09f5a6926eae99a098a8e
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-10 17:33:23 -0500

    Time: 4 ms (85.78%), Space: 43.7 MB (30.71%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -7,7 +7,7 @@ class Solution {
                 if(!s.isEmpty()) {
                     s.pop();
                 }
-            }else if( !a.equals(".") && !a.isEmpty()) {//.ani empty
+            } else if( !a.equals(".") && !a.isEmpty()) {//.ani empty
                 s.push(a);
             }
         }
