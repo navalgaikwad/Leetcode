44ada52eb99e36afa69c7ff0b71c4192185a814b
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-11 14:47:57 -0500

    Time: 3 ms (82.82%), Space: 42.4 MB (80.38%) - LeetHub

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, ArrayList<int[]>> map = new TreeMap<>();
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        dfs(root, 0, 0);
+        List<List<Integer>> result = new ArrayList<>();
+        for(ArrayList<int[]> list: map.values()) {
+           Collections.sort(list, (a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
+            ArrayList<Integer> current = new ArrayList<>();
+            for(int[] num :  list) {
+                current.add(num[1]);
+            }
+            result.add(current);
+        }
+        return result;
+        
+    }
+
+    void dfs(TreeNode root, int index, int dept) { //very impoort dept
+        if(root == null) {
+            return;
+        }
+        map.putIfAbsent(index, new ArrayList<>());
+        map.get(index).add(new int[]{dept, root.val});
+        dfs(root.left, index - 1, dept + 1);
+        dfs(root.right, index + 1, dept + 1);
+    }
+}
