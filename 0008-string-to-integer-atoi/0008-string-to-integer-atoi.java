22cd52f00508ce0c60804f5fc74fdfbe0976e199
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-15 20:08:39 -0500

    Time: 1 ms (100%), Space: 42.6 MB (48.75%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,35 +1,32 @@
 class Solution {
     public int myAtoi(String s) {
-        int sign = 1;
         int index =0;
-        int number =0;
-        if(s.length() == 0) {
-            return 0;
-        }
-        while(index < s.length() && s.charAt(index)==' ') {
+        if(s.length() == 0) return 0;
+        while(index < s.length() && s.charAt(index) == ' ') {
             index++;
         }
-        if(s.length() == index) {
-            return 0;
+        if(index == s.length()) return 0;
+        
+        int sign = 1;
+        if(s.charAt(index)=='-' || s.charAt(index) == '+') {
+            sign = s.charAt(index)=='-' ? -1: 1;
+            index++;//vvimp
         }
-        if(s.charAt(index) =='-' || s.charAt(index) == '+') {
-            sign = s.charAt(index) =='-' ? -1 : 1;
-            index++;
-        }
+        int total = 0;
         while(index < s.length()) {
-            int c = s.charAt(index) -'0';
-            
-            if( c < 0 || c > 9) {
+            int number = Character.getNumericValue(s.charAt(index));
+            if(number < 0 || number >9) {
                 break;
             }
-            if(Integer.MAX_VALUE/10 < number || 
-               Integer.MAX_VALUE/10 == number && 
-               Integer.MAX_VALUE%10 < c) {
-                return sign == 1 ? Integer.MAX_VALUE:Integer.MIN_VALUE;
+            if(Integer.MAX_VALUE/10 < total || 
+               Integer.MAX_VALUE/10 == total && 
+               Integer.MAX_VALUE%10 < number) {
+                return sign == 1 ? Integer.MAX_VALUE:Integer.MIN_VALUE; 
             }
-            number = number*10 + c;
+            
+            total = total *10 + number;
             index++;
         }
-        return number*sign;
+        return total*sign;//vvimp
     }
 }
