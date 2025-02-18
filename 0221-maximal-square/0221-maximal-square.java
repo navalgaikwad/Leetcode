2841a493f55381db0527da7eefd433e08fba87e1
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-17 19:31:45 -0500

    Time: 9 ms (37.68%), Space: 60.7 MB (27.07%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,27 +1,27 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-          if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return 0; // Edge case: empty matrix
         }
-        
+        int max = 0;
         int rows = matrix.length, cols = matrix[0].length;
         int[][] dp = new int[rows][cols]; // DP array to store square sizes
-        int maxSide = 0;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == '1') { // Only consider '1' cells
-                    int left = (i > 0) ? dp[i - 1][j] : 0;
-                    int above = (j > 0) ? dp[i][j - 1] : 0;
-                    int diag = (i > 0 && j > 0) ? dp[i - 1][j - 1] : 0;
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                int left = 0, above = 0, diag = 0;
+                if(matrix[i][j] == '1') {
+                    if( i > 0)  left = dp[i-1][j];
+                    if( j > 0) above = dp[i][j-1];
+                    if(i > 0 && j >0) diag = dp[i-1][j-1];
 
                     dp[i][j] = Math.min(left, Math.min(above, diag)) + 1;
-                    maxSide = Math.max(maxSide, dp[i][j]);
+                    max =Math.max(max, dp[i][j]);
                 }
             }
-        }
-        
-        return maxSide * maxSide; // Return the area of the largest square
-    }
-}
 
+        }
+        return max * max;
+
+    }
+
+}
