e60f7c6c4a13ea579332979bd434571550219713
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-17 20:53:45 -0500

    Time: 1 ms (84%), Space: 41 MB (56.11%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,18 +1,15 @@
 class Solution {
     public int reverse(int x) {
         long reverse = 0;
-        int temp;
-
-        while (x != 0) {
-            temp = x % 10;
+        while(x != 0) {
+            int temp = x % 10;
             reverse = reverse * 10 + temp;
-            x = x / 10;
-
+            x = x/10;
             if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
                 return 0;
             }
         }
 
-        return (int)reverse;
+        return (int) reverse;
     }
 }
