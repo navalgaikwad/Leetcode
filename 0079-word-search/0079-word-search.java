ca88daa4b6b22d7e7597e8be27428dfc34b713d7
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-17 20:58:35 -0500

    Time: 137 ms (72.25%), Space: 41.5 MB (88.46%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,29 +1,31 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board[0].length; j++) {
-                if(board[i][j] == word.charAt(0) && helper(board, word, i, j, 0)) {
+        for(int i=0; i<board.length ;i++) {
+            for(int j=0; j<board[0].length; j++ ) {
+                if(board[i][j] == word.charAt(0) && dfs(board, word, i, j , 0)) {
                     return true;
                 }
             }
         }
         return false;
     }
-    boolean helper(char[][] board, String word, int row, int col, int index) {
-         if(index == word.length()) return true;
-        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col]!= word.charAt(index)) {
+    boolean dfs(char[][] board, String word, int i, int j, int index) {
+         if(index == word.length()) {
+            return true;
+        }
+        if( i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word.charAt(index)) {
             return false;
         }
         
        
-        
-        char c =  board[row][col];
-        board[row][col] = '$';
-        boolean left = helper(board, word, row+1, col, index+1);
-        boolean right = helper(board, word, row, col+1, index+1);
-        boolean up = helper(board, word, row-1, col, index+1);
-        boolean bottom = helper(board, word, row, col-1, index+1);
-        board[row][col]= c;
-        return left || right || up || bottom;
+        char temp = board[i][j];
+        board[i][j] = '$';
+        boolean left = dfs(board, word, i + 1, j, index + 1);
+        boolean right = dfs(board, word, i, j + 1, index + 1);
+        boolean top = dfs(board, word, i-1, j , index + 1);
+        boolean bottom = dfs(board, word, i, j - 1, index + 1);
+        board[i][j] = temp;
+        return left || right || top || bottom;
+
     }
 }
