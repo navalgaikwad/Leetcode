d8e4c1d92a7c275e4b2c09dc1491ede27b87e13b
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-20 15:02:58 -0500

    Time: 6 ms (32.74%), Space: 45.6 MB (9.29%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,15 +10,11 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-         if(root == null) {
-            return null;
-        }
-        if(root.val == p.val || root.val == q.val) {
+          if(root == null || root.val == p.val || root.val == q.val) {
             return root;
         }
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        
         if(left == null) {
             return right;
         }else if(right == null) {
@@ -27,4 +23,3 @@ class Solution {
         return root;
     }
 }
-
