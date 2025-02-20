95b65619f7a337ba90a92468ec16a01803d8da49
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-20 14:34:32 -0500

    Time: 63 ms (98.61%), Space: 75.9 MB (31.39%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -1,20 +1,17 @@
 class Solution {
     public int maxEvents(int[][] events) {
-        Arrays.sort(events, (a, b)->a[0] - b[0]);
-        int time =0;
-        int count =0;
+        Arrays.sort(events, (a,b)->a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         int index =0;
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        int count =0;
+        int time = 0;
         while(!pq.isEmpty() || index < events.length) {
             if(pq.isEmpty()) {
                 time = events[index][0];
             }
-            //add all the event of that day
-            while(index < events.length && events[index][0]  == time) {
-                pq.add(events[index][1]);//2
-                index++;
+            while(index < events.length && events[index][0] == time) {
+                pq.add(events[index++][1]);
             }
-            //for next door
             pq.remove();
             time++;
             count++;
@@ -23,5 +20,6 @@ class Solution {
             }
         }
         return count;
+        
     }
 }
