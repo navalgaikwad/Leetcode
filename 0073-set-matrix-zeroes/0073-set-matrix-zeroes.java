eb547a759a4e129084a69f4ae8a0fa747a9b393b
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-20 18:59:03 -0500

    Time: 1 ms (75.42%), Space: 45.4 MB (96.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,32 +1,26 @@
 class Solution {
-
     public void setZeroes(int[][] matrix) {
-        int row = matrix.length;
-        int column = matrix[0].length;
-        int[][] arr = new int[row][column];
-
-        // copy arr
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                arr[i][j] = matrix[i][j];
+        
+        int row = 0;
+        int col =0;
+        int[][] arr = new int[matrix.length][matrix[0].length];
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+               arr[i][j] = matrix[i][j];
             }
         }
-
-        // start
-
-        for (int a = 0; a < row; a++) {
-            for (int b = 0; b < column; b++) {
-                if (arr[a][b] == 0) {
-                    for (int i = 0; i < column; i++) {
-                        matrix[a][i] = 0;
+         for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(arr[i][j] == 0) {
+                    for(int a=0; a< matrix[0].length; a++) {
+                        matrix[i][a] = 0;
                     }
-                    for (int i = 0; i < row; i++) {
-                        matrix[i][b] = 0;
+                    for (int b = 0; b < matrix.length; b++) {
+                        matrix[b][j] = 0;
                     }
-
                 }
-            }
-        }
 
+        }
+      }
     }
 }
