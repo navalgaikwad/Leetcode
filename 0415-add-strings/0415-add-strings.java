2c585352d0c96f781038ef3fbffa4f2702e831ee
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-21 16:13:25 -0500

    Time: 1 ms (100%), Space: 42.7 MB (61.83%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,29 +1,28 @@
-
-  class Solution {
+class Solution {
     public String addStrings(String num1, String num2) {
-        int m = num1.length();
-        int n = num2.length();
-        int[] pos = new int[Math.max(m, n) + 1];
-        int i = m - 1, j = n - 1, k = pos.length - 1;
+        int m = num1.length(); int n = num2.length();
+        int[] arr = new int[Math.max(m, n) + 1];
+        int i =0, j =0;
+        m = m-1;
+        n = n-1;
+        int k = arr.length - 1;
+        while(m>=0 || n>=0) {
+            int digit1 = (m>=0) ? (num1.charAt(m) -'0') : 0;
+            int digit2 = (n>=0) ? (num2.charAt(n) -'0') : 0;
 
-        while (i >= 0 || j >= 0) {
-            int digit1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
-            int digit2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
-            int sum = digit1 + digit2 + pos[k];
-            pos[k] = sum % 10;
-            pos[k - 1] += sum / 10;//carry
-            i--;
-            j--;
+            int sum = digit1 + digit2 + arr[k];
+            arr[k] = sum%10;
+            arr[k-1] += sum/10;
+            m--;
+            n--;
             k--;
         }
-
         StringBuilder sb = new StringBuilder();
-        for (int p : pos) {
-            if (!(sb.length() == 0 && p == 0)) {
-                sb.append(p);
+        for(int a : arr) {
+            if(!(sb.length()==0 && a == 0) ) {
+                sb.append(a);
             }
         }
-
-        return sb.length() == 0 ? "0" : sb.toString();
+        return sb.toString().length() == 0 ? "0" : sb.toString();
     }
 }
