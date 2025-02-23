36fb90933d89a41fe621afb92a5ac8edf78e0132
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-23 17:37:09 -0500

    Time: 1 ms (100%), Space: 45.1 MB (16.86%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -8,10 +8,15 @@ class Solution {
     }
     
     public int AreaOfIsland(int[][] grid, int i, int j){
-        if( i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1){
-            grid[i][j] = 0;
-            return 1 + AreaOfIsland(grid, i+1, j) + AreaOfIsland(grid, i-1, j) + AreaOfIsland(grid, i, j-1) + AreaOfIsland(grid, i, j+1);
+        if( i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0){ 
+            return 0;
         }
-        return 0;
+            grid[i][j] = 0;
+            int left = AreaOfIsland(grid, i+1, j);
+            int right = AreaOfIsland(grid, i-1, j);
+            int up = AreaOfIsland(grid, i, j-1);
+            int down = AreaOfIsland(grid, i, j+1);
+        
+       return 1 + left + right + up + down;
     }
 }
