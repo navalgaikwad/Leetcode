585f6bbfce9a910b2759c7a2e7e8a0c0345c9117
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-23 17:35:56 -0500

    Time: 1 ms (97.49%), Space: 45.7 MB (44.37%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,28 +1,17 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int len =flowerbed.length-1;
-        for(int i =0; i<flowerbed.length; i++) {
-            if(flowerbed[i]==0 && (i ==0 || flowerbed[i-1] == 0) &&(i== len || flowerbed[i+1]==0) ) {
-                flowerbed[i] = 1;
+        if(n ==0) return true;
+        
+        for(int i=0; i<flowerbed.length; i++) {
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
                 n--;
+                flowerbed[i] = 1;
+            }
+            if(n == 0) {
+                return true;
             }
         }
-        return n<=0;
+        return false;
     }
 }
 
-/*
- [1,0,0,0,1], n = 2
-[1,0,1,0,1,0,1] 1
-*/
-// class Solution {
-//     public boolean canPlaceFlowers(int[] arr, int n) {
-//         for(int i=0; i<arr.length; i++){
-//             if(arr[i]==0 && (i==0 || arr[i-1]==0) && (i==arr.length-1 || arr[i+1]==0)){
-//                 arr[i]=1;
-//                 n--;
-//             }
-//         }
-//         return n<=0;
-//     }
-// }
