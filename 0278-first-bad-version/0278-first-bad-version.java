95953f7d150aa8d75319b32ff0d67dd4a93696b1
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-23 17:27:34 -0500

    Time: 25 ms (71.74%), Space: 40.3 MB (63.44%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -5,9 +5,9 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
-        int firstBadVersion = n;
+       
         
-        while(left < right){
+        while(left < right) {
             int mid = left + (right - left)/2;
             //search untill we find first bad version search it on left side after that
             //all will be bad version, because we need to find first badversion
