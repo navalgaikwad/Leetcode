6b3d695a6b9adb90e5a0b6a6606c157346201a08
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-23 17:36:49 -0500

    Time: 8 ms (87.37%), Space: 45.1 MB (54.44%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,32 +1,31 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-        int m =  matrix.length;
-        int n =  matrix[0].length;
-        boolean[][] visited= new boolean[m][n];
-        int max=0;
-        int[][] dp =new int[m][n];
-        for(int i=0; i<m; i++){
-           for(int j=0; j<matrix[i].length; j++){
-               dfs(matrix, i, j, visited, matrix[i][j]-1, dp);
-               max=Math.max(max, dp[i][j]);
-           } 
+           int row = matrix.length;
+        int col =matrix[0].length;
+          int max =0;
+        Integer[][] memo = new Integer[matrix.length][matrix[0].length];
+        //HashMap<String, Integer> memo = new HashMap<>();
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+               int sum = dfs(matrix, i, j, memo, matrix[i][j]-1);
+                max=Math.max(max, sum);
+            }
         }
         return max;
     }
     
-    int dfs(int[][] matrix, int i, int j, boolean[][] visited, int prev, int[][] dp){
-        if(i <0 || i>=matrix.length || j<0 || j>=matrix[0].length || matrix[i][j]<=prev){
+    int dfs(int[][] matrix, int i, int j, Integer[][] memo, int prev) {
+        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || prev >= matrix[i][j]) {
             return 0;
         }
-        if(dp[i][j]!=0){
-            return dp[i][j];
-        }
-        //visited[i][j]=true;
-        int top = dfs(matrix, i+1, j, visited, matrix[i][j], dp);
-        int bottom = dfs(matrix, i-1, j, visited, matrix[i][j], dp);
-        int left = dfs(matrix, i, j-1, visited, matrix[i][j], dp);
-        int right = dfs(matrix, i, j+1, visited, matrix[i][j], dp);
-        dp[i][j]=1+Math.max(Math.max(top,  bottom), Math.max(left, right));
-        return dp[i][j];
+        if(memo[i][j]!=null) return memo[i][j];
+        int left = dfs(matrix, i + 1, j, memo, matrix[i][j]);
+        int right = dfs(matrix, i, j + 1, memo, matrix[i][j]);
+        int top = dfs(matrix, i - 1, j, memo, matrix[i][j]);
+        int bottom = dfs(matrix, i, j - 1, memo, matrix[i][j]);
+        
+        int value = 1 + Math.max(left, Math.max(right, Math.max(top, bottom)));
+        memo[i][j] = value;
+        return value;
     }
 }
