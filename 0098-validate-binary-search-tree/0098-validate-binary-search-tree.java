d080630d75a40635444a2907cfd30f80fb9398e1
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-02-23 17:21:17 -0500

    Time: 0 ms (100%), Space: 43.8 MB (40.14%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,17 +15,16 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+       return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-    
+
     boolean helper(TreeNode root, long min, long max) {
-        if(root == null) {
-            return true;
-        }
-        if(root.val >= max || root.val <= min) {
+        if(root ==null) return true; 
+        if(root.val <= min || root.val >= max) {
             return false;
         }
-        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+        boolean left = helper(root.left, min, root.val);
+        boolean right = helper(root.right, root.val, max);
+        return left && right;
     }
-    
 }
