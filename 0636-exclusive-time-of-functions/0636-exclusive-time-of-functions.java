a39df9c65ff9b030e32cf19e7121b572aaf70ee3
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-02 18:24:40 -0500

    Time: 11 ms (87.45%), Space: 45.9 MB (7.63%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
@@ -2,19 +2,19 @@ class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
         int[] result = new int[n];
         Stack<Integer> st = new Stack<>();
-         int prev = 0;
-        for(String log : logs) {
+        int prev = 0;
+        for (String log : logs) {
             String[] split = log.split(":");
-            if(split[1].equals("start")) {
-                if(!st.isEmpty()) {
-                    result[st.peek()]+= Integer.parseInt(split[2]) - prev;
+            int number = Integer.parseInt(split[2]);
+            if (split[1].equals("start")) {
+                if (!st.isEmpty()) {
+                    result[st.peek()] += number - prev;
                 }
-
+                prev = number;
                 st.push(Integer.parseInt(split[0]));
-                prev = Integer.parseInt(split[2]);
-            }else {
-                result[st.pop()]+= Integer.parseInt(split[2]) - prev + 1;
-                prev = Integer.parseInt(split[2])+1;
+            } else {
+                result[st.pop()] += number - prev + 1;
+                prev = number + 1;
             }
         }
         return result;
