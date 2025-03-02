b7a9dc911d2e75d47304e3f195ca71edfdb2e075
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-02 13:55:56 -0500

    Time: 24 ms (80.92%), Space: 50 MB (45.44%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.java
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.java
@@ -1,30 +1,34 @@
 class Solution {
-    Random dom = new Random();
-    int[] wSum;
+    int[] w;
+    double[] p;
+    Random random;
     public Solution(int[] w) {
-        for(int i=1; i<w.length; ++i) {
-            w[i] +=w[i-1];
+        random = new Random();
+        this.w = w;
+        double sum =0;
+        this.p = new double[w.length];
+        for(int we : w) {
+            sum+=we;
         }
-        this.wSum = w;
+        p[0] = w[0] / sum;
+        for(int i=1; i<w.length; i++) {
+            p[i] = p[i-1] + (w[i]/sum); 
+        }
     }
     
     public int pickIndex() {
-        int left =0;
-        int right = wSum.length -1;
-        int idx = dom.nextInt(wSum[wSum.length - 1]) + 1;
-        
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-            if(idx == wSum[mid]) {
-                return mid;
-            }
-            if(idx < wSum[mid]) {
-                right = mid - 1;
-            }else {
-                left = mid + 1;
-            }
+      double target = random.nextDouble();
+      int left =0;
+      int right = p.length-1;
+      while(left < right) {
+        int mid = left + (right - left)/2;
+        if(p[mid] < target) {
+            left = mid + 1;
+        }else {
+            right = mid;
         }
-        return left;
+      }
+      return left;
     }
 }
 
