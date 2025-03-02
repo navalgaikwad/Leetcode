0453ed9337e08fea4b59ba318cbbd842bfa5d07e
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-02 16:59:14 -0500

    Time: 14 ms (42.05%), Space: 43.5 MB (9.34%) - LeetHub

---
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    Map<Integer, ArrayList<Integer>> adj = new HashMap<>();
+    void helper(TreeNode parent, TreeNode child) {
+        if(child == null) {
+            return;
+        }
+        adj.putIfAbsent(child.val, new ArrayList<>());
+        if(parent!=null) {
+            adj.get(parent.val).add(child.val);
+            adj.get(child.val).add(parent.val);
+        }
+        helper(child, child.left);
+        helper(child, child.right);
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        helper(null, root);
+        if(adj.containsKey(target.val)) {
+            System.out.print("Present");
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        Set<Integer> set = new HashSet<>();
+        set.add(target.val);
+        queue.add(new int[]{target.val, 0});
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            int[] array = queue.remove();
+            int current = array[0];
+            int level = array[1];
+            if(level == k) {
+                ans.add(current);
+                continue;
+            }
+            for(Integer neighbours: adj.get(current)) {
+                if(!set.contains(neighbours) && level <= k) {
+                    set.add(neighbours);
+                    queue.add(new int[]{neighbours, level + 1});
+                }
+            }
+        }
+        return ans;
+    }
+}
