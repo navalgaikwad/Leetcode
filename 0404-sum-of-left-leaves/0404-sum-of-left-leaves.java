17c0ffffeea29a3e4a49ce0053d022c48e62e61c
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-09 20:31:13 -0400

    Time: 0 ms (100%), Space: 41.3 MB (79.61%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -14,20 +14,20 @@
  * }
  */
 class Solution {
-    int sum = 0;
+    int total = 0;
     public int sumOfLeftLeaves(TreeNode root) {
-        if(root==null )
-        {
-            return 0;
-        }
-        if(root.left!=null && root.left.left == null && root.left.right ==null)
-        {
-            sum += root.left.val;
-        }
-
-        sumOfLeftLeaves(root.left);
-        sumOfLeftLeaves(root.right);
+        dfs(root);
+        return total;
         
-        return sum;
+    }
+    void dfs(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        if(root.left!=null && root.left.left == null && root.left.right ==null) {
+            total+=root.left.val;
+        }
+        dfs(root.left);
+        dfs(root.right);
     }
 }
