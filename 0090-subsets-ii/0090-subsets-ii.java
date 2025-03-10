60dce1a350ed4f970e2daaf3c64798aba973287a
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-10 00:22:42 -0400

    Time: 2 ms (31.8%), Space: 43.5 MB (61.21%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -10,9 +10,6 @@ class Solution {
     void helper(int[] nums, int index, ArrayList<Integer> runningList) {
         result.add(new ArrayList<>(runningList));
         for(int i= index; i<nums.length; i++) {
-            // if(i > index && nums[i] == nums[i-1]) {
-            //     break;
-            // }
             runningList.add(nums[i]);
             helper(nums, i + 1, runningList);
             runningList.remove(runningList.size() - 1);
