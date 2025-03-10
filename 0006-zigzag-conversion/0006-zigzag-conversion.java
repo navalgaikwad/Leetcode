2d3f71112f28007f7f3ec809da47aec36c03fc89
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-10 02:04:43 -0400

    Time: 7 ms (32.52%), Space: 45.7 MB (18.21%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,20 +1,21 @@
 class Solution {
     public String convert(String s, int numRows) {
-        ArrayList<Character>list[] = new ArrayList[numRows];
+        int n = s.length();
+        ArrayList<Character> adj[] = new ArrayList[numRows];
         for(int i=0; i<numRows; i++) {
-            list[i] = new ArrayList<>();
+            adj[i] = new ArrayList<>();
         }
         int j =0;
-        for(int i =0; i<s.length(); i++) {
-            for(int k =0; k<numRows && j < s.length(); k++) {
-                list[k].add(s.charAt(j++));
+        for(int i = 0; i<n; i++) {
+            for(int k=0; k < numRows && j < n; k++) {
+                adj[k].add(s.charAt(j++));
             }
-            for(int k =numRows-2; k>=1 && j < s.length(); k--) {
-                list[k].add(s.charAt(j++));
+            for(int k=numRows-2; k >= 1 && j < n; k--) {
+                adj[k].add(s.charAt(j++));
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for(ArrayList<Character> li : list) {
+         StringBuilder sb = new StringBuilder();
+        for(ArrayList<Character> li : adj) {
             for(Character val: li) {
                 sb.append(val);
             }
