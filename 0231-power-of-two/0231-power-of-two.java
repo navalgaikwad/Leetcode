ffa2619f9500c2a6649ee1cbca1bcbd76181070f
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-10 01:23:10 -0400

    Time: 0 ms (100%), Space: 40.8 MB (79.65%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        
+        while (n > 0) {
+            if (n == 1) return true;
+            if (n % 2 != 0) break;
+            n /= 2;
+        }
+        return false;
+    }
+}
