3f45372d93638f9ded936cc309b849972df5519b
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-11 18:28:10 -0400

    Time: 7 ms (100%), Space: 48.7 MB (99.64%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,16 +1,15 @@
 class NumArray {
-
     int[] nums;
-
     public NumArray(int[] nums) {
-        for(int i=1; i<nums.length; i++)
-            nums[i] =nums[i] + nums[i-1];
         this.nums = nums;
+        for(int i =1; i<nums.length; i++) {
+            nums[i] = nums[i - 1] + nums[i];
+        }
     }
     
     public int sumRange(int left, int right) {
         if(left == 0) return nums[right];
-        return nums[right] - nums[left-1];
+        return nums[right] - nums[left - 1];
     }
 }
 
