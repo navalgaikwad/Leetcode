429d20a8bb760a5600ab251ee4400ffc2be83c81
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-11 01:28:02 -0400

    Time: 350 ms (7.15%), Space: 105.7 MB (22.39%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -3,52 +3,54 @@ class WordDictionary {
         char c;
         Map<Character, Node> childrens;
         boolean isEnd;
+
         Node(char c) {
             this.c = c;
             this.childrens = new HashMap<>();
-            this.isEnd= false;
+            this.isEnd = false;
         }
     }
+
     Node root;
+
     public WordDictionary() {
         root = new Node('\0');
     }
-    
+
     public void addWord(String word) {
         Node current = root;
-        for(char c : word.toCharArray()) {
-            if(!current.childrens.containsKey(c)) {
+        for (char c : word.toCharArray()) {
+            if (!current.childrens.containsKey(c)) {
                 current.childrens.put(c, new Node(c));
             }
             current = current.childrens.get(c);
         }
         current.isEnd = true;
     }
-    
+
     public boolean search(String word) {
         Node current = root;
-       
+
         return searchWord(word, current);
     }
-    
+
     boolean searchWord(String word, Node node) {
-        for(int i = 0; i < word.length(); i++) {
-            if(word.charAt(i) == '.') {
-                 for(Node c : node.childrens.values()) {
-                if(searchWord(word.substring(i + 1), c)) {
-                    return true;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == '.') {
+                for (Node c : node.childrens.values()) {
+                    if (searchWord(word.substring(i + 1), c)) {
+                        return true;
+                    }
                 }
             }
-         }
-            if(!node.childrens.containsKey(word.charAt(i))) {
+            if (!node.childrens.containsKey(word.charAt(i))) {
                 return false;
             }
-          node = node.childrens.get(word.charAt(i));
+            node = node.childrens.get(word.charAt(i));
         }
         return node.isEnd;
     }
-    
-    
+
 }
 
 /**
