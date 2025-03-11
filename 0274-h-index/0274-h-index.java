942f620fff364beb09c6531905847eb00acc67b6
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-11 18:40:14 -0400

    Time: 2 ms (71.64%), Space: 41.8 MB (40.02%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -2,10 +2,10 @@ class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
         int count = 0;
-        for (int i = citations.length - 1; i >= 0; i--) {
-            if (citations[i] > count) {
+        for (int i = citations.length - 1; i >= 0 && citations[i] > count; i--) {
+           
                 count++;
-            }
+      
         }
         return count;
     }
