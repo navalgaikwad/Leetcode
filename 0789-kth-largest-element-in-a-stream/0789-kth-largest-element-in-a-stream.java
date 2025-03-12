ada9a3b444cb7441afeeb89915618e79cca9930e
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-11 21:19:36 -0400

    Time: 21 ms (54.69%), Space: 50 MB (80.77%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
@@ -0,0 +1,27 @@
+class KthLargest {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for(int num : nums) {
+            pq.add(num);
+            if(pq.size() > k) {
+                pq.remove();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        pq.add(val);
+        if(pq.size() > k) {
+             pq.remove();
+        }
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
