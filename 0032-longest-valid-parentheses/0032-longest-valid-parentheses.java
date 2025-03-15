f826f1cd2450ba7faf32f2f736f7c78b155d6e65
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-15 17:57:06 -0400

    Time: 3 ms (74.46%), Space: 43.2 MB (35.19%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,21 +1,20 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        if(s.length() == 0) return 0;
         Stack<Integer> st = new Stack<>();
-        int len = 0;
-        st.push(-1);
-        for(int i =0; i<s.length(); i++) {
+        st.push(-1); //vvimp
+        int max = 0;
+        for(int i=0; i<s.length(); i++) {
             if(s.charAt(i) == '(') {
                 st.push(i);
             }else {
                 st.pop();
                 if(!st.isEmpty()) {
-                    len = Math.max(len, i - st.peek());
+                    max = Math.max(max, i - st.peek());
                 }else {
                     st.push(i);
                 }
             }
         }
-        return len;
+        return max;
     }
 }
