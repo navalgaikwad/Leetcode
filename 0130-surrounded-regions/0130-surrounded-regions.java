f7946f168174a40e90c3955081b9771bfabc5648
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-14 20:00:44 -0400

    Time: 1 ms (99.61%), Space: 45.2 MB (92.5%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,39 +1,37 @@
 class Solution {
     public void solve(char[][] board) {
-        for(int i=0; i<board[0].length; i++) {
+        int m = board.length;
+        int n = board[0].length;
+        for(int i=0; i<n; i++) {//col
             if(board[0][i] == 'O') {
-                dfs(board, 0, i);
+              dfs(board, 0, i);  
             }
-            if(board[board.length - 1][i] == 'O') {
-                dfs(board, board.length - 1, i);
+            if(board[m-1][i] == 'O') {
+              dfs(board, m-1, i);  
             }
         }
-        
-        for(int i=0; i<board.length; i++) {
+        for(int i=0; i<m; i++) {//row
             if(board[i][0] == 'O') {
-                dfs(board, i, 0);
+              dfs(board, i, 0);  
             }
-            if(board[i][board[0].length - 1] == 'O') {
-                dfs(board, i, board[0].length - 1);
+            if(board[i][n-1] == 'O') {
+              dfs(board, i, n-1);  
             }
         }
         
-        for(int i =0; i<board.length; i++) {
-            for(int j =0; j<board[0].length; j++) {
-                if(board[i][j]=='O') {
-                   board[i][j] = 'X'; 
-                }else if(board[i][j]=='$') {
-                    board[i][j] = 'O'; 
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }else if(board[i][j] == '$') {
+                    board[i][j] = 'O';
                 }
             }
         }
-           // System.out.println(); // Move to the next line after printing each row
-        
-        
     }
     
     void dfs(char[][] board, int i, int j) {
-        if(i >=board.length || i < 0 || j>=board[0].length || j<0 || board[i][j] =='X' || board[i][j]=='$') {
+        if(i < 0 || i>=board.length || j<0 || j>=board[0].length || board[i][j]=='$' || board[i][j]=='X') {
             return;
         }
         board[i][j] = '$';
@@ -43,3 +41,8 @@ class Solution {
         dfs(board, i-1, j);
     }
 }
+// /
+// [["X","$","X","X"],
+//  ["X","$","$","X"],
+//  ["X","X","$","X"],
+//  ["X","$","X","X"]]
