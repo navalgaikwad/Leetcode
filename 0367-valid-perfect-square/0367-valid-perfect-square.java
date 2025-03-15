b494a6698425073d62087946ab4760ed9205a1cc
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-15 17:44:28 -0400

    Time: 1 ms (18.02%), Space: 40.2 MB (83.75%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num == 1) {
+            return true;
+        }
+        for (int i = 1; i <= num / i; i++) {
+            if ((i * i) == num)
+                return true;
+        }
+        return false;
+    }
+}
+
