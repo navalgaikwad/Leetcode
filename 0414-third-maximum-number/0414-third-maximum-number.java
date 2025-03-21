888f6d67a0a064707a4d75e8a4e43910a4e348a3
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-21 11:23:11 -0400

    Time: 1 ms (96.84%), Space: 43.2 MB (59.14%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long first = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE;
+        long third = Long.MIN_VALUE;
+        
+        for(int num : nums) {
+            if(first == num || (second == num) || (third == num)) {
+                continue;
+            }
+            if(num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if(num > second  ) {
+                third = second;
+                second = num;
+            } else if(num > third) {
+                third = num;
+            }
+        }
+        
+        return third == Long.MIN_VALUE ? (int)first : (int)third;
+    }
+}
