0a15ebe9c377b1a153fe51aa95acb71b7cea1e09
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-22 23:56:32 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.java
+++ 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.java
@@ -1,28 +1,26 @@
 class Solution {
     public int numFriendRequests(int[] ages) {
-        int res = 0;
+        int result = 0;
         Arrays.sort(ages);
-        for(int i=0; i<ages.length; i++) {
-            int low = helper(ages, (ages[i]/2) + 7);
-            int upper = helper(ages, ages[i]);
-
-            res+=Math.max(upper-low-1, 0);
+        for (int i = 0; i < ages.length; i++) {
+            int lower = helper(ages, 0, ages.length - 1, (ages[i] / 2) + 7);
+            int higher = helper(ages, 0, ages.length - 1, (ages[i]));
+            result += Math.max(higher - lower - 1, 0);
         }
-        return res;
+        return result;
     }
 
-    int helper(int[] ages, int target) {
-        int s = 0;
-        int e = ages.length-1;
-        while(s <= e) {
-            int mid = s + (e - s)/2;
-            if(ages[mid] <= target) {
-                s = mid + 1;
-            }else {
-                e = mid - 1;
+    int helper(int[] ages, int left, int right, int target) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (ages[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return s;
-
+        return left;
     }
 }
+//sort karycha
+/
