2f2339b701d6fcba81990a39bec808c27b6f72a7
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-27 15:13:25 -0400

    Time: 3 ms (2.58%), Space: 41.9 MB (8.21%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,27 +1,26 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        int balance = 0;
+        int open = 0;
+        int close = 0;
         StringBuilder sb = new StringBuilder();
-        int count =0;
         for(char c : s.toCharArray()) {
             if(c == '(') {
-                balance++;
+                open++;
             }else {
-                if(balance > 0) {
+                if(open > 0) {
                     sb.append("(");
-                    balance--;
+                    open--;
                 }else {
-                    count++;
+                    close++;
                 }
             }
             sb.append(c);
         }
-        int val = balance;
-        while (val-- > 0) {
-            sb.append(')');
+        int val = open;
+        while(val-- > 0) {
+            sb.append(")");
         }
         System.out.print(sb.toString());
-        return count + balance;
-        
+        return open + close;
     }
 }
