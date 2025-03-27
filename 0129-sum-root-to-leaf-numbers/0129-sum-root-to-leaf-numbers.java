1cfec68803bdf765874429a6ac17e7821a65bbf7
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-27 15:18:51 -0400

    Time: 0 ms (100%), Space: 41.2 MB (65.8%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -15,22 +15,15 @@
  */
 class Solution {
     public int sumNumbers(TreeNode root) {
-        int totalUptillNow = 0;
-        int sum = helper(root, totalUptillNow);
-        return sum;
+        return helper(root, 0);
     }
-    
     int helper(TreeNode root, int sum) {
         if(root == null) return 0;
-        
-        sum = sum *10+root.val;
-        if(root.left == null && root.right==null) return sum;//leaf path vvimp
-        
+        sum = sum * 10 + root.val;
+        if(root.left == null && root.right == null) return sum;
         int left = helper(root.left, sum);
         int right = helper(root.right, sum);
-        
         return left + right;
+
     }
 }
-
-
