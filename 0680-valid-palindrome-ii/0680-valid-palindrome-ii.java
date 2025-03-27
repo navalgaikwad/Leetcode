06d1a82f88f54617ff41d23ddcbf0deda1aa60ad
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-27 11:17:12 -0400

    Time: 4 ms (99.79%), Space: 45.8 MB (10.5%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,25 +1,23 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int st = 0;
-        int e = s.length()-1;
-        while(st < e) {
-            if(s.charAt(st) != s.charAt(e)) {
-                return palindrome(s, st+1, e) || palindrome(s, st, e-1);
+        int  i =0; int j = s.length()-1;
+        while(i<= j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return helper(s, i + 1, j) || helper(s, i, j-1);
             }
-            st++;
-            e--;
+            i++;
+            j--;
         }
         return true;
-        
     }
 
-    boolean palindrome(String st, int s, int e) {
-        while(s<=e) {
-            if(st.charAt(s) != st.charAt(e)) {
+    boolean helper(String s, int i, int j) {
+        while(i <= j) {
+            if(s.charAt(i) != s.charAt(j)) {
                 return false;
             }
-            s++;
-            e--;
+            i++;
+            j--;
         }
         return true;
     }
