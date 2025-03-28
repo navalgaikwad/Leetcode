bfa2923e3f81f35b84fc294c97584606488585aa
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-28 11:38:13 -0400

    Time: 1 ms (100%), Space: 42.9 MB (15.94%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,32 +1,29 @@
 class Solution {
     public int myAtoi(String s) {
-        int index =0;
-        if(s.length() == 0) return 0;
+        int index = 0;
         while(index < s.length() && s.charAt(index) == ' ') {
             index++;
         }
-        if(index == s.length()) return 0;
-        
+         if(index == s.length()) return 0;
         int sign = 1;
-        if(s.charAt(index)=='-' || s.charAt(index) == '+') {
-            sign = s.charAt(index)=='-' ? -1: 1;
-            index++;//vvimp
+        if(s.charAt(index) == '+' || s.charAt(index) == '-') {
+            sign = s.charAt(index) == '+' ? 1 : -1;
+            index++;
         }
         int total = 0;
+
         while(index < s.length()) {
-            int number = Character.getNumericValue(s.charAt(index));
-            if(number < 0 || number >9) {
+            if(s.charAt(index) < '0' || s.charAt(index) > '9') {
                 break;
             }
             if(Integer.MAX_VALUE/10 < total || 
                Integer.MAX_VALUE/10 == total && 
-               Integer.MAX_VALUE%10 < number) {
+               Integer.MAX_VALUE%10 < s.charAt(index) - '0') {
                 return sign == 1 ? Integer.MAX_VALUE:Integer.MIN_VALUE; 
             }
-            
-            total = total *10 + number;
+            total = total * 10 + s.charAt(index) - '0';
             index++;
         }
-        return total*sign;//vvimp
+        return total * sign;
     }
 }
