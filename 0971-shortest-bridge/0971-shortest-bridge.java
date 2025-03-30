65645bcf7654d5778819eb27ea5e10158dcdc254
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-03-30 01:22:06 -0400

    Time: 8 ms (84.33%), Space: 45.6 MB (38.52%) - LeetHub

--- 0971-shortest-bridge/0971-shortest-bridge.java
+++ 0971-shortest-bridge/0971-shortest-bridge.java
@@ -1,16 +1,5 @@
 class Solution {
-    Queue<int[]> q = new LinkedList<>();
-
-    void dfs(int[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1)
-            return;
-        q.add(new int[] { i, j, 0 });
-        grid[i][j] = 2;
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
-    }
+    Queue<int[]> queue = new LinkedList<>();
 
     public int shortestBridge(int[][] grid) {
         int startI = 0;
@@ -25,23 +14,23 @@ class Solution {
             }
         }
         dfs(grid, startI, startJ);
-        int[][] dirs = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
-        while (!q.isEmpty()) {
-            int[] current = q.remove();
-            int x = current[0];
-            int y = current[1];
-
+        int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+        while (!queue.isEmpty()) {
+            int[] current = queue.remove();
+            int i = current[0];
+            int j = current[1];
+            int level = current[2];
             for (int[] dir : dirs) {
-                int i = x + dir[0];
-                int j = y + dir[1];
-                if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length) {
-                    if (grid[i][j] == 1) {
-                        return current[2];
-                    }
-                    if (grid[i][j] == 0) {
-                        grid[i][j] = -1;
-                        q.add(new int[] { i, j, current[2] + 1 });
+                int m = i + dir[0];
+                int n = j + dir[1];
 
+                if (m >= 0 && m < grid.length && n >= 0 && n < grid[0].length) {
+                    if (grid[m][n] == 1) {
+                        return level;
+                    }
+                    if (grid[m][n] == 0) {
+                        grid[m][n] = -2;
+                        queue.add(new int[] { m, n, level + 1 });
                     }
                 }
             }
@@ -49,4 +38,17 @@ class Solution {
         return 0;
 
     }
+
+    void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == -2 || grid[i][j] == 0) {//vvimp
+            return;
+        }
+        queue.add(new int[] { i, j, 0 });
+        grid[i][j] = -2;
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
 }
