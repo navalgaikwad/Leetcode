b547dc49c2694823693b1805de5c6dcb1b665be0
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-04-04 22:58:33 -0400

    Time: 1 ms (99.7%), Space: 41.7 MB (90.41%) - LeetHub

--- 0851-goat-latin/0851-goat-latin.java
+++ 0851-goat-latin/0851-goat-latin.java
@@ -1,24 +1,23 @@
 class Solution {
     public String toGoatLatin(String sentence) {
         String vowels = "aeiouAEIOU";
-        StringBuffer sb = new StringBuffer();
-        int i=1;
-        String[] split = sentence.split(" ");
-        for(String word : split) {
-            char c = word.charAt(0);
-            if(vowels.indexOf(c) != -1) {
-                sb.append(word);
-            }else {
-                sb.append(word.substring(1));
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        String[] arr = sentence.split(" ");
+        for (String a : arr) {
+            char c = a.charAt(0);
+            if (vowels.indexOf(c) != -1) {
+                sb.append(a);
+            } else {
+                sb.append(a.substring(1));
                 sb.append(c);
             }
 
             sb.append("ma");
-            sb.append("a".repeat(i++));
+            sb.append("a".repeat(i++));//vvimp string la repeat
             sb.append(" ");
-
-
         }
         return sb.toString().substring(0, sb.length() - 1);
+
     }
 }
