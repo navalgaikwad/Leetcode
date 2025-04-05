fc7ceb75785bf1c933eacbb4c68352b19d198475
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-04-04 21:53:15 -0400

    Time: 8 ms (87.4%), Space: 45.3 MB (32.39%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,31 +1,27 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-           int row = matrix.length;
-        int col =matrix[0].length;
-          int max =0;
+        int max = Integer.MIN_VALUE;
         Integer[][] memo = new Integer[matrix.length][matrix[0].length];
-        //HashMap<String, Integer> memo = new HashMap<>();
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
-               int sum = dfs(matrix, i, j, memo, matrix[i][j]-1);
-                max=Math.max(max, sum);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                max = Math.max(max, dfs(matrix, i, j, memo, matrix[i][j] - 1));
             }
         }
         return max;
     }
-    
+
     int dfs(int[][] matrix, int i, int j, Integer[][] memo, int prev) {
-        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || prev >= matrix[i][j]) {
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] <= prev) {//vvimp
             return 0;
         }
-        if(memo[i][j]!=null) return memo[i][j];
+        if (memo[i][j] != null) {
+            return memo[i][j];
+        }
         int left = dfs(matrix, i + 1, j, memo, matrix[i][j]);
         int right = dfs(matrix, i, j + 1, memo, matrix[i][j]);
         int top = dfs(matrix, i - 1, j, memo, matrix[i][j]);
         int bottom = dfs(matrix, i, j - 1, memo, matrix[i][j]);
-        
-        int value = 1 + Math.max(left, Math.max(right, Math.max(top, bottom)));
-        memo[i][j] = value;
-        return value;
+        memo[i][j] = 1 + Math.max(left, Math.max(right, Math.max(top, bottom)));
+        return memo[i][j];
     }
 }
