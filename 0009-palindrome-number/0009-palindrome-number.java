89996c483a269c021aba273b6eea1da75e0e8221
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-04-04 23:09:52 -0400

    Time: 4 ms (100%), Space: 43.8 MB (95.22%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x); // Convert to String
-        int n = s.length(); // Store the String length to int n
-
-        for (int i=0; i<n/2; i++) {
-            // We check whether the elements at the same distance from
-            // beginning and from ending are same, if not we return false
-            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+        if(x < 0 || (x!=0 && x%10 == 0)) {
+            return false;
+        }
+        int rev = 0;
+        while(x > rev) {
+            rev = rev * 10 + x % 10;
+            x = x /10;
         }
 
-        // if no flaws are found we return true
-        return true;
+        return x == rev || x == rev / 10;
+        
     }
 }
