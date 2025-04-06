8c116f7a411aafe46ecf6778dcf6e27edc6ddf89
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-04-06 08:26:20 -0400

    Time: 1 ms (100%), Space: 42.2 MB (97.59%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,42 +1,33 @@
 class Solution {
     public boolean isNumber(String s) {
         s = s.trim();
-        //digit seen
-        //+ or - seen
-        //. seen
-        //e seen or digit not seen
-        boolean digitSeen = false;
+        boolean eseen = false;
+        boolean digit = false;
         boolean dotSeen = false;
-        boolean eSeen = false;
-
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(Character.isDigit(c)) {
-                digitSeen = true;
-            }else if(c == '+' || c == '-') {
-                if(i > 0 && s.charAt(i - 1) != 'E' && s.charAt(i - 1) != 'e') {
+            if (Character.isDigit(c)) {
+                digit = true;
+            } else if (c == '+' || c == '-') {
+                if (i > 0 && s.charAt(i - 1) != 'E' && s.charAt(i - 1) != 'e') {
                     return false;
                 }
-               // eSeen = true;
-            }else if(c == '.') {
-                if(dotSeen || eSeen) {
+            } else if (c == '.') {
+                if (dotSeen || eseen) { //de
                     return false;
                 }
                 dotSeen = true;
-            }else if(c == 'E' || c== 'e') {
-                if(eSeen || !digitSeen) {
+            } else if (c == 'e' || c == 'E') {
+                if (eseen || !digit) {//edi
                     return false;
                 }
-                eSeen = true;
-                digitSeen = false;
+                eseen = true;
+                digit = false;
             } else {
                 return false;
             }
         }
 
-        return digitSeen;
+        return digit;
     }
 }
-//+ || - asel tar pathi mghe e or E shivay dusra kon nko
-//. mg dotSeen true if already bghtla mg false resutun kar
-// e || E eSeen asel alerady return false
