f80c133b668001f74858a13d7e9bbf246854f85c
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-04-06 21:07:52 -0400

    Time: 103 ms (38.39%), Space: 56.5 MB (66.22%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.java
+++ 0480-sliding-window-median/0480-sliding-window-median.java
@@ -1,27 +1,37 @@
 class Solution {
     public double[] medianSlidingWindow(int[] nums, int k) {
-        TreeSet<Integer> minSet = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : Integer.compare(nums[a], nums[b]));//asending
-        TreeSet<Integer> maxSet = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : Integer.compare(nums[a], nums[b]));//descending
-        double[] medians = new double[nums.length - k + 1];
+        // Min Heap (stores larger half of elements)
+        TreeSet<Integer> minset = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : Integer.compare(nums[a], nums[b]));
+        // Max Heap (stores smaller half of elements)
+        TreeSet<Integer> maxset = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : Integer.compare(nums[b], nums[a]));
+
+        double[] result = new double[nums.length - k + 1];
+
         for (int i = 0; i < nums.length; i++) {
-            if (minSet.size() <= maxSet.size()) {
-                maxSet.add(i);
-                minSet.add(maxSet.pollFirst());
+            // Add new element
+            if (maxset.size() <= minset.size()) {
+                minset.add(i);
+                maxset.add(minset.pollFirst());
             } else {
-                minSet.add(i);
-                maxSet.add(minSet.pollLast());
+                maxset.add(i);
+                minset.add(maxset.pollFirst());
             }
-            if (i - k + 1 >= 0) {
+
+            // When the window reaches size k, compute median
+            if (i >= k - 1) {
                 if (k % 2 == 0) {
-                    medians[i - k + 1] = ((double) nums[minSet.last()] + nums[maxSet.first()]) / 2;
+                    result[i - k + 1] = ((double) nums[minset.first()] + nums[maxset.first()]) / 2.0;
                 } else {
-                    medians[i - k + 1] = (double) nums[minSet.last()];
+                    result[i - k + 1] = (double) nums[maxset.first()];
                 }
-                if (!minSet.remove(i - k + 1)) {
-                    maxSet.remove(i - k + 1);
+
+                // Remove outdated element
+                int outdatedIndex = i - k + 1;
+                if (!minset.remove(outdatedIndex)) {
+                    maxset.remove(outdatedIndex);
                 }
             }
         }
-        return medians;
+        return result;
     }
 }
