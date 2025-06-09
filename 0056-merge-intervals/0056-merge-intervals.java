355bb398b95a2164c1d507ac8259287de22b7777
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-06-09 01:45:55 -0400

    Time: 10 ms (22.97%), Space: 46.2 MB (96.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,23 +1,36 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        // List<int[]> allIntervals = new ArrayList<>();
+
+        // // Add all intervals from both arrays
+        // Collections.addAll(allIntervals, interval1);
+        // Collections.addAll(allIntervals, interval2);
+
+//  int totalLength = intervals1.length + intervals2.length;
+//         int[][] intervals = new int[totalLength][2];
+//         int index = 0;
+//         for (int[] interval : intervals1) {
+//             intervals[index++] = interval;
+//         }
+//         for (int[] interval : intervals2) {
+//             intervals[index++] = interval;
+//         }
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         LinkedList<int[]> list = new LinkedList<>();
-        Arrays.sort(intervals, (a,b)->a[0] - b[0]);
-        
-        for(int i=0; i<intervals.length; i++) {
-            if(!list.isEmpty() && intervals[i][0] <= list.getLast()[1] ) {
-            
-                list.getLast()[1] =  Math.max(list.getLast()[1],intervals[i][1] );
-            }else {
-                list.add(intervals[i]);
+        for (int[] interval : intervals) {
+            if (!list.isEmpty() && list.getLast()[1] >= interval[0]) {
+                list.getLast()[1] = Math.max(list.getLast()[1], interval[1]);
+            } else {
+                list.addLast(interval);
             }
         }
         int[][] result = new int[list.size()][2];
         int i = 0;
-        for (int[] interval : list){
+        for (int[] interval : list) {
             result[i] = interval;
             i += 1;
         }
-        
+
         return result;
     }
 }
