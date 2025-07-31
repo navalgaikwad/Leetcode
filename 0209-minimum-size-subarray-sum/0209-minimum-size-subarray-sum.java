3cab54cbd66e72e051c430ed47391163ae65d73b
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-07-31 01:01:29 -0500

    Time: 1 ms (99.68%), Space: 58.6 MB (11.84%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,16 +1,19 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left = 0;
-        int sum =0;
-        int len = Integer.MAX_VALUE ;
-        for(int right =0; right < nums.length; right++) {
-            sum +=nums[right];
+        int start = 0;
+      //  int end = nums.length-1;
+        int sum = 0;
+        int count = 0;
+        int len = Integer.MAX_VALUE;
+        for(int end = 0; end < nums.length; end++) {
+            sum+=nums[end];
             while(sum >= target) {
-                len = Math.min(len, right - left + 1);
-                sum-=nums[left];
-                left++;
+                len = Math.min(len, end - start + 1);
+                sum-=nums[start];
+                start++;
             }
+
         }
-        return len == Integer.MAX_VALUE ? 0 : len;
+        return len == Integer.MAX_VALUE? 0 : len;
     }
 }
