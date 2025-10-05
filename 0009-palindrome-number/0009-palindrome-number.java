b768a27a05a2bffc94404b880ca50a72265a68d1
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-10-05 02:01:14 -0500

    Time: 5 ms (64.77%), Space: 43.9 MB (89.45%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0 || (x!=0 && x%10 == 0)) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
             return false;
         }
         int rev = 0;
-        while(x > rev) {
+        while (x > rev) {
             rev = rev * 10 + x % 10;
-            x = x /10;
+            x = x / 10;
         }
 
         return x == rev || x == rev / 10;
-        
+
     }
 }
