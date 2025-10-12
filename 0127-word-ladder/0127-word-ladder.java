c385d5b3b0c9c0cba0ac6fb5b5e0b2d05d52f88d
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-10-11 20:28:45 -0500

    Time: 72 ms (66.79%), Space: 47.3 MB (36.8%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,37 +1,39 @@
 class Solution {
     class Pair {
-        String word;
+        String wrd;
         int level;
-        Pair(String word, int level) {
-            this.word = word;
+
+        Pair(String wrd, int level) {
+            this.wrd = wrd;
             this.level = level;
         }
     }
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String>set = new HashSet<>();
-        for(String word : wordList) {
+        Set<String> set = new HashSet<>();
+        for (String word : wordList) {
             set.add(word);
         }
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(beginWord, 1));
-        while(!q.isEmpty()) {
-            Pair current = q.remove();
-            String word = current.word;
-            int level = current.level;
-            if(word.equals(endWord)) {
+        while (!q.isEmpty()) {
+            Pair p = q.remove();
+            String word = p.wrd;
+            int level = p.level;
+            if (word.equals(endWord)) {
                 return level;
             }
-            for(int i=0; i<word.length(); i++) {
-                char[] wrd = word.toCharArray();
-                for(char c = 'a'; c<='z'; c++) {
-                    wrd[i] = c;
-                    if(set.contains(String.valueOf(wrd))) {
-                        q.add(new Pair(String.valueOf(wrd), level  + 1));
-                        set.remove(String.valueOf(wrd));
+
+            for (int i = 0; i < word.length(); i++) {
+                char[] array = word.toCharArray();
+                for (char c = 'a'; c <= 'z'; c++) {
+                    array[i] = c;
+                    if (set.contains(String.valueOf(array))) {
+                        set.remove(String.valueOf(array));
+                        q.add(new Pair(String.valueOf(array), level + 1));
                     }
                 }
             }
-            
         }
         return 0;
     }
