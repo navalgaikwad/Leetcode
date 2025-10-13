d9b1b7ca42c89e681fc10c52b4381b909b046bb6
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-10-12 22:28:40 -0500

    Time: 0 ms (100%), Space: 42.7 MB (33.13%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,7 +1,6 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        
-		if(nums.length == 1) return 0; // single element
+        if(nums.length == 1) return 0; // single element
         
         int n = nums.length;
         
@@ -10,22 +9,22 @@ class Solution {
         if(nums[n-1] > nums[n-2]) return n-1;
 		
 		// search in the remaining array
-        int start = 1;
-        int end = n-2;
+       
+        int s = 1;
+        int e = n-2;
+        while(s <= e) {
+            int mid = s + (e - s)/2;
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if(nums[mid] > nums[mid + 1]) {
+                e = mid - 1;
+            }else {
+                s = mid + 1;
+            }
+        }
+        return s;
         
-        // int start =0;
-        // int end =nums.length - 1;
-       while(start <= end) {
-           int mid = start + (end - start)/2;
-           if(nums[mid -1] < nums[mid] && nums[mid] > nums[mid + 1]) {
-               return mid;
-           }
-           if(nums[mid] > nums[mid + 1]) {
-               end = mid - 1;     
-           }else  {
-               start = mid + 1;   
-           }
-       }
-        return start;
     }
+
 }
