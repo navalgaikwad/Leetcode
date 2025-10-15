40df2987568e6c89a08a2a9352921a9c9335a178
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-10-14 22:31:48 -0500

    Time: 5 ms (59.08%), Space: 45 MB (36.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -8,20 +8,19 @@ class Solution {
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
-      // int prev = 0;
-       int total = 0;
-       int prev = Integer.MAX_VALUE;
-       for(int i = s.length()-1; i >= 0; i--) {
-        char c = s.charAt(i);
-        int value = map.get(c);
-        //IV
-        if(prev != Integer.MAX_VALUE && value < prev) {
-            total = total - value;
-        }else {
-            total+=value;
+        int prev = 0;
+        int total = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int val = map.get(c);
+            if(val < prev) {
+                total = total - val;
+            }else {
+                total+=val;
+            }
+            prev = val;
+
         }
-        prev = value;
-       }
-       return total;
+        return total;
     }
 }
