d75a6cf4ef3906b46023e683bd3a85b97d94fd02
Naval Gaikwad <navalgaikwad1991@outlook.com>
2025-11-15 20:16:44 -0600

    Time: 0 ms (100%), Space: 46.6 MB (8.77%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -16,19 +16,18 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         Map<Node, Node> map = new HashMap<>();
-        Node p = head;
-        while(p!=null) {
-            map.put(p, new Node(p.val));
-            p = p.next;
+        Node q = head;
+        while (q != null) {
+            map.put(q, new Node(q.val));
+            q = q.next;
         }
-        p = head;
-        while(p!=null) {
-            map.get(p).next = map.get(p.next);
-            map.get(p).random = map.get(p.random);
-            p = p.next;
+        q = head;
+        while (q != null) {
+            map.get(q).next = map.get(q.next);
+            map.get(q).random = map.get(q.random);
+            q = q.next;
         }
-
         return map.get(head);
-        
+
     }
 }
